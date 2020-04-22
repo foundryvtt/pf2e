339bd46bde1f8bdf08bd48dc11b476ef59d8d409
Andrew Johnson <lordxuqra@gmail.com>
2020-04-22 00:10:55 -0400

    issue-232: calculate hp if toughness is taken

--- dist/module/actor/actor.js
+++ dist/module/actor/actor.js
@@ -68,7 +68,11 @@ export default class extends Actor {
     data.details.xp.max = 1000;
     data.details.xp.pct = Math.min(Math.round((data.details.xp.value) * 100 / 1000), 99.5);
 
-    data.attributes.hp.max = parseInt(data.attributes.ancestryhp.value, 10) + (parseInt(data.attributes.classhp.value, 10) + data.abilities['con'].mod) * data.details.level.value;
+    const hasToughness = this.data.items.some(item => item.name === 'Toughness' && item.type === 'feat')
+    console.log(hasToughness, hasToughness ? data.details.level.value : 0)
+    data.attributes.hp.max = parseInt(data.attributes.ancestryhp.value, 10)
+      + (parseInt(data.attributes.classhp.value, 10) + data.abilities['con'].mod) * data.details.level.value
+      + (hasToughness ? data.details.level.value : 0);
 
     // Saves
     for (const save of Object.values(data.saves)) {
