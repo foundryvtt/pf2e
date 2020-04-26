d6cdb2d06c91132dff580e107a011266ae860012
Trey Pendragon <tpendragon@princeton.edu>
2020-04-26 16:54:03 -0700

    Add a test for focus spell heightening.

--- tests/module/item/spell.test.js
+++ tests/module/item/spell.test.js
@@ -3,6 +3,7 @@ import Spell from 'module/item/spell.js';
 const characterData = require('tests/fixtures/characterData.json');
 const electricArc = require('tests/fixtures/spells/electric_arc.json');
 const shatteringGem = require('tests/fixtures/spells/shattering_gem.json');
+const tempestSurge = require('tests/fixtures/spells/tempest_surge.json');
 const spellcastingEntry = require('tests/fixtures/items/spellcastingEntry.json');
 
 const spellcastingEntryItem = {
@@ -39,5 +40,11 @@ describe('#damageParts', () => {
     const spell = new Spell(electricArc, { castingActor: actor, castLevel: 3 });
 
     expect(spell.damageParts).toEqual(['1d4', 3, '1d4', '1d4']);
+  });
+  test('automatically heightens focus spells to caster max level', () => {
+    characterData.data.details.level.value = 5;
+    const spell = new Spell(tempestSurge, { castingActor: actor, castLevel: 3 });
+
+    expect(spell.damageParts).toEqual(['1d12', '1d12', '1d12']);
   });
 });
