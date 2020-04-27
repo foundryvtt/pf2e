a9a09af67993b9fb1db6d1f2689dea96a68cdcde
yjeroen <jeroendenhaan@yahoo.com>
2020-04-27 10:59:15 +0200

    Changed chat damage icons, with thanks to feedback Nerull#6333

--- src/scripts/chat/chatdamagebuttonsPF2e.js
+++ src/scripts/chat/chatdamagebuttonsPF2e.js
@@ -9,12 +9,12 @@ class ChatDamageButtonsPF2e extends Application {
 
       const btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
 
-      const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply full damage to selected token(s)."></i></button>`);
-      const halfDamageButton = $(`<button class="dice-total-halfDamage-btn" style="${btnStyling}"><i class="fas fa-user-check" title="Click to apply half damage to selected token(s)."></i></button>`);
-      const doubleDamageButton = $(`<button class="dice-total-doubleDamage-btn" style="${btnStyling}"><i class="fas fa-user-times" title="Click to apply double damage to selected token(s)."></i></button>`);
+      const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-bahai" title="Click to apply full damage to selected token(s)."></i></button>`);
+      const halfDamageButton = $(`<button class="dice-total-halfDamage-btn" style="${btnStyling}"><i class="fas fa-chevron-down" title="Click to apply half damage to selected token(s)."></i></button>`);
+      const doubleDamageButton = $(`<button class="dice-total-doubleDamage-btn" style="${btnStyling}"><i class="fas fa-angle-double-up" title="Click to apply double damage to selected token(s)."></i></button>`);
       // need to rework to a shield raised status, instead of using a GM global CONFIG
-      const shieldButton = $(`<button class="dice-total-shield-btn" style="${btnStyling}"><i class="fas fa-user-shield" title="Click to toggle the shield block status of the selected token(s)."></i></button>`);
-	    const fullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-user-plus" title="Click to apply full healing to selected token(s)."></i></button>`);
+      const shieldButton = $(`<button class="dice-total-shield-btn" style="${btnStyling}"><i class="fas fa-shield-alt" title="Click to toggle the shield block status of the selected token(s)."></i></button>`);
+	    const fullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-heart" title="Click to apply full healing to selected token(s)."></i></button>`);
 
       const btnContainer1 = $(`<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>`);
       const btnContainer2 = $(`<span class="dmgBtn-container" style="position:absolute; top:0; right:0; bottom:1px;"></span>`);
