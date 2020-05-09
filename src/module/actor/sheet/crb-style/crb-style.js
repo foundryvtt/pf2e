79084d07e8dffe51559483257eae966378ec5fd4
mdizo <mdizo6482@gmail.com>
2020-05-09 10:26:04 +0800

    Refactored crb-style.js

--- src/module/actor/sheet/crb-style/crb-style.js
+++ src/module/actor/sheet/crb-style/crb-style.js
@@ -1,15 +1,18 @@
 import ActorSheetPF2eCharacter from '../character.js';
 
+/**
+ * Extend the base ActorSheet
+ * @extends {ActorSheetPF2eCharacter}
+ */
+
 export default class CRBStyleCharacterActorSheetPF2E extends ActorSheetPF2eCharacter {
   static get defaultOptions() {
-    const options = super.defaultOptions;
-    mergeObject(options, {
-      classes: options.classes.concat(['crb-style']),
+    return mergeObject(super.defaultOptions, {
+      classes: ['crb-style'],
+      template: 'systems/pf2e/templates/actor/crb-style/actor-sheet.html',
+      width: 700,
+      height: 800,
+      tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'character-stats' }],
     });
-    return options;
-  }
-
-  get template() {
-    return 'systems/pf2e/templates/actors/crb-style/actor-sheet.html';
   }
 }
