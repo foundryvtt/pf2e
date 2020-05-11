733b99b5882e9a767c1bdd18958eb50b98ed0220
mdizo <mdizo6482@gmail.com>
2020-05-11 23:23:24 +0800

    html structure WIP

--- .prettierrc.js
+++ .prettierrc.js
@@ -8,7 +8,16 @@ module.exports = {
             files: ['*.scss', '*.css'],
             options: {
                 requirePragma: false,
+                parser: 'scss',
             },
         },
+        {
+            files: '*.html',
+            options: {
+                requirePragma: false,
+                parser: 'html',
+                htmlWhitespaceSensitivity: 'ignore',
+            }
+        }
     ],
 };
