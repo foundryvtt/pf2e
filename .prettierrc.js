8af0c28b07633aea3deaa0f8d70fe16e842bfa88
mdizo <mdizo6482@gmail.com>
2020-05-11 22:52:13 +0800

    Updated prettier to run on SCSS

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-  trailingComma: 'es5',
-  singleQuote: true,
-  tabWidth: 4,
-  requirePragma: true,
+    trailingComma: 'es5',
+    singleQuote: true,
+    tabWidth: 4,
+    requirePragma: true,
+    overrides: [
+        {
+            files: ['*.scss', '*.css'],
+            options: {
+                requirePragma: false,
+            },
+        },
+    ],
 };
