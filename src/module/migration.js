dd2bb08dc41068237ef361b28aad5d084c554f06
Bernhard Posselt <dev@bernhard-posselt.com>
2020-05-22 14:46:47 +0200

    add migrations for bulk capacity items

--- src/module/migration.js
+++ src/module/migration.js
@@ -150,11 +150,84 @@ export const migrateActorData = function (actor, worldSchemaVersion) {
         if (worldSchemaVersion < 0.574) {
             migrateActorBulkItems(actor, updateData);
         }
+        if (worldSchemaVersion < 0.579) {
+            addActorContainerAttributes(item, updateData);
+        }
     }
     return updateData;
 };
 
 /* -------------------------------------------- */
+
+function addContainerAttributes(item, itemData) {
+    const itemName = item?.name?.trim();
+    if (itemName === 'Backpack') {
+        itemData['data.bulkCapacity.value'] = '4';
+        itemData['data.negateBulk.value'] = '2';
+    } else if (itemName === 'Bag of Devouring Type I') {
+        itemData['data.bulkCapacity.value'] = '50';
+        itemData['data.negateBulk.value'] = '50';
+    } else if (itemName === 'Bag of Devouring Type II') {
+        itemData['data.bulkCapacity.value'] = '100';
+        itemData['data.negateBulk.value'] = '100';
+    } else if (itemName === 'Bag of Devouring Type III') {
+        itemData['data.bulkCapacity.value'] = '150';
+        itemData['data.negateBulk.value'] = '150';
+    } else if (itemName === 'Bag of Holding (Type I)') {
+        itemData['data.bulkCapacity.value'] = '25';
+        itemData['data.negateBulk.value'] = '25';
+    } else if (itemName === 'Bag of Holding (Type II)') {
+        itemData['data.bulkCapacity.value'] = '50';
+        itemData['data.negateBulk.value'] = '50';
+    } else if (itemName === 'Bag of Holding (Type III)') {
+        itemData['data.bulkCapacity.value'] = '100';
+        itemData['data.negateBulk.value'] = '100';
+    } else if (itemName === 'Bag of Holding (Type IV)') {
+        itemData['data.bulkCapacity.value'] = '150';
+        itemData['data.negateBulk.value'] = '150';
+    } else if (itemName === 'Bag of Weasels') {
+        itemData['data.bulkCapacity.value'] = '25';
+        itemData['data.negateBulk.value'] = '25';
+    } else if (itemName === 'Gloves of Carelessness') {
+        itemData['data.bulkCapacity.value'] = '1';
+        itemData['data.negateBulk.value'] = '1';
+    } else if (itemName === 'Gloves of Storing') {
+        itemData['data.bulkCapacity.value'] = '1';
+        itemData['data.negateBulk.value'] = '1';
+    } else if (itemName === 'Belt Pouch') {
+        itemData['data.bulkCapacity.value'] = '4L';
+        itemData['data.negateBulk.value'] = '0';
+    } else if (itemName === 'Pathfinder\'s Pouch') {
+        itemData['data.bulkCapacity.value'] = '1; 4L';
+        itemData['data.negateBulk.value'] = '1';
+    } else if (itemName === 'Knapsack of Halflingkind') {
+        itemData['data.bulkCapacity.value'] = '50';
+        itemData['data.negateBulk.value'] = '50';
+    } else if (itemName === 'Knapsack of Halflingkind (Greater)') {
+        itemData['data.bulkCapacity.value'] = '50';
+        itemData['data.negateBulk.value'] = '50';
+    } else if (itemName === 'Sack') {
+        itemData['data.bulkCapacity.value'] = '8';
+        itemData['data.negateBulk.value'] = '0';
+    } else if (itemName === 'Satchel') {
+        itemData['data.bulkCapacity.value'] = '2';
+        itemData['data.negateBulk.value'] = '0';
+    } else if (itemName === 'Bandolier') {
+        itemData['data.bulkCapacity.value'] = '8L';
+        itemData['data.negateBulk.value'] = '0';
+    } else if (itemName === 'Saddlebags') {
+        itemData['data.bulkCapacity.value'] = '6';
+        itemData['data.negateBulk.value'] = '0';
+    } else if (itemName === 'Chest') {
+        itemData['data.bulkCapacity.value'] = '8';
+        itemData['data.negateBulk.value'] = '0';
+    } else {
+        itemData['data.bulkCapacity.value'] = '';
+        itemData['data.negateBulk.value'] = '0';
+    }
+    itemData['data.containerId.value'] = '';
+    return itemData;
+}
 
 async function addCoin(actorEntity, treasureId, denomination, quantity) {
     if (quantity !== null && (`${quantity}`).trim() !== '0') {
@@ -242,6 +315,9 @@ export const migrateItemData = function (item, worldSchemaVersion) {
     if (worldSchemaVersion < 0.574) {
         migrateBulk(item, updateData);
     }
+    if (worldSchemaVersion < 0.579) {
+        addContainerAttributes(item, updateData);
+    }
     // Return the migrated update data
     return updateData;
 };
@@ -312,14 +388,14 @@ function _migrateHitPointData(actor, updateData) {
     updateData['data.attributes.classhp'] = parseInt((actor.data.attributes.classhp || {}).value) || 0;
 }
 
-function migrateActorBulkItems(actor, updateData) {
+function migrateActorItems(actor, updateData, itemUpdateFunction) {
     if (!actor.items) return;
     let updatedItems = [];
     const items = duplicate(actor.items);
 
     items.forEach(item => {
         let updatedItem = item;
-        let updatedData = migrateBulk(item, {});
+        let updatedData = itemUpdateFunction(item, {});
         if (!isObjectEmpty(updatedData)) {
             updatedItem = mergeObject(updatedItem, updatedData);
         }
@@ -327,6 +403,15 @@ function migrateActorBulkItems(actor, updateData) {
     });
 
     updateData['items'] = updatedItems;
+}
+
+function migrateActorBulkItems(actor, updateData) {
+    return migrateActorItems(actor, updateData, migrateBulk);
+}
+
+
+function addActorContainerAttributes(actor, updateData) {
+    return migrateActorItems(actor, updateData, addContainerAttributes);
 }
 
 function _migrateNPCItemAttackEffects(actor, updateData) {
