6626513e14ebda580b150f45de3774c640c99cbc
Putty <bernhard@posselt.at>
2020-05-23 00:32:12 +0000

    Containers

--- src/module/actor/sheet/character.js
+++ src/module/actor/sheet/character.js
@@ -1,6 +1,7 @@
 import ActorSheetPF2e from './base.js';
-import { calculateBulk, itemsFromActorData, stacks, toItemOrContainer, formatBulk } from '../../item/bulk.js';
+import { calculateBulk, itemsFromActorData, stacks, formatBulk, indexBulkItemsById } from '../../item/bulk.js';
 import { calculateEncumbrance } from '../../item/encumbrance.js';
+import { getContainerMap } from '../../item/container.js';
 
 class ActorSheetPF2eCharacter extends ActorSheetPF2e {
   static get defaultOptions() {
@@ -140,9 +141,17 @@ class ActorSheetPF2eCharacter extends ActorSheetPF2e {
         ignoreCoinBulk: game.settings.get('pf2e', 'ignoreCoinBulk'),
         ignoreContainerOverflow: game.settings.get('pf2e', 'ignoreContainerOverflow'),
     };
+
+    const bulkItems = itemsFromActorData(actorData);
+    const indexedBulkItems = indexBulkItemsById(bulkItems);
+    const containers = getContainerMap(actorData.items, indexedBulkItems, stacks, bulkConfig);
+    
     for (const i of actorData.items) {
       i.img = i.img || CONST.DEFAULT_TOKEN;
-
+      i.containerData = containers.get(i._id);
+      i.isContainer = i.containerData.isContainer;
+      i.isNotInContainer = i.containerData.isNotInContainer;
+            
       // Read-Only Equipment
       if (i.type === 'armor' || i.type === 'equipment' || i.type === 'consumable' || i.type === 'backpack') {
         readonlyEquipment.push(i);
@@ -152,11 +161,13 @@ class ActorSheetPF2eCharacter extends ActorSheetPF2e {
         i.armorEquipped = equipped?' active':'';
       }
 
-      // Inventory
+      i.canBeEquipped = i.isNotInContainer && i.isArmor;
+
+        // Inventory
       if (Object.keys(inventory).includes(i.type)) {
         i.data.quantity.value = i.data.quantity.value || 0;
         i.data.weight.value = i.data.weight.value || 0;
-        const [approximatedBulk] = calculateBulk([toItemOrContainer(i)], stacks, false, bulkConfig);
+        const [approximatedBulk] = calculateBulk([indexedBulkItems.get(i._id)], stacks, false, bulkConfig);
         i.totalWeight = formatBulk(approximatedBulk);
         i.hasCharges = (i.type === 'consumable') && i.data.charges.max > 0;
         i.isTwoHanded = (i.type === 'weapon') && !!((i.data.traits.value || []).find((x) => x.startsWith('two-hand')));
@@ -397,8 +408,7 @@ class ActorSheetPF2eCharacter extends ActorSheetPF2e {
 
 
     // Inventory encumbrance
-    const items = itemsFromActorData(actorData);
-    const [bulk] = calculateBulk(items, stacks, false, bulkConfig);
+    const [bulk] = calculateBulk(bulkItems, stacks, false, bulkConfig);
     actorData.data.attributes.encumbrance = calculateEncumbrance(
       actorData.data.abilities.str.mod,
       actorData.data.attributes.bonusbulk,
---
+++ src/module/item/container.js
@@ -0,0 +1,137 @@
+import { Bulk, calculateBulk, formatBulk, weightToBulk } from './bulk.js';
+import { groupBy } from '../utils.js';
+
+/**
+ * Datatype that holds container information for *every* item, even non containers
+ */
+class ContainerData {
+    constructor({
+        item,
+        heldItems,
+        negateBulk,
+        capacity,
+        heldItemBulk,
+        isInContainer,
+        formattedNegateBulk,
+        formattedHeldItemBulk,
+        formattedCapacity,
+    }) {
+        this.item = item;
+        this.heldItems = heldItems;
+        this.negateBulk = negateBulk;
+        this.heldItemBulk = heldItemBulk;
+        this.isInContainer = isInContainer;
+        this.formattedHeldItemBulk = formattedHeldItemBulk;
+        this.formattedNegateBulk = formattedNegateBulk;
+        this.formattedCapacity = formattedCapacity;
+        this.capacity = capacity;
+    }
+
+    get isContainer() {
+        return !this.capacity.isNegligible;
+    }
+
+    get isNotInContainer() {
+        return !this.isInContainer;
+    }
+
+    _getLightBulkCapacityThreshold() {
+        if (this.capacity.normal > 0) {
+            // light bulk don't count towards bulk limit
+            return this.capacity.toLightBulk() + 10;
+        }
+        // but do if the container only stores light bulk
+        return this.capacity.light;
+    }
+
+    get fullPercentage() {
+        const capacity = this._getLightBulkCapacityThreshold();
+        if (capacity === 0) {
+            return 0;
+        }
+        const heldLightBulk = this.heldItemBulk.toLightBulk();
+        return Math.floor((heldLightBulk / capacity) * 100);
+    }
+
+    get isOverLoaded() {
+        if (this.capacity.normal > 0) {
+            return this.heldItemBulk.toLightBulk() >= (this.capacity.toLightBulk() + 10);
+        }
+        return this.heldItemBulk.toLightBulk() > this.capacity.light;
+    }
+}
+
+/**
+ * Creates container meta data
+ * @param item
+ * @param heldItems
+ * @param heldBulkItems
+ * @param isInContainer
+ * @param stackDefinitions
+ * @param bulkConfig
+ * @return {ContainerData}
+ */
+function toContainer(item, heldItems = [], heldBulkItems = [], isInContainer, stackDefinitions, bulkConfig) {
+    const negateBulk = weightToBulk(item.data?.negateBulk?.value) ?? new Bulk();
+    const [heldItemBulk] = calculateBulk(heldBulkItems, stackDefinitions, false, bulkConfig);
+    const capacity = weightToBulk(item.data?.bulkCapacity?.value) ?? new Bulk();
+    return new ContainerData({
+        item,
+        heldItems,
+        negateBulk,
+        capacity,
+        heldItemBulk,
+        isInContainer,
+        formattedNegateBulk: formatBulk(negateBulk),
+        formattedHeldItemBulk: formatBulk(heldItemBulk),
+        formattedCapacity: formatBulk(capacity),
+    });
+}
+
+
+/**
+ * Returns a map where the key is an item id and the value is the container data.
+ * Every item has container data, even if it's not a container. The relevant
+ * values for non container items are just empty in that case. This is useful
+ * in the templates, because you don't have a lot of leeway there
+ * @param items all items on the actor
+ * @param bulkItemsById all items on the actor transformed into bulk items; used to look up how much bulk a container stores
+ * @param stackDefinitions used to calculated bulk
+ * @param bulkConfig used to calculated bulk
+ * @return {Map<string, ContainerData>}
+ */
+// eslint-disable-next-line import/prefer-default-export
+export function getContainerMap(items = [], bulkItemsById = new Map(), stackDefinitions, bulkConfig) {
+    const allIds = groupBy(items, item => item._id);
+
+    const containerGroups = groupBy(items, item => {
+        const containerId = item?.data?.containerId?.value;
+        if (allIds.has(containerId)) {
+            return containerId;
+        }
+        return null;
+    });
+
+    const idIndexedContainerData = new Map();
+    items
+        .map(item => {
+            const itemId = item._id;
+            const isInContainer = containerGroups.has(item?.data?.containerId?.value);
+            if (containerGroups.has(itemId)) {
+                return [itemId, containerGroups.get(itemId), isInContainer];
+            }
+            return [itemId, [], isInContainer];
+        })
+        .forEach(([id, heldItems, isInContainer]) => {
+            idIndexedContainerData.set(id, toContainer(
+                allIds.get(id)[0],
+                heldItems,
+                bulkItemsById.get(id)?.holdsItems ?? [],
+                isInContainer,
+                stackDefinitions,
+                bulkConfig
+            ));
+        });
+
+    return idIndexedContainerData;
+}
