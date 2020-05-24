c2abe21435b95eef0f3fcb9aa72bb0eebf001bc8
Bernhard Posselt <dev@bernhard-posselt.com>
2020-05-24 11:50:36 +0200

    add function to prevent cycle

--- src/module/actor/sheet/base.js
+++ src/module/actor/sheet/base.js
@@ -1,5 +1,6 @@
 import {calculateWealth} from '../../item/treasure.js';
 import { AddCoinsPopup } from './AddCoinsPopup.js';
+import {isCycle} from "../../item/container";
 
 /**
  * Extend the basic ActorSheet class to do all the PF2e things!
@@ -1225,7 +1226,8 @@ class ActorSheetPF2e extends ActorSheet {
         if (container[0] !== undefined) {
             const droppedItemId = container.attr('data-item-id')?.trim();
             const item = await getItem();
-            if (item.type !== 'spell') {
+            console.log(item, this.actor);
+            if (item.type !== 'spell' && !isCycle(item._id, droppedItemId, this.actor.data.items)) {
                 return item.update({
                     'data.containerId.value': droppedItemId,
                     'data.equipped.value': false,
