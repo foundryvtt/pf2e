0e0e8aaac29a1f223ef8422dcafcd0c978b63468
Hayden Jackson <hayden.adam.jackson@gmail.com>
2020-06-27 11:16:16 -0700

    Ensure game settings have defaults for test.

--- tests/setup.js
+++ tests/setup.js
@@ -12,3 +12,23 @@ global.fetchSpell = async (name) => {
   }
   return null;
 };
+
+global.game = Object.freeze({
+  settings: Object.freeze({
+    get: (module, settingKey) => {
+      switch (settingKey)
+      {
+        /* Proficiency Modifiers */
+        case 'proficiencyUntrainedModifer': return 0;
+        case 'proficiencyTrainedModifer':   return 2;
+        case 'proficiencyExpertModifer':    return 4;
+        case 'proficiencyMasterModifer':    return 6;
+        case 'proficiencyLegendaryModifer': return 8;
+
+        /* Variant rules */
+        case 'proficiencyVariant':          return 'ProficiencyWithLevel';
+        default: throw new Error("Undefined setting.");
+      }
+    },
+  }),
+});
