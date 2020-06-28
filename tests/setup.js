504e3919ec781b80dc63d29ed61fa673f35dc9f4
Hayden Jackson <hayden.adam.jackson@gmail.com>
2020-06-28 04:34:50 +0000

    Add support for proficiency variant rules.
    - Rules are described in the Gamemastery Guide, pg. 198.
    - Allows individual modification of proficiency level modifiers.
      - This allows a user to set untrained to -2 as desired if they wish to
        follow the recommendation in the Gamemastery Guide.
    - Modifiers need a recompute via modification or reload if changed
      during play.
    - How to handle the feat 'Untrained Improvisation' is undefined in the
      Game Mastery guide. Algorithm as follows:
      - Take the difference of between trained and untrained modifiers.
      - If below level 7, add a bonus of half the difference.
      - if level 7 of above, add the difference (making it the trained
        modifier but remember they cannot use trained actions).
    - There may be other feats or rules I have missed.

--- tests/setup.js
+++ tests/setup.js
@@ -12,3 +12,23 @@ global.fetchSpell = async (name) => {
   }
   return null;
 };
+
+global.game = Object.freeze({
+  settings: Object.freeze({
+    get: (module, settingKey) => {
+      switch (settingKey)
+      {
+        /* Proficiency Modifiers */
+        case 'proficiencyUntrainedModifier': return 0;
+        case 'proficiencyTrainedModifier':   return 2;
+        case 'proficiencyExpertModifier':    return 4;
+        case 'proficiencyMasterModifier':    return 6;
+        case 'proficiencyLegendaryModifier': return 8;
+
+        /* Variant rules */
+        case 'proficiencyVariant':           return 'ProficiencyWithLevel';
+        default: throw new Error("Undefined setting.");
+      }
+    },
+  }),
+});
