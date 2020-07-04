14cab3592752ba906538d0debc5660d5fddaa79c
Bernhard Posselt <dev@bernhard-posselt.com>
2020-07-04 19:48:42 +0200

    type another function

--- src/module/actor/actor-entity.ts
+++ src/module/actor/actor-entity.ts
@@ -16,5 +16,5 @@ export interface PF2ActorEntity {
   data: PF2Actor;
   getOwnedItem(itemId: string): PF2ItemEntity; 
   createOwnedItem(itemData: PF2ItemData, options?: CreateOptions);
-  deleteEmbeddedEntity(type: string, id: string);
+  deleteEmbeddedEntity(type: string, id: string | string[]);
 }
