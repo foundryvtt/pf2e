1a8e0b6484ef190ee73e94ee078cb371b11ce3b2
Bernhard Posselt <dev@bernhard-posselt.com>
2020-07-04 19:34:50 +0200

    type sellTreasure

--- src/module/actor/actor-entity.ts
+++ src/module/actor/actor-entity.ts
@@ -15,5 +15,6 @@ export interface CreateOptions {
 export interface PF2ActorEntity {
   data: PF2Actor;
   getOwnedItem(itemId: string): PF2ItemEntity; 
-  createOwnedItem(itemData: PF2ItemData, options?: CreateOptions)
+  createOwnedItem(itemData: PF2ItemData, options?: CreateOptions);
+  deleteEmbeddedEntity(type: string, id: string);
 }
