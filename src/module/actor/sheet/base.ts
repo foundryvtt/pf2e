cc2bd15a3f31b9a5b1b78778e694e4efd507af4f
Kevin Hjelden <fryguy@burntpopcorn.net>
2020-07-05 15:37:51 -0700

    No tool checks, either.

--- src/module/actor/sheet/base.ts
+++ src/module/actor/sheet/base.ts
@@ -1547,9 +1547,6 @@ abstract class ActorSheetPF2e extends ActorSheet<PF2EActor> {
         case 'consumable':
           if (chatData.hasCharges) buttons.append(`<span class="tag"><button class="consume" data-action="consume">${localize('PF2E.ConsumableUseLabel')} ${item.name}</button></span>`);
           break;
-        case 'tool':
-          buttons.append(`<span class="tag"><button class="tool_check" data-action="toolCheck" data-ability="${chatData.ability.value}">${localize('PF2E.ConsumableUseLabel')} ${item.name}</button></span>`);
-          break;
       }
       if (game.user.isGM && isUnidentified && identificationData.identifiedItemId) {
         buttons.append(`<span class="tag"><button data-action="identify">${localize("PF2E.ItemIdentifyLabel")}</button>`);
