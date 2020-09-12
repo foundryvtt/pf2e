5a68e4934d30a5d15b96de71672754e590f6ff7c
nikolaj-a <na@deck.dk>
2020-09-12 07:30:45 +0200

    Toggle property rule element
    Add toggle property rule element, which will allow toggling flags like roll options on and off.

--- src/module/rules/rules.ts
+++ src/module/rules/rules.ts
@@ -7,6 +7,7 @@ import {PF2FixedProficiencyRuleElement} from "./elements/fixed-proficiency";
 import {PF2TempHPRuleElement} from "./elements/temphp";
 import {PF2DexterityModifierCapRuleElement} from "./elements/dexterity-modifier-cap";
 import {PF2DamageDiceRuleElement} from "./elements/damage-dice";
+import {PF2TogglePropertyRuleElement} from "./elements/toggle-property";
 
 export {PF2RuleElement};
 
@@ -19,6 +20,7 @@ export class PF2RuleElements {
         'PF2E.RuleElement.FixedProficiency':  (ruleData, item) => new PF2FixedProficiencyRuleElement(ruleData, item),
         'PF2E.RuleElement.TempHP':  (ruleData, item) => new PF2TempHPRuleElement(ruleData, item),
         'PF2E.RuleElement.DamageDice': (ruleData, item) => new PF2DamageDiceRuleElement(ruleData, item),
+        'PF2E.RuleElement.ToggleProperty': (ruleData, item) => new PF2TogglePropertyRuleElement(ruleData, item),
     });
 
     static custom: Record<string, (ruleData: PF2RuleElementData, item: ItemData) => PF2RuleElement> = {}
