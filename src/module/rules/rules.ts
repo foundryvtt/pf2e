da7064b775fe8df9d03013c63fb0e00d59c0c8f6
nikolaj-a <na@deck.dk>
2020-09-20 23:03:18 +0200

    Bass speed rule element
    Add base speed rule element, which will make it possible to add a new base speed to an actor.

--- src/module/rules/rules.ts
+++ src/module/rules/rules.ts
@@ -10,6 +10,7 @@ import {PF2DamageDiceRuleElement} from "./elements/damage-dice";
 import {PF2TogglePropertyRuleElement} from "./elements/toggle-property";
 // import {PF2TokenImageRuleElement} from "./elements/token-image";
 import {PF2TokenSizeRuleElement} from "./elements/token-size";
+import {PF2BaseSpeedRuleElement} from "./elements/base-speed";
 
 export {PF2RuleElement};
 
@@ -25,6 +26,7 @@ export class PF2RuleElements {
         'PF2E.RuleElement.ToggleProperty': (ruleData, item) => new PF2TogglePropertyRuleElement(ruleData, item),
         // 'PF2E.RuleElement.TokenImage': (ruleData, item) => new PF2TokenImageRuleElement(ruleData, item),
         'PF2E.RuleElement.TokenSize': (ruleData, item) => new PF2TokenSizeRuleElement(ruleData, item),
+        'PF2E.RuleElement.BaseSpeed': (ruleData, item) => new PF2BaseSpeedRuleElement(ruleData, item),
     });
 
     static custom: Record<string, (ruleData: PF2RuleElementData, item: ItemData) => PF2RuleElement> = {}
