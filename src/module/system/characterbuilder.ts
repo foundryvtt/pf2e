d647adcd451ff884d89564ac67ab969ead6df5b0
Mirko Rainer <mirkorainer@outlook.com>
2020-12-05 20:57:58 -0500

    Log Drop Data, Item, and containerId. It works!

--- src/module/system/characterbuilder.ts
+++ src/module/system/characterbuilder.ts
@@ -2,7 +2,6 @@
 
 import PF2EActor from "../actor/actor";
 import { RawCharacterData } from "../actor/actorDataDefinitions";
-import { ItemData } from "../item/dataDefinitions";
 
 /**
  * Character build page
@@ -24,15 +23,44 @@ export class CharacterBuilder extends FormApplication {
     }
   }
 
-    static get defaultOptions() {
-      const options = super.defaultOptions;
-      options.id = 'character-builder';
-      options.classes = [];
-      options.title = 'Character Builder';
-      options.template = 'systems/pf2e/templates/actors/character-builder.html';
-      options.width = 500;
-      options.height = 700;
-      return options;
+  async _onDrop(event: DragEvent) {
+    event.preventDefault();
+    
+    console.log(event.dataTransfer.getData('text/plain'));
+    console.log(event);
+
+    const dropTarget = $(event.target);
+    const dragData = event.dataTransfer.getData('text/plain');
+    const dragItem = JSON.parse(dragData);
+    const containerId = dropTarget.data('containerId') ?? dropTarget.parents('[data-container-id]').data('containerId');
+
+    if (dragItem.type !== 'Item') return;
+
+    let item;
+    if (dragItem.pack) {
+        item = await game.packs.get(dragItem.pack).getEntity(dragItem.id);
+    } else {
+        item = game.items.get(dragItem.id);
+    }
+
+    console.log(containerId)
+    console.log(item)
+  }
+
+  _canDragDrop(selector) {
+    return this.actor.owner;
+}
+
+  static get defaultOptions() {
+    const options = super.defaultOptions;
+    options.id = 'character-builder';
+    options.classes = [];
+    options.title = 'Character Builder';
+    options.template = 'systems/pf2e/templates/actors/character-builder.html';
+    options.width = 500;
+    options.height = 700;
+    options.dragDrop = [{dropSelector: '.inventory-header'}]; // What css selector is droppable
+    return options;
   }
 
   getData() {
