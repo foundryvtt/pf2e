b39dd97bd4314bbe6cd5eff1e74bc7e3471543d8
nikolaj-a <na@deck.dk>
2020-12-19 08:01:36 +0100

    Align apply damage icons with context menu

--- src/scripts/chat/chatdamagebuttonsPF2e.ts
+++ src/scripts/chat/chatdamagebuttonsPF2e.ts
@@ -7,14 +7,14 @@ class ChatDamageButtonsPF2e extends Application {
       const damageRoll: any = message.getFlag(game.system.id, 'damageRoll');
       if (damageRoll || !message.isRoll || message.roll?.dice[0]?.faces === 20) return;
 
-      const btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
+      const btnStyling = 'width: 22px; height:22px; font-size:10px; line-height:1px; padding-left: 5px;';
 
-      const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-bahai" title="Click to apply full damage to selected token(s)."></i></button>`);
-      const halfDamageButton = $(`<button class="dice-total-halfDamage-btn" style="${btnStyling}"><i class="fas fa-chevron-down" title="Click to apply half damage to selected token(s)."></i></button>`);
-      const doubleDamageButton = $(`<button class="dice-total-doubleDamage-btn" style="${btnStyling}"><i class="fas fa-angle-double-up" title="Click to apply double damage to selected token(s)."></i></button>`);
+      const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply full damage to selected token(s)."></i></button>`);
+      const halfDamageButton = $(`<button class="dice-total-halfDamage-btn" style="${btnStyling}"><i class="fas fa-user-shield" title="Click to apply half damage to selected token(s)."></i></button>`);
+      const doubleDamageButton = $(`<button class="dice-total-doubleDamage-btn" style="${btnStyling}"><i class="fas fa-user-injured" title="Click to apply double damage to selected token(s)."></i></button>`);
       // need to rework to a shield raised status, instead of using a GM global CONFIG
       const shieldButton = $(`<button class="dice-total-shield-btn" style="${btnStyling}"><i class="fas fa-shield-alt" title="Click to toggle the shield block status of the selected token(s)."></i></button>`);
-	    const fullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-heart" title="Click to apply full healing to selected token(s)."></i></button>`);
+      const fullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-user-plus" title="Click to apply full healing to selected token(s)."></i></button>`);
 
       const btnContainer1 = $(`<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>`);
       const btnContainer2 = $(`<span class="dmgBtn-container" style="position:absolute; top:0; right:0; bottom:1px;"></span>`);
@@ -94,11 +94,11 @@ class ChatDamageButtonsPF2e extends Application {
       const damageRoll: any = message.getFlag(game.system.id, 'damageRoll');
       if (!damageRoll) return;
 
-      const full = $(`<button style="flex: 1 1 0;" title="Apply full damage to selected tokens."><i class="fas fa-heart-broken"></i></button>`);
-      const half = $(`<button style="flex: 1 1 0;" title="Apply half damage to selected tokens."><i class="fas fa-chevron-down"></i></button>`);
-      const double = $(`<button style="flex: 1 1 0;" title="Apply double damage to selected tokens."><i class="fas fa-angle-double-up"></i></button>`);
+      const full = $(`<button style="flex: 1 1 0;" title="Apply full damage to selected tokens."><i class="fas fa-user-minus"></i></button>`);
+      const double = $(`<button style="flex: 1 1 0;" title="Apply double damage to selected tokens."><i class="fas fa-user-injured"></i></button>`);
+      const half = $(`<button style="flex: 1 1 0;" title="Apply half damage to selected tokens."><i class="fas fa-user-shield"></i></button>`);
       const shield = $(`<button class="dice-total-shield-btn" style="flex: 1 1 0;" title="Toggle the shield block status of the selected tokens."><i class="fas fa-shield-alt"></i></button>`);
-      const heal = $(`<button style="flex: 1 1 0;" title="Apply full healing to selected tokens."><i class="fas fa-heart"></i></button>`);
+      const heal = $(`<button style="flex: 1 1 0;" title="Apply full healing to selected tokens."><i class="fas fa-user-plus"></i></button>`);
       
       const buttons = $(`<div style="display: flex; margin-top: 3px;"></div>`);
       buttons.append(full, half, double, shield, heal);
