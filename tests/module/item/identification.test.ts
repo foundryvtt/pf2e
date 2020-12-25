0a71232fa04be7ec60c4508a1ad22144613fffb0
Putty <bernhard@posselt.at>
2020-12-25 02:28:36 +0000

    do not show invested button if unidentified, clean up code, show actions but turn name for unidentified items into Unidentified Item

--- tests/module/item/identification.test.ts
+++ tests/module/item/identification.test.ts
@@ -1,8 +1,29 @@
 import {PhysicalItemData, Rarity} from '../../../src/module/item/dataDefinitions';
-import {identifyItem} from '../../../src/module/item/identification';
+import {identifyItem, isMagical} from '../../../src/module/item/identification';
 
-function createItem(level: number, rarity: Rarity, traits: string[]): PhysicalItemData {
+interface TestItemData {
+    level: number;
+    rarity: Rarity;
+    traits?: string[];
+    potencyRune?: string;
+    strikingRune?: string;
+    resilienceRune?: string;
+    type?: string;
+}
+
+function createItem(
+    {
+        level,
+        rarity,
+        traits,
+        potencyRune,
+        strikingRune,
+        resilienceRune,
+        type,
+    }: TestItemData,
+): PhysicalItemData {
     return {
+        type,
         data: {
             level: {
                 value: level,
@@ -13,13 +34,22 @@ function createItem(level: number, rarity: Rarity, traits: string[]): PhysicalIt
             traits: {
                 value: traits,
             },
+            potencyRune: {
+                value: potencyRune,
+            },
+            strikingRune: {
+                value: strikingRune,
+            },
+            resiliencyRune: {
+                value: resilienceRune,
+            },
         },
-    } as PhysicalItemData;
+    } as unknown as PhysicalItemData;
 }
 
 describe('test identification DCs', () => {
     test('identify normal item', () => {
-        const item = createItem(2, 'common', ['magical']);
+        const item = createItem({level: 2, rarity: 'common', traits: ['magical']});
         const dcs = identifyItem(item, {notMatchingTraditionModifier: 5});
         expect(dcs).toEqual({
             arc: 16,
@@ -30,7 +60,7 @@ describe('test identification DCs', () => {
     });
 
     test('identify rare item', () => {
-        const item = createItem(2, 'rare', ['magical']);
+        const item = createItem({level: 2, rarity: 'rare', traits: ['magical']});
         const dcs = identifyItem(item, {notMatchingTraditionModifier: 5});
         expect(dcs).toEqual({
             arc: 21,
@@ -41,7 +71,7 @@ describe('test identification DCs', () => {
     });
 
     test('identify rare item with tradition', () => {
-        const item = createItem(2, 'rare', ['primal', 'occult']);
+        const item = createItem({level: 2, rarity: 'rare', traits: ['primal', 'occult']});
         const dcs = identifyItem(item, {notMatchingTraditionModifier: 3});
         expect(dcs).toEqual({
             arc: 24,
@@ -52,7 +82,7 @@ describe('test identification DCs', () => {
     });
 
     test('identify rare alchemical ingredient', () => {
-        const item = createItem(2, 'rare', ['alchemical']);
+        const item = createItem({level: 2, rarity: 'rare', traits: ['alchemical']});
         const dcs = identifyItem(item, {notMatchingTraditionModifier: 3});
         expect(dcs).toEqual({
             cra: 21,
@@ -74,5 +104,41 @@ describe('test identification DCs', () => {
         expect(dcs).toEqual({
             dc: 14,
         });
+    });
+
+    test('potency runes are magical', () => {
+        const item = createItem({
+            level: 2, 
+            rarity: 'rare', 
+            traits: [], 
+            potencyRune: '1', 
+            type: 'weapon'
+        });
+        expect(isMagical(item))
+            .toBe(true);
+    });
+
+    test('striking runes are magical', () => {
+        const item = createItem({
+            level: 2, 
+            rarity: 'rare', 
+            traits: [], 
+            strikingRune: '2',  
+            type: 'weapon'
+        });
+        expect(isMagical(item))
+            .toBe(true);
+    });
+
+    test('resiliency runes are magical', () => {
+        const item = createItem({
+            level: 2, 
+            rarity: 'rare', 
+            traits: [], 
+            resilienceRune: '3',  
+            type: 'armor'
+        });
+        expect(isMagical(item))
+            .toBe(true);
     });
 });
