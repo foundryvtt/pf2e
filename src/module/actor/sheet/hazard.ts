71a86e977d1336d6baed85ee8a2d075da7e6cc63
John Haverkamp <john@haverkamp.us>
2020-12-26 16:33:34 -0600

    Fix issues with weaknesses resistances and immunities on hazard sheet.

--- src/module/actor/sheet/hazard.ts
+++ src/module/actor/sheet/hazard.ts
@@ -50,7 +50,9 @@ export class ActorSheetPF2eHazard extends ActorSheetPF2e {
     sheetData.stealthDC = (sheetData.data.attributes.stealth?.value ?? 0) + 10;
     sheetData.hasStealthDescription = sheetData.data.attributes.stealth?.details || false;
 
-    sheetData.hasImmunities = sheetData.data.traits.di.value !== [];
+    sheetData.hasImmunities = sheetData.data.traits.di.value.length ? sheetData.data.traits.di.value : false;
+    sheetData.hasResistences = sheetData.data.traits.dr.length ? Array.isArray(sheetData.data.traits.dr) : false;
+    sheetData.hasWeaknesses = sheetData.data.traits.dv.length ? Array.isArray(sheetData.data.traits.dv) : false;
     sheetData.hasDescription = sheetData.data.details.description || false;
     sheetData.hasDisable = sheetData.data.details.disable || false;
     sheetData.hasRoutineDetails = sheetData.data.details.routine || false;
