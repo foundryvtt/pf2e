539ebddc16ac228c3bb56d5ab781bf24224ffd48
Bernhard Posselt <dev@bernhard-posselt.com>
2021-01-08 20:42:51 +0100

    reformat

--- src/module/earn-income.ts
+++ src/module/earn-income.ts
@@ -2,10 +2,10 @@
  * Implementation of Earn Income rules on https://2e.aonprd.com/Skills.aspx?ID=2&General=true
  */
 
-import {ProficiencyRank} from './item/dataDefinitions';
-import {Coins} from './item/treasure';
-import {calculateDC, DCOptions} from './dc';
-import {DegreeOfSuccess, DieRoll, calculateDegreeOfSuccess} from './degree-of-success';
+import { ProficiencyRank } from './item/dataDefinitions';
+import { Coins } from './item/treasure';
+import { calculateDC, DCOptions } from './dc';
+import { calculateDegreeOfSuccess, DegreeOfSuccess, DieRoll } from './degree-of-success';
 
 // you have to be at least trained to earn income
 type TrainedProficiencies = Exclude<ProficiencyRank, 'untrained'>;
@@ -29,28 +29,28 @@ function buildRewards(...rewards: Partial<Coins>[]): Rewards {
 }
 
 const earnIncomeTable = {
-    0: {failure: {cp: 1}, rewards: buildRewards({cp: 5})},
-    1: {failure: {cp: 2}, rewards: buildRewards({sp: 2})},
-    2: {failure: {cp: 4}, rewards: buildRewards({sp: 3})},
-    3: {failure: {cp: 8}, rewards: buildRewards({sp: 5})},
-    4: {failure: {sp: 1}, rewards: buildRewards({sp: 7}, {sp: 8})},
-    5: {failure: {sp: 2}, rewards: buildRewards({sp: 9}, {gp: 1})},
-    6: {failure: {sp: 3}, rewards: buildRewards({gp: 1, sp: 5}, {gp: 2})},
-    7: {failure: {sp: 4}, rewards: buildRewards({gp: 2}, {gp: 2, sp: 5})},
-    8: {failure: {sp: 5}, rewards: buildRewards({gp: 2, sp: 5}, {gp: 3})},
-    9: {failure: {sp: 6}, rewards: buildRewards({gp: 3}, {gp: 4})},
-    10: {failure: {sp: 7}, rewards: buildRewards({gp: 4}, {gp: 5}, {gp: 6})},
-    11: {failure: {sp: 8}, rewards: buildRewards({gp: 5}, {gp: 6}, {gp: 8})},
-    12: {failure: {sp: 9}, rewards: buildRewards({gp: 6}, {gp: 8}, {gp: 10})},
-    13: {failure: {gp: 1}, rewards: buildRewards({gp: 7}, {gp: 10}, {gp: 15})},
-    14: {failure: {gp: 1, sp: 5}, rewards: buildRewards({gp: 8}, {gp: 15}, {gp: 20})},
-    15: {failure: {gp: 2}, rewards: buildRewards({gp: 10}, {gp: 20}, {gp: 28})},
-    16: {failure: {gp: 2, sp: 5}, rewards: buildRewards({gp: 13}, {gp: 25}, {gp: 36}, {gp: 40})},
-    17: {failure: {gp: 3}, rewards: buildRewards({gp: 15}, {gp: 30}, {gp: 45}, {gp: 55})},
-    18: {failure: {gp: 4}, rewards: buildRewards({gp: 20}, {gp: 45}, {gp: 70}, {gp: 90})},
-    19: {failure: {gp: 6}, rewards: buildRewards({gp: 30}, {gp: 60}, {gp: 100}, {gp: 130})},
-    20: {failure: {gp: 8}, rewards: buildRewards({gp: 40}, {gp: 75}, {gp: 150}, {gp: 200})},
-    21: {failure: {}, rewards: buildRewards({gp: 50}, {gp: 90}, {gp: 175}, {gp: 300})},
+    0: { failure: { cp: 1 }, rewards: buildRewards({ cp: 5 }) },
+    1: { failure: { cp: 2 }, rewards: buildRewards({ sp: 2 }) },
+    2: { failure: { cp: 4 }, rewards: buildRewards({ sp: 3 }) },
+    3: { failure: { cp: 8 }, rewards: buildRewards({ sp: 5 }) },
+    4: { failure: { sp: 1 }, rewards: buildRewards({ sp: 7 }, { sp: 8 }) },
+    5: { failure: { sp: 2 }, rewards: buildRewards({ sp: 9 }, { gp: 1 }) },
+    6: { failure: { sp: 3 }, rewards: buildRewards({ gp: 1, sp: 5 }, { gp: 2 }) },
+    7: { failure: { sp: 4 }, rewards: buildRewards({ gp: 2 }, { gp: 2, sp: 5 }) },
+    8: { failure: { sp: 5 }, rewards: buildRewards({ gp: 2, sp: 5 }, { gp: 3 }) },
+    9: { failure: { sp: 6 }, rewards: buildRewards({ gp: 3 }, { gp: 4 }) },
+    10: { failure: { sp: 7 }, rewards: buildRewards({ gp: 4 }, { gp: 5 }, { gp: 6 }) },
+    11: { failure: { sp: 8 }, rewards: buildRewards({ gp: 5 }, { gp: 6 }, { gp: 8 }) },
+    12: { failure: { sp: 9 }, rewards: buildRewards({ gp: 6 }, { gp: 8 }, { gp: 10 }) },
+    13: { failure: { gp: 1 }, rewards: buildRewards({ gp: 7 }, { gp: 10 }, { gp: 15 }) },
+    14: { failure: { gp: 1, sp: 5 }, rewards: buildRewards({ gp: 8 }, { gp: 15 }, { gp: 20 }) },
+    15: { failure: { gp: 2 }, rewards: buildRewards({ gp: 10 }, { gp: 20 }, { gp: 28 }) },
+    16: { failure: { gp: 2, sp: 5 }, rewards: buildRewards({ gp: 13 }, { gp: 25 }, { gp: 36 }, { gp: 40 }) },
+    17: { failure: { gp: 3 }, rewards: buildRewards({ gp: 15 }, { gp: 30 }, { gp: 45 }, { gp: 55 }) },
+    18: { failure: { gp: 4 }, rewards: buildRewards({ gp: 20 }, { gp: 45 }, { gp: 70 }, { gp: 90 }) },
+    19: { failure: { gp: 6 }, rewards: buildRewards({ gp: 30 }, { gp: 60 }, { gp: 100 }, { gp: 130 }) },
+    20: { failure: { gp: 8 }, rewards: buildRewards({ gp: 40 }, { gp: 75 }, { gp: 150 }, { gp: 200 }) },
+    21: { failure: {}, rewards: buildRewards({ gp: 50 }, { gp: 90 }, { gp: 175 }, { gp: 300 }) },
 };
 
 function getIncomeForLevel(level: number) {
