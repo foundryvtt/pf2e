64db393a1e17382cfb30d576c7316b5d33029b23
Shark that walks like a man <rsimoes@gmail.com>
2021-01-10 15:47:41 -0600

    Update webpack to 5, minimize & inject css from main.bundle.js

---
+++ webpack.config.ts
@@ -0,0 +1,123 @@
+import * as fs from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
+import * as process from 'process';
+import { Compiler, Configuration, Stats } from 'webpack';
+import copyWebpackPlugin from 'copy-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+// import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'; Will hopefully be fixed in the next few days
+import WebpackBar from 'webpackbar';
+
+const mode = (process.argv[2] === '--mode' && process.argv[3]) ?? 'development';
+const isProductionBuild = mode === 'production';
+
+const pf2eSystemPath = (() => {
+    const configPath = path.resolve(process.cwd(), 'foundryconfig.json');
+    const configData = fs.existsSync(configPath) ? fs.readJSONSync(configPath) : undefined;
+    return configData !== undefined ? path.join(configData.dataPath, 'Data', 'systems', configData.systemName) : null;
+})();
+
+type Optimization = Configuration['optimization'];
+const optimization: Optimization = isProductionBuild
+    ? {
+          minimize: true,
+          minimizer: [
+              new TerserPlugin({
+                  terserOptions: {
+                      mangle: false,
+                  },
+              }),
+              '...',
+              new CssMinimizerPlugin(),
+          ],
+      }
+    : undefined;
+
+const config: Configuration = {
+    context: __dirname,
+    entry: {
+        main: path.resolve(__dirname, './src/pf2e.ts'),
+    },
+    mode: isProductionBuild ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json'),
+                            experimentalWatchApi: !isProductionBuild,
+                            happyPackMode: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                loader: 'thread-loader',
+                options: {
+                    workers: os.cpus.length - 1,
+                    poolTimeout: Infinity,
+                },
+            },
+        ],
+    },
+    optimization: optimization,
+    devtool: isProductionBuild ? undefined : 'inline-source-map',
+    bail: isProductionBuild,
+    watch: !isProductionBuild,
+    plugins: [
+        new CleanWebpackPlugin(),
+        new copyWebpackPlugin({
+            patterns: [{ from: 'static' }, { from: 'system.json' }],
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+        new WebpackBar({}),
+        {
+            apply: (compiler: Compiler) => {
+                compiler.hooks.done.tap('DonePlugin', (_stats: Stats) => {
+                    if (isProductionBuild) {
+                        setTimeout(() => {
+                            process.exit(0);
+                        });
+                    }
+                });
+            },
+        },
+    ],
+    resolve: {
+        extensions: ['.ts'],
+        plugins: [
+            // new TsconfigPathsPlugin({ configFile: "./path/to/tsconfig.json" }),
+        ],
+    },
+    output: {
+        path: pf2eSystemPath ?? path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+};
+
+export default config;
