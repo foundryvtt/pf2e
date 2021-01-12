6e54af4c0a4d232f7b567302f691514b85e52db3
Bernhard Posselt <dev@bernhard-posselt.com>
2021-01-12 01:36:51 +0100

    add some wiring

--- src/module/gm/travel/travel-speed-sheet.ts
+++ src/module/gm/travel/travel-speed-sheet.ts
@@ -1,4 +1,25 @@
+import { calculateTravelDuration, Length, speedToVelocity, Terrain, Trip } from './travel-speed';
+
+type DetectionModeData = 'none' | 'everything' | 'before';
+type SpeedUnitData = 'feet' | 'miles';
+type TerrainData = 'normal' | 'difficult' | 'greaterDifficult';
+
+interface FormData {
+    detectionMode: DetectionModeData[];
+    speed: string[];
+    speedUnit: SpeedUnitData[];
+    explorationActivity: string[];
+    distance: number;
+    distanceUnit: SpeedUnitData;
+    terrain: TerrainData;
+    normalTerrainPenalty;
+    difficultTerrainPenalty: string;
+    greaterDifficultTerrainPenalty: string;
+}
+
 class TravelSpeedSheet extends FormApplication {
+    private travelPlan: FormData = undefined;
+
     static get defaultOptions() {
         const options = super.defaultOptions;
         options.id = 'travel-speed';
@@ -11,8 +32,9 @@ class TravelSpeedSheet extends FormApplication {
         return options;
     }
 
-    async _updateObject(event: Event, formData: any) {
-        console.log(formData);
+    async _updateObject(event: Event, formData: FormData) {
+        this.travelPlan = formData;
+        this.render(true);
     }
 
     getData() {
@@ -37,7 +59,43 @@ class TravelSpeedSheet extends FormApplication {
                 name: actor.name,
             };
         });
+        console.log('hi')
+        if (this.travelPlan !== undefined) {
+            const journey: Trip[] = [
+                {
+                    terrainCost: {
+                        difficult: parseTerrainCost(this.travelPlan.difficultTerrainPenalty),
+                        greaterDifficult: parseTerrainCost(this.travelPlan.greaterDifficultTerrainPenalty),
+                        normal: parseTerrainCost(this.travelPlan.normalTerrainPenalty),
+                    },
+                    terrain: Terrain.NORMAL,
+                    distance: {
+                        value: this.travelPlan.distance,
+                        unit: parseDistanceUnit(this.travelPlan.distanceUnit),
+                    },
+                },
+            ];
+            const velocity = speedToVelocity(30);
+            sheetData.travelDuration = calculateTravelDuration(journey, velocity);
+            console.log(sheetData.travelDuration);
+        }
         return sheetData;
+    }
+}
+
+function parseDistanceUnit(unit: SpeedUnitData): Length {
+    if (unit === 'feet') {
+        return Length.FEET;
+    } else {
+        return Length.MILES;
+    }
+}
+
+function parseTerrainCost(value: string) {
+    if (/^[\d\/]+$/.test(value)) {
+        return 1 / eval(value);
+    } else {
+        return 1;
     }
 }
 
