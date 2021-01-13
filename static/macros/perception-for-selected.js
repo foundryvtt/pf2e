9f900aa7d1c745486baa74f06ca87599135baea2
John Haverkamp <john@haverkamp.us>
2021-01-13 07:41:24 -0600

    Add macro changes to .js files as well

--- static/macros/perception-for-selected.js
+++ static/macros/perception-for-selected.js
@@ -1,7 +1,10 @@
 const tokens = canvas.tokens.controlled;
 
-if (tokens.length === 0) {
+if (tokens.filter((token) => token.actor.data.type === 'npc' || token.actor.data.type === 'character').length === 0) {
     ui.notifications.error(`You must select at least one npc/pc token`);
 } else {
-    tokens.map((p) => p.actor).forEach((actor) => actor.data.data.attributes.perception.roll(event, ['secret']));
+    tokens
+        .filter((token) => token.actor.data.type === 'npc' || token.actor.data.type === 'character')
+        .map((p) => p.actor)
+        .forEach((actor) => actor.data.data.attributes.perception.roll(event, ['secret']));
 }
