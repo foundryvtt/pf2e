fcfa88cddc49fee07b0951d02703d9d319e04453
Ladis <thedragonladis@gmail.com>
2021-01-20 21:19:33 +0100

    Fix npc skills popup not displaying its style correctly.

--- src/module/system/npc-skills-editor.ts
+++ src/module/system/npc-skills-editor.ts
@@ -21,7 +21,7 @@ export class NPCSkillsEditor extends FormApplication<PF2ENPC> {
         const options = super.defaultOptions;
 
         options.id = 'npc-skills-selector';
-        options.classes = ['pf2e'];
+        options.classes = ['pf2e', 'npc'];
         options.title = game.i18n.localize('PF2E.NPC.SkillsEditor.TitleLabel');
         options.template = 'systems/pf2e/templates/actors/npc/forms/npc-skills-editor.html';
         options.width = 'auto';
