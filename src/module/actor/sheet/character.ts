2bb8e4e5b7896f521fab5cbb0f5d5012a4d68f38
Darth Apples <mcarthur.alford@gmail.com>
2021-01-22 00:25:02 +1000

    Replaced " with '

--- src/module/actor/sheet/character.ts
+++ src/module/actor/sheet/character.ts
@@ -608,7 +608,7 @@ export class CRBStyleCharacterActorSheetPF2E extends ActorSheetPF2eCreature<PF2E
         });
 
         // Spontaneous Spell slot increment handler:
-        html.find(".spell-slots-increment-down").on('click', (event) => {
+        html.find('.spell-slots-increment-down').on('click', (event) => {
             let target = $(event.currentTarget);
             let itemId = target.data().itemId;
             let itemLevel = target.data().level;
@@ -616,16 +616,16 @@ export class CRBStyleCharacterActorSheetPF2E extends ActorSheetPF2eCreature<PF2E
             let item = actor.getOwnedItem(itemId);
             let data: SpellcastingEntryData = duplicate(item.data) as SpellcastingEntryData;
     
-            data.data.slots["slot"+itemLevel].value-=1;
-            if(data.data.slots["slot"+itemLevel].value<0) {
-                data.data.slots["slot"+itemLevel].value = 0;
+            data.data.slots['slot'+itemLevel].value-=1;
+            if(data.data.slots['slot'+itemLevel].value<0) {
+                data.data.slots['slot'+itemLevel].value = 0;
             }
             
             item.update(data);
         });
     
         // Spontaneous Spell slot reset handler:
-        html.find(".spell-slots-increment-reset").on('click', (event) => {
+        html.find('.spell-slots-increment-reset').on('click', (event) => {
             let target = $(event.currentTarget);
             let itemId = target.data().itemId;
             let itemLevel = target.data().level;
@@ -633,7 +633,7 @@ export class CRBStyleCharacterActorSheetPF2E extends ActorSheetPF2eCreature<PF2E
             let item = actor.getOwnedItem(itemId);
             let data: SpellcastingEntryData = duplicate(item.data) as SpellcastingEntryData;
 
-            data.data.slots["slot"+itemLevel].value=data.data.slots["slot"+itemLevel].max;
+            data.data.slots['slot'+itemLevel].value=data.data.slots['slot'+itemLevel].max;
             
             item.update(data);
         });
