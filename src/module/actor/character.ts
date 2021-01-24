2ac88d3b2ced71efd5398a594f0ebb9e4e6df814
In3luki <daniel.grafflage@gmail.com>
2021-01-24 01:04:04 +0100

    Add the ability to roll skill checks with a DC

--- src/module/actor/character.ts
+++ src/module/actor/character.ts
@@ -204,7 +204,7 @@ export class PF2ECharacter extends PF2EActor {
                 });
                 PF2Check.roll(
                     new PF2CheckModifier(label, stat),
-                    { actor: this, type: 'saving-throw', options: args.options ?? [], notes },
+                    { actor: this, type: 'saving-throw', options: args.options ?? [], dc: args.dc, notes },
                     args.event,
                     args.callback,
                 );
@@ -255,7 +255,7 @@ export class PF2ECharacter extends PF2EActor {
                 const label = game.i18n.localize('PF2E.PerceptionCheck');
                 PF2Check.roll(
                     new PF2CheckModifier(label, stat),
-                    { actor: this, type: 'perception-check', options: args.options ?? [], notes },
+                    { actor: this, type: 'perception-check', options: args.options ?? [], dc: args.dc, notes },
                     args.event,
                     args.callback,
                 );
@@ -400,7 +400,7 @@ export class PF2ECharacter extends PF2EActor {
                 });
                 PF2Check.roll(
                     new PF2CheckModifier(label, stat),
-                    { actor: this, type: 'skill-check', options: args.options ?? [], notes },
+                    { actor: this, type: 'skill-check', options: args.options ?? [], dc: args.dc, notes },
                     args.event,
                     args.callback,
                 );
@@ -445,7 +445,7 @@ export class PF2ECharacter extends PF2EActor {
                     const label = game.i18n.format('PF2E.SkillCheckWithName', { skillName: skill.name });
                     PF2Check.roll(
                         new PF2CheckModifier(label, stat),
-                        { actor: this, type: 'skill-check', options: args.options ?? [], notes },
+                        { actor: this, type: 'skill-check', options: args.options ?? [], dc: args.dc, notes },
                         args.event,
                         args.callback,
                     );
