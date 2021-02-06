10a2b9ce6ea61c9d9337be309b7964d6a19dd40a
Kevin Hjelden <fryguy@burntpopcorn.net>
2021-02-05 22:00:31 -0800

    Move FakeActor/Scene/Item to their own files (no-op)

---
+++ tests/fakes/fake-actor.ts
@@ -0,0 +1,62 @@
+import { ActorDataPF2e } from '@actor/actorDataDefinitions';
+
+export class FakeActor {
+    _data: Partial<ActorDataPF2e>;
+    constructor(data: Partial<ActorDataPF2e>) {
+        this._data = duplicate(data);
+        this._data.items = this._data.items ?? [];
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get name() {
+        return this._data.name;
+    }
+
+    update(changes: object) {
+        for (const [k, v] of Object.entries(changes)) {
+            global.setProperty(this._data, k, v);
+        }
+    }
+
+    updateEmbeddedEntity(type: string, data: any | any[]) {
+        // make sure data is an array, since it expects multiple
+        data = data instanceof Array ? data : [data];
+
+        for (const itemChanges of data) {
+            let obj;
+            if (type == 'OwnedItem') {
+                obj = this._data.items.find((x) => x._id === itemChanges._id);
+            }
+
+            for (const [k, v] of Object.entries(itemChanges)) {
+                global.setProperty(obj, k, v);
+            }
+        }
+    }
+
+    createEmbeddedEntity(type: string, data: any | any[]) {
+        // make sure data is an array, since it expects multiple
+        data = data instanceof Array ? data : [data];
+
+        if (type == 'OwnedItem') {
+            for (const obj of data) {
+                obj._id = 'item1';
+                this._data.items.push(obj);
+            }
+        }
+    }
+
+    deleteEmbeddedEntity(type: string, data: string | string[]) {
+        // make sure data is an array, since it expects multiple
+        data = data instanceof Array ? data : [data];
+
+        if (type == 'OwnedItem') {
+            for (const id of data) {
+                this._data.items = this._data.items.filter((x: any) => x._id !== id);
+            }
+        }
+    }
+}
---
+++ tests/fakes/fake-scene.ts
@@ -0,0 +1,66 @@
+export class FakeScene {
+    data: Partial<SceneData>;
+    constructor(data?: Partial<SceneData>) {
+        this.data = data ?? {};
+        this.data.tokens = [];
+    }
+
+    get name() {
+        return this.data.name;
+    }
+
+    addToken(token: Partial<TokenData>) {
+        this.data.tokens.push({
+            _id: '',
+            flags: [],
+            x: 0,
+            y: 0,
+            height: 100,
+            width: 100,
+            locked: false,
+            brightLight: 0,
+            dimLight: 0,
+            lightAlpha: 0,
+            lightAngle: 0,
+            lightAnimation: { type: '', speed: 0, intensity: 0 },
+            lightColor: '',
+            name: 'test',
+            displayName: 1,
+            img: '',
+            scale: 1,
+            elevation: 0,
+            lockRotation: false,
+            effects: [],
+            overlayEffect: '',
+            vision: false,
+            dimSight: 0,
+            brightSight: 0,
+            sightAngle: 0,
+            hidden: false,
+            actorId: '',
+            actorLink: false,
+            actorData: {},
+            disposition: 0,
+            displayBars: 0,
+            bar1: {},
+            bar2: {},
+            ...token,
+        });
+    }
+
+    update(changes: object) {
+        for (const [k, v] of Object.entries(changes)) {
+            global.setProperty(this.data, k, v);
+        }
+    }
+
+    updateEmbeddedEntity(entityType: string, changes: any) {
+        let obj;
+        if (entityType === 'Token') {
+            obj = this.data.tokens.find((x) => x._id === changes._id);
+        }
+        for (const [k, v] of Object.entries(changes)) {
+            global.setProperty(obj, k, v);
+        }
+    }
+}
