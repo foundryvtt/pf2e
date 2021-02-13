941c4e912b48c92a24a206866e813dc865e42eb6
iBoMbY <bomby@newsnode.de>
2021-02-13 18:25:35 +0100

    Add predicate check to PF2BaseSpeedRuleElement

--- src/module/rules/elements/base-speed.ts
+++ src/module/rules/elements/base-speed.ts
@@ -1,6 +1,8 @@
 import { PF2RuleElement } from '../rule-element';
 import { PF2RuleElementSynthetics } from '../rulesDataDefinitions';
 import { CharacterData, FamiliarData, NpcData } from '../../actor/actorDataDefinitions';
+import { PF2ModifierPredicate } from '../../modifiers';
+import { PF2EActor } from '../../actor/actor';
 
 /**
  * @category RuleElement
@@ -10,22 +12,32 @@ export class PF2BaseSpeedRuleElement extends PF2RuleElement {
         const value = super.resolveValue(this.ruleData.value, this.ruleData, this.item, actorData);
         const label = super.getDefaultLabel(this.ruleData, this.item);
         if (this.ruleData.selector && label && value) {
-            const selector = this.ruleData.selector.endsWith('-speed')
-                ? this.ruleData.selector.substring(-6)
-                : this.ruleData.selector;
-            const existing = (actorData as any).data.attributes.speed.otherSpeeds.find((speed) => {
-                return speed.type === selector;
-            });
-            if (existing) {
-                if (existing.value < value) {
-                    existing.value = value;
-                }
-            } else {
-                (actorData as any).data.attributes.speed.otherSpeeds.push({
-                    label: selector.charAt(0).toUpperCase() + selector.slice(1),
-                    type: selector,
-                    value,
+            const predicateCheck =
+                this.ruleData.predicate && this.ruleData['roll-options']
+                    ? PF2ModifierPredicate.test(
+                          new PF2ModifierPredicate(this.ruleData.predicate),
+                          PF2EActor.getRollOptions(actorData.flags, this.ruleData['roll-options']),
+                      )
+                    : true;
+
+            if (predicateCheck) {
+                const selector = this.ruleData.selector.endsWith('-speed')
+                    ? this.ruleData.selector.substring(-6)
+                    : this.ruleData.selector;
+                const existing = (actorData as any).data.attributes.speed.otherSpeeds.find((speed) => {
+                    return speed.type === selector;
                 });
+                if (existing) {
+                    if (existing.value < value) {
+                        existing.value = value;
+                    }
+                } else {
+                    (actorData as any).data.attributes.speed.otherSpeeds.push({
+                        label: selector.charAt(0).toUpperCase() + selector.slice(1),
+                        type: selector,
+                        value,
+                    });
+                }
             }
         } else {
             console.warn(
