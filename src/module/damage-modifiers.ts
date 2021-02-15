e426ee8e91d584f8bea466c6cd2382ef2747fd1a
Bernhard Posselt <dev@bernhard-posselt.com>
2021-02-15 17:32:16 +0100

    begin work on testing resistance parsing

--- src/module/damage-modifiers.ts
+++ src/module/damage-modifiers.ts
@@ -1,6 +1,5 @@
 import { Alignment, DamageImmunities, LabeledValue } from '@actor/actorDataDefinitions';
-import { combineMaps, groupBy, max, toNumber } from './utils';
-import { isChaotic, isEvil, isGood, isLawful } from './alignment';
+import { groupBy, max, toNumber } from './utils';
 
 /**
  * Looks through all values and only keeps the highest ones
@@ -75,11 +74,14 @@ export function isDamageType(value: string): value is DamageType {
 
 type Damage = Map<DamageType, number>;
 
+export type AttackType = 'unarmed' | 'melee' | 'ranged' | 'spell';
+
 export type AttackTraits =
     | 'nonlethal'
     | 'magical'
     | 'adamantine'
     | 'coldiron'
+    | 'ghostTouch'
     | 'darkwood'
     | 'mithral'
     | 'silver'
@@ -91,111 +93,116 @@ interface SplashDamage {
     value: number;
 }
 
-function combineDamages(damages: Damage[]): Damage {
-    return damages.reduce((previous, current) => {
-        return combineMaps(previous, current, (a, b) => a + b);
-    }, new Map());
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// function combineDamages(damages: Damage[]): Damage {
+//     return damages.reduce((previous, current) => {
+//         return combineMaps(previous, current, (a, b) => a + b);
+//     }, new Map());
+// }
 
-function applyImmunities({
-    normalDamage,
-    criticalDamage,
-    additionalCriticalDamage,
-    ignoreImmunities,
-    attackTraits,
-    immunities,
-    splashDamage,
-}: {
-    normalDamage: Damage;
-    splashDamage?: SplashDamage;
-    criticalDamage: Damage; // separate parameter because you can double damage or just roll double dice
-    additionalCriticalDamage: Damage; // damage which gets added after doubling the previous damage
-    attackTraits: Set<string>;
-    immunities: Set<string>;
-    ignoreImmunities: Set<string>;
-}) {
-    // build immunities based on immunities that add immunities
-    const copiedImmunities = new Set(...immunities);
-    if (copiedImmunities.has('object-immunities')) {
-        ['bleed', 'poison', 'nonlethal attacks'].forEach((immunity) => copiedImmunities.add(immunity));
-    }
-    ignoreImmunities.forEach((ignoredImmunity) => copiedImmunities.delete(ignoredImmunity));
+// function applyImmunities({
+//     normalDamage,
+//     criticalDamage,
+//     additionalCriticalDamage,
+//     ignoreImmunities,
+//     attackTraits,
+//     immunities,
+//     splashDamage,
+// }: {
+//     normalDamage: Damage;
+//     splashDamage?: SplashDamage;
+//     criticalDamage: Damage; // separate parameter because you can double damage or just roll double dice
+//     additionalCriticalDamage: Damage; // damage which gets added after doubling the previous damage
+//     attackTraits: Set<string>;
+//     immunities: Immunities;
+//     ignoreImmunities: Set<string>;
+// }) {
+// // build immunities based on immunities that add immunities
+// const copiedImmunities = new Set(...immunities);
+// if (copiedImmunities.has('object-immunities')) {
+//     ['bleed', 'poison', 'nonlethal attacks'].forEach((immunity) => copiedImmunities.add(immunity));
+// }
+// ignoreImmunities.forEach((ignoredImmunity) => copiedImmunities.delete(ignoredImmunity));
+//
+// // apply immunities to damage
+// let damage = combineDamages([normalDamage, additionalCriticalDamage]);
+// if (splashDamage !== undefined) {
+//     const splash = new Map();
+//     splash.set(splashDamage.type, splashDamage.value);
+//     damage = combineDamages([damage, splash]);
+// }
+// if (!copiedImmunities.has('critical-hits')) {
+//     // immunity to critical hits still triggers deadly but doesn't add double damage
+//     damage = combineDamages([damage, criticalDamage]);
+// }
+// if (attackTraits.has('nonlethal') && copiedImmunities.has('nonlethal attacks')) {
+//     damage.clear();
+// }
+// copiedImmunities.forEach((immunity) => {
+//     if (isDamageType(immunity)) {
+//         damage.delete(immunity);
+//     }
+// });
+// return damage;
+// }
 
-    // apply immunities to damage
-    let damage = combineDamages([normalDamage, additionalCriticalDamage]);
-    if (splashDamage !== undefined) {
-        const splash = new Map();
-        splash.set(splashDamage.type, splashDamage.value);
-        damage = combineDamages([damage, splash]);
-    }
-    if (!copiedImmunities.has('critical-hits')) {
-        // immunity to critical hits still triggers deadly but doesn't add double damage
-        damage = combineDamages([damage, criticalDamage]);
-    }
-    if (attackTraits.has('nonlethal') && copiedImmunities.has('nonlethal attacks')) {
-        damage.clear();
-    }
-    copiedImmunities.forEach((immunity) => {
-        if (isDamageType(immunity)) {
-            damage.delete(immunity);
-        }
-    });
-    return damage;
-}
+// function removeAlignmentDamage(alignment: Alignment, damage: Damage) {
+//     if (!isEvil(alignment)) {
+//         damage.delete('good');
+//     }
+//     if (!isGood(alignment)) {
+//         damage.delete('evil');
+//     }
+//     if (!isLawful(alignment)) {
+//         damage.delete('chaotic');
+//     }
+//     if (!isChaotic(alignment)) {
+//         damage.delete('lawful');
+//     }
+// }
 
-function removeAlignmentDamage(alignment: Alignment, damage: Damage) {
-    if (!isEvil(alignment)) {
-        damage.delete('good');
-    }
-    if (!isGood(alignment)) {
-        damage.delete('evil');
-    }
-    if (!isLawful(alignment)) {
-        damage.delete('chaotic');
-    }
-    if (!isChaotic(alignment)) {
-        damage.delete('lawful');
-    }
-}
-
-function removeUndeadLivingDamage(damage: Damage, isLiving: boolean, isUndead: boolean) {
-    if (isLiving) {
-        damage.delete('positive');
-    } else if (isUndead) {
-        damage.delete('negative');
-        // Another special type of physical damage is bleed damage.
-        // This is persistent damage that represents loss of blood. As such, it has
-        // no effect on nonliving creatures or living creatures that don't need blood to live.
-        damage.delete('bleed');
-    }
-}
-
-export interface DamageException {
-    traits: string[];
-    doubleResistanceVsNonMagical: boolean;
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// function removeUndeadLivingDamage(damage: Damage, isLiving: boolean, isUndead: boolean) {
+//     if (isLiving) {
+//         damage.delete('positive');
+//     } else if (isUndead) {
+//         damage.delete('negative');
+//         // Another special type of physical damage is bleed damage.
+//         // This is persistent damage that represents loss of blood. As such, it has
+//         // no effect on nonliving creatures or living creatures that don't need blood to live.
+//         damage.delete('bleed');
+//     }
+// }
 
 export interface Resistance {
     damageType: string;
     value: number;
-    exceptions: DamageException[];
+    doubleResistanceVsNonMagical: boolean;
+    except: Set<string>[];
 }
 
-export type Resistances = Map<string, Resistance>;
+export type Resistances = Resistance[];
 
 export interface Weakness {
     damageType: string;
     value: number;
 }
 
-export type Weaknesses = Map<string, Weakness>;
+export type Weaknesses = Weakness[];
+
+export interface Immunity {
+    damageType: string;
+    except: Set<string>[];
+}
+
+export type Immunities = Immunity[];
 
 /**
  * This method needs to deal with the following string value crap:
  * * physical 10 (except magical silver)
  * * physical 24 (except bludgeoning adamantine)
  * * physical 15 (except magic bludgeoning)
- * * all 5 (except force, ghost touch, or positive; double resistance to non-magical)
+ * * all 5 (except force, ghost touch, or positive; double resistance vs. non-magical)
  * * physical 20 (except vorpal adamantine)
  * * all 5 (except force, ghost touch, or negative; double resistance vs. non-magical)
  * * physical 12 (except adamantine or bludgeoning)
@@ -206,137 +213,159 @@ export type Weaknesses = Map<string, Weakness>;
  * * all 5 (except force or ghost touch)
  * @param exceptions
  */
-export function parseExceptions(exceptions: string | undefined): DamageException[] {
+export function parseExceptions(
+    exceptions: string | undefined,
+): { doubleResistanceVsNonMagical: boolean; except: Set<string>[] } {
     if (exceptions === undefined) {
-        return [];
+        return {
+            doubleResistanceVsNonMagical: false,
+            except: [],
+        };
     } else {
         const sanitizedExceptions = exceptions
             .toLocaleLowerCase()
             .replace('except', '')
             .replace(', or ', ' or ')
             .replace(', ', ' or ')
+            .replace('unarmed attacks', 'unarmed')
+            .replace('ghost touch', 'ghostTouch')
             .replace('cold iron', 'coldiron') // needed to match trait
             .trim();
 
         // double-resistance is trailing after the ; normal resistances are before that
         const traitExceptions = sanitizedExceptions.split(';');
         const traitCombinations = traitExceptions[0].split(' or ').map((value) => value.trim());
-        return traitCombinations.map((traitCombination) => {
-            // assume traits to be separated by space
-            return {
-                traits: traitCombination.split(' '),
-                doubleResistanceVsNonMagical: traitExceptions[1]?.includes('double-resistance') ?? false,
-            };
-        });
+        const doubleResistanceVsNonMagical = traitExceptions[1]?.trim() === 'double resistance vs. non-magical';
+        return {
+            doubleResistanceVsNonMagical,
+            except: traitCombinations.map((traitCombination) => {
+                // assume traits to be separated by space
+                return new Set(traitCombination.split(' '));
+            }),
+        };
     }
 }
 
 export function parseResistances(values: LabeledValue[]): Resistances {
-    const result = new Map();
-    values.forEach((value) => {
-        result.set(value.type, {
+    return values.map((value) => {
+        const { doubleResistanceVsNonMagical, except } = parseExceptions(value.exceptions);
+        return {
+            damageType: value.type,
             value: toNumber(value.value) ?? 0,
-            exceptions: parseExceptions(value.exceptions),
-        });
+            doubleResistanceVsNonMagical,
+            except,
+        };
     });
-    return result;
 }
 
 export function parseWeakness(values: LabeledValue[]): Weaknesses {
-    const result = new Map();
-    values.forEach((value) => {
-        result.set(value.type, {
+    return values.map((value) => {
+        const { except } = parseExceptions(value.exceptions);
+        return {
+            damageType: value.type,
             value: toNumber(value.value) ?? 0,
-        });
+            except,
+        };
     });
-    return result;
 }
 
-function applyWeaknessIfPositive(damage: Damage, type: DamageType, value: number) {
-    const existingValue = damage.get(type);
-    if (existingValue !== undefined && existingValue > 0) {
-        damage.set(type, existingValue + value);
-    }
+export function parseImmunities(values: LabeledValue[]): Immunities {
+    return values.map((value) => {
+        const { except } = parseExceptions(value.exceptions);
+        return {
+            damageType: value.type,
+            except,
+        };
+    });
 }
 
-function increasePhysicalDamage(damage: Damage, config: Weakness) {
-    applyWeaknessIfPositive(damage, 'slashing', config.value);
-    applyWeaknessIfPositive(damage, 'bludgeoning', config.value);
-    applyWeaknessIfPositive(damage, 'piercing', config.value);
-}
+// function applyWeaknessIfPositive(damage: Damage, type: DamageType, value: number) {
+//     const existingValue = damage.get(type);
+//     if (existingValue !== undefined && existingValue > 0) {
+//         damage.set(type, existingValue + value);
+//     }
+// }
 
-function hasPhysicalDamage(damage: Damage) {
-    return damage.has('piercing') || damage.has('slashing') || damage.has('bludgeoning');
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// function increasePhysicalDamage(damage: Damage, config: Weakness) {
+//     applyWeaknessIfPositive(damage, 'slashing', config.value);
+//     applyWeaknessIfPositive(damage, 'bludgeoning', config.value);
+//     applyWeaknessIfPositive(damage, 'piercing', config.value);
+// }
+//
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// function hasPhysicalDamage(damage: Damage) {
+//     return damage.has('piercing') || damage.has('slashing') || damage.has('bludgeoning');
+// }
 
-function applyWeaknesses({
-    damage,
-    isAreaDamage,
-    splashDamage,
-    weaknesses,
-    attackTraits,
-}: {
-    damage: Damage;
-    isAreaDamage: boolean;
-    splashDamage?: SplashDamage;
-    attackTraits: Set<string>;
-    weaknesses: Resistances;
-}) {
-    const firstDamageType = damage.keys()[0];
-    for (const [type, config] of weaknesses.entries()) {
-        if (type === 'vorpal weapons' && attackTraits.has('vorpal')) {
-            increasePhysicalDamage(damage, config);
-        } else if (attackTraits.has(type)) {
-            increasePhysicalDamage(damage, config);
-        } else if (type === 'critical hits') {
-            // https://2e.aonprd.com/MonsterFamilies.aspx?ID=103 lists critical weaknesses
-            // if physical damage is there, increase that, otherwise simply take the first
-            // damage type and increase it
-            if (hasPhysicalDamage(damage)) {
-                increasePhysicalDamage(damage, config);
-            } else {
-                applyWeaknessIfPositive(damage, firstDamageType, config.value);
-            }
-        } else if (type === 'splash-damage' && splashDamage !== undefined) {
-            applyWeaknessIfPositive(damage, splashDamage.type, config.value);
-        } else if (type === 'area-damage' && isAreaDamage) {
-            applyWeaknessIfPositive(damage, firstDamageType, config.value);
-        } else {
-            if (isDamageType(type)) {
-                applyWeaknessIfPositive(damage, type, config.value);
-            }
-        }
-    }
-}
+// function applyWeaknesses({
+//     damage,
+//     isAreaDamage,
+//     splashDamage,
+//     weaknesses,
+//     attackTraits,
+// }: {
+//     damage: Damage;
+//     isAreaDamage: boolean;
+//     splashDamage?: SplashDamage;
+//     attackTraits: Set<string>;
+//     weaknesses: Resistances;
+// }) {
+// const firstDamageType = damage.keys()[0];
+// for (const [type, config] of weaknesses.entries()) {
+//     if (type === 'vorpal weapons' && attackTraits.has('vorpal')) {
+//         increasePhysicalDamage(damage, config);
+//     } else if (attackTraits.has(type)) {
+//         increasePhysicalDamage(damage, config);
+//     } else if (type === 'critical hits') {
+//         // https://2e.aonprd.com/MonsterFamilies.aspx?ID=103 lists critical weaknesses
+//         // if physical damage is there, increase that, otherwise simply take the first
+//         // damage type and increase it
+//         if (hasPhysicalDamage(damage)) {
+//             increasePhysicalDamage(damage, config);
+//         } else {
+//             applyWeaknessIfPositive(damage, firstDamageType, config.value);
+//         }
+//     } else if (type === 'splash-damage' && splashDamage !== undefined) {
+//         applyWeaknessIfPositive(damage, splashDamage.type, config.value);
+//     } else if (type === 'area-damage' && isAreaDamage) {
+//         applyWeaknessIfPositive(damage, firstDamageType, config.value);
+//     } else {
+//         if (isDamageType(type)) {
+//             applyWeaknessIfPositive(damage, type, config.value);
+//         }
+//     }
+// }
+// }
 
-function applyResistances({
-    damage,
-    isCriticalHit,
-    isUnarmedAttack,
-    reduceResistances,
-    resistances,
-}: {
-    damage: Damage;
-    isCriticalHit: boolean;
-    isUnarmedAttack: boolean;
-    reduceResistances: Damage;
-    resistances: Weaknesses;
-}): number {
-    // TODO
-    console.log(damage);
-    console.log(isCriticalHit);
-    console.log(isUnarmedAttack);
-    console.log(reduceResistances);
-    console.log(resistances);
-    return 0;
-}
+// function applyResistances({
+//     damage,
+//     isCriticalHit,
+//     attackType,
+//     reduceResistances,
+//     resistances,
+// }: {
+//     damage: Damage;
+//     isCriticalHit: boolean;
+//     attackType: AttackType;
+//     reduceResistances: Damage;
+//     resistances: Weaknesses;
+// }): number {
+//     // TODO
+//     console.log(damage);
+//     console.log(isCriticalHit);
+//     console.log(attackType);
+//     console.log(reduceResistances);
+//     console.log(resistances);
+//     return 0;
+// }
 
 /**
  * Implementation of https://2e.aonprd.com/Rules.aspx?ID=342
  */
 export function calculateDamage({
     isCriticalHit,
-    isUnarmedAttack,
+    attackType,
     isAreaDamage,
     normalDamage,
     splashDamage,
@@ -353,7 +382,7 @@ export function calculateDamage({
     weaknesses,
 }: {
     isCriticalHit: boolean; // some monsters have weaknesses or resistances to critical hits
-    isUnarmedAttack: boolean; // some monsters have resistance to everything except unarmed attacks
+    attackType: AttackType; // some monsters have resistance to everything except unarmed attacks or spells
     isAreaDamage: boolean;
     normalDamage: Damage;
     splashDamage?: SplashDamage;
@@ -364,22 +393,23 @@ export function calculateDamage({
     attackTraits: Set<AttackTraits>;
     isUndead: boolean;
     isLiving: boolean;
-    immunities: Set<string>;
+    immunities: Immunities;
     resistances: Weaknesses;
     weaknesses: Resistances;
     alignment: Alignment;
 }): number {
-    const damage = applyImmunities({
-        normalDamage,
-        criticalDamage,
-        additionalCriticalDamage,
-        attackTraits,
-        immunities,
-        ignoreImmunities,
-        splashDamage,
-    });
-    removeUndeadLivingDamage(damage, isLiving, isUndead);
-    removeAlignmentDamage(alignment, damage);
-    applyWeaknesses({ damage, weaknesses, attackTraits, splashDamage, isAreaDamage });
-    return applyResistances({ damage, resistances, reduceResistances, isCriticalHit, isUnarmedAttack });
+    // const damage = applyImmunities({
+    //     normalDamage,
+    //     criticalDamage,
+    //     additionalCriticalDamage,
+    //     attackTraits,
+    //     immunities,
+    //     ignoreImmunities,
+    //     splashDamage,
+    // });
+    // removeUndeadLivingDamage(damage, isLiving, isUndead);
+    // removeAlignmentDamage(alignment, damage);
+    // applyWeaknesses({ damage, weaknesses, attackTraits, splashDamage, isAreaDamage });
+    // return applyResistances({ damage, resistances, reduceResistances, isCriticalHit, attackType });
+    return 0;
 }
--- tests/module/damage-modifiers.test.ts
+++ tests/module/damage-modifiers.test.ts
@@ -1,4 +1,4 @@
-import { mergeImmunities, mergeResistancesOrWeaknesses } from '../../src/module/damage-modifiers';
+import { mergeImmunities, mergeResistancesOrWeaknesses, parseExceptions } from '../../src/module/damage-modifiers';
 import { LabeledValue } from '@actor/actorDataDefinitions';
 
 function createLabeledValue(type: string, value: number, exceptions?: string): LabeledValue {
@@ -29,5 +29,108 @@ describe('Test Damage Modifiers', () => {
             createLabeledValue('cold', 5, 'silver'),
             createLabeledValue('fire', 3),
         ]);
+    });
+});
+
+describe('Test Parsing Exceptions', () => {
+    /**
+     * This method needs to deal with the following string value crap:
+     * * physical 10 (except magical silver)
+     * * physical 24 (except bludgeoning adamantine)
+     * * physical 15 (except magic bludgeoning)
+     * * physical 20 (except vorpal adamantine)
+     * * all 5 (except force, ghost touch, or negative; double resistance vs. non-magical)
+     * * physical 12 (except adamantine or bludgeoning)
+     * * physical 15 (except cold iron)
+     * * physical 5 (except magical)
+     * * all 15 (except unarmed attacks)
+     * * all 15 (except non-magical)
+     * * all 5 (except force or ghost touch)
+     * @param exceptions
+     */
+    const testCases = [
+        {
+            exception: 'except magical silver',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['magical', 'silver'])],
+            },
+        },
+        {
+            exception: 'except bludgeoning adamantine',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['bludgeoning', 'adamantine'])],
+            },
+        },
+        {
+            exception: 'except magical bludgeoning',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['magical', 'bludgeoning'])],
+            },
+        },
+        {
+            exception: 'except force, ghost touch, or positive; double resistance vs. non-magical',
+            expected: {
+                doubleResistanceVsNonMagical: true,
+                except: [new Set(['force']), new Set(['ghostTouch']), new Set(['positive'])],
+            },
+        },
+        {
+            exception: 'except vorpal adamantine',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['vorpal', 'adamantine'])],
+            },
+        },
+        {
+            exception: 'except adamantine or bludgeoning',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['adamantine']), new Set(['bludgeoning'])],
+            },
+        },
+        {
+            exception: 'except cold iron',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['coldiron'])],
+            },
+        },
+        {
+            exception: 'except magical',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['magical'])],
+            },
+        },
+        {
+            exception: 'except unarmed attacks',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['unarmed'])],
+            },
+        },
+        {
+            exception: 'except non-magical',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['non-magical'])],
+            },
+        },
+        {
+            exception: 'except force or ghost touch',
+            expected: {
+                doubleResistanceVsNonMagical: false,
+                except: [new Set(['force']), new Set(['ghostTouch'])],
+            },
+        },
+    ];
+
+    testCases.forEach((testCase) => {
+        test(`test ${testCase.exception}`, () => {
+            expect(parseExceptions(testCase.exception)).toEqual(testCase.expected);
+        });
     });
 });
