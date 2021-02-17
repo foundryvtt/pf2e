f151c10348864485e7abc7bb6d6d3e498b58587f
Bernhard Posselt <dev@bernhard-posselt.com>
2021-02-17 10:03:50 +0100

    add magic immunity

--- src/module/damage-modifiers.ts
+++ src/module/damage-modifiers.ts
@@ -93,6 +93,9 @@ export type AttackTrait =
     | 'silver'
     | 'orichalcum'
     | 'vorpal'
+    | 'magic'
+    // use the magic trait if it was a spell attack, exclude it if it was a spell attack that wasn't relevant
+    // for instance a Will-o’-Wisp would exclude magic missile
     | 'unarmed';
 
 const allAttackTraits = new Set<string>();
@@ -263,6 +266,17 @@ function applyImmunities({
             damage,
             attackTraits,
             applicableModifierTypes: ['nonlethal attacks'],
+            applyModifier: () => damage.clear(),
+        });
+    }
+
+    // same for magic
+    if (attackTraits.has('magic')) {
+        ifModifierApplies({
+            modifiersByType: immunitiesByType,
+            damage,
+            attackTraits,
+            applicableModifierTypes: ['magic'],
             applyModifier: () => damage.clear(),
         });
     }
