c59f7409c281596e6077c12e53b2b17e85096923
Théo Chapelot <theo.chapelot@icloud.com>
2021-02-18 00:16:58 +0100

    revert fixes

--- src/module/actor/sheet/character.ts
+++ src/module/actor/sheet/character.ts
@@ -847,13 +847,12 @@ export class CRBStyleCharacterActorSheetPF2E extends ActorSheetPF2eCreature<PF2E
         const item = await PF2EItem.fromDropData(data);
         const itemData = duplicate(item.data);
 
-        if (itemData.type === 'feat') {
-            // Placing it inside since physical items don't have such data
-            const { slotId, featType } =
-                event.target !== null
-                    ? $(event.target).closest('.item').data()
-                    : { slotId: undefined, featType: undefined };
+        const { slotId, featType } =
+            event.target !== null
+                ? $(event.target).closest('.item').data()
+                : { slotId: undefined, featType: undefined };
 
+        if (itemData.type === 'feat') {
             if (slotId !== undefined && featType === itemData.data?.featType?.value) {
                 itemData.data.location = slotId;
                 const items = await Promise.all([
