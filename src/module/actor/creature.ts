25e190fbf2663d40aed0a83db13ed51b0256f0bf
Shark that walks like a man <rsimoes@gmail.com>
2021-03-05 15:15:57 -0600

    Make a parallel intermediate CreatureData

--- src/module/actor/creature.ts
+++ src/module/actor/creature.ts
@@ -1,4 +1,10 @@
 import { PF2EActor } from './actor';
+import { CreatureData } from './actor-data-definitions';
 
 /** An "actor" a Pathfinder sense rather than a Foundry one: all should contain attributes and abilities */
 export abstract class PF2ECreature extends PF2EActor {}
+
+export interface PF2ECreature {
+    data: CreatureData;
+    _data: CreatureData;
+}
