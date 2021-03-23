1e11524fe49a275f8d924a59c59ffc95b169c032
Carlos Fernandez <cfern1990@gmail.com>
2021-03-22 21:07:57 -0400

    Refactor chat damage buttons

--- src/scripts/chat/chat-damage-buttons-pf2e.ts
+++ src/scripts/chat/chat-damage-buttons-pf2e.ts
@@ -1,202 +1,87 @@
 import { ActorPF2e } from '@actor/base';
 
-class ChatDamageButtonsPF2e extends Application {
-    init() {
-        Hooks.on('renderChatMessage', (message, html, _data) => {
-            const damageRoll: any = message.getFlag(game.system.id, 'damageRoll');
-            if (damageRoll || !message.isRoll || message.roll?.dice[0]?.faces === 20) return;
+class ChatDamageButtonsPF2e {
+    /**
+     * Adds apply damage buttons to a chat message.
+     * @param message message to attach the buttons to
+     * @param html html of the message to attach the buttons to
+     */
+    static attach(message: ChatMessage, html: JQuery<HTMLElement>) {
+        const damageRoll: any = message.getFlag(game.system.id, 'damageRoll');
+        const isRoll = damageRoll || message.isRoll;
+        const isD20 = message.roll && message.roll.dice[0]?.faces === 20;
+        if (!isRoll || isD20) return;
 
-            function makeButton(key: string, icon: string, className: string) {
-                const btnStyling = 'width: 22px; height:22px; font-size:10px; line-height:1px; padding-left: 5px;';
-                const title = game.i18n.localize(key);
-                return $(
-                    `<button class="${className}" style="${btnStyling}" title="${title}"><i class="fas ${icon}"></i></button>`,
-                );
-            }
+        function makeButton(key: string, icon: string, className = '') {
+            const classStr = className ? `class=${className}` : '';
+            const title = game.i18n.localize(key);
+            return $(`<button ${classStr} title="${title}"><i class="fas ${icon}"></i></button>`);
+        }
 
-            const fullDamageButton = makeButton('PF2E.DamageButton.Full', 'fa-user-minus', 'dice-total-fullDamage-btn');
-            const halfDamageButton = makeButton(
-                'PF2E.DamageButton.Half',
-                'fa-user-shield',
-                'dice-total-halfDamage-btn',
-            );
-            const doubleDamageButton = makeButton(
-                'PF2E.DamageButton.Double',
-                'fa-user-injured',
-                'dice-total-doubleDamage-btn',
-            );
+        const full = makeButton('PF2E.DamageButton.Full', 'fa-user-minus');
+        const double = makeButton('PF2E.DamageButton.Double', 'fa-user-injured');
+        const half = makeButton('PF2E.DamageButton.Half', 'fa-user-shield');
+        const shield = makeButton('PF2E.DamageButton.ShieldBlock', 'fa-shield-alt', 'dice-total-shield-btn');
+        const heal = makeButton('PF2E.DamageButton.Healing', 'fa-user-plus');
 
-            // need to rework to a shield raised status, instead of using a GM global CONFIG
-            const shieldButton = makeButton('PF2E.DamageButton.ShieldBlock', 'fa-shield-alt', 'dice-total-shield-btn');
-            const fullHealingButton = makeButton(
-                'PF2E.DamageButton.Healing',
-                'fa-user-plus',
-                'dice-total-fullHealing-btn',
-            );
-
-            const btnContainer1 = $(
-                `<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>`,
-            );
-            const btnContainer2 = $(
-                `<span class="dmgBtn-container" style="position:absolute; top:0; right:0; bottom:1px;"></span>`,
-            );
-            btnContainer1.append(fullDamageButton);
-            btnContainer1.append(halfDamageButton);
-            btnContainer1.append(doubleDamageButton);
-            btnContainer2.append(shieldButton);
-            btnContainer2.append(fullHealingButton);
-
-            html.find('.dice-total').wrapInner('<span id="value"></span>').append(btnContainer1);
-            html.find('.dice-formula').append(btnContainer2);
-
-            // Handle button clicks
-            fullDamageButton.click((ev) => {
-                ev.stopPropagation();
-                let attribute = 'attributes.hp';
-                if (CONFIG.PF2E.chatDamageButtonShieldToggle) {
-                    attribute = 'attributes.shield';
-                    html.find('.dice-total-shield-btn').toggleClass('shield-activated');
-                    CONFIG.PF2E.chatDamageButtonShieldToggle = false;
-                }
-                if (ev.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, 1, attribute);
-                } else {
-                    ActorPF2e.applyDamage(html, 1, attribute);
-                }
-            });
-
-            halfDamageButton.click((ev) => {
-                ev.stopPropagation();
-                let attribute = 'attributes.hp';
-                if (CONFIG.PF2E.chatDamageButtonShieldToggle) {
-                    attribute = 'attributes.shield';
-                    html.find('.dice-total-shield-btn').toggleClass('shield-activated');
-                    CONFIG.PF2E.chatDamageButtonShieldToggle = false;
-                }
-                if (ev.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, 0.5, attribute);
-                } else {
-                    ActorPF2e.applyDamage(html, 0.5, attribute);
-                }
-            });
-
-            doubleDamageButton.click((ev) => {
-                ev.stopPropagation();
-                let attribute = 'attributes.hp';
-                if (CONFIG.PF2E.chatDamageButtonShieldToggle) {
-                    attribute = 'attributes.shield';
-                    html.find('.dice-total-shield-btn').toggleClass('shield-activated');
-                    CONFIG.PF2E.chatDamageButtonShieldToggle = false;
-                }
-                if (ev.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, 2, attribute);
-                } else {
-                    ActorPF2e.applyDamage(html, 2, attribute);
-                }
-            });
-
-            shieldButton.click((ev) => {
-                ev.stopPropagation();
-                html.find('.dice-total-shield-btn').toggleClass('shield-activated');
-                CONFIG.PF2E.chatDamageButtonShieldToggle = !CONFIG.PF2E.chatDamageButtonShieldToggle;
-            });
-
-            fullHealingButton.click((ev) => {
-                ev.stopPropagation();
-                if (ev.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, -1);
-                } else {
-                    ActorPF2e.applyDamage(html, -1);
-                }
-            });
-        });
-
-        Hooks.on('renderChatMessage', (message, html, _data) => {
-            const damageRoll: any = message.getFlag(game.system.id, 'damageRoll');
-            if (!damageRoll) return;
-
-            function makeButton(key: string, icon: string, className = '') {
-                const classStr = className ? `class=${className}` : '';
-                const title = game.i18n.localize(key);
-                return $(
-                    `<button style="flex: 1 1 0;" ${classStr} title="${title}"><i class="fas ${icon}"></i></button>`,
-                );
-            }
-
-            const full = makeButton('PF2E.DamageButton.Full', 'fa-user-minus');
-            const double = makeButton('PF2E.DamageButton.Double', 'fa-user-injured');
-            const half = makeButton('PF2E.DamageButton.Half', 'fa-user-shield');
-            const shield = makeButton('PF2E.DamageButton.ShieldBlock', 'fa-shield-alt', 'dice-total-shield-btn');
-            const heal = makeButton('PF2E.DamageButton.Healing', 'fa-user-plus');
-
-            const buttons = $(`<div style="display: flex; margin-top: 3px;"></div>`);
+        if (damageRoll) {
+            // Buttons at the bottom of the damage prompt
+            const buttons = $(`<div class="chat-damage-buttons"></div>`);
             buttons.append(full, half, double, shield, heal);
             html.append(buttons);
+        } else {
+            // Overlay damage buttons
+            const btnContainer1 = $(`<span class="chat-damage-buttons-overlay"></span>`);
+            const btnContainer2 = $(`<span class="chat-damage-buttons-overlay"></span>`);
+            btnContainer1.append(shield, heal);
+            btnContainer2.append(full, half, double);
+            html.find('.dice-formula').append(btnContainer1);
+            html.find('.dice-total').wrapInner('<span id="value"></span>').append(btnContainer2);
+        }
 
-            // Handle button clicks
-            full.on('click', (event) => {
-                event.stopPropagation();
-                let attribute = 'attributes.hp';
-                if (CONFIG.PF2E.chatDamageButtonShieldToggle) {
-                    attribute = 'attributes.shield';
-                    shield.toggleClass('shield-activated');
-                    CONFIG.PF2E.chatDamageButtonShieldToggle = false;
-                }
-                if (event.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, 1, attribute);
-                } else {
-                    ActorPF2e.applyDamage(html, 1, attribute);
-                }
-            });
+        // Handle button clicks
+        full.on('click', (event) => {
+            event.stopPropagation();
+            ChatDamageButtonsPF2e.applyDamage(html, 1, event.shiftKey);
+        });
 
-            half.on('click', (event) => {
-                event.stopPropagation();
-                let attribute = 'attributes.hp';
-                if (CONFIG.PF2E.chatDamageButtonShieldToggle) {
-                    attribute = 'attributes.shield';
-                    shield.toggleClass('shield-activated');
-                    CONFIG.PF2E.chatDamageButtonShieldToggle = false;
-                }
-                if (event.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, 0.5, attribute);
-                } else {
-                    ActorPF2e.applyDamage(html, 0.5, attribute);
-                }
-            });
+        half.on('click', (event) => {
+            event.stopPropagation();
+            ChatDamageButtonsPF2e.applyDamage(html, 0.5, event.shiftKey);
+        });
 
-            double.on('click', (event) => {
-                event.stopPropagation();
-                let attribute = 'attributes.hp';
-                if (CONFIG.PF2E.chatDamageButtonShieldToggle) {
-                    attribute = 'attributes.shield';
-                    shield.toggleClass('shield-activated');
-                    CONFIG.PF2E.chatDamageButtonShieldToggle = false;
-                }
-                if (event.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, 2, attribute);
-                } else {
-                    ActorPF2e.applyDamage(html, 2, attribute);
-                }
-            });
+        double.on('click', (event) => {
+            event.stopPropagation();
+            ChatDamageButtonsPF2e.applyDamage(html, 2, event.shiftKey);
+        });
 
-            shield.on('click', (event) => {
-                event.stopPropagation();
-                shield.toggleClass('shield-activated');
-                CONFIG.PF2E.chatDamageButtonShieldToggle = !CONFIG.PF2E.chatDamageButtonShieldToggle;
-            });
+        shield.on('click', (event) => {
+            event.stopPropagation();
+            shield.toggleClass('shield-activated');
+            CONFIG.PF2E.chatDamageButtonShieldToggle = !CONFIG.PF2E.chatDamageButtonShieldToggle;
+        });
 
-            heal.on('click', (event) => {
-                event.stopPropagation();
-                if (event.shiftKey) {
-                    ChatDamageButtonsPF2e.shiftModifyDamage(html, -1);
-                } else {
-                    ActorPF2e.applyDamage(html, -1);
-                }
-            });
+        heal.on('click', (event) => {
+            event.stopPropagation();
+            ChatDamageButtonsPF2e.applyDamage(html, -1, event.shiftKey);
         });
     }
 
-    static shiftModifyDamage(html, multiplier, attributePassed = 'attributes.hp') {
+    static applyDamage(html: JQuery<HTMLElement>, multiplier: number, promptModifier = false) {
+        let attribute = 'attributes.hp';
+        if (CONFIG.PF2E.chatDamageButtonShieldToggle && multiplier > 0) {
+            attribute = 'attributes.shield';
+            html.find('.dice-total-shield-btn').toggleClass('shield-activated');
+            CONFIG.PF2E.chatDamageButtonShieldToggle = false;
+        }
+        if (promptModifier) {
+            ChatDamageButtonsPF2e.shiftModifyDamage(html, multiplier, attribute);
+        } else {
+            ActorPF2e.applyDamage(html, multiplier, attribute);
+        }
+    }
+
+    static shiftModifyDamage(html: JQuery<HTMLElement>, multiplier: number, attributePassed = 'attributes.hp') {
         new Dialog({
             title: game.i18n.localize('PF2E.UI.shiftModifyDamageTitle'),
             content: `<form>
@@ -231,5 +116,7 @@ class ChatDamageButtonsPF2e extends Application {
     }
 }
 
-const chatButtons = new ChatDamageButtonsPF2e();
-chatButtons.init();
+// Add apply damage cards after a chat message is rendered
+Hooks.on('renderChatMessage', (message, html, _data) => {
+    ChatDamageButtonsPF2e.attach(message, html);
+});
