b2628c3cee4cf5c30d6a6c166d3839b4e3376c32
Shark that walks like a man <rsimoes@gmail.com>
2021-03-27 12:08:05 -0500

    Move `FeatPF2e` class to its own file

---
+++ src/module/item/feat.ts
@@ -0,0 +1,29 @@
+import { ItemPF2e } from "./base";
+import { FeatData, FeatType } from "./data-definitions";
+
+export class FeatPF2e extends ItemPF2e {
+    get featType(): { value: FeatType; label: string } {
+        return {
+            value: this.data.data.featType.value,
+            label: game.i18n.localize(CONFIG.PF2E.featTypes[this.data.data.featType.value]),
+        };
+    }
+
+    /**
+     * Prepare chat card data for items of the "Feat" type
+     */
+    getChatData(htmlOptions?: Record<string, boolean>) {
+        const data = this.data.data;
+        const properties = [
+            `Level ${data.level.value || 0}`,
+            data.actionType.value ? CONFIG.PF2E.actionTypes[data.actionType.value] : null,
+        ].filter((p) => p);
+        const traits = ItemPF2e.traitChatData(data.traits, CONFIG.PF2E.featTraits);
+        return this.processChatData({ ...data, properties, traits }, htmlOptions);
+    }
+}
+
+export interface FeatPF2e {
+    data: FeatData;
+    _data: FeatData;
+}
--- src/module/item/others.ts
+++ src/module/item/others.ts
@@ -6,8 +6,6 @@ import {
     ConditionData,
     ConsumableData,
     EquipmentData,
-    FeatData,
-    FeatType,
     KitData,
     LoreData,
     MartialData,
@@ -92,32 +90,6 @@ export class EquipmentPF2e extends PhysicalItemPF2e {
 export interface EquipmentPF2e {
     data: EquipmentData;
     _data: EquipmentData;
-}
-
-export class FeatPF2e extends ItemPF2e {
-    get featType(): { value: FeatType; label: string } {
-        return {
-            value: this.data.data.featType.value,
-            label: game.i18n.localize(CONFIG.PF2E.featTypes[this.data.data.featType.value]),
-        };
-    }
-
-    /**
-     * Prepare chat card data for items of the "Feat" type
-     */
-    getChatData(htmlOptions?: Record<string, boolean>) {
-        const data = this.data.data;
-        const properties = [
-            `Level ${data.level.value || 0}`,
-            data.actionType.value ? CONFIG.PF2E.actionTypes[data.actionType.value] : null,
-        ].filter((p) => p);
-        const traits = ItemPF2e.traitChatData(data.traits, CONFIG.PF2E.featTraits);
-        return this.processChatData({ ...data, properties, traits }, htmlOptions);
-    }
-}
-export interface FeatPF2e {
-    data: FeatData;
-    _data: FeatData;
 }
 
 export class LorePF2e extends ItemPF2e {
