fb7b28eb26aab79c61819a74e0812b79fde69a6f
Shark that walks like a man <rsimoes@gmail.com>
2021-04-12 05:17:16 -0500

    Fix migration 615 so it actually updates the traits array

--- src/module/migrations/615-remove-instinct-trait.ts
+++ src/module/migrations/615-remove-instinct-trait.ts
@@ -1,11 +1,11 @@
 import { MigrationBase } from './base';
 import { ItemDataPF2e } from '@item/data-definitions';
 
-/** Numify melee bonus.value property */
+/** Remove "instinct" trait from feats */
 export class Migration615RemoveInstinctTrait extends MigrationBase {
     static version = 0.615;
 
     async updateItem(itemData: ItemDataPF2e) {
-        itemData.data.traits.value.filter((trait) => trait !== 'instinct');
+        itemData.data.traits.value = itemData.data.traits.value.filter((trait) => trait !== 'instinct');
     }
 }
