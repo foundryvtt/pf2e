b4273260fb7898533f46a60f12c96286d12d0f6a
In3luki <daniel.grafflage@gmail.com>
2021-04-13 19:04:49 +0200

    Tagify feat prerequisites

--- src/module/migrations/index.ts
+++ src/module/migrations/index.ts
@@ -46,6 +46,7 @@ import { Migration612NormalizeRarities } from './612-normalize-rarities';
 import { Migration613RemoveAmmoCharges } from '@module/migrations/613-remove-ammo-charges';
 import { Migration614NumifyMeleeBonuses } from './614-numify-melee-bonuses';
 import { Migration615RemoveInstinctTrait } from '@module/migrations/615-remove-instinct-trait';
+import { Migration616MigrateFeatPrerequisites } from './616-migrate-feat-prerequisites';
 
 export class Migrations {
     private static list = [
@@ -96,6 +97,7 @@ export class Migrations {
         Migration613RemoveAmmoCharges,
         Migration614NumifyMeleeBonuses,
         Migration615RemoveInstinctTrait,
+        Migration616MigrateFeatPrerequisites,
     ];
 
     static get latestVersion(): number {
