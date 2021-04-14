efac254a2bd268056fb7ee5db8bced45333da9f2
Gunnar.Busch <gunnar.busch@googlemail.com>
2021-04-14 22:43:13 +0200

    Mountain Stance Mage armor and feats macro

--- static/macros/mountain-stance.js
+++ static/macros/mountain-stance.js
@@ -1,4 +1,19 @@
-const ITEM_UUID = 'Compendium.pf2e.feat-effects.gYpy9XBPScIlY93p'; // Stance: Mountain Stance
+let ITEM_UUID = 'Compendium.pf2e.feat-effects.gYpy9XBPScIlY93p'; // Stance: Mountain Stance
+
+function checkFeat(slug) {
+    if (token.actor.items.find((i) => i.data.data.slug === slug && i.type === 'feat')) {
+        return true;
+    }
+    return false;
+}
+
+if (checkFeat('mountain-stronghold')) {
+    ITEM_UUID = 'Compendium.pf2e.feat-effects.LXbVcutEIW8eWZEz';
+}
+if (checkFeat('mountain-quake')) {
+    ITEM_UUID = 'Compendium.pf2e.feat-effects.wuERa7exfXXl6I37';
+}
+
 (async () => {
     const effect = duplicate(await fromUuid(ITEM_UUID));
     effect.flags.core = effect.flags.core ?? {};
@@ -39,7 +54,14 @@ const ITEM_UUID = 'Compendium.pf2e.feat-effects.gYpy9XBPScIlY93p'; // Stance: Mo
                 ?.find((item) => item.data.data.invested.value)
                 ? 3
                 : bracers;
-            let itemBonus = Math.max(bracers, clothingPotency);
+            let mageArmorBonus = 0;
+            const mageArmor = token.actor.items
+                .filter((item) => item.data.type === 'effect')
+                ?.find((item) => item.data.data.slug === 'spell-effect-mage-armor');
+            if (mageArmor) {
+                mageArmorBonus = mageArmor.data.data.level.value === 10 ? 3 : mageArmor.data.data.level.value >= 5 ? 2 : 1;
+            }
+            let itemBonus = Math.max(bracers, clothingPotency, mageArmorBonus);
             const rule = effect.data.rules.find(
                 (r) => r.selector === 'ac' && r.key === 'PF2E.RuleElement.FlatModifier',
             );
