2bf16925c5c2a3603d3802186d16d3fbfde9da34
Shark that walks like a man <rsimoes@gmail.com>
2021-04-16 13:59:33 -0500

    Fix display of traits in action sheet sidebar

--- src/module/item/sheet/action.ts
+++ src/module/item/sheet/action.ts
@@ -1,10 +1,10 @@
 import { ActionPF2e } from '@item/action';
-import { WeaponData } from '@item/data-definitions';
-import { ItemSheetPF2e } from './base';
+import { FeatData, WeaponData } from '@item/data-definitions';
+import { ItemSheetDataPF2e, ItemSheetPF2e } from './base';
 
 export class ActionSheetPF2e extends ItemSheetPF2e<ActionPF2e> {
     getData() {
-        const data = super.getData();
+        const data: ItemSheetDataPF2e<FeatData> = super.getData();
         const actorWeapons: WeaponData[] =
             this.actor?.data.items.filter((itemData): itemData is WeaponData => itemData.type === 'weapon') ?? [];
         const actionType = data.data.actionType.value || 'action';
@@ -15,16 +15,17 @@ export class ActionSheetPF2e extends ItemSheetPF2e<ActionPF2e> {
         else if (actionType === 'passive') actionImg = 'passive';
 
         data.item.img = this.getActionImg(actionImg.toString());
-        data.categories = CONFIG.PF2E.actionCategories;
-        data.weapons = actorWeapons;
-        data.actionTypes = CONFIG.PF2E.actionTypes;
-        data.actionsNumber = CONFIG.PF2E.actionsNumber;
-        data.featTraits = CONFIG.PF2E.featTraits;
-        data.skills = CONFIG.PF2E.skillList;
-        data.proficiencies = CONFIG.PF2E.proficiencyLevels;
-        data.actionTags = [data.data.traits.value].filter((t) => !!t);
 
-        this.prepareTraits(data.data.traits, CONFIG.PF2E.featTraits);
-        return data;
+        return {
+            ...data,
+            categories: CONFIG.PF2E.actionCategories,
+            weapons: actorWeapons,
+            actionTypes: CONFIG.PF2E.actionTypes,
+            actionsNumber: CONFIG.PF2E.actionsNumber,
+            featTraits: CONFIG.PF2E.featTraits,
+            skills: CONFIG.PF2E.skillList,
+            proficiencies: CONFIG.PF2E.proficiencyLevels,
+            traits: this.prepareOptions(CONFIG.PF2E.featTraits, data.data.traits),
+        };
     }
 }
