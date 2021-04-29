e70534c6e4a7018e8edded0c1e625628d8d8d0cb
TikaelSol <tikael@gmail.com>
2021-04-29 22:47:02 +0000

    Replace tabs with spaces

--- src/scripts/macros/encouraging-words.ts
+++ src/scripts/macros/encouraging-words.ts
@@ -3,121 +3,121 @@ import { LocalizePF2e } from '../../module/system/localize';
 import { ActionDefaultOptions } from '../..//module/system/actions/actions';
 
 export function encouragingWords(options: ActionDefaultOptions): void {
-	const translations = LocalizePF2e.translations.PF2E.Actions.EncouragingWords;
+    const translations = LocalizePF2e.translations.PF2E.Actions.EncouragingWords;
 
-	const actors = Array.isArray(options.actors) ? options.actors : [options.actors];
+    const actors = Array.isArray(options.actors) ? options.actors : [options.actors];
     const actor = actors[0];
     if (actors.length > 1 || !(actor instanceof CharacterPF2e)) {
         ui.notifications.error(translations.BadArgs);
         return;
     }
 
-	const encouragingWordsMacro = async ({ DC, bonus, dip }) => {
-	    const options = actor.getRollOptions(['all', 'skill-check', 'diplomacy']);
+    const encouragingWordsMacro = async ({ DC, bonus, dip }) => {
+        const options = actor.getRollOptions(['all', 'skill-check', 'diplomacy']);
 
-	    options.push(translations.Title);
-	    options.push('action:encourage-words');
+        options.push(translations.Title);
+        options.push('action:encourage-words');
 
-	    const dc = {
-	        value: DC,
-	    };
+        const dc = {
+            value: DC,
+        };
 
-	    dip.roll({
-	        dc: dc,
-	        event: event,
-	        options: options,
-	        callback: (roll) => {
-	            let healFormula, successLabel;
+        dip.roll({
+            dc: dc,
+            event: event,
+            options: options,
+            callback: (roll) => {
+                let healFormula, successLabel;
 
-	            const bonusString = bonus > 0 ? `+ ${bonus}` : '';
-	            if (roll.data.degreeOfSuccess === 3) {
-	                healFormula = `4d8${bonusString}`;
-	                successLabel = translations.CritSuccess;
-	            } else if (roll.data.degreeOfSuccess === 2) {
-	                healFormula = `2d8${bonusString}`;
-	                successLabel = translations.Success;
-	            } else if (roll.data.degreeOfSuccess === 1) {
-	                successLabel = translations.Failure;
-	            } else if (roll.data.degreeOfSuccess === 0) {
-	                healFormula = '1d8';
-	                successLabel = translations.CritFailure;
-	            }
-	            if (healFormula !== undefined) {
-	                const healRoll = new Roll(healFormula).roll();
-	                const rollType = roll.data.degreeOfSuccess > 1 ? translations.Recovery : translations.Damage;
-	                ChatMessage.create(
-	                    {
-	                        user: game.user.id,
-	                        type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-	                        flavor: `<strong>${rollType}$ ${translations.Title}</strong> (${successLabel})`,
-	                        roll: healRoll,
-	                        speaker: ChatMessage.getSpeaker(),
-	                    }
-	                );
-	            }
-	        },
-	    });
-	};
+                const bonusString = bonus > 0 ? `+ ${bonus}` : '';
+                if (roll.data.degreeOfSuccess === 3) {
+                    healFormula = `4d8${bonusString}`;
+                    successLabel = translations.CritSuccess;
+                } else if (roll.data.degreeOfSuccess === 2) {
+                    healFormula = `2d8${bonusString}`;
+                    successLabel = translations.Success;
+                } else if (roll.data.degreeOfSuccess === 1) {
+                    successLabel = translations.Failure;
+                } else if (roll.data.degreeOfSuccess === 0) {
+                    healFormula = '1d8';
+                    successLabel = translations.CritFailure;
+                }
+                if (healFormula !== undefined) {
+                    const healRoll = new Roll(healFormula).roll();
+                    const rollType = roll.data.degreeOfSuccess > 1 ? translations.Recovery : translations.Damage;
+                    ChatMessage.create(
+                        {
+                            user: game.user.id,
+                            type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+                            flavor: `<strong>${rollType}$ ${translations.Title}</strong> (${successLabel})`,
+                            roll: healRoll,
+                            speaker: ChatMessage.getSpeaker(),
+                        }
+                    );
+                }
+            },
+        });
+    };
 
-	async function applyChanges($html) {
-	        var { dip } = actor.data.data.skills;
-	        const { name } = actor;
-	        const mod = parseInt($html.find('[name="modifier"]').val()) || 0;
-	        
-	        var usedProf = 0;
-	        usedProf = dip.rank;
+    async function applyChanges($html) {
+            var { dip } = actor.data.data.skills;
+            const { name } = actor;
+            const mod = parseInt($html.find('[name="modifier"]').val()) || 0;
 
-	        const roll = [
-	            () => ui.notifications.warn(game.i18n.format(translations.NotTrained, {
-	                name: name,})),
-	            () => encouragingWordsMacro({ DC: 15 + mod, bonus: 0, dip}),
-	            () => encouragingWordsMacro({ DC: 20 + mod, bonus: 5, dip}),
-	            () => encouragingWordsMacro({ DC: 30 + mod, bonus: 15, dip}),
-	            () => encouragingWordsMacro({ DC: 40 + mod, bonus: 25, dip}),
-	        ][usedProf];
+            var usedProf = 0;
+            usedProf = dip.rank;
 
-	        roll();
-	}
+            const roll = [
+                () => ui.notifications.warn(game.i18n.format(translations.NotTrained, {
+                    name: name,})),
+                () => encouragingWordsMacro({ DC: 15 + mod, bonus: 0, dip}),
+                () => encouragingWordsMacro({ DC: 20 + mod, bonus: 5, dip}),
+                () => encouragingWordsMacro({ DC: 30 + mod, bonus: 15, dip}),
+                () => encouragingWordsMacro({ DC: 40 + mod, bonus: 25, dip}),
+            ][usedProf];
 
-	if (actor === undefined) {
-	    ui.notifications.warn(translations.BadArgs);
-	} else {
-	    const dialog = new Dialog({
-	        title: translations.Title,
-	        content: `
-	<div>${translations.ContentMain}</div>
-	<hr/>
-	<form>
-	<div class="form-group">
-	<label>${translations.ContentLabel1}</label>
-	<select id="dc-type" name="dc-type">
-	<option value="1">${translations.ContentOption1}</option>
-	<option value="2">${translations.ContentOption2}</option>
-	<option value="3">${translations.ContentOption3}</option>
-	<option value="4">${translations.ContentOption4}</option>
-	</select>
-	</div>
-	</form>
-	<form>
-	<div class="form-group">
-	<label>${translations.ContentLabel2}</label>
-	<input id="modifier" name="modifier" type="number"/>
-	</div>
-	</form>
-	`,
-	        buttons: {
-	            yes: {
-	                icon: `<i class="fas fa-hand-holding-dipical"></i>`,
-	                label: translations.Title,
-	                callback: applyChanges,
-	            },
-	            no: {
-	                icon: `<i class="fas fa-times"></i>`,
-	                label: translations.Cancel,
-	            },
-	        },
-	        default: 'yes',
-	    });
-	    dialog.render(true);
-	}
+            roll();
+    }
+
+    if (actor === undefined) {
+        ui.notifications.warn(translations.BadArgs);
+    } else {
+        const dialog = new Dialog({
+            title: translations.Title,
+            content: `
+    <div>${translations.ContentMain}</div>
+    <hr/>
+    <form>
+    <div class="form-group">
+    <label>${translations.ContentLabel1}</label>
+    <select id="dc-type" name="dc-type">
+    <option value="1">${translations.ContentOption1}</option>
+    <option value="2">${translations.ContentOption2}</option>
+    <option value="3">${translations.ContentOption3}</option>
+    <option value="4">${translations.ContentOption4}</option>
+    </select>
+    </div>
+    </form>
+    <form>
+    <div class="form-group">
+    <label>${translations.ContentLabel2}</label>
+    <input id="modifier" name="modifier" type="number"/>
+    </div>
+    </form>
+    `,
+            buttons: {
+                yes: {
+                    icon: `<i class="fas fa-hand-holding-dipical"></i>`,
+                    label: translations.Title,
+                    callback: applyChanges,
+                },
+                no: {
+                    icon: `<i class="fas fa-times"></i>`,
+                    label: translations.Cancel,
+                },
+            },
+            default: 'yes',
+        });
+        dialog.render(true);
+    }
 }
