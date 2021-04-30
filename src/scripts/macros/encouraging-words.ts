66b4b3edaaddba320071bec0f6f207ea0c682bd0
TikaelSol <tikael@gmail.com>
2021-04-30 02:49:59 +0000

    Be more explicit about types.

--- src/scripts/macros/encouraging-words.ts
+++ src/scripts/macros/encouraging-words.ts
@@ -12,7 +12,7 @@ export function encouragingWords(options: ActionDefaultOptions): void {
         return;
     }
 
-    const encouragingWordsMacro = async ({ DC, bonus, dip }) => {
+    const encouragingWordsMacro = async ({ DC: any, bonus: any, dip: any }) => {
         const options = actor.getRollOptions(['all', 'skill-check', 'diplomacy']);
 
         options.push(translations.Title);
@@ -25,7 +25,7 @@ export function encouragingWords(options: ActionDefaultOptions): void {
         dip.roll({
             dc: dc,
             options: options,
-            callback: (roll) => {
+            callback: (roll: any) => {
                 let healFormula, successLabel;
 
                 const bonusString = bonus > 0 ? `+ ${bonus}` : '';
@@ -56,7 +56,7 @@ export function encouragingWords(options: ActionDefaultOptions): void {
         });
     };
 
-    async function applyChanges($html) {
+    async function applyChanges($html: any, actor: any) {
         const { dip } = actor.data.data.skills;
         const { name } = actor;
         const mod = parseInt($html.find('[name="modifier"]').val()) || 0;
@@ -110,7 +110,7 @@ export function encouragingWords(options: ActionDefaultOptions): void {
                 yes: {
                     icon: `<i class="fas fa-hand-holding-dipical"></i>`,
                     label: translations.Title,
-                    callback: applyChanges,
+                    callback: applyChanges({$html:$html, actor: actor}),
                 },
                 no: {
                     icon: `<i class="fas fa-times"></i>`,
