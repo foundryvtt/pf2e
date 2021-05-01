2011bbd1ac2e2f338a3175f87cf06ebf652bcb11
Carlos Fernandez <cfern1990@gmail.com>
2021-05-01 02:37:38 -0400

    Even better toLowerCase() typings

--- src/module/utils.ts
+++ src/module/utils.ts
@@ -216,8 +216,7 @@ export function ErrorPF2e(message: string) {
  * Make something lowercase in a type safe way. At the time of writing,
  * the string class does not properly handle toLowerCase().
  */
-export function toLowerCase(value: null | undefined): undefined;
-export function toLowerCase<T extends string>(value: T): Lowercase<T>;
+export function toLowerCase<T extends string | null | undefined>(value: T): T extends string ? Lowercase<T> : undefined;
 export function toLowerCase<T extends string>(value: T): Lowercase<T> {
     return value?.toLowerCase() as Lowercase<T>;
 }
