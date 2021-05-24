d32b29eebd634e25d122371d0425f80b3c834b9b
Marco Dizo <mdizo6482@gmail.com>
2021-05-24 22:19:41 +0800

    Event handler for panels based on attributes

--- src/module/actor/npc/sheet.ts
+++ src/module/actor/npc/sheet.ts
@@ -305,6 +305,9 @@ export class NPCSheetPF2e extends CreatureSheetPF2e<NPCPF2e> {
             .on('mouseenter', (event) => this.showControls(event))
             .on('mouseleave', (event) => this.hideControls(event));
 
+        // Expand Hidden Descriptions
+        html.find('[data-item-panel="toggle"]').on('click', (event) => this.onClickExpandPanel(event));
+
         // Don't subscribe to edit buttons it the sheet is NOT editable
         if (!this.options.editable) return;
 
@@ -945,6 +948,21 @@ export class NPCSheetPF2e extends CreatureSheetPF2e<NPCPF2e> {
         const skillsEditor = new NPCSkillsEditor(this.actor, options);
 
         skillsEditor.render(true);
+    }
+
+    private onClickExpandPanel(event: JQuery.ClickEvent): void {
+        const $details = $(event.currentTarget).closest('li.item').find('[data-item-panel="content"]');
+
+        const panelState = $details.attr('data-visibility');
+        if (panelState == 'visible') {
+            $details.slideUp(200, () => {
+                $details.attr('data-visibility', 'hidden');
+            });
+        } else {
+            $details.slideDown(200, () => {
+                $details.attr('data-visibility', 'visible');
+            });
+        }
     }
 
     private onClickExpandable(event: JQuery.ClickEvent): void {
