6c3b256f805c8e4e30b699860d73f9cdba1e5637
Marco Dizo <mdizo6482@gmail.com>
2021-05-28 22:27:19 +0800

    TinyMCE option ofr inline headers

--- src/scripts/hooks/init.ts
+++ src/scripts/hooks/init.ts
@@ -54,12 +54,36 @@ export function listen(): void {
         CONFIG.TinyMCE.extended_valid_elements = 'pf2-action[action|glyph]';
         CONFIG.TinyMCE.content_css = (CONFIG.TinyMCE.content_css ?? []).concat(
             `systems/${game.system.id}/styles/pf2e.css`,
+            `systems/${game.system.id}/styles/tinymce.css`,
         );
         CONFIG.TinyMCE.style_formats = (CONFIG.TinyMCE.style_formats ?? []).concat({
-            title: 'Icons A D T F R',
-            inline: 'span',
-            classes: ['pf2-icon'],
-            wrapper: true,
+            title: 'PF2E',
+            items: [
+                {
+                    title: 'Icons A D T F R',
+                    inline: 'span',
+                    classes: ['pf2-icon'],
+                    wrapper: true,
+                },
+                {
+                    title: 'Inline Header',
+                    block: 'h4',
+                    classes: 'inline-header',
+                },
+                {
+                    title: 'Info Block',
+                    block: 'section',
+                    classes: 'info',
+                    wrapper: true,
+                    exact: true,
+                    merge_siblings: false,
+                },
+                {
+                    title: 'Info Header',
+                    block: 'h4',
+                    classes: 'info__header',
+                },
+            ],
         });
 
         PlayerConfigPF2e.hookOnRenderSettings();
