f9f3041eb963997f9ad21c810e2df5618c7ddb67
nikolaj-a <na@deck.dk>
2021-05-29 09:11:44 +0200

    Fix measured template placement

--- src/module/system/measure.ts
+++ src/module/system/measure.ts
@@ -12,16 +12,13 @@ TemplateLayer.prototype._onDragLeftStart = function _onDragLeftStart(event: Elem
     // Create the new preview template
     const tool = game.activeTool;
     const origin = event.data.origin;
-    let pos: Point;
-    if (['cone', 'circle'].includes(tool)) {
-        pos = canvas.grid.getSnappedPosition(origin.x, origin.y, 2);
-    } else pos = canvas.grid.getSnappedPosition(origin.x, origin.y, 2);
+    const pos = canvas.grid.getSnappedPosition(origin.x, origin.y, 2);
     origin.x = pos.x;
     origin.y = pos.y;
 
     // Create the template
     const data = {
-        user: game.user._id,
+        user: game.user.id,
         t: tool,
         x: pos.x,
         y: pos.y,
@@ -33,7 +30,7 @@ TemplateLayer.prototype._onDragLeftStart = function _onDragLeftStart(event: Elem
     else if (tool === 'ray') data.width = 5;
 
     // Assign the template
-    const template = new MeasuredTemplate(data);
+    const template = new MeasuredTemplate(new MeasuredTemplateDocument(data, { parent: canvas.scene }));
     event.data.preview = this.preview.addChild(template);
     template.draw();
 };
@@ -55,13 +52,13 @@ TemplateLayer.prototype._onDragLeftMove = function _onDragLeftMove(event: Elemen
         // Update the shape data
         if (['cone', 'circle'].includes(template.data.t)) {
             const direction = ray.angle;
-            template.data.direction = toDegrees(
+            template.data.direction = Math.toDegrees(
                 Math.floor((direction + Math.PI * 0.125) / (Math.PI * 0.25)) * (Math.PI * 0.25),
             );
             const distance = ray.distance / ratio;
             template.data.distance = Math.floor(distance / canvas.dimensions.distance) * canvas.dimensions.distance;
         } else {
-            template.data.direction = toDegrees(ray.angle);
+            template.data.direction = Math.toDegrees(ray.angle);
             template.data.distance = ray.distance / ratio;
         }
 
