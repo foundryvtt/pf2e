04162a011bf5625a2c2bab57074c26e1f4a4890c
Shark that walks like a man <rsimoes@gmail.com>
2021-06-12 14:43:53 -0500

    Start filling out Foundry shader types

---
+++ types/foundry/client/pixi/shader/torch-illumination-shader.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Allow coloring of illumination
+ * @author SecretFire
+ */
+declare class TorchIlluminationShader extends StandardIlluminationShader {
+    static override fragmentShader: string;
+}
