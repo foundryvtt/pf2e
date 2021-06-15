ffb70f7856a2e8e8e83b80720870bb8af990860f
nikolaj-a <na@deck.dk>
2021-06-15 06:05:18 +0200

    Hide action

---
+++ src/module/system/actions/stealth/hide.ts
@@ -0,0 +1,19 @@
+import { ActionsPF2e, SkillActionOptions } from '../actions';
+
+export function hide(options: SkillActionOptions) {
+    const { checkType, property, stat, subtitle } = ActionsPF2e.resolveStat(options?.skill ?? 'stealth');
+    ActionsPF2e.simpleRollActionCheck(
+        options.actors,
+        property,
+        options.glyph ?? 'A',
+        'PF2E.Actions.Hide',
+        subtitle,
+        options.modifiers,
+        ['all', checkType, stat, 'action:hide'],
+        ['action:hide'],
+        ['secret'],
+        checkType,
+        options.event,
+        (target) => target.perception,
+    );
+}
