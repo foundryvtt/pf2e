f78a64df3ceb38e3836cdaaf0c391adb96f15d47
Shark that walks like a man <rsimoes@gmail.com>
2021-07-05 18:51:43 -0500

    Fix adjustment of perceived brightness from sourced lighting

--- src/module/canvas/ambient-light.ts
+++ src/module/canvas/ambient-light.ts
@@ -1,4 +1,3 @@
-import { CreaturePF2e } from '@actor';
 import { LightLevels, ScenePF2e } from '@module/scene';
 import { LightingLayerPF2e } from './lighting-layer';
 
@@ -10,11 +9,9 @@ export class AmbientLightPF2e extends AmbientLight {
         const isDaylight = canvas.scene.getLightLevel() > LightLevels.DARKNESS;
         if (this.source.isDarkness || isDaylight) return super.brightRadius;
 
-        const lowLightActors = canvas.tokens.controlled.flatMap((token) => {
-            return token.actor instanceof CreaturePF2e && token.actor.hasLowLightVision ? token.actor : [];
-        });
+        const lowLightTokens = canvas.tokens.controlled.filter((token) => token.hasLowLightVision);
 
-        return lowLightActors.length > 0 ? Math.max(this.dimRadius, super.brightRadius) : super.brightRadius;
+        return lowLightTokens.length > 0 ? Math.max(this.dimRadius, super.brightRadius) : super.brightRadius;
     }
 }
 
