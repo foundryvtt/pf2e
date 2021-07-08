9f2f7ab87bf3dbb6210ed62a7d16bd60795a8170
Shark that walks like a man <rsimoes@gmail.com>
2021-07-08 00:07:08 -0500

    Set initial schema version on newly-created owned items

--- src/module/actor/base.ts
+++ src/module/actor/base.ts
@@ -1192,7 +1192,7 @@ export class ActorPF2e extends Actor<TokenDocumentPF2e> {
         user: UserPF2e,
     ): Promise<void> {
         await super._preCreate(data, options, user);
-        if (user.id !== game.user.id || options.parent) return;
+        if (options.parent) return;
         await MigrationRunner.ensureSchemaVersion(this, Migrations.constructFromVersion());
     }
 
