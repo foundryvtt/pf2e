d655ffdccbd29148783b807aeaa7fc8af5f67695
Drental <gunnar.busch@googlemail.com>
2021-07-10 23:49:39 +0200

    do the shark thing

--- src/scripts/hooks/render-sidebar-tab.ts
+++ src/scripts/hooks/render-sidebar-tab.ts
@@ -1,19 +1,12 @@
 export function listen(): void {
-    Hooks.on('renderSidebarTab', async (object, html) => {
+    Hooks.on('renderSidebarTab', async (object, $html) => {
         if (object instanceof Settings) {
-            const details = html.find('#game-details');
-            details
-                .children()[1]
-                .insertAdjacentHTML(
-                    'afterend',
-                    "<li class='system-links' align='right'><a title='Guide' href='https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/wikis/Getting-Started-Guide/Getting-Started' style='color:#daa520;'>Wiki</a> | <a title='Changelog' href='https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/blob/release-0.8.x/CHANGELOG.md' style='color:#daa520;'>Changelog</a></li>",
-                );
-            details
-                .children()[0]
-                .insertAdjacentHTML(
-                    'afterend',
-                    "<li class='system-links' align='right'><a title='Guide' href='https://foundryvtt.com/kb/' style='color:#daa520;'>Wiki</a> | <a title='Changelog' href='https://foundryvtt.com/releases/' style='color:#daa520;'>Changelog</a></li>",
-                );
+            const $systemRow = $html.find('li.system');
+            const $guideLog = $systemRow.clone();
+            $guideLog[0].classList.replace('system', 'system-info');
+            $guideLog[0].innerHTML =
+                "<a title='Guide' href='https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/wikis/Getting-Started-Guide/Getting-Started' style='color:#daa520;'>Guide</a> | <a title='Changelog' href='https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/blob/release-0.8.x/CHANGELOG.md' style='color:#daa520;'>Changelog</a>";
+            $('#game-details li.system').after($guideLog);
         }
     });
 }
