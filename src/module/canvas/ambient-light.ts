7f082e6f3dd13316fd8a52474ce31f98ac3b7af9
Shark that walks like a man <rsimoes@gmail.com>
2021-07-11 04:15:49 -0500

    Address subtle perceived-light edge cases and reduce flickering

--- src/module/canvas/ambient-light.ts
+++ src/module/canvas/ambient-light.ts
@@ -1,16 +1,15 @@
-import { AmbientLightDocumentPF2e, LightLevels } from '@module/scene';
+import { AmbientLightDocumentPF2e } from '@module/scene';
 import { LightingLayerPF2e } from './lighting-layer';
 
 export class AmbientLightPF2e extends AmbientLight<AmbientLightDocumentPF2e> {
     /** Return a bright radius of the greater of dim and bright radii if any controlled tokens have low-light vision */
     override get brightRadius(): number {
-        if (!(canvas.scene && canvas.sight.rulesBasedVision)) return super.brightRadius;
-
-        const isDaylight = game.user.getPerceivedLightLevel() > LightLevels.DARKNESS;
-        if (this.source.isDarkness || isDaylight) return super.brightRadius;
+        const ignoreLight = this.source.isDarkness || !this.visible;
+        if (!canvas.sight.rulesBasedVision || ignoreLight) {
+            return super.brightRadius;
+        }
 
         const lowLightTokens = canvas.tokens.controlled.filter((token) => token.hasLowLightVision);
-
         return lowLightTokens.length > 0 ? Math.max(this.dimRadius, super.brightRadius) : super.brightRadius;
     }
 
--- src/module/canvas/lighting-layer.ts
+++ src/module/canvas/lighting-layer.ts
@@ -1,26 +1,35 @@
 import { AmbientLightPF2e } from './ambient-light';
 
 export class LightingLayerPF2e extends LightingLayer<AmbientLightPF2e> {
+    setPerceivedLightLevel({ defer = true } = {}): void {
+        if (!canvas.sight.rulesBasedVision) return;
+
+        const lightEmitters = [
+            ...canvas.tokens.placeables.filter((token) => token.visible && token.emitsLight),
+            ...canvas.lighting.placeables.filter((light) => light.visible && !light.isDarkness),
+        ];
+        for (const emitter of lightEmitters) emitter.updateSource({ defer: true });
+
+        if (!defer) {
+            canvas.perception.schedule({
+                lighting: { refresh: true },
+                sight: { initialize: true, refresh: true },
+            });
+        }
+    }
+
     /** Set the perceived brightness of sourced lighting */
     override refresh(darkness?: number | null): void {
-        if (!canvas.sight.rulesBasedVision) return super.refresh(darkness);
-
         if (canvas.tokens.controlled.some((token) => token.hasLowLightVision)) {
             for (const source of this.sources) {
                 if (source.isDarkness || !source.active) continue;
-                const original = { dim: source.dim, bright: source.bright };
+                source.bright = Math.max(source.dim, source.bright);
                 source.dim = 0;
-                source.bright = Math.max(original.dim, original.bright);
                 source.ratio = 1;
             }
         }
 
         this.darknessLevel = Math.min(this.darknessLevel, 0.925);
         super.refresh(darkness);
-    }
-
-    protected override _onDarknessChange(darkness: number, prior: number): void {
-        game.user.setPerceivedLightLevel({ defer: true });
-        super._onDarknessChange(darkness, prior);
     }
 }
