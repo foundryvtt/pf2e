f09bd916d59d07e6ed119416534a059f7c873842
Shark that walks like a man <rsimoes@gmail.com>
2021-08-02 00:40:50 -0500

    Redraw the darkvision mask on light updates

--- src/module/scene/ambient-light-document.ts
+++ src/module/scene/ambient-light-document.ts
@@ -6,6 +6,29 @@ export class AmbientLightDocumentPF2e extends AmbientLightDocument {
     get isDarkness(): boolean {
         return this.object.source.isDarkness;
     }
+
+    protected override _onCreate(
+        data: this["data"]["_source"],
+        options: SceneEmbeddedModificationContext,
+        userId: string
+    ) {
+        super._onCreate(data, options, userId);
+        canvas.darkvision.draw();
+    }
+
+    protected override _onUpdate(
+        changed: DeepPartial<this["data"]["_source"]>,
+        options: SceneEmbeddedModificationContext,
+        userId: string
+    ) {
+        super._onUpdate(changed, options, userId);
+        canvas.darkvision.draw();
+    }
+
+    protected override _onDelete(options: SceneEmbeddedModificationContext, userId: string) {
+        super._onDelete(options, userId);
+        canvas.darkvision.draw();
+    }
 }
 
 export interface AmbientLightDocumentPF2e extends AmbientLightDocument {
