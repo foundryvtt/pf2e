157cef15e4cca897a93ccfd37c121473439299aa
Shark that walks like a man <rsimoes@gmail.com>
2021-08-08 18:47:34 -0500

    Set several character proficiencies from class data and remove ability to directly edit

--- src/module/item/class/index.ts
+++ src/module/item/class/index.ts
@@ -1,5 +1,9 @@
 import { CharacterPF2e } from "@actor";
+import { SAVE_TYPES } from "@actor/data/values";
+import { ARMOR_CATEGORIES } from "@item/armor/data";
 import { FeatPF2e } from "@item/feat";
+import { WEAPON_CATEGORIES } from "@item/weapon/data";
+import { ZeroToFour } from "@module/data";
 import { ABCItemPF2e } from "../abc";
 import { ClassData, ClassTrait } from "./data";
 
@@ -15,11 +19,31 @@ export class ClassPF2e extends ABCItemPF2e {
     /** Prepare a character's data derived from their class */
     prepareActorData(this: Embedded<ClassPF2e>) {
         if (!(this.actor instanceof CharacterPF2e)) {
-            console.error("Only a character can have aa class");
+            console.error("Only a character can have a class");
             return;
         }
 
-        this.actor.data.data.attributes.classhp = this.hpPerLevel;
+        const classDetails = this.data.data;
+        const { attributes, details, martial, saves } = this.actor.data.data;
+        attributes.classhp = this.hpPerLevel;
+
+        attributes.perception.rank = Math.max(attributes.perception.rank, classDetails.perception) as ZeroToFour;
+        attributes.classDC.rank = Math.max(attributes.classDC.rank, classDetails.classDC) as ZeroToFour;
+        if (classDetails.keyAbility.value.length === 1) {
+            details.keyability.value = classDetails.keyAbility.value[0];
+        }
+
+        for (const category of ARMOR_CATEGORIES) {
+            martial[category].rank = Math.max(martial[category].rank, classDetails.defenses[category]) as ZeroToFour;
+        }
+
+        for (const category of WEAPON_CATEGORIES) {
+            martial[category].rank = Math.max(martial[category].rank, classDetails.attacks[category]) as ZeroToFour;
+        }
+
+        for (const save of SAVE_TYPES) {
+            saves[save].rank = Math.max(saves[save].rank, classDetails.savingThrows[save]) as ZeroToFour;
+        }
     }
 
     /** In addition to automatically granted features, retrieve feats with a class trait of this class */
