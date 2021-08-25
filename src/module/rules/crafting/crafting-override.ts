bb1d8fbc02f8e239785c76fa3cb5d8052cbb7bae
Edward Buckle <edward.buckle0@gmail.com>
2021-08-25 17:30:39 +0100

    Remove override

--- src/module/rules/crafting/crafting-override.ts
+++
@@ -1,7 +0,0 @@
-import { RuleElementPF2e } from "../rule-element";
-
-export class PF2CraftingOverrideRuleElement extends RuleElementPF2e {
-    override onBeforePrepareData(actorData: CharacterData | NPCData) {
-        console.log(actorData);
-    }
-}
