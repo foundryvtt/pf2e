de1a8792e69ade221ba8d7c08c084e0aebb45cdc
Edward Buckle <edward.buckle0@gmail.com>
2021-08-28 00:50:14 +0100

    en.json

--- src/module/item/formula/sheet.ts
+++ src/module/item/formula/sheet.ts
@@ -16,23 +16,17 @@ export class FormulaSheetPF2e extends ItemSheetPF2e<FormulaPF2e> {
                 if (compendiumItem) {
                     const item = compendiumItem as ItemPF2e;
                     data.item.img = item.img;
-                    data.item.name = `Formula of ${item.name}`;
+                    data.item.name = `${game.i18n.localize("PF2E.FormulaSheet.NamePrefix")} ${item.name}`;
                     data.item.data.description.value = item.description;
                 }
             } catch {
                 data.item.img = "systems/pf2e/icons/default-icons/lore.svg";
-                data.item.name = "Unknown Formula";
-                data.item.data.description.value =
-                    "This formula is completely filled with drawings of small\
-                    stick men, stains from some unknown liquid, non-sensical\
-                    rhymes written in children's letters, and other seemingly\
-                    random markings. It must have been made by goblins, and\
-                    makes no sense to you.<hr><b>Note:</b> This formula is\
-                        referencing a missing or invalid item.";
+                data.item.name = game.i18n.localize("PF2E.FormulaSheet.NameUnknown");
+                data.item.data.description.value = game.i18n.localize("PF2E.FormulaSheet.DescriptionUnknown");
             }
         } else {
             data.item.img = "systems/pf2e/icons/default-icons/lore.svg";
-            data.item.name = "Empty Formula";
+            data.item.name = game.i18n.localize("PF2E.FormulaSheet.NameEmpty");
             data.item.data.description.value = "";
         }
     }
