14ca2015a24e60f64b4463c701ec959fd8701c0c
Abaddon <marcoseither@googlemail.com>
2021-09-13 17:51:19 +0000

    Update 668-npc-attack-effects.ts

--- src/module/migration/migrations/669-npc-attack-effects.ts
+++ src/module/migration/migrations/669-npc-attack-effects.ts
@@ -3,8 +3,8 @@ import { ItemSourcePF2e } from "@item/data";
 import { MigrationBase } from "../base";
 import { sluggify } from "@module/utils";
 
-export class Migration668npcAttackEffects extends MigrationBase {
-    static override version = 0.668;
+export class Migration669npcAttackEffects extends MigrationBase {
+    static override version = 0.669;
 
     override async updateItem(item: ItemSourcePF2e, actor?: ActorSourcePF2e) {
         if (!actor || item.type !== "melee") return;
