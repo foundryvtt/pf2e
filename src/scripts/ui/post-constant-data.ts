4c3eaeed48f88632508a5daba5046df7c4ed7cb5
MS-Abaddon <marcoseither@googlemail.com>
2021-09-25 02:35:39 +0200

    Combine find and filter

--- src/scripts/ui/post-constant-data.ts
+++ src/scripts/ui/post-constant-data.ts
@@ -2,7 +2,7 @@ import { InlineRollsLinks } from "@scripts/ui/inline-roll-links";
 
 export const PostConstantData = {
     listen: ($html: JQuery): void => {
-        const $links = $html.find("span, p, div").filter("[data-pf2-const]");
+        const $links = $html.find("span[data-pf2-const], p[data-pf2-const], div[data-pf2-const]");
         $links.each((_idx, link) => {
             const pf2Const = link?.dataset.pf2Const ?? "";
             if (pf2Const == "text" || pf2Const == "")
