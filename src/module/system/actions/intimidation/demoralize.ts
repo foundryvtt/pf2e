e8f65d210f5f6c0d94636052217d6f695bd099a2
nikolaj-a <na@deck.dk>
2021-10-09 02:38:23 +0200

    Convert simple action check parameter from options interface

--- src/module/system/actions/intimidation/demoralize.ts
+++ src/module/system/actions/intimidation/demoralize.ts
@@ -2,22 +2,22 @@ import { ActionsPF2e, SkillActionOptions } from "../actions";
 
 export function demoralize(options: SkillActionOptions) {
     const { checkType, property, stat, subtitle } = ActionsPF2e.resolveStat(options?.skill ?? "intimidation");
-    ActionsPF2e.simpleRollActionCheck(
-        options.actors,
-        property,
-        options.glyph ?? "A",
-        "PF2E.Actions.Demoralize.Title",
+    ActionsPF2e.simpleRollActionCheck({
+        actors: options.actors,
+        statName: property,
+        actionGlyph: options.glyph ?? "A",
+        title: "PF2E.Actions.Demoralize.Title",
         subtitle,
-        options.modifiers,
-        ["all", checkType, stat, "action:demoralize"],
-        ["action:demoralize"],
-        ["auditory", "concentrate", "emotion", "fear", "mental"],
+        modifiers: options.modifiers,
+        rollOptions: ["all", checkType, stat, "action:demoralize"],
+        extraOptions: ["action:demoralize"],
+        traits: ["auditory", "concentrate", "emotion", "fear", "mental"],
         checkType,
-        options.event,
-        (target) => target.will,
-        (selector: string) => [
+        event: options.event,
+        difficultyClassStatistic: (target) => target.will,
+        extraNotes: (selector: string) => [
             ActionsPF2e.note(selector, "PF2E.Actions.Demoralize", "criticalSuccess"),
             ActionsPF2e.note(selector, "PF2E.Actions.Demoralize", "success"),
-        ]
-    );
+        ],
+    });
 }
--- src/module/system/actions/stealth/hide.ts
+++ src/module/system/actions/stealth/hide.ts
@@ -2,19 +2,19 @@ import { ActionsPF2e, SkillActionOptions } from "../actions";
 
 export function hide(options: SkillActionOptions) {
     const { checkType, property, stat, subtitle } = ActionsPF2e.resolveStat(options?.skill ?? "stealth");
-    ActionsPF2e.simpleRollActionCheck(
-        options.actors,
-        property,
-        options.glyph ?? "A",
-        "PF2E.Actions.Hide.Title",
+    ActionsPF2e.simpleRollActionCheck({
+        actors: options.actors,
+        statName: property,
+        actionGlyph: options.glyph ?? "A",
+        title: "PF2E.Actions.Hide.Title",
         subtitle,
-        options.modifiers,
-        ["all", checkType, stat, "action:hide"],
-        ["action:hide"],
-        ["secret"],
+        modifiers: options.modifiers,
+        rollOptions: ["all", checkType, stat, "action:hide"],
+        extraOptions: ["action:hide"],
+        traits: ["secret"],
         checkType,
-        options.event,
-        (target) => target.perception,
-        (selector: string) => [ActionsPF2e.note(selector, "PF2E.Actions.Hide", "success")]
-    );
+        event: options.event,
+        difficultyClassStatistic: (target) => target.perception,
+        extraNotes: (selector: string) => [ActionsPF2e.note(selector, "PF2E.Actions.Hide", "success")],
+    });
 }
