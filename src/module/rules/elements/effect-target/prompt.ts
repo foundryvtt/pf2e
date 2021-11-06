5c0bf28dd5d17cb3af7a9ec1b53b52409dab7621
Shark that walks like a man <rsimoes@gmail.com>
2021-11-06 02:21:24 -0500

    Generalize the effect-target prompt for use by other rules elements

--- src/module/rules/elements/effect-target/prompt.ts
+++ src/module/rules/elements/effect-target/prompt.ts
@@ -1,118 +1,55 @@
-import { ActorPF2e } from "@actor";
 import { ItemPF2e } from "@item";
-import { PredicatePF2e } from "@system/predication";
-import { EffectTargetSource } from "./data";
+import { RulesElementPromptData, PromptChoice, RulesElementPrompt } from "@module/rules/apps/prompt";
 
 /** Prompt the user for the target of the effect they just added to an actor */
-export class TargetPrompt extends Application {
+export class EffectTargetPrompt extends RulesElementPrompt<Embedded<ItemPF2e>> {
     private scope: "armor" | "weapon";
 
-    private item: Embedded<ItemPF2e>;
-
-    private predicate: PredicatePF2e;
-
-    private resolve?: (value: Embedded<ItemPF2e> | null) => void;
-
-    private selection: Embedded<ItemPF2e> | null = null;
-
-    private choices: Embedded<ItemPF2e>[] = [];
-
-    constructor(data: EffectTargetSource, { item }: { item: Embedded<ItemPF2e> }) {
-        super();
-        this.item = item;
+    constructor(data: TargetPromptData) {
+        super(data);
         this.scope = data.scope === "armor" || data.scope === "weapon" ? data.scope : "weapon";
-        this.predicate = new PredicatePF2e(data.predicate);
-        this.options.title = item.name;
-    }
-
-    get actor(): ActorPF2e {
-        return this.item.actor;
     }
 
     static override get defaultOptions(): ApplicationOptions {
         const options = super.defaultOptions;
         options.id = "effect-target-prompt";
-        options.width = 350;
-        options.resizable = false;
-        options.height = "auto";
         return options;
     }
 
     override get template(): string {
-        return "systems/pf2e/templates/system/effect-target-prompt.html";
-    }
-
-    /** Return a promise containing the user's item selection, or `null` if no selection was made */
-    async resolveTarget(): Promise<ItemPF2e | null> {
-        this.choices = this.getChoices();
-        // Exit early if there are no valid choices
-        if (this.choices.length === 0) {
-            await this.close({ force: true });
-            return null;
-        }
-
-        this.render(true);
-        return new Promise((resolve) => {
-            this.resolve = resolve;
-        });
+        return "systems/pf2e/templates/system/rules-elements/effect-target-prompt.html";
     }
 
     /** Collect all options within the specified scope and then eliminate any that fail the predicate test */
-    private getChoices(): Embedded<ItemPF2e>[] {
-        const allOptions = ((): { object: Embedded<ItemPF2e>; domain: string[] }[] => {
+    protected override getChoices(): PromptChoice<Embedded<ItemPF2e>>[] {
+        return ((): PromptChoice<Embedded<ItemPF2e>>[] => {
             switch (this.scope) {
                 case "armor": {
                     return this.actor.itemTypes.armor
                         .filter((armor) => armor.isArmor)
                         .map((armor) => ({
-                            object: armor,
+                            value: armor,
+                            label: armor.name,
                             domain: armor.getContextStrings(),
                         }));
                 }
                 case "weapon": {
                     return this.actor.itemTypes.weapon.map((weapon) => ({
-                        object: weapon,
+                        value: weapon,
+                        label: weapon.name,
                         domain: weapon.getContextStrings(),
                     }));
                 }
             }
-        })();
-        return allOptions.filter((option) => this.predicate.test(option.domain)).map((option) => option.object);
+        })().filter((choice) => this.predicate.test(choice.domain));
     }
 
-    override getData(): TargetPromptData {
-        return { choices: this.choices };
-    }
-
-    override activateListeners($html: JQuery): void {
-        $html.find("a[data-choice-id]").on("click", (event) => {
-            const itemId = $(event.currentTarget).attr("data-choice-id") ?? "";
-            this.selection = this.actor.items.get(itemId) ?? null;
-            this.close();
-        });
-    }
-
-    /** Close the dialog, applying the effect with configured target or warning the user that something went wrong. */
-    override async close({ force = false } = {}) {
-        if (!this.selection) {
-            if (force) {
-                ui.notifications.warn(
-                    game.i18n.format("PF2E.UI.RuleElements.EffectTarget.NoValidOptions", {
-                        actor: this.actor.name,
-                        effect: this.item.name,
-                    })
-                );
-            } else {
-                ui.notifications.warn(
-                    game.i18n.format("PF2E.UI.RuleElements.EffectTarget.NoSelectionMade", { effect: this.item.name })
-                );
-            }
-        }
-        this.resolve?.(this.selection);
-        await super.close({ force });
+    protected getSelection(event: JQuery.ClickEvent): PromptChoice<Embedded<ItemPF2e>> | null {
+        const itemId = $(event.currentTarget).attr("data-choice");
+        return this.choices.find((choice) => choice.value.id === itemId) ?? null;
     }
 }
 
-interface TargetPromptData {
-    choices: Embedded<ItemPF2e>[];
+interface TargetPromptData extends RulesElementPromptData<Embedded<ItemPF2e>> {
+    scope: string;
 }
