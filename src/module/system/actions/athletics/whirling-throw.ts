8a431dbbe6b8a29d7d288ba120c023c3706a6783
TikaelSol <tikael@gmail.com>
2021-11-08 07:39:14 +0000

    Add action macro for Whirling Throw feat

---
+++ src/module/system/actions/athletics/whirling-throw.ts
@@ -0,0 +1,57 @@
+import { ActionsPF2e, SkillActionOptions } from "../actions";
+import { MODIFIER_TYPE, ModifierPF2e } from "@module/modifiers";
+import { CreaturePF2e } from "@actor";
+import type { ActorPF2e } from "@actor/base";
+import { ActorSizePF2e } from "@actor/data/size";
+
+function determineSizeBonus(actorSize: ActorSizePF2e, targetSize: ActorSizePF2e) {
+    const sizeDifference = actorSize.difference(targetSize);
+
+    return Math.clamped(2 * sizeDifference, -4, 4);
+}
+
+export function whirlingThrow(options: SkillActionOptions) {
+    const { checkType, property, stat, subtitle } = ActionsPF2e.resolveStat(options?.skill ?? "athletics");
+
+    const actors = canvas.tokens.controlled.map((token) => token.actor) as ActorPF2e[];
+    const actor = actors[0];
+
+    const targets = Array.from(game.user.targets).filter((token) => token.actor instanceof CreaturePF2e);
+    const target = targets[0].actor;
+
+    options.modifiers ||= [];
+
+    if (target instanceof CreaturePF2e && actor instanceof CreaturePF2e) {
+        const actorSize = actor.data.data.traits.size;
+        const targetSize = target.data.data.traits.size;
+        const sizeModifier = new ModifierPF2e(
+            "Size Modifier",
+            determineSizeBonus(actorSize, targetSize),
+            MODIFIER_TYPE.CIRCUMSTANCE
+        );
+        if (sizeModifier.modifier) {
+            options.modifiers.push(sizeModifier);
+        }
+    }
+
+    ActionsPF2e.simpleRollActionCheck({
+        actors: options.actors,
+        statName: property,
+        actionGlyph: options.glyph ?? "A",
+        title: "PF2E.Actions.WhirlingThrow.Title",
+        subtitle,
+        modifiers: options.modifiers,
+        rollOptions: ["all", checkType, stat, "action:whirling-throw"],
+        extraOptions: ["action:whirling-throw"],
+        traits: ["monk"],
+        checkType,
+        event: options.event,
+        difficultyClassStatistic: (target) => target.fortitude,
+        extraNotes: (selector: string) => [
+            ActionsPF2e.note(selector, "PF2E.Actions.WhirlingThrow", "criticalSuccess"),
+            ActionsPF2e.note(selector, "PF2E.Actions.WhirlingThrow", "success"),
+            ActionsPF2e.note(selector, "PF2E.Actions.WhirlingThrow", "failure"),
+            ActionsPF2e.note(selector, "PF2E.Actions.WhirlingThrow", "criticalFailure"),
+        ],
+    });
+}
