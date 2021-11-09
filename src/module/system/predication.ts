c6e84ee3b2b145b054c4c38ca9809a5121961402
Shark that walks like a man <rsimoes@gmail.com>
2021-11-08 21:55:43 -0600

    Remove training wheels from compound predication
    This reverts commit da1891c3844aa35b3588b0af8c1db89bbcc11ae3.

--- src/module/system/predication.ts
+++ src/module/system/predication.ts
@@ -118,7 +118,7 @@ class StatementValidator {
         return (
             Object.keys(statement).length === 1 &&
             Array.isArray(statement.and) &&
-            statement.and.every((subProp) => this.isAtomic(subProp))
+            statement.and.every((subProp) => this.isStatement(subProp))
         );
     }
 
@@ -126,7 +126,7 @@ class StatementValidator {
         return (
             Object.keys(statement).length === 1 &&
             Array.isArray(statement.or) &&
-            statement.or.every((subProp) => this.isAtomic(subProp))
+            statement.or.every((subProp) => this.isStatement(subProp))
         );
     }
 
@@ -134,16 +134,18 @@ class StatementValidator {
         return (
             Object.keys(statement).length === 1 &&
             Array.isArray(statement.nor) &&
-            statement.nor.every((subProp) => this.isAtomic(subProp))
+            statement.nor.every((subProp) => this.isStatement(subProp))
         );
     }
 
     private static isNot(statement: { not?: unknown }): boolean {
-        return Object.keys(statement).length === 1 && !!statement.not && this.isAtomic(statement.not);
+        return Object.keys(statement).length === 1 && !!statement.not && this.isStatement(statement.not);
     }
 
     private static isIf(statement: { if?: unknown; then?: unknown }): boolean {
-        return Object.keys(statement).length === 2 && this.isAtomic(statement.if) && this.isAtomic(statement.then);
+        return (
+            Object.keys(statement).length === 2 && this.isStatement(statement.if) && this.isStatement(statement.then)
+        );
     }
 }
 
