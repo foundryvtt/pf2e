c47f555edae44f193d6cd8e9f254184d715a79a4
Kevin Hjelden <fryguy@burntpopcorn.net>
2021-11-11 22:38:37 -0800

    Replace hard-coded hefty hauler/lifting belt code with an AELike.

---
+++ src/module/migration/migrations/689-encumberance-aes.ts
@@ -0,0 +1,50 @@
+import { ItemSourcePF2e } from "@item/data";
+import { RuleElementSource } from "@module/rules/rules-data-definitions";
+import { sluggify } from "@util";
+import { MigrationBase } from "../base";
+
+/** Don't store the max value of the focus pool */
+export class Migration689EncumberanceActiveEffects extends MigrationBase {
+    static override version = 0.689;
+
+    override async updateItem(itemSource: ItemSourcePF2e): Promise<void> {
+        const systemData = itemSource.data;
+        const slug = systemData.slug ?? sluggify(itemSource.name);
+
+        const amountToIncreaseBy = (() => {
+            if (itemSource.type === "feat" && slug === "hefty-hauler") {
+                return 2;
+            } else if (itemSource.type === "feat" && slug === "hardy-traveler") {
+                return 1;
+            } else if (itemSource.type === "equipment" && slug === "lifting-belt") {
+                return 1;
+            }
+
+            return 0;
+        })();
+
+        if (amountToIncreaseBy === 0) return;
+
+        const alreadyMigrated = systemData.rules.some((x: { key: string }) => x.key === "ActiveEffectLike");
+        if (alreadyMigrated) return;
+
+        const rules: (RuleElementSource & { [key: string]: unknown })[] = [
+            {
+                key: "ActiveEffectLike",
+                path: "data.attributes.bonusEncumbranceBulk",
+                mode: "add",
+                value: amountToIncreaseBy,
+            },
+            {
+                key: "ActiveEffectLike",
+                path: "data.attributes.bonusLimitBulk",
+                mode: "add",
+                value: amountToIncreaseBy,
+            },
+        ];
+
+        for (const rule of rules) {
+            systemData.rules.push(rule);
+        }
+    }
+}
