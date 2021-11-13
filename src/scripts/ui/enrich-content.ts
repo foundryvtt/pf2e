82644581ff7357982c1062cd5851ea1675b48fc9
MS-Abaddon <marcoseither@googlemail.com>
2021-11-13 19:21:47 +0100

    Update to @template inline buttons

--- src/scripts/ui/enrich-content.ts
+++ src/scripts/ui/enrich-content.ts
@@ -1,95 +1,143 @@
+import { ActorSizePF2e } from "@actor/data/size";
+
+//mini type definition for rollData within EnrichHTMLOptions
+interface RollOptions extends EnrichHTMLOptions {
+    rollData?: RollData;
+}
+
+interface RollData {
+    actor?: {
+        traits: {
+            size: ActorSizePF2e;
+        };
+    };
+    item?: {
+        name: string;
+        traits: {
+            value: string[];
+            custom: string;
+        };
+    };
+}
+
 export const EnrichContent = {
+    //get the different parameters of the @inline command
     getParams: (data: string): Map<string, string> | string => {
         const error = "Wrong notation for params - use [type1:value1|type2:value2|...]";
         const parameters = new Map();
 
-        const params = data.trim().split("|");
-        if (!Array.isArray(params)) return error;
+        const paramString = data.trim().split("|");
+        if (!Array.isArray(paramString)) return error;
 
-        for (const arr of params) {
-            const a = arr.trim().split(":");
-            if (!(a.length === 2)) return error;
+        for (const param of paramString) {
+            const paramComponents = param.trim().split(":");
+            if (paramComponents.length != 2) return error;
 
-            parameters.set(a[0].trim(), a[1].trim());
+            parameters.set(paramComponents[0].trim(), paramComponents[1].trim());
         }
 
         return parameters;
     },
 
-    enrichString: (data: string, options?: EnrichHTMLOptions): string => {
+    enrichString: (data: string, rollOptions?: RollOptions): string => {
+        /*
+        enrich @inline commands: Localize, Template
+        replacement is repeated until nothing gets changed in order to also enrich data coming from @Localize
+        */
         const entityTypes: String[] = ["Localize", "Template"];
         const rgx = new RegExp(`@(${entityTypes.join("|")})\\[([^\\]]+)\\](?:{([^}]+)})?`, "g");
-        if (!(typeof data === "string")) return data;
         let replaced = true;
         let replacedData;
         do {
-            replacedData = data.replace(rgx, (match: string, p1: string, p2: string, p3: string) => {
-                switch (p1) {
-                    case "Localize":
-                        return EnrichContent.createLocalize(p2, options);
-                    case "Template":
-                        return EnrichContent.createTemplate(p2, p3, options);
+            replacedData = data.replace(
+                rgx,
+                (match: string, inlineType: string, paramString: string, buttonLabel: string) => {
+                    switch (inlineType) {
+                        case "Localize":
+                            return EnrichContent.createLocalize(paramString, rollOptions);
+                        case "Template":
+                            return EnrichContent.createTemplate(paramString, buttonLabel, rollOptions);
+                    }
+                    return match;
                 }
-                return match;
-            });
+            );
             if (replacedData === data) replaced = false;
             data = replacedData;
         } while (replaced);
         return data;
     },
-    createLocalize(data: string, options?: EnrichHTMLOptions): string {
-        return EnrichContent.enrichString(game.i18n.localize(data), options);
+
+    //get localized description from @Localize command
+    createLocalize(paramString: string, rollOptions?: RollOptions): string {
+        return EnrichContent.enrichString(game.i18n.localize(paramString), rollOptions);
     },
-    createTemplate(data: string, label?: string, options?: EnrichHTMLOptions): string {
+
+    //create inline template button from @template command
+    createTemplate(paramString: string, label?: string, rollOptions?: RollOptions): string {
         //get parameters from data
-        const params = EnrichContent.getParams(data);
+        const rawParams = EnrichContent.getParams(paramString);
 
-        //check for correct notation
-        if (typeof params === "string") return params;
-        if (!params.has("type")) return "Error in @Template: type parameter is mandatory";
-        if (!params.has("distance")) return "Error in @Template: distance parameter is mandatory";
+        //check for correct syntax
+        if (typeof rawParams === "string") return rawParams;
 
-        if (!["cone", "emanation", "burst", "line"].includes(params.get("type") ?? ""))
-            return `Error in @Template: type ${params.get("type")} is not supported`;
-        if (isNaN(+(params.get("distance") ?? "")))
-            return `Error in @Template: dimension ${params.get("distance")} is not a number`;
+        const params = Object.fromEntries(rawParams);
 
-        if (params.has("width")) {
-            if (isNaN(+(params.get("width") ?? "")))
-                return `Error in @Template: width ${params.get("width")} is not a number`;
-        }
+        //check for correct param notation
+        if (!params.type) return "Error in @Template: type parameter is mandatory";
+        if (!params.distance) return "Error in @Template: distance parameter is mandatory";
+        if (!["cone", "emanation", "burst", "line"].includes(params.type))
+            return `Error in @Template: type ${params.type} is not supported`;
+        if (isNaN(+params.distance)) return `Error in @Template: dimension ${params.distance} is not a number`;
+        if (params.width && isNaN(+params.width)) return `Error in @Template: width ${params.width} is not a number`;
 
-        //if no traits are entered directly use the traits from rollOptions if available
-        if (!params.has("traits")) {
-            if (options?.rollData) {
-                for (const [k, v] of Object.entries(options.rollData)) {
-                    if (k === "item") {
-                        const t = v.traits.value.join(",");
-                        if (!(v.traits.custom === "")) t.append(`, ${v.traits.custom}`);
-                        params.set("traits", t);
-                    } else params.set("traits", "");
+        //if no traits are entered manually use the traits from rollOptions if available
+        if (!params.traits) {
+            params.traits = "";
+            if (rollOptions?.rollData?.item) {
+                const itemData = rollOptions.rollData.item;
+                let traits = itemData.traits.value.join(",");
+                if (!(itemData.traits.custom === "")) {
+                    traits = traits.concat(`, ${itemData.traits.custom}`);
                 }
-            } else params.set("traits", "");
+                params.traits = traits;
+            }
         }
 
-        //add damaging-effect if param damaging = true and not already included
-        if (params.get("damaging") === "true") {
-            const traits = params.get("traits") ?? "";
-            if (traits.search("damaging-effect") === -1)
-                params.set("traits", traits.concat(",damaging-effect").replace(/^,/, ""));
-        }
-
-        const html = document.createElement("span");
-        html.setAttribute("data-pf2-effect-area", params.get("type") ?? "");
-        html.setAttribute("data-pf2-distance", params.get("distance") ?? "");
-        if (!(params.get("traits") === "")) html.setAttribute("data-pf2-traits", params.get("traits") ?? "");
-        html.innerHTML =
-            label ??
-            game.i18n.format("PF2E.InlineButtons.TemplateLabel", {
-                type: params.get("type") ?? "",
-                unit: params.get("distance") ?? "",
+        //add extraTraits (added to default traits from rollData or manually set traits)
+        if (params.extraTraits) {
+            let traits = params.traits;
+            const extraTraits = params.extraTraits.split(",");
+            extraTraits.forEach((element) => {
+                if (traits.search(element) === -1) traits = traits.concat(`,${element}`);
             });
-        if (params.get("type") === "line") html.setAttribute("data-pf2-width", params.get("width") ?? "5");
+            params.traits = traits.replace(/^,/, "");
+        }
+
+        //if no button label is entered directly create default label
+        if (!label) {
+            let templateSize = game.i18n.format("PF2E.TemplateLabels.FeetDistance", {
+                distance: params.distance,
+                unit: game.i18n.localize("PF2E.TemplateLabels.InlineButtons.Feet"),
+            });
+            let templateType = game.i18n.localize(
+                `PF2E.TemplateLabels.InlineButtons.TemplateTypeLabels.${
+                    params.type.charAt(0).toUpperCase() + params.type.slice(1)
+                }`
+            );
+
+            label = game.i18n.format("PF2E.TemplateLabels.TemplateLabel", {
+                distance: templateSize,
+                type: templateType,
+            });
+        }
+
+        //add the html elements used for the inline buttons
+        const html = document.createElement("span");
+        html.innerHTML = label;
+        html.setAttribute("data-pf2-effect-area", params.type);
+        html.setAttribute("data-pf2-distance", params.distance);
+        if (params.traits != "") html.setAttribute("data-pf2-traits", params.traits);
+        if (params.type === "line") html.setAttribute("data-pf2-width", params.width ?? "5");
         return html.outerHTML;
     },
 };
