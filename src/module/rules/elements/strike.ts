8380ce9e5dee96e57aee55cc784c7eb63a368af1
Shark that walks like a man <rsimoes@gmail.com>
2021-11-20 20:59:09 -0600

    Tidy up Strike rule element, have it accommodate invalid/omitted range properties

--- src/module/rules/elements/strike.ts
+++ src/module/rules/elements/strike.ts
@@ -1,48 +1,50 @@
 import { ActorType } from "@actor/data";
-import { WeaponPF2e } from "@item";
+import { ItemPF2e, WeaponPF2e } from "@item";
 import {
     BaseWeaponType,
     WeaponCategory,
     WeaponDamage,
     WeaponGroup,
+    WeaponRange,
     WeaponSource,
     WeaponTrait,
-    WEAPON_RANGES,
 } from "@item/weapon/data";
-import { tupleHasValue } from "@util";
 import { RuleElementPF2e } from "../rule-element";
-import { RuleElementData, RuleElementSynthetics } from "../rules-data-definitions";
+import { RuleElementData, RuleElementSource, RuleElementSynthetics } from "../rules-data-definitions";
 
 /**
- * @category RuleElement
+   Create an ephemeral strike on an actor
+   @category RuleElement
  */
-export class StrikeRuleElement extends RuleElementPF2e {
+class StrikeRuleElement extends RuleElementPF2e {
     protected static override validActorTypes: ActorType[] = ["character", "npc"];
 
+    constructor(data: StrikeSource, item: Embedded<ItemPF2e>) {
+        data.range = Number(data.range) || null;
+        super(data, item);
+
+        this.data.category ??= "unarmed";
+        this.data.group ??= "brawling";
+        this.data.baseType ??= null;
+        this.data.range ??= null;
+        this.data.traits ??= [];
+    }
+
     override onBeforePrepareData(_actorData: unknown, { strikes }: RuleElementSynthetics) {
-        if (
-            !(
-                tupleHasValue(WEAPON_RANGES, this.data.range) ||
-                this.data.range === null ||
-                this.data.range === undefined
-            )
-        ) {
-            return;
-        }
         const source: PreCreate<WeaponSource> = {
             _id: this.item.id,
-            name: this.label || this.item.name,
+            name: this.label,
             type: "weapon",
             img: this.data.img ?? this.item.img,
             data: {
                 slug: this.data.slug ?? null,
                 description: { value: "" },
-                category: this.data.category || "unarmed",
-                group: this.data.group || "brawling",
-                baseItem: this.data.baseType ?? null,
-                damage: this.data.damage?.base,
-                range: this.data.range || null,
-                traits: { value: this.data.traits ?? [], rarity: { value: "common" }, custom: "" },
+                category: this.data.category,
+                group: this.data.group,
+                baseItem: this.data.baseType,
+                damage: this.data.damage?.base ?? { damageType: "bludgeoning", dice: 1, die: "d4" },
+                range: this.data.range,
+                traits: { value: this.data.traits, rarity: { value: "common" }, custom: "" },
                 options: { value: this.data.options ?? [] },
                 equipped: { value: true },
             },
@@ -51,16 +53,32 @@ export class StrikeRuleElement extends RuleElementPF2e {
     }
 }
 
-export interface StrikeRuleElement {
-    data: RuleElementData & {
-        slug?: string | null;
-        img?: ImagePath;
-        category?: WeaponCategory;
-        group?: WeaponGroup;
-        baseType?: BaseWeaponType | null;
-        damage?: { base?: WeaponDamage };
-        range?: number | null;
-        traits?: WeaponTrait[];
-        options?: string[];
-    };
+interface StrikeRuleElement {
+    data: StrikeData;
 }
+
+interface StrikeSource extends RuleElementSource {
+    slug?: string;
+    img?: unknown;
+    category?: unknown;
+    group?: unknown;
+    baseType?: unknown;
+    damage?: unknown;
+    range?: unknown;
+    traits?: unknown;
+    options?: unknown;
+}
+
+interface StrikeData extends RuleElementData {
+    slug?: string;
+    img?: ImagePath;
+    category: WeaponCategory;
+    group: WeaponGroup;
+    baseType: BaseWeaponType | null;
+    damage?: { base?: WeaponDamage };
+    range: WeaponRange | null;
+    traits: WeaponTrait[];
+    options?: string[];
+}
+
+export { StrikeRuleElement };
