7bdc0724a70a1cafe383c68049a3b26abd935a53
Brandon Maier <brandon.maier@gmail.com>
2021-11-26 19:16:31 -0600

    Add creature table to XP macro
    Add the creature table to the XP macro. With the Encounter Budget and
    Creature XP tables, the macro can be used to balance encounters.
    
    Also add a new row to the Encounter Budget table that shows the amount
    of XP needed to reach each threat level. This can be used to rebalance
    existing encounters. For example when adjusting an adventure path
    4-player encounter to another party size.

--- static/macros/xp.js
+++ static/macros/xp.js
@@ -54,32 +54,91 @@ function dialogTemplate(xp) {
     <tr>
         <th>${game.i18n.localize("PF2E.Encounter.Budget.Threat")}</th>
         <th>${game.i18n.localize("PF2E.Encounter.Budget.XPBudget")}</th>
+        <th>${game.i18n.localize("PF2E.Encounter.Budget.XPNeeded")}</th>
         <th>${game.i18n.localize("PF2E.Encounter.Budget.Reward")}</th>
     </tr>
     <tr>
         <td>${game.i18n.localize("PF2E.Encounter.Budget.Threats.trivial")}</td>
-        <td>${xp.encounterBudgets.trivial} XP</td>
-        <td>40 XP</td>
+        <td>${xp.encounterBudgets.trivial}</td>
+        <td>${xp.encounterBudgets.trivial - xp.totalXP}</td>
+        <td>40</td>
     </tr>
     <tr>
         <td>${game.i18n.localize("PF2E.Encounter.Budget.Threats.low")}</td>
-        <td>${xp.encounterBudgets.low} XP</td>
-        <td>60 XP</td>
+        <td>${xp.encounterBudgets.low}</td>
+        <td>${xp.encounterBudgets.low - xp.totalXP}</td>
+        <td>60</td>
     </tr>
     <tr>
         <td>${game.i18n.localize("PF2E.Encounter.Budget.Threats.moderate")}</td>
-        <td>${xp.encounterBudgets.moderate} XP</td>
-        <td>80 XP</td>
+        <td>${xp.encounterBudgets.moderate}</td>
+        <td>${xp.encounterBudgets.moderate - xp.totalXP}</td>
+        <td>80</td>
     </tr>
     <tr>
         <td>${game.i18n.localize("PF2E.Encounter.Budget.Threats.severe")}</td>
-        <td>${xp.encounterBudgets.severe} XP</td>
-        <td>120 XP</td>
+        <td>${xp.encounterBudgets.severe}</td>
+        <td>${xp.encounterBudgets.severe - xp.totalXP}</td>
+        <td>120</td>
     </tr>
     <tr>
         <td>${game.i18n.localize("PF2E.Encounter.Budget.Threats.extreme")}</td>
-        <td>${xp.encounterBudgets.extreme} XP</td>
-        <td>160 XP</td>
+        <td>${xp.encounterBudgets.extreme}</td>
+        <td>${xp.encounterBudgets.extreme - xp.totalXP}</td>
+        <td>160</td>
+    </tr>
+</table>
+<h2>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureXPAndRole")}</h2>
+<table>
+    <tr>
+        <th>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevel")}</th>
+        <th>XP</th>
+        <th>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.SuggestedRole")}</th>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel - 4}</td>
+        <td>10</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.-4")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel - 3}</td>
+        <td>15</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.-3")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel - 2}</td>
+        <td>20</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.-2")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel - 1}</td>
+        <td>30</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.-1")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel}</td>
+        <td>40</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.0")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel + 1}</td>
+        <td>60</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.1")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel + 2}</td>
+        <td>80</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.2")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel + 3}</td>
+        <td>120</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.3")}</td>
+    </tr>
+    <tr>
+        <td>${xp.partyLevel + 4}</td>
+        <td>160</td>
+        <td>${game.i18n.localize("PF2E.Encounter.CreatureXPAndRole.CreatureLevels.4")}</td>
     </tr>
 </table>`;
 }
