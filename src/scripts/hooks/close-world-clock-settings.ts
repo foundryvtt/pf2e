eeeed7a54738b03e019d722c55f22a9c00e30a64
Shark that walks like a man <rsimoes@gmail.com>
2021-11-30 00:23:48 -0600

    Clean up and tighten types of Foundry hooks

--- src/scripts/hooks/close-world-clock-settings.ts
+++ src/scripts/hooks/close-world-clock-settings.ts
@@ -1,8 +1,10 @@
 /** Refresh scene controls for non-GMs when WorldClockSettings is updated */
-export function listen(): void {
-    Hooks.on("closeWorldClockSettings", (): void => {
-        if (game.user.isGM) {
-            game.socket.emit("system.pf2e", { request: "refreshSceneControls", data: { layer: "TokenLayer" } });
-        }
-    });
-}
+export const CloseWorldClockSettings = {
+    listen: (): void => {
+        Hooks.on("closeWorldClockSettings", (): void => {
+            if (game.user.isGM) {
+                game.socket.emit("system.pf2e", { request: "refreshSceneControls", data: { layer: "TokenLayer" } });
+            }
+        });
+    },
+};
--- src/scripts/hooks/index.ts
+++ src/scripts/hooks/index.ts
@@ -1,13 +1,13 @@
-import * as CanvasInit from "./canvas-init";
-import * as CanvasReady from "./canvas-ready";
-import * as CloseWorldClockSettings from "./close-world-clock-settings";
+import { CanvasInit } from "./canvas-init";
+import { CanvasReady } from "./canvas-ready";
+import { CloseWorldClockSettings } from "./close-world-clock-settings";
 import { Init } from "./init";
-import * as HotbarDrop from "./hotbar-drop";
-import * as Ready from "./ready";
-import * as Setup from "./setup";
-import * as UpdateWorldTime from "./update-world-time";
-import * as RenderActorDirectory from "./render-actor-directory";
-import * as DropCanvasData from "./drop-canvas-data";
+import { HotbarDrop } from "./hotbar-drop";
+import { Ready } from "./ready";
+import { Setup } from "./setup";
+import { UpdateWorldTime } from "./update-world-time";
+import { RenderActorDirectory } from "./render-actor-directory";
+import { DropCanvasData } from "./drop-canvas-data";
 import { RenderDialog } from "./render-dialog";
 import { RenderSettings } from "./render-settings";
 import { GetSceneControlButtons } from "./get-scene-control-buttons";
@@ -15,19 +15,24 @@ import { RenderSceneControls } from "./render-scene-controls";
 
 export const HooksPF2e = {
     listen(): void {
-        CanvasInit.listen();
-        CanvasReady.listen();
-        CloseWorldClockSettings.listen();
-        DropCanvasData.listen();
-        GetSceneControlButtons.listen();
-        Init.listen();
-        HotbarDrop.listen();
-        Ready.listen();
-        RenderActorDirectory.listen();
-        RenderSceneControls.listen();
-        Setup.listen();
-        UpdateWorldTime.listen();
-        RenderDialog.listen();
-        RenderSettings.listen();
+        const listeners: { listen(): void }[] = [
+            CanvasInit,
+            CanvasReady,
+            CloseWorldClockSettings,
+            DropCanvasData,
+            GetSceneControlButtons,
+            Init,
+            HotbarDrop,
+            Ready,
+            RenderActorDirectory,
+            RenderSceneControls,
+            Setup,
+            UpdateWorldTime,
+            RenderDialog,
+            RenderSettings,
+        ];
+        for (const Listener of listeners) {
+            Listener.listen();
+        }
     },
 };
