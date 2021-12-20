e18e79089c05b60917e8e3c1a6ef29ddd9f479dd
SkepticRobot <busf18@gmail.com>
2021-12-20 20:36:24 +0000

    Add badge parameter to effect

--- src/module/item/effect/data.ts
+++ src/module/item/effect/data.ts
@@ -1,5 +1,6 @@
 import { ItemLevelData, ItemSystemData } from "@item/data/base";
 import { BaseNonPhysicalItemData, BaseNonPhysicalItemSource } from "@item/data/non-physical";
+import { OneToFour } from "@module/data";
 import { EffectPF2e } from ".";
 
 export type EffectSource = BaseNonPhysicalItemSource<"effect", EffectSystemSource>;
@@ -30,9 +31,20 @@ interface EffectSystemSource extends ItemSystemData, ItemLevelData {
     };
     target: string | null;
     expired?: boolean;
+    badge: EffectBadge | null;
+}
+
+export interface EffectBadge {
+    value: number | DiceExpression;
+    tickRule: EffectTickType;
 }
 
 export type EffectExpiryType = "turn-start" | "turn-end";
+
+export type EffectTickType = "turn-start";
+
+type DieFaceCount = 4 | 6 | 8 | 10 | 12 | 20;
+export type DiceExpression = `${OneToFour | ""}d${DieFaceCount}`;
 
 export interface EffectSystemData extends ItemSystemData, EffectSystemSource {
     expired: boolean;
