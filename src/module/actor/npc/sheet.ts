049687308447e9982c8b4abd1a7c44eacf0e21a6
SkepticRobot <BenLewis@skepticrobot.tech>
2022-01-02 12:16:03 -0500

    base effect tab setup

--- src/module/actor/npc/sheet.ts
+++ src/module/actor/npc/sheet.ts
@@ -156,7 +156,7 @@ export class NPCSheetPF2e extends CreatureSheetPF2e<NPCPF2e> {
             height: 680,
             showUnpreparedSpells: true, // Not sure what it does in an NPC, copied from old code
             tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main" }],
-            scrollY: [".tab.main", ".tab.inventory", ".tab.spells", ".tab.notes"],
+            scrollY: [".tab.main", ".tab.inventory", ".tab.spells", ".tab.effects", ".tab.notes"],
         });
         return options;
     }
