8797e5d227f02adc16708859ced7f8d16fee3b72
Shark that walks like a man <rsimoes@gmail.com>
2022-01-07 01:07:59 -0600

    Have skill feats added from backgrounds via GrantItem appear in proper section of PC sheet

--- src/module/item/background/index.ts
+++ src/module/item/background/index.ts
@@ -1,4 +1,5 @@
 import { CharacterPF2e } from "@actor";
+import type { ItemPF2e, FeatPF2e } from "@item";
 import { OneToFour } from "@module/data";
 import { ABCItemPF2e } from "../abc";
 import { BackgroundData } from "./data";
@@ -6,6 +7,24 @@ import { BackgroundData } from "./data";
 export class BackgroundPF2e extends ABCItemPF2e {
     static override get schema(): typeof BackgroundData {
         return BackgroundData;
+    }
+
+    /** Set a skill feat granted by a GrantItem RE as one of this background's configured items */
+    override prepareSiblingData(this: Embedded<BackgroundPF2e>): void {
+        if (Object.keys(this.data.data.items).length > 0) return;
+        const grantedSkillFeat = this.data.flags.pf2e.itemGrants
+            .flatMap((id) => this.actor.items.get(id) ?? [])
+            .find((i: Embedded<ItemPF2e> & { featType?: unknown }): i is Embedded<FeatPF2e> => i.featType === "skill");
+
+        if (grantedSkillFeat) {
+            this.data.data.items["GRANT"] = {
+                id: grantedSkillFeat.id,
+                img: grantedSkillFeat.img,
+                name: grantedSkillFeat.name,
+                level: 1,
+            };
+            grantedSkillFeat.data.data.location = this.id;
+        }
     }
 
     override prepareActorData(this: Embedded<BackgroundPF2e>): void {
