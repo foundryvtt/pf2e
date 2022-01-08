6b2b81e20434815fe482ee2466032d255bf121ce
Shark that walks like a man <rsimoes@gmail.com>
2022-01-08 03:39:07 -0600

    Remove toNumber utility function

--- tests/module/utils.test.ts
+++ tests/module/utils.test.ts
@@ -1,4 +1,4 @@
-import { add, addSign, applyNTimes, combineObjects, padArray, toNumber, zip } from "@util";
+import { add, addSign, applyNTimes, combineObjects, padArray, zip } from "@util";
 
 describe("should combine objects", () => {
     test("combine two empty objects", () => {
@@ -29,28 +29,6 @@ describe("should combine objects", () => {
         const result = combineObjects({ a: 1, b: 5 }, { a: 3 }, add);
 
         expect(result).toEqual({ a: 4, b: 5 });
-    });
-});
-
-describe("parse numbers", () => {
-    test("null", () => {
-        expect(toNumber(null)).toEqual(null);
-    });
-
-    test("undefined", () => {
-        expect(toNumber(undefined)).toEqual(undefined);
-    });
-
-    test("empty string", () => {
-        expect(toNumber("")).toEqual(undefined);
-    });
-
-    test("1", () => {
-        expect(toNumber("1")).toEqual(1);
-    });
-
-    test("2", () => {
-        expect(toNumber(2)).toEqual(2);
     });
 });
 
