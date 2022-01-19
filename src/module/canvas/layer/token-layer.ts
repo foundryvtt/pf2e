fdb45ab179b275f5a9cc92057d86f4af551c987f
Shark that walks like a man <rsimoes@gmail.com>
2022-01-19 05:41:46 -0600

    Work around core bug in which TokenLayer is checked by constructor name

--- src/module/canvas/layer/token-layer.ts
+++ src/module/canvas/layer/token-layer.ts
@@ -1,3 +1,9 @@
 import { TokenPF2e } from "../token";
 
-export class TokenLayerPF2e<TObject extends TokenPF2e = TokenPF2e> extends TokenLayer<TObject> {}
+export class TokenLayerPF2e<TObject extends TokenPF2e = TokenPF2e> extends TokenLayer<TObject> {
+    constructor() {
+        super();
+        // Work around upstream bug (as of V9.242) in which active layer is checked for by constructor name lookup
+        Object.defineProperty(this, "name", { value: "TokenLayer" });
+    }
+}
