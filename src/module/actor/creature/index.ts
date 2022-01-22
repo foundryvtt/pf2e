7797d90d182f7beb736bdf11111efe4378f4b3cd
Shark that walks like a man <rsimoes@gmail.com>
2022-01-22 01:19:21 -0600

    Add a shield-hardness increase to Everstand Stance

--- src/module/actor/creature/index.ts
+++ src/module/actor/creature/index.ts
@@ -201,6 +201,10 @@ export abstract class CreaturePF2e extends ActorPF2e {
             rule.onApplyActiveEffects?.();
         }
 
+        if ("shield" in this.attributes && this.attributes.shield.itemId) {
+            this.rollOptions.all["self:shield:equipped"] = true;
+        }
+
         for (const changeEntries of Object.values(this.data.data.autoChanges)) {
             changeEntries!.sort((a, b) => (Number(a.level) > Number(b.level) ? 1 : -1));
         }
