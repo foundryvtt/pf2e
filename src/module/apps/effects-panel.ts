e28bbd2c898b995b27a52e5bb23768ff12dfaed8
Shark that walks like a man <rsimoes@gmail.com>
2022-01-28 04:52:59 +0000

    Revert "Merge branch 'fix-effects-panel-position' into 'master'"
    This reverts merge request !7214

--- src/module/apps/effects-panel.ts
+++ src/module/apps/effects-panel.ts
@@ -23,7 +23,6 @@ export class EffectsPanel extends Application {
 
     static override get defaultOptions() {
         return mergeObject(super.defaultOptions, {
-            id: "pf2e-effects-panel",
             popOut: false,
             template: "systems/pf2e/templates/system/effects-panel.html",
         });
