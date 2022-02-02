ec32afb9ed26cd8297c0e5db49b5850ade05d65c
Shark that walks like a man <rsimoes@gmail.com>
2022-02-02 00:50:32 -0600

    Add a TokenLight rule element

---
+++ src/module/rules/rule-element/token-light.ts
@@ -0,0 +1,52 @@
+import { ItemPF2e } from "@item";
+import { isObject } from "@util";
+import { RuleElementPF2e, RuleElementData, RuleElementOptions } from "./";
+import { RuleElementSource } from "./data";
+
+/**
+ * Change the light emitted by a token
+ * @category RuleElement
+ */
+class TokenLightRuleElement extends RuleElementPF2e {
+    constructor(data: RuleElementSource, item: Embedded<ItemPF2e>, options?: RuleElementOptions) {
+        super(data, item, options);
+        this.validateData();
+    }
+
+    validateData(): void {
+        const light = this.data.value;
+        if (!isObject<foundry.data.LightSource>(light)) return;
+
+        for (const key of ["dim", "bright"] as const) {
+            if (light[key] !== undefined) {
+                const resolvedValue = this.resolveValue(light[key]);
+                if (typeof resolvedValue === "number") {
+                    light[key] = resolvedValue;
+                }
+            }
+        }
+
+        try {
+            new foundry.data.LightData(light);
+        } catch (error) {
+            if (error instanceof Error) this.failValidation(error.message);
+        }
+    }
+
+    override afterPrepareData() {
+        if (this.ignored) return;
+
+        const light = new foundry.data.LightData(this.data.value).toObject(false);
+        mergeObject(this.actor.overrides, { token: { light } });
+    }
+}
+
+interface TokenLightRuleElement extends RuleElementPF2e {
+    data: TokenLightData;
+}
+
+interface TokenLightData extends RuleElementData {
+    value: DeepPartial<foundry.data.LightSource>;
+}
+
+export { TokenLightRuleElement };
