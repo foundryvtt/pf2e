0b152a260264829b0768dd6ee5eefb606a3a9e18
Shark that walks like a man <rsimoes@gmail.com>
2022-02-04 21:05:01 -0600

    Add option to conditionally disable toggle in ToggleProperty RE

--- src/module/rules/rule-element/toggle-property.ts
+++ src/module/rules/rule-element/toggle-property.ts
@@ -1,6 +1,7 @@
 import { CharacterPF2e, NPCPF2e } from "@actor";
 import { ActorType } from "@actor/data";
 import { ItemPF2e } from "@item";
+import { PredicatePF2e, RawPredicate } from "@system/predication";
 import { RuleElementSource } from "..";
 import { RuleElementPF2e, RuleElementData } from "./";
 import { RuleElementOptions } from "./base";
@@ -11,19 +12,25 @@ import { RuleElementOptions } from "./base";
 export class TogglePropertyRuleElement extends RuleElementPF2e {
     protected static override validActorTypes: ActorType[] = ["character", "npc"];
 
+    /** Whether the checkbox is toggled on and the property set to `true` */
     checked: boolean;
+
+    /** Whether the checkbox is interactable in an actor sheet */
+    enabled: PredicatePF2e;
 
     constructor(data: TogglePropertySource, item: Embedded<ItemPF2e>, options?: RuleElementOptions) {
         super(data, item, options);
 
         this.data.default = typeof data.default === "boolean" ? data.default : false;
         this.checked = Boolean(getProperty(this.actor.data, this.data.property) ?? this.data.default);
+        this.enabled = new PredicatePF2e(this.data.enabled);
         setProperty(this.actor.data, this.data.property, this.checked);
     }
 
     override beforePrepareData(): void {
         const { predicate } = this;
-        if (predicate && !predicate.test(this.actor.getRollOptions())) {
+        const rollOptions = this.actor.getRollOptions();
+        if (predicate && !predicate.test(rollOptions)) {
             setProperty(this.actor.data, this.data.property, false);
             return;
         }
@@ -34,6 +41,7 @@ export class TogglePropertyRuleElement extends RuleElementPF2e {
                 label: this.label,
                 inputName: this.data.property,
                 checked: this.checked,
+                enabled: this.enabled.test(rollOptions),
             });
         } else {
             this.failValidation("Toggle flag requires a property field with an object path");
@@ -46,12 +54,14 @@ export interface TogglePropertyRuleElement {
     data: TogglePropertyData;
 }
 
-interface TogglePropertySource extends RuleElementSource {
-    property?: unknown;
-    default?: unknown;
-}
-
 interface TogglePropertyData extends RuleElementData {
     property: string;
+    enabled?: RawPredicate;
     default: boolean;
+}
+
+interface TogglePropertySource extends RuleElementSource {
+    property?: unknown;
+    enabled?: unknown;
+    default?: unknown;
 }
