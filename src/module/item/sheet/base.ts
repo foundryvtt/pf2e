1d5abe41b601d415e3e14f2101b0ac248a9f9bf2
Shark that walks like a man <rsimoes@gmail.com>
2022-02-07 09:51:59 +0000

    Track whether a feat must be taken at level 1 or can be taken more than once

--- src/module/item/sheet/base.ts
+++ src/module/item/sheet/base.ts
@@ -38,8 +38,8 @@ export class ItemSheetPF2e<TItem extends ItemPF2e> extends ItemSheet<TItem> {
         return options;
     }
 
-    override async getData() {
-        const data: any = this.getBaseData();
+    override async getData(options?: Partial<DocumentSheetOptions>) {
+        const data: any = this.getBaseData(options);
         data.abilities = CONFIG.PF2E.abilities;
         data.saves = CONFIG.PF2E.saves;
 
