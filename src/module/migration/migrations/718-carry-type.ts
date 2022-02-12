3f69b3020dc88d1edeff2ce75ac9e8b2f30466e6
Shark that walks like a man <rsimoes@gmail.com>
2022-02-11 18:29:04 -0600

    Keep equipped property on all physical items

--- src/module/migration/migrations/718-carry-type.ts
+++ src/module/migration/migrations/718-carry-type.ts
@@ -1,10 +1,8 @@
-import { ItemSourcePF2e, PhysicalItemSource } from "@item/data";
-import { isObject, setHasElement } from "@util";
+import { ItemSourcePF2e } from "@item/data";
 import { isPhysicalData } from "@item/data/helpers";
-import { ITEM_CARRY_TYPES } from "@item/data/values";
 import { MigrationBase } from "../base";
 import { ActorSourcePF2e } from "@actor/data";
-import { EquippedData, PhysicalSystemData } from "@item/physical/data";
+import { EquippedData } from "@item/physical/data";
 import { getUsageDetails } from "@item/physical/usage";
 
 /** Update physical item usage and equipped to reflect carry types (held, worn, stowed) */
@@ -13,39 +11,54 @@ export class Migration718CarryType extends MigrationBase {
 
     override async updateItem(itemData: ItemSourcePF2e, actor?: ActorSourcePF2e): Promise<void> {
         if (!isPhysicalData(itemData)) return;
-        const physicalItemData: PhysicalItemSource = itemData;
-        const systemData = physicalItemData.data;
+        const systemData = itemData.data;
 
-        if (isObject(systemData.usage) && systemData.usage.value === "worn-gloves") {
+        // Correct erronous hyphen in worngloves usage
+        if (systemData.usage.value === "worn-gloves") {
             systemData.usage.value = "worngloves";
         }
 
-        if (!["character", "npc"].includes(actor?.type ?? "")) {
-            if ("equipped" in systemData) {
-                const existing: ExistingSystemData = physicalItemData.data;
-                existing["-=equipped"] = null;
-                delete existing.equipped;
-            }
+        if ("game" in globalThis || actor) {
+            systemData.equipped ??= { carryType: "worn" };
+            systemData.equipped.carryType ??= "worn";
+        } else {
+            delete (systemData as { equipped?: unknown }).equipped;
             return;
         }
 
-        if (isObject(systemData.equipped) && "value" in systemData.equipped) {
-            const equipped: ExistingEquipped = systemData.equipped;
-            if (!setHasElement(ITEM_CARRY_TYPES, systemData.equipped.carryType)) {
-                equipped.carryType = systemData.containerId.value ? "stowed" : "worn";
-                equipped.handsHeld = 0;
-                const usage = getUsageDetails(systemData.usage.value);
+        const equipped: OldEquippedData = systemData.equipped;
 
-                if (equipped.value) {
-                    if (usage.type === "held") {
-                        equipped.carryType = "held";
-                        equipped.handsHeld = usage.hands ?? 1;
-                    } else if (["held", "worn"].includes(usage.type)) {
-                        equipped.carryType = "worn";
-                    }
-                }
+        if (actor) {
+            const containerId = itemData.data.containerId.value;
+            const inStowingContainer = actor.items.some(
+                (i) => i.type === "backpack" && i.data.stowing && i._id === containerId
+            );
+            systemData.equipped.carryType = inStowingContainer ? "stowed" : "worn";
+
+            if (!["character", "npc"].includes(actor.type ?? "")) {
+                equipped["-=value"] = null;
+                delete equipped.value;
+                return;
+            }
+        }
+
+        if ("value" in systemData.equipped) {
+            const usage = getUsageDetails(systemData.usage.value);
+            if (actor) {
+                equipped.carryType = systemData.containerId.value ? "stowed" : "worn";
                 if (usage.type === "worn" && usage.where) {
                     equipped.inSlot = !!equipped.value;
+                } else if (usage.type === "held") {
+                    equipped.handsHeld = 0;
+                }
+            }
+
+            if (equipped.value) {
+                if (usage.type === "held") {
+                    equipped.carryType = "held";
+                    equipped.handsHeld = usage.hands ?? 1;
+                } else if (["held", "worn"].includes(usage.type)) {
+                    equipped.carryType = "worn";
                 }
             }
 
@@ -55,12 +68,7 @@ export class Migration718CarryType extends MigrationBase {
     }
 }
 
-type ExistingEquipped = EquippedData & {
+type OldEquippedData = EquippedData & {
     value?: boolean;
     "-=value"?: null;
-};
-
-type ExistingSystemData = Omit<PhysicalSystemData, "equipped"> & {
-    equipped?: EquippedData;
-    "-=equipped"?: null;
 };
