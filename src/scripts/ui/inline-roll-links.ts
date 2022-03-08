3083b9ae73a1c801bfba7953f9ece1a39b575a2c
Shark that walks like a man <rsimoes@gmail.com>
2022-03-08 12:52:53 -0600

    Make small tidy pass on InlineRollLinks

--- src/scripts/ui/inline-roll-links.ts
+++ src/scripts/ui/inline-roll-links.ts
@@ -21,10 +21,10 @@ function resolveActors(): ActorPF2e[] {
 
 const inlineSelector = ["action", "check", "effect-area", "repost"].map((keyword) => `[data-pf2-${keyword}]`).join(",");
 
-export const InlineRollsLinks = {
+export const InlineRollLinks = {
     // Conditionally show DCs in the text
-    injectDCText: ($links: JQuery) => {
-        $links.each((_idx, link) => {
+    injectDCText: ($links: JQuery): void => {
+        for (const link of $links) {
             const dc = Number(link.dataset.pf2Dc?.trim() ?? "");
             const role = link.dataset.pf2ShowDc?.trim() ?? "";
             const userCanView = ["all", "owner"].includes(role) || (role === "gm" && game.user.isGM);
@@ -32,32 +32,33 @@ export const InlineRollsLinks = {
                 const text = link.innerHTML;
                 link.innerHTML = game.i18n.format("PF2E.DCWithValue", { dc, text });
             }
-        });
+        }
     },
 
-    injectRepostElement: ($links: JQuery) => {
-        $links.each((_idx, link) => {
-            if (game.user.isGM) {
-                if (!link.querySelector("[data-pf2-repost]")) {
-                    const child = document.createElement("i");
-                    child.classList.add("fas");
-                    child.classList.add("fa-comment-alt");
-                    child.setAttribute("data-pf2-repost", "");
-                    child.setAttribute("title", game.i18n.localize("PF2E.Repost"));
-                    link.appendChild(child);
-                }
-            }
-        });
+    injectRepostElement: ($links: JQuery): void => {
+        if (!game.user.isGM) return;
+
+        for (const link of $links) {
+            if (link.querySelector("[data-pf2-repost]")) continue;
+
+            const child = document.createElement("i");
+            child.classList.add("fas");
+            child.classList.add("fa-comment-alt");
+            child.setAttribute("data-pf2-repost", "");
+            child.setAttribute("title", game.i18n.localize("PF2E.Repost"));
+            link.appendChild(child);
+        }
     },
 
     listen: ($html: JQuery): void => {
         const $links = $html.find("span").filter(inlineSelector);
-        InlineRollsLinks.injectDCText($links);
-        InlineRollsLinks.injectRepostElement($links);
+        InlineRollLinks.injectDCText($links);
+        InlineRollLinks.injectRepostElement($links);
         const $repostLinks = $html.find("i.fas.fa-comment-alt").filter(inlineSelector);
 
         $repostLinks.filter("[data-pf2-repost]").on("click", (event) => {
-            InlineRollsLinks.repostAction(event.target.parentElement!);
+            const parent = event.target.parentElement;
+            if (parent) InlineRollLinks.repostAction(parent);
             event.stopPropagation();
         });
 
