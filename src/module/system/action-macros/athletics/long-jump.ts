eab4fb967fe6622804e96958985de04dae65253a
Shark that walks like a man <rsimoes@gmail.com>
2022-04-07 23:57:52 -0500

    Do the same for paths

---
+++ src/module/system/action-macros/athletics/long-jump.ts
@@ -0,0 +1,24 @@
+import { ActionMacros, SkillActionOptions } from "..";
+
+export function longJump(options: SkillActionOptions) {
+    const { checkType, property, stat, subtitle } = ActionMacros.resolveStat(options?.skill ?? "athletics");
+    ActionMacros.simpleRollActionCheck({
+        actors: options.actors,
+        statName: property,
+        actionGlyph: options.glyph ?? "D",
+        title: "PF2E.Actions.LongJump.Title",
+        subtitle,
+        modifiers: options.modifiers,
+        rollOptions: ["all", checkType, stat, "action:stride", "action:leap", "action:long-jump"],
+        extraOptions: ["action:stride", "action:leap", "action:long-jump"],
+        traits: ["move"],
+        checkType,
+        event: options.event,
+        difficultyClass: options.difficultyClass,
+        extraNotes: (selector: string) => [
+            ActionMacros.note(selector, "PF2E.Actions.LongJump", "success"),
+            ActionMacros.note(selector, "PF2E.Actions.LongJump", "failure"),
+            ActionMacros.note(selector, "PF2E.Actions.LongJump", "criticalFailure"),
+        ],
+    });
+}
---
+++ src/module/system/action-macros/intimidation/demoralize.ts
@@ -0,0 +1,24 @@
+import { ActionMacros, SkillActionOptions } from "..";
+
+export function demoralize(options: SkillActionOptions) {
+    const { checkType, property, stat, subtitle } = ActionMacros.resolveStat(options?.skill ?? "intimidation");
+    ActionMacros.simpleRollActionCheck({
+        actors: options.actors,
+        statName: property,
+        actionGlyph: options.glyph ?? "A",
+        title: "PF2E.Actions.Demoralize.Title",
+        subtitle,
+        modifiers: options.modifiers,
+        rollOptions: ["all", checkType, stat, "action:demoralize"],
+        extraOptions: ["action:demoralize"],
+        traits: ["auditory", "concentrate", "emotion", "fear", "mental"],
+        checkType,
+        event: options.event,
+        difficultyClass: options.difficultyClass,
+        difficultyClassStatistic: (target) => target.saves.will,
+        extraNotes: (selector: string) => [
+            ActionMacros.note(selector, "PF2E.Actions.Demoralize", "criticalSuccess"),
+            ActionMacros.note(selector, "PF2E.Actions.Demoralize", "success"),
+        ],
+    });
+}
