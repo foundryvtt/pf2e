f4afb6e7f4038c83c319577c63d567daf0564314
Shark that walks like a man <rsimoes@gmail.com>
2022-04-26 06:32:28 -0500

    Log user ID in roll data
    Rolls handle their own rendering but had no information on who the roller was for the purpose of conditional visibility

--- src/module/system/check/roll.ts
+++ src/module/system/check/roll.ts
@@ -1,3 +1,22 @@
+import { UserPF2e } from "@module/user";
 import { RollDataPF2e } from "@system/rolls";
 
-export class CheckRoll extends Roll<RollDataPF2e> {}
+class CheckRoll extends Roll<RollDataPF2e> {
+    roller: UserPF2e | null;
+
+    constructor(formula: string, data?: Partial<RollDataPF2e>, options?: Partial<RollDataPF2e>) {
+        super(formula, data, options);
+
+        this.roller = game.users.get(this.data.rollerId ?? "") ?? null;
+    }
+
+    override toJSON(): CheckRollJSON {
+        return mergeObject(super.toJSON(), { data: { rollerId: this.roller?.id } });
+    }
+}
+
+interface CheckRollJSON extends RollJSON {
+    data?: Partial<RollDataPF2e>;
+}
+
+export { CheckRoll, CheckRollJSON };
