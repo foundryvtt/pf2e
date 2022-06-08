b41221062a5b91c8a2075ec0e9730e3db4b37289
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-06-08 16:52:25 -0500

    Clear a few dozen anys in type definitions (#2394)

--- types/foundry/common/types.d.ts
+++ types/foundry/common/types.d.ts
@@ -155,4 +155,37 @@ declare global {
         /** The Set of pressed Buttons */
         activeButtons: Set<string>;
     }
+
+    type RequestData = object | object[] | string | string[];
+
+    interface SocketRequest {
+        /** The type of object being modified */
+        type?: string;
+        /** The server-side action being requested */
+        action?: string;
+        /** Data applied to the operation */
+        data?: RequestData;
+        query?: object;
+        /** The type of parent document */
+        parentType?: string;
+        /** The ID of a parent document */
+        parentId?: string;
+        /** A Compendium pack name */
+        pack?: string;
+        /** Additional options applied to the request */
+        options?: object;
+    }
+
+    interface SocketResponse {
+        /** The initial request */
+        request: SocketRequest;
+        /** An error, if one occurred */
+        error?: Error;
+        /** The status of the request */
+        status?: string;
+        /** The ID of the requesting User */
+        userId?: string;
+        /** Data returned as a result of the request */
+        data?: RequestData;
+    }
 }
