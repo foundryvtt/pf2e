1fe8fde693cf746e91b004802a17bd2e59528553
Shark that walks like a man <rsimoes@gmail.com>
2022-06-19 11:59:36 -0500

    Refine behavior of token-stack cycling

--- src/module/canvas/layer/token.ts
+++ src/module/canvas/layer/token.ts
@@ -3,14 +3,14 @@ import { TokenPF2e } from "../token";
 export class TokenLayerPF2e<TToken extends TokenPF2e = TokenPF2e> extends TokenLayer<TToken> {
     /** Cycle Z indices of a hovered token stack */
     cycleStack(): boolean {
-        const top = this._hover;
-        if (!top) return false;
+        const hovered = this._hover;
+        if (!hovered) return false;
 
-        const stack = this.placeables.filter((t) => top.distanceTo(t) === 0);
+        const stack = this.placeables.filter((t) => hovered.distanceTo(t) === 0);
         if (stack.length < 2) return false;
 
         const last = stack.pop();
-        if (last !== top) return false;
+        if (!last) return false;
         stack.unshift(last);
 
         for (let i = 0; i < stack.length; i++) {
