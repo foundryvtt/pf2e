dd43bcdc491cac548d9282048c6d2f7106a485ca
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-06-26 21:02:32 -0500

    Make deity sheet's use of Tagify generally available (#2729)
    Also removes max suggestions from tagify'd deity sheet inputs

--- src/util/tags.ts
+++ src/util/tags.ts
@@ -1,5 +1,6 @@
 import { TraitViewData } from "@actor/data/base";
-import { objectHasKey } from "./misc";
+import Tagify from "@yaireo/tagify";
+import { ErrorPF2e, objectHasKey } from "./misc";
 
 function traitSlugToObject(trait: string, dictionary: Record<string, string | undefined>): TraitViewData {
     // Look up trait labels from `npcAttackTraits` instead of `weaponTraits` in case a battle form attack is
@@ -16,4 +17,41 @@ function traitSlugToObject(trait: string, dictionary: Record<string, string | un
     return traitObject;
 }
 
-export { traitSlugToObject };
+/** Create a tagify select menu out of a JSON input element */
+function tagify(input: HTMLInputElement | null, { whitelist, maxTags }: TagifyOptions): Tagify<TagRecord> {
+    if (input?.dataset.dtype !== "JSON") {
+        throw ErrorPF2e("Usable only on input elements with JSON data-dtype");
+    }
+
+    return new Tagify(input, {
+        enforceWhitelist: true,
+        keepInvalidTags: false,
+        skipInvalid: true,
+        maxTags,
+        dropdown: {
+            closeOnSelect: false,
+            enabled: 0,
+            maxItems: Object.keys(whitelist).length,
+            searchKeys: ["id", "value"],
+        },
+        whitelist: Object.entries(whitelist).map(([key, locPath]) => ({
+            id: key,
+            value: game.i18n.localize(typeof locPath === "string" ? locPath : locPath.label),
+        })),
+    });
+}
+
+/**
+ * Standard properties expected by Tagify, where the `id` and `value` is what Foundry and the system would respectively
+ * call the `value` and `label`
+ */
+type TagRecord = Record<"id" | "value", string>;
+
+interface TagifyOptions {
+    /** The maximum number of tags that may be added to the input */
+    maxTags: number;
+    /** A whitelist record, typically pulled from `CONFIG.PF2E` */
+    whitelist: Record<string, string | { label: string }>;
+}
+
+export { tagify, traitSlugToObject };
