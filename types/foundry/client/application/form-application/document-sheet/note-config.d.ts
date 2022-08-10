44f981c68d6d616f028a2193d271fa92a905687e
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-08-10 14:10:10 -0500

    Move journal-entry-related sheets to proper folder and index

--- types/foundry/client/application/form-application/document-sheet/note-config.d.ts
+++ types/foundry/client/application/form-application/document-sheet/note-config.d.ts
@@ -3,7 +3,7 @@
  * @param note    The Note object for which settings are being configured
  * @param options Additional Application options
  */
-declare class NoteConfig extends FormApplication {
+declare class NoteConfig<TDocument extends NoteDocument = NoteDocument> extends DocumentSheet<TDocument> {
     /**
      * Extend the logic applied when the application is closed to clear any preview notes
      */
