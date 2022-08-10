2753c06e62a8dfc3572ccc1332a1ec77865daf92
Shark that walks like a man <rsimoes@gmail.com>
2022-08-10 03:39:23 -0500

    Remove remaining RBV canvas code and token-auras module check

--- src/module/canvas/layer/token.ts
+++ src/module/canvas/layer/token.ts
@@ -1,14 +1,6 @@
 import { TokenPF2e } from "../token";
 
 export class TokenLayerPF2e<TToken extends TokenPF2e = TokenPF2e> extends TokenLayer<TToken> {
-    /** Whether the Token Auras module is active */
-    kimsNaughtyModule: boolean;
-
-    constructor() {
-        super();
-        this.kimsNaughtyModule = game.modules.get("token-auras")?.active ?? false;
-    }
-
     /** Cycle Z indices of a hovered token stack */
     cycleStack(): boolean {
         const hovered = this._hover;
