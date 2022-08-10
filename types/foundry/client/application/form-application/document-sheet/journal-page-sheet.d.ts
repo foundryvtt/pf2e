64dad5c62da54c05c5ee5faa0c10412c42c407c4
Shark that walks like a man <rsimoes@gmail.com>
2022-08-10 13:47:57 -0500

    Move journal-entry-related sheets to proper folder and index

--- types/foundry/client/application/form-application/document-sheet/journal-page-sheet.d.ts
+++ types/foundry/client/application/form-application/document-sheet/journal-page-sheet.d.ts
@@ -1,8 +1,7 @@
 /**
  * The Application responsible for displaying and editing a single JournalEntryPage document.
- * @extends {DocumentSheet}
- * @param {JournalEntryPage} object         The JournalEntryPage instance which is being edited.
- * @param {DocumentSheetOptions} [options]  Application options.
+ * @param object    The JournalEntryPage instance which is being edited.
+ * @param [options] Application options.
  */
 declare class JournalPageSheet<
     TJournalEntryPage extends JournalEntryPage = JournalEntryPage
