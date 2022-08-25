57da0a09c59f641929d3789420d045c4a89b3b27
Carlos Fernandez <cfern1990@gmail.com>
2022-08-24 23:42:37 -0400

    Add badges to roll options

--- src/module/item/abstract-effect/document.ts
+++ src/module/item/abstract-effect/document.ts
@@ -1,10 +1,42 @@
 import { ItemPF2e } from "@item";
+import { sluggify } from "@util";
 import { EffectBadge } from "./data";
 
 /** Base effect type for all PF2e effects including conditions and afflictions */
 export abstract class AbstractEffectPF2e extends ItemPF2e {
+    /** A normalized version of the slug that shows in roll options, removing certain prefixes */
+    rollOptionSlug!: string;
+
     abstract get badge(): EffectBadge | null;
 
     abstract increase(): Promise<void>;
     abstract decrease(): Promise<void>;
+
+    override prepareBaseData(): void {
+        super.prepareBaseData();
+
+        const slug = this.slug ?? sluggify(this.name);
+        this.rollOptionSlug = slug.replace(/^(?:[a-z]+-)?(?:effect|stance)-/, "");
+    }
+
+    /** Set a self roll option for this effect */
+    override prepareActorData(): void {
+        const actor = this.actor!;
+        actor.rollOptions.all[`self:${this.type}:${this.rollOptionSlug}`] = true;
+
+        // Add the badge value to roll options but only if it is a number and the highest value
+        const badge = this.badge;
+        if (typeof badge?.value === "number") {
+            const otherEffects = actor.items.filter(
+                (i): i is Embedded<AbstractEffectPF2e> =>
+                    i instanceof AbstractEffectPF2e && i.rollOptionSlug === this.rollOptionSlug
+            );
+            const values = otherEffects
+                .map((effect) => effect.badge?.value)
+                .filter((value): value is number => typeof value === "number");
+            if (badge.value >= Math.max(...values)) {
+                actor.rollOptions.all[`self:${this.type}:${this.rollOptionSlug}:${badge.value}`] = true;
+            }
+        }
+    }
 }
