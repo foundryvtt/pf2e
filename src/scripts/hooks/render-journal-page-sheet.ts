8ce12eb0844e341e73d7dceab977c92cc6828395
Carlos Fernandez <cfern1990@gmail.com>
2022-08-26 03:28:54 -0400

    Fix journals for v10 testing 5

--- src/scripts/hooks/render-journal-page-sheet.ts
+++ src/scripts/hooks/render-journal-page-sheet.ts
@@ -7,20 +7,9 @@ export const RenderJournalPageSheet = {
             if (!parentSheetClass || !pageEl) return;
 
             const theme = parentSheetClass.theme ? String(parentSheetClass.theme) : null;
-            if (!theme) return;
-
-            // All pages regardless of type obtain the theme
-            pageEl.classList.add(theme);
-
-            // All text pages receive a .text-content css class to facilitate styling
-            // We need to handle both prosemirror edit sheets and views in the journal entry
-            // TinyMCE is handled separately
-            if (sheet.object.type === "text") {
-                pageEl.querySelector(".editor-content")?.classList.add("text-content");
-                if (pageEl.tagName === "ARTICLE") {
-                    const sectionEl = pageEl.querySelector(":scope > section");
-                    sectionEl?.classList.add("text-content");
-                }
+            if (theme) {
+                // All pages regardless of type obtain the theme
+                pageEl.classList.add(theme);
             }
         });
     },
