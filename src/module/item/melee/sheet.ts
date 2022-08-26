02da63926c6a3ce235080e0b9abec7cffc7399c5
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2022-08-26 04:55:19 -0400

    Refactor melee sheet events to melee sheet (#3654)

--- src/module/item/melee/sheet.ts
+++ src/module/item/melee/sheet.ts
@@ -13,6 +13,29 @@ export class MeleeSheetPF2e extends ItemSheetPF2e<MeleePF2e> {
             attackEffects: createSheetOptions(this.getAttackEffectOptions(), item.system.attackEffects),
         };
     }
+
+    override activateListeners($html: JQuery<HTMLElement>): void {
+        super.activateListeners($html);
+
+        // Add Damage Roll
+        $html.find(".add-damage").on("click", (event) => {
+            event.preventDefault();
+            const newKey = randomID();
+            const newDamageRoll = { damage: "", damageType: "" };
+            this.item.update({
+                [`system.damageRolls.${newKey}`]: newDamageRoll,
+            });
+        });
+
+        // Remove Damage Roll
+        $html.find(".delete-damage").on("click", (event) => {
+            event.preventDefault();
+            const targetKey = event.target.closest<HTMLElement>("[data-damage-part]")?.dataset.damagePart;
+            if (targetKey) {
+                this.item.update({ [`system.damageRolls.-=${targetKey}`]: null });
+            }
+        });
+    }
 }
 
 interface MeleeSheetData extends ItemSheetDataPF2e<MeleePF2e> {
