e2690234a84125f25b8db353bf695dd95e7fdba5
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-08-27 00:44:31 -0500

    Remove usage of jQuery from "renderSettings" sidebar hook (#3662)

--- src/scripts/hooks/render-settings.ts
+++ src/scripts/hooks/render-settings.ts
@@ -1,50 +1,69 @@
 import { MigrationSummary } from "@module/apps/migration-summary";
-import { fontAwesomeIcon } from "@util";
+import { ErrorPF2e, fontAwesomeIcon } from "@util";
 
 /** Attach system buttons and other knickknacks to the settings sidebar */
 export const RenderSettings = {
     listen: (): void => {
         Hooks.on("renderSettings", async (_app, $html) => {
-            // Guide and Changelog
-            const $systemRow = $html.find("#game-details > li.system");
-            const $guideLog = $systemRow.clone().empty().removeClass("system").addClass("system-info");
-            const links = {
-                guide: {
-                    url: "https://github.com/foundryvtt/pf2e/wiki",
-                    label: game.i18n.localize("PF2E.SETTINGS.Sidebar.Wiki"),
-                },
-                changelog: {
+            const html = $html[0]!;
+            // Additional system information resources
+            const systemRow = html.querySelector<HTMLLIElement>(".settings-sidebar li.system");
+            const systemInfo = systemRow?.cloneNode(false);
+            if (!(systemInfo instanceof HTMLLIElement)) {
+                throw ErrorPF2e("Unexpected error attaching system information to settings sidebar");
+            }
+
+            systemInfo.classList.remove("system");
+            systemInfo.classList.add("system-info");
+            const links = [
+                {
                     url: "https://github.com/foundryvtt/pf2e/blob/release-v9/CHANGELOG.md",
-                    label: game.i18n.localize("PF2E.SETTINGS.Sidebar.Changelog"),
+                    label: "PF2E.SETTINGS.Sidebar.Changelog",
                 },
-                discord: {
+                {
+                    url: "https://github.com/foundryvtt/pf2e/wiki",
+                    label: "PF2E.SETTINGS.Sidebar.Wiki",
+                },
+                {
                     url: "https://discord.gg/SajryVzCyf",
-                    label: game.i18n.localize("PF2E.SETTINGS.Sidebar.Discord"),
+                    label: "PF2E.SETTINGS.Sidebar.Discord",
                 },
-            };
-            $guideLog.append(
-                `<a href="${links.guide.url}">${links.guide.label}</a>`,
-                `<a href="${links.changelog.url}">${links.changelog.label}</a>`,
-                `<a href="${links.discord.url}">${links.discord.label}</a>`
-            );
-            $systemRow.after($guideLog);
+            ].map((data): HTMLAnchorElement => {
+                const anchor = document.createElement("a");
+                anchor.href = data.url;
+                anchor.innerText = game.i18n.localize(data.label);
+                anchor.target = "_blank";
+                return anchor;
+            });
+            systemInfo.append(...links);
+            systemRow?.after(systemInfo);
+
+            if (!game.user.hasRole("GAMEMASTER")) return;
 
             // Paizo License and Migration Troubleshooting
-            const $license = $("<div>").attr({ id: "pf2e-license" });
-            const $licenseButton = $('<button type="button">')
-                .append(fontAwesomeIcon("balance-scale"), game.i18n.localize("PF2E.LicenseViewer.Label"))
-                .on("click", () => game.pf2e.licenseViewer.render(true));
-            $license.append($licenseButton);
+            const license = document.createElement("div");
+            license.id = "pf2e-license";
+            const licenseButton = document.createElement("button");
+            licenseButton.type = "button";
+            licenseButton.append(fontAwesomeIcon("balance-scale"), game.i18n.localize("PF2E.LicenseViewer.Label"));
+            licenseButton.addEventListener("click", () => {
+                game.pf2e.licenseViewer.render(true);
+            });
+            license.append(licenseButton);
 
-            if (game.user.hasRole("GAMEMASTER")) {
-                const $troubleshooting = $("<div>").attr({ id: "pf2e-troubleshooting" });
-                const $shootButton = $('<button type="button">')
-                    .append(fontAwesomeIcon("wrench"), game.i18n.localize("PF2E.Migrations.Troubleshooting"))
-                    .on("click", () => new MigrationSummary({ troubleshoot: true }).render(true));
-                $troubleshooting.append($shootButton);
+            const troubleshooting = document.createElement("div");
+            troubleshooting.id = "pf2e-troubleshooting";
+            const shootButton = document.createElement("button");
+            shootButton.type = "button";
+            shootButton.append(fontAwesomeIcon("wrench"), game.i18n.localize("PF2E.Migrations.Troubleshooting"));
+            shootButton.addEventListener("click", () => {
+                new MigrationSummary({ troubleshoot: true }).render(true);
+            });
+            troubleshooting.append(shootButton);
 
-                $("#settings-documentation").after("<h2>Pathfinder 2e</h2>", $license, $troubleshooting);
-            }
+            const header = document.createElement("h2");
+            header.innerText = "Pathfinder 2e";
+            html.querySelector("#settings-documentation")?.after(header, license, troubleshooting);
         });
     },
 };
