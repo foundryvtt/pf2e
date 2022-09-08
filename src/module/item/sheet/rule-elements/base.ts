7ce044d196c19d0c993469b38ed43a6c7fda90e2
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2022-09-07 21:47:32 -0400

    Apply suggestions from code review
    Co-authored-by: Shark that walks like a man <106829671+stwlam@users.noreply.github.com>

--- src/module/item/sheet/rule-elements/base.ts
+++ src/module/item/sheet/rule-elements/base.ts
@@ -14,14 +14,14 @@ function coerceNumber<T extends string | unknown>(value: T): T | number {
 class RuleElementForm<TSource extends RuleElementSource = RuleElementSource> {
     template = "systems/pf2e/templates/items/rules/default.html";
     constructor(protected item: ItemPF2e, protected index: number, protected rule: TSource) {}
-    async getData() {
+    async getData(): Promise<{ index: number; rule: TSource }> {
         return {
             index: this.index,
             rule: this.rule,
         };
     }
 
-    async render() {
+    async render(): Promise<string> {
         const data = await this.getData();
         return renderTemplate(this.template, data);
     }
@@ -30,14 +30,14 @@ class RuleElementForm<TSource extends RuleElementSource = RuleElementSource> {
      * Helper to update the item with the new rule data.
      * This function exists because array updates in foundry are currently clunky
      */
-    updateItem(updates: Partial<TSource>) {
+    updateItem(updates: Partial<TSource>): void {
         const rules: Record<string, unknown>[] = this.item.toObject().system.rules;
         rules[this.index] = mergeObject(this.rule, updates, { performDeletions: true });
         this.item.update({ [`system.rules`]: rules });
     }
 
-    activateListeners(_html: HTMLElement) {}
-    _updateObject(_formData: Partial<Record<string, unknown>>) {}
+    activateListeners(_html: HTMLElement): void {}
+    _updateObject(_formData: Partial<Record<string, unknown>>): void {}
 }
 
 export { RuleElementForm, coerceNumber };
