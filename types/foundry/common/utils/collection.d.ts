fdcfc8c843907c100ba88128e83395599862fdda
Carlos Fernandez <cfern1990@gmail.com>
2022-09-14 18:17:48 -0400

    Move spell events and spell preparation form to creature

--- types/foundry/common/utils/collection.d.ts
+++ types/foundry/common/utils/collection.d.ts
@@ -65,7 +65,7 @@ declare module foundry {
              * c.get("d"); // null
              * c.get("d", {strict: true}); // throws Error
              */
-            get<T extends V = V>(key: string, { strict }: { strict: true }): T;
+            get<T extends V = V>(key: string | null | undefined, { strict }: { strict: true }): T;
             get<T extends V = V>(key: string, { strict }?: { strict?: boolean }): T | undefined;
 
             /**
