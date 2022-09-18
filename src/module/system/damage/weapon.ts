4cf00436dbd67108b35d028fd016614d4fd97c30
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-09-18 14:11:38 -0500

    Add support for explicit false value of `DamageDicePF2e#critical` (#4045)

--- src/module/system/damage/weapon.ts
+++ src/module/system/damage/weapon.ts
@@ -455,7 +455,7 @@ class WeaponDamagePF2e {
             (dm): dm is DiceModifierPF2e & { override: DamageDiceOverride } => !!(dm.enabled && dm.override)
         );
         for (const override of damageOverrides) {
-            if ((critical && override.critical) || !override.critical) {
+            if ((critical && override.critical !== false) || (!critical && !override.critical)) {
                 base.dieSize = override.override.dieSize ?? base.dieSize;
                 base.damageType = override.override.damageType ?? base.damageType;
                 base.diceNumber = override.override.diceNumber ?? base.diceNumber;
