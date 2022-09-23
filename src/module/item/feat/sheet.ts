5cd3fbf5002f0dadb4107734ea81a0178bca8f3c
In3luki <41452412+In3luki@users.noreply.github.com>
2022-09-23 03:00:23 +0200

    Remove JQuery from base item sheet (#4077)

--- src/module/item/feat/sheet.ts
+++ src/module/item/feat/sheet.ts
@@ -1,6 +1,7 @@
 import { FeatPF2e } from "@item/feat";
 import { FeatSheetData } from "../sheet/data-types";
 import { ItemSheetPF2e } from "../sheet/base";
+import Tagify from "@yaireo/tagify";
 
 export class FeatSheetPF2e extends ItemSheetPF2e<FeatPF2e> {
     override get validTraits(): Record<string, string> {
@@ -31,13 +32,21 @@ export class FeatSheetPF2e extends ItemSheetPF2e<FeatPF2e> {
 
     override activateListeners($html: JQuery<HTMLElement>): void {
         super.activateListeners($html);
+        const html = $html[0];
 
-        $html.find("[data-action=frequency-add]").on("click", () => {
+        const prerequisites = html.querySelector<HTMLInputElement>('input[name="system.prerequisites.value"]');
+        if (prerequisites) {
+            new Tagify(prerequisites, {
+                editTags: 1,
+            });
+        }
+
+        html.querySelector<HTMLAnchorElement>("a[data-action=frequency-add]")?.addEventListener("click", () => {
             const per = CONFIG.PF2E.frequencies.day;
             this.item.update({ system: { frequency: { max: 1, per } } });
         });
 
-        $html.find("[data-action=frequency-delete]").on("click", () => {
+        html.querySelector("a[data-action=frequency-delete]")?.addEventListener("click", () => {
             this.item.update({ "system.-=frequency": null });
         });
     }
