b734dc30d77b43d220d27526e4601e47e2f33146
SkepticRobot <BenLewis@skepticrobot.tech>
2022-10-27 13:14:37 -0400

    add target functionality to adjust-text
    clean up adjust-text
    add data validation

--- src/module/rules/rule-element/adjust-text.ts
+++ src/module/rules/rule-element/adjust-text.ts
@@ -1,32 +1,82 @@
 import { ItemPF2e } from "@item";
-import { RuleElementOptions, RuleElementPF2e, RuleElementSource } from "./";
+import { tupleHasValue } from "@util";
+import { RuleElementData, RuleElementOptions, RuleElementPF2e, RuleElementSource } from "./";
 
 /**
  * Change text of an item.
  * @category RuleElement
  */
- export class AdjustTextRuleElement extends RuleElementPF2e {
+ class AdjustTextRuleElement extends RuleElementPF2e {
 
-    target: string;
-    mode: string;
+    target: AdjustTextTarget;
+    mode: AdjustTextChangeMode;
     text: string;
+
+    static TARGETS = ["self", "granted-by"] as const;
+
+    static CHANGE_MODES = ["add", "replace"] as const;
 
     constructor(data: AdjustTextSource, item: Embedded<ItemPF2e>, options?: RuleElementOptions){
         super(data, item, options);
 
-        this.target = String(data.target);
-        this.mode = String(data.mode);
+        this.target = tupleHasValue(AdjustTextRuleElement.TARGETS, data.target) ? data.target : "self";
+        this.mode = tupleHasValue(AdjustTextRuleElement.CHANGE_MODES, data.mode) ? data.mode : "add";
         this.text = String(data.text);
         
     }
 
-    override async afterPrepareData(){
-        this.item.system.description.value = this.text;
+    #resolveTarget(): Embedded<ItemPF2e> | null {
+        switch(this.target) {
+            case "self": {
+                return this.item;
+            }
+            case "granted-by": {
+                const itemID = this.item.flags.pf2e.grantedBy?.id ?? "";
+                const item = this.actor.items.get(itemID);
+                return item || null;
+            }
+        }
+    }
+
+    protected validateData(): void {
+        if(!tupleHasValue(AdjustTextRuleElement.TARGETS, this.data.target)){
+            return this.failValidation(`target property is missing or invalid`);
+        }
+        if(!tupleHasValue(AdjustTextRuleElement.CHANGE_MODES, this.data.mode)){
+            return this.failValidation(`mode property is missing or invalid`);
+        }
+        if(this.data.target == "granted-by" && !this.item.flags.pf2e.grantedBy){
+            return this.failValidation(`Item is not granted by another item`);
+        }
+
+    }
+
+    override beforePrepareData(): void {
+        const targetItem = this.#resolveTarget();
+        if(targetItem == null){
+            return this.failValidation(`Target item does not exist`);
+        }
+        if(this.mode == "add"){
+            targetItem.system.description.value += this.text;
+        }else if (this.mode == "replace"){
+            targetItem.system.description.value = this.text;
+        }
+        
     }
 }
-
+interface AdjustTextRuleElement extends RuleElementPF2e {
+    data: AdjustTextData;
+}
+type AdjustTextTarget = "self" | "granted-by";
+type AdjustTextChangeMode = "add" | "replace";
+interface AdjustTextData extends RuleElementData {
+    target: AdjustTextTarget;
+    mode: AdjustTextChangeMode;
+}
 interface AdjustTextSource extends RuleElementSource {
     target?: unknown;
     mode?: unknown;
     text?: unknown;
 }
+
+export {AdjustTextData, AdjustTextRuleElement, AdjustTextSource}
