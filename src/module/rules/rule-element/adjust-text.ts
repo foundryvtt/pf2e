e600302d93e34f91e847d2a62c0acc1e61654b22
SkepticRobot <BenLewis@skepticrobot.tech>
2022-10-27 14:14:48 -0400

    actually validate data

--- src/module/rules/rule-element/adjust-text.ts
+++ src/module/rules/rule-element/adjust-text.ts
@@ -21,8 +21,7 @@ import { RuleElementData, RuleElementOptions, RuleElementPF2e, RuleElementSource
 
         this.target = tupleHasValue(AdjustTextRuleElement.TARGETS, data.target) ? data.target : "self";
         this.mode = tupleHasValue(AdjustTextRuleElement.CHANGE_MODES, data.mode) ? data.mode : "add";
-        this.text = String(data.text);
-        
+        this.text = typeof data.text === "string" ? data.text : "";
     }
 
     #resolveTarget(): Embedded<ItemPF2e> | null {
@@ -48,31 +47,36 @@ import { RuleElementData, RuleElementOptions, RuleElementPF2e, RuleElementSource
         if(this.data.target == "granted-by" && !this.item.flags.pf2e.grantedBy){
             return this.failValidation(`Item is not granted by another item`);
         }
-
     }
 
     override beforePrepareData(): void {
+        this.validateData();
+
         const targetItem = this.#resolveTarget();
         if(targetItem == null){
             return this.failValidation(`Target item does not exist`);
         }
+
         if(this.mode == "add"){
             targetItem.system.description.value += this.text;
         }else if (this.mode == "replace"){
             targetItem.system.description.value = this.text;
         }
-        
     }
 }
+
 interface AdjustTextRuleElement extends RuleElementPF2e {
     data: AdjustTextData;
 }
+
 type AdjustTextTarget = "self" | "granted-by";
 type AdjustTextChangeMode = "add" | "replace";
+
 interface AdjustTextData extends RuleElementData {
     target: AdjustTextTarget;
     mode: AdjustTextChangeMode;
 }
+
 interface AdjustTextSource extends RuleElementSource {
     target?: unknown;
     mode?: unknown;
