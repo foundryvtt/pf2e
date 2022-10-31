8334c243d724561decee13411f16cb28ab55a96e
Cerapter <cerap@protonmail.com>
2022-10-31 23:51:54 +0100

    Check if actor has ID before adding it as a rollOption

--- src/module/actor/base.ts
+++ src/module/actor/base.ts
@@ -504,7 +504,9 @@ class ActorPF2e extends Actor<TokenDocumentPF2e, ItemTypeMap> {
         });
 
         // Always add the actor's ID to the rolloptions.
-        this.rollOptions.all[`self:id:${this.id}`] = true;
+        if (this.id) {
+            this.rollOptions.all[`self:id:${this.id}`] = true;
+        }
 
         this.setEncounterRollOptions();
     }
