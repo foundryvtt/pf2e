382e18b99bc7c78b2f944964ffb9090da44375ed
Shark that walks like a man <rsimoes@gmail.com>
2022-10-30 20:34:13 -0500

    Fix labels on sense selector application

--- src/module/system/tag-selector/senses.ts
+++ src/module/system/tag-selector/senses.ts
@@ -9,7 +9,7 @@ export class SenseSelector<TActor extends ActorPF2e> extends BaseTagSelector<TAc
     static override get defaultOptions(): FormApplicationOptions {
         return mergeObject(super.defaultOptions, {
             template: "systems/pf2e/templates/system/tag-selector/senses.html",
-            title: "PF2E.Sense.Label",
+            title: "PF2E.Actor.Creature.Sense.Label",
         });
     }
 
