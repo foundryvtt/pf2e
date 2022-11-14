3602310f0e71eee3db0463261b9963312f414dd8
Simon Ward <simonward@mac.com>
2022-11-13 23:03:34 -0800

    Allow build script to run on Mac

--- packs/scripts/build.ts
+++ packs/scripts/build.ts
@@ -3,7 +3,9 @@ import * as fs from "fs";
 import { CompendiumPack, PackError } from "./packman/compendium-pack";
 
 const packsDataPath = path.resolve(__dirname, "../data");
-const packDirPaths = fs.readdirSync(packsDataPath).map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
+const packDirPaths = fs.readdirSync(packsDataPath)
+    .filter((dirName) => dirName !== ".DS_Store") // Filter out Mac .DS_Store files
+    .map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
 
 // Loads all packs into memory for the sake of making all entity name/id mappings available
 const packs = packDirPaths.map((p) => CompendiumPack.loadJSON(p));
