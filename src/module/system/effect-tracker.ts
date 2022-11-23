602dd422094fc9f92c706906c92c676ae87998d6
Shark that walks like a man <rsimoes@gmail.com>
2022-11-22 23:57:43 -0600

    Rename EffectTracker#trackedEffects to EffectTracker#effects

--- src/module/system/effect-tracker.ts
+++ src/module/system/effect-tracker.ts
@@ -3,38 +3,38 @@ import type { EffectPF2e } from "@item/index";
 import { EncounterPF2e } from "@module/encounter";
 
 export class EffectTracker {
-    private trackedEffects: Embedded<EffectPF2e>[] = [];
+    effects: Embedded<EffectPF2e>[] = [];
 
     /** A separate collection of aura effects, including ones with unlimited duration */
     auraEffects: Collection<Embedded<EffectPF2e>> = new Collection();
 
     private insert(effect: Embedded<EffectPF2e>, duration: { expired: boolean; remaining: number }): void {
-        if (this.trackedEffects.length === 0) {
-            this.trackedEffects.push(effect);
+        if (this.effects.length === 0) {
+            this.effects.push(effect);
         } else {
-            for (let index = 0; index < this.trackedEffects.length; index++) {
-                const other = this.trackedEffects[index];
+            for (let index = 0; index < this.effects.length; index++) {
+                const other = this.effects[index];
                 const remaining = other.remainingDuration.remaining;
                 // compare duration and insert if other effect has later expiration
                 if (duration.remaining > remaining) {
                     // new effect has longer remaining duration - skip ahead
                 } else if (remaining > duration.remaining) {
-                    this.trackedEffects.splice(index, 0, effect);
+                    this.effects.splice(index, 0, effect);
                     return;
                 } else if ((effect.system.start.initiative ?? 0) > (other.system.start.initiative ?? 0)) {
                     // new effect has later initiative - skip ahead
                 } else if ((other.system.start.initiative ?? 0) > (effect.system.start.initiative ?? 0)) {
-                    this.trackedEffects.splice(index, 0, effect);
+                    this.effects.splice(index, 0, effect);
                     return;
                 } else if (
                     other.system.duration.expiry === "turn-start" &&
                     effect.system.duration.expiry === "turn-end"
                 ) {
-                    this.trackedEffects.splice(index, 0, effect);
+                    this.effects.splice(index, 0, effect);
                     return;
                 }
             }
-            this.trackedEffects.push(effect);
+            this.effects.push(effect);
         }
     }
 
@@ -43,28 +43,28 @@ export class EffectTracker {
             this.auraEffects.set(effect.uuid, effect);
         }
 
-        const index = this.trackedEffects.findIndex((e) => e.id === effect.id);
+        const index = this.effects.findIndex((e) => e.id === effect.id);
         const systemData = effect.system;
         const duration = systemData.duration.unit;
         switch (duration) {
             case "unlimited":
             case "encounter": {
                 if (duration === "unlimited") systemData.expired = false;
-                if (index >= 0 && index < this.trackedEffects.length) {
-                    this.trackedEffects.splice(index, 1);
+                if (index >= 0 && index < this.effects.length) {
+                    this.effects.splice(index, 1);
                 }
                 return;
             }
             default: {
                 const duration = effect.remainingDuration;
                 effect.system.expired = duration.expired;
-                if (this.trackedEffects.length === 0 || index < 0) {
+                if (this.effects.length === 0 || index < 0) {
                     this.insert(effect, duration);
                 } else {
-                    const existing = this.trackedEffects[index];
+                    const existing = this.effects[index];
                     // compare duration and update if different
                     if (duration.remaining !== existing.remainingDuration.remaining) {
-                        this.trackedEffects.splice(index, 1);
+                        this.effects.splice(index, 1);
                         this.insert(effect, duration);
                     }
                 }
@@ -73,12 +73,12 @@ export class EffectTracker {
     }
 
     unregister(toRemove: Embedded<EffectPF2e>): void {
-        this.trackedEffects = this.trackedEffects.filter((effect) => effect !== toRemove);
+        this.effects = this.effects.filter((effect) => effect !== toRemove);
         this.auraEffects.delete(toRemove.uuid);
     }
 
     async refresh(): Promise<void> {
-        const actorsToUpdate = new Set(this.trackedEffects.filter((e) => e.isExpired).map((e) => e.actor));
+        const actorsToUpdate = new Set(this.effects.filter((e) => e.isExpired).map((e) => e.actor));
 
         if (game.settings.get("pf2e", "automation.removeExpiredEffects")) {
             for (const actor of actorsToUpdate) {
