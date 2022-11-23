a35906a5c8521bc9f62b53d2171d6bf6a9ab7f83
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-11-23 04:05:36 -0600

    Use `EffectTracker#refresh` in "updateWorldTime" hook (#4926)

--- src/scripts/hooks/update-world-time.ts
+++ src/scripts/hooks/update-world-time.ts
@@ -1,13 +1,8 @@
-import { resetAndRerenderActors } from "@actor/helpers";
-
 export const UpdateWorldTime = {
     listen: (): void => {
         Hooks.on("updateWorldTime", async (_total, diff) => {
             /** Handle effect-tracking by encounter turns when an encounter is active */
-            if (!game.combat?.started) {
-                const actors = new Set(game.pf2e.effectTracker.effects.map((e) => e.actor));
-                await resetAndRerenderActors(actors);
-            }
+            if (!game.combat?.started) game.pf2e.effectTracker.refresh();
 
             // Add micro-delay due to the Calendar/Weather module waiting until the JQuery $(document).ready event fires
             // to set its hook.
