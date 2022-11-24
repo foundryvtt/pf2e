042f61306d7af97623abdd56a9fe45942f4912b5
Shark that walks like a man <rsimoes@gmail.com>
2022-11-24 12:23:16 -0600

    Fix expired effect removal when out of combat

--- src/scripts/hooks/update-world-time.ts
+++ src/scripts/hooks/update-world-time.ts
@@ -2,7 +2,7 @@ export const UpdateWorldTime = {
     listen: (): void => {
         Hooks.on("updateWorldTime", async (_total, diff) => {
             /** Handle effect-tracking by encounter turns when an encounter is active */
-            if (!game.combat?.started) game.pf2e.effectTracker.refresh();
+            if (!game.combat?.started) game.pf2e.effectTracker.refresh({ resetItemData: true });
 
             // Add micro-delay due to the Calendar/Weather module waiting until the JQuery $(document).ready event fires
             // to set its hook.
