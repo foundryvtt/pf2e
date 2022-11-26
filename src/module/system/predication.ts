5d73ee7a68ce1e9a0d08954fa5e543f55f57a7be
Shark that walks like a man <rsimoes@gmail.com>
2022-11-26 17:50:47 -0600

    Accommodate Supe's imaginary use case of non-integer comparisons

--- src/module/system/predication.ts
+++ src/module/system/predication.ts
@@ -88,13 +88,15 @@ class PredicatePF2e extends Array<PredicateStatement> {
         } else {
             const operator = Object.keys(statement)[0];
 
+            // Allow for tests of partial statements against numeric values
+            // E.g., `{ "gt": ["actor:level", 5] }` would match against "actor:level:6" and "actor:level:7"
             const [left, right] = Object.values(statement)[0];
             const domainArray = Array.from(domain);
-            const leftValues = Number.isInteger(Number(left))
+            const leftValues = !Number.isNaN(Number(left))
                 ? [Number(left)]
                 : domainArray.flatMap((d) => (d.startsWith(left) ? Number(/:(-?\d+)$/.exec(d)?.[1]) : []));
             const rightValues =
-                typeof right === "number" || Number.isInteger(Number(right))
+                typeof right === "number" || !Number.isNaN(Number(right))
                     ? [Number(right)]
                     : domainArray.flatMap((d) => (d.startsWith(right) ? Number(/:(-?\d+)$/.exec(d)?.[1]) : []));
 
