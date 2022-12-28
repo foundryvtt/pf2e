076262c64c991d941f137041860052ded9b73008
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-12-28 00:06:51 -0600

    Fix path check in migration 809 (#5337)

--- src/module/migration/migrations/809-automaton-enhancements.ts
+++ src/module/migration/migrations/809-automaton-enhancements.ts
@@ -1,15 +1,17 @@
 import { ItemSourcePF2e } from "@item/data";
+import { AELikeSource } from "@module/rules/rule-element/ae-like";
 import { MigrationBase } from "../base";
 
 /** Add base REs to Automaton anncestry to allow for automation of enhancements */
 export class Migration809AutomatonEnhancements extends MigrationBase {
     static override version = 0.809;
 
-    get #automatonEnhancements() {
+    get #automatonEnhancements(): AELikeSource {
         return {
             key: "ActiveEffectLike",
-            path: "flags.pf2e.automaton.enhancements",
             mode: "override",
+            path: "flags.pf2e.automaton.enhancements",
+            priority: 10,
             value: { greater: [], lesser: [] },
         };
     }
@@ -17,7 +19,7 @@ export class Migration809AutomatonEnhancements extends MigrationBase {
     override async updateItem(source: ItemSourcePF2e): Promise<void> {
         const isAutomaton = source.type === "ancestry" && source.system.slug === "automaton";
         const rules: Record<string, unknown>[] = source.system.rules;
-        if (isAutomaton && !rules.some((r) => r.path === "flags.pf2e.automaton")) {
+        if (isAutomaton && !rules.some((r) => r.path === "flags.pf2e.automaton.enhancements")) {
             source.system.rules.push(this.#automatonEnhancements);
         }
     }
