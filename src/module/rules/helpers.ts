78d3fd693b6eebd009fef75581af55a6a924f557
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2022-12-29 01:32:32 -0600

    Move calling of RE `preUpdateActor` to `ActorPF2e.updateDocuments` (#5349)

--- src/module/rules/helpers.ts
+++ src/module/rules/helpers.ts
@@ -1,8 +1,11 @@
+import { ActorPF2e } from "@actor";
 import { DamageDicePF2e, DeferredValueParams, ModifierAdjustment, ModifierPF2e } from "@actor/modifiers";
+import { ItemSourcePF2e } from "@item/data";
 import { RollNotePF2e } from "@module/notes";
 import { DegreeOfSuccessAdjustment } from "@system/degree-of-success";
 import { RollTwiceOption } from "@system/rolls";
 import { isObject, pick } from "@util";
+import { RuleElementPF2e } from "./rule-element";
 import { BracketedValue } from "./rule-element/data";
 import { DamageDiceSynthetics, RollSubstitution, RollTwiceSynthetic, RuleElementSynthetics } from "./synthetics";
 
@@ -80,6 +83,42 @@ function isBracketedValue(value: unknown): value is BracketedValue {
     return isObject<{ brackets?: unknown }>(value) && Array.isArray(value.brackets);
 }
 
+async function processPreUpdateActorHooks(
+    changed: DocumentUpdateData<ActorPF2e>,
+    { pack }: { pack: string | null }
+): Promise<void> {
+    const actorId = String(changed._id);
+    const actor = pack ? await game.packs.get(pack)?.getDocument(actorId) : game.actors.get(actorId);
+    if (!(actor instanceof ActorPF2e)) return;
+
+    // Run preUpdateActor rule element callbacks
+    type WithPreUpdateActor = RuleElementPF2e & {
+        preUpdateActor: NonNullable<RuleElementPF2e["preUpdateActor"]>;
+    };
+    const rules = actor.rules.filter((r): r is WithPreUpdateActor => !!r.preUpdateActor);
+    if (rules.length === 0) return;
+
+    actor.flags.pf2e.rollOptions = actor.clone(changed, { keepId: true }).flags.pf2e.rollOptions;
+    const createDeletes = (
+        await Promise.all(
+            rules.map(
+                (r): Promise<{ create: ItemSourcePF2e[]; delete: string[] }> =>
+                    actor.items.has(r.item.id) ? r.preUpdateActor() : new Promise(() => ({ create: [], delete: [] }))
+            )
+        )
+    ).reduce(
+        (combined, cd) => ({
+            create: [...combined.create, ...cd.create],
+            delete: Array.from(new Set([...combined.delete, ...cd.delete])),
+        }),
+        { create: [], delete: [] }
+    );
+    createDeletes.delete = createDeletes.delete.filter((id) => actor.items.has(id));
+
+    await actor.createEmbeddedDocuments("Item", createDeletes.create, { keepId: true, render: false });
+    await actor.deleteEmbeddedDocuments("Item", createDeletes.delete, { render: false });
+}
+
 export {
     extractDamageDice,
     extractDegreeOfSuccessAdjustments,
@@ -89,4 +128,5 @@ export {
     extractRollSubstitutions,
     extractRollTwice,
     isBracketedValue,
+    processPreUpdateActorHooks,
 };
