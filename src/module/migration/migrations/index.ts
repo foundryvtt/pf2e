b9f2e10df7be6fa575b251159b51e5180825011d
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-01-13 03:19:32 -0600

    Migrate calculated/expanded splash REs (#5657)

--- src/module/migration/migrations/index.ts
+++ src/module/migration/migrations/index.ts
@@ -211,3 +211,4 @@ export { Migration810LootDescriptionValue } from "./810-loot-description-value";
 export { Migration811InlineDamageRollsPersistent } from "./811-inline-damage-rolls-persistent";
 export { Migration812RestructureIWR } from "./812-restructure-iwr";
 export { Migration813NormalizeColdIron } from "./813-normalize-cold-iron";
+export { Migration814CalculatedExpandedSplash } from "./814-calculated-expanded-splash";
