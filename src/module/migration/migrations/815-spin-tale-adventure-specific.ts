723c069b98397b1b1e087ddbb6a18f88e24ac272
Simon Ward <simonward@mac.com>
2023-01-14 15:35:10 -0800

    Also check for id form

--- src/module/migration/migrations/815-spin-tale-adventure-specific.ts
+++ src/module/migration/migrations/815-spin-tale-adventure-specific.ts
@@ -10,13 +10,21 @@ export class Migration815SpinTaleAdventureSpecific extends MigrationBase {
             const oldSpinTale = "Compendium.pf2e.adventure-specific-actions.Spin Tale";
             const newSpinTale = "Compendium.pf2e.actionspf2e.Spin Tale";
 
+            const oldSpinTaleId = "Compendium.pf2e.adventure-specific-actions.5gahZQXf3UVwATSC";
+            const newSpinTaleId = "Compendium.pf2e.actionspf2e.hPZQ5vA9QHEPtjFW";
+
             if (source.system.description.value) {
                 source.system.description.value = source.system.description.value.replace(oldSpinTale, newSpinTale);
+                source.system.description.value = source.system.description.value.replace(oldSpinTaleId, newSpinTaleId);
             }
 
             for (const rule of source.system.rules) {
                 if (rule.key === "GrantItem" && "uuid" in rule && typeof rule.uuid === "string") {
                     rule.uuid = rule.uuid.replace(oldSpinTale, newSpinTale);
+
+                    if (typeof rule.uuid === "string") {
+                        rule.uuid = rule.uuid.replace(oldSpinTaleId, newSpinTaleId);
+                    }
                 }
             }
         }
