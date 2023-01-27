97bcd22171b1b22dd43acf3e2dd94524693fb78a
LebombJames <77904738+LebombJames@users.noreply.github.com>
2023-01-27 06:42:46 +0000

    Add item level display to item directory (#5934)

--- src/module/apps/sidebar/index.ts
+++ src/module/apps/sidebar/index.ts
@@ -1,4 +1,5 @@
 export { ActorDirectoryPF2e } from "./actor-directory";
+export { ItemDirectoryPF2e } from "./item-directory";
 export { ChatLogPF2e } from "./chat-log";
 export { CompendiumDirectoryPF2e } from "./compendium-directory";
 export { EncounterTrackerPF2e } from "./encounter-tracker";
