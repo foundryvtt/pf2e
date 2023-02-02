10deaab187c111f3fa12f857912bae080eb5b4f7
Trent Piepho <tpiepho@gmail.com>
2023-02-02 01:29:41 -0800

    Use Math.max() rather than .sort().pop()
    It's O(n) rather than O(n log n).  Also less code.

--- src/module/actor/creature/document.ts
+++ src/module/actor/creature/document.ts
@@ -726,8 +726,8 @@ abstract class CreaturePF2e extends ActorPF2e {
             const landSpeed = systemData.attributes.speed;
             landSpeed.value = Number(landSpeed.value) || 0;
 
-            const fromSynthetics = (this.synthetics.movementTypes[movementType] ?? []).map((d) => d() ?? []).flat();
-            landSpeed.value = [landSpeed.value, ...fromSynthetics.map((s) => s.value)].sort().pop()!;
+            const fromSynthetics = (this.synthetics.movementTypes[movementType] ?? []).flatMap((d) => d() ?? []);
+            landSpeed.value = Math.max(landSpeed.value, ...fromSynthetics.map((s) => s.value));
 
             const base = landSpeed.value;
             const modifiers = extractModifiers(this.synthetics, domains);
