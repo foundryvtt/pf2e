3ea8e6463ff3acb9fd83dced554e8a54cd6f0bc8
nikolaj-a <na@deck.dk>
2023-02-12 03:16:31 +0100

    Move action-specific bonuses from traits to the individual actions
    Refactor action check roll code to have item bonus logic from traits in the action code itself. This is mostly affecting the various combat maneuver actions with the attack trait at this point. The refactoring should cut down a bit on the shared action code while still making it more flexible for future actions.

--- src/module/system/action-macros/athletics/disarm.ts
+++ src/module/system/action-macros/athletics/disarm.ts
@@ -1,14 +1,26 @@
 import { ActionMacroHelpers, SkillActionOptions } from "..";
+import { WeaponPF2e } from "@item";
+import { ModifierPF2e } from "@actor/modifiers";
 
 export function disarm(options: SkillActionOptions) {
     const { checkType, property, stat, subtitle } = ActionMacroHelpers.resolveStat(options?.skill ?? "athletics");
-    ActionMacroHelpers.simpleRollActionCheck({
+    ActionMacroHelpers.simpleRollActionCheck<Embedded<WeaponPF2e>>({
         actors: options.actors,
         statName: property,
         actionGlyph: options.glyph ?? "A",
         title: "PF2E.Actions.Disarm.Title",
         subtitle,
-        modifiers: options.modifiers,
+        item: (actor) => (ActionMacroHelpers.getApplicableEquippedWeapons(actor, "disarm") ?? []).shift(),
+        modifiers: (args) => {
+            const modifiers: ModifierPF2e[] = [];
+            if (args.item && args.item.slug !== "basic-unarmed") {
+                const modifier = ActionMacroHelpers.getWeaponPotencyModifier(args.item, stat);
+                if (modifier) {
+                    modifiers.push(modifier);
+                }
+            }
+            return modifiers.concat(options.modifiers ?? []);
+        },
         rollOptions: ["all", checkType, stat, "action:disarm"],
         extraOptions: ["action:disarm"],
         traits: ["attack"],
@@ -22,6 +34,5 @@ export function disarm(options: SkillActionOptions) {
             ActionMacroHelpers.note(selector, "PF2E.Actions.Disarm", "success"),
             ActionMacroHelpers.note(selector, "PF2E.Actions.Disarm", "criticalFailure"),
         ],
-        weaponTrait: "disarm",
     });
 }
--- src/module/system/action-macros/athletics/shove.ts
+++ src/module/system/action-macros/athletics/shove.ts
@@ -1,14 +1,26 @@
 import { ActionMacroHelpers, SkillActionOptions } from "..";
+import { ModifierPF2e } from "@actor/modifiers";
+import { WeaponPF2e } from "@item";
 
 export function shove(options: SkillActionOptions) {
     const { checkType, property, stat, subtitle } = ActionMacroHelpers.resolveStat(options?.skill ?? "athletics");
-    ActionMacroHelpers.simpleRollActionCheck({
+    ActionMacroHelpers.simpleRollActionCheck<Embedded<WeaponPF2e>>({
         actors: options.actors,
         statName: property,
         actionGlyph: options.glyph ?? "A",
         title: "PF2E.Actions.Shove.Title",
         subtitle,
-        modifiers: options.modifiers,
+        item: (actor) => (ActionMacroHelpers.getApplicableEquippedWeapons(actor, "shove") ?? []).shift(),
+        modifiers: (args) => {
+            const modifiers: ModifierPF2e[] = [];
+            if (args.item && args.item.traits.has("shove")) {
+                const modifier = ActionMacroHelpers.getWeaponPotencyModifier(args.item, stat);
+                if (modifier) {
+                    modifiers.push(modifier);
+                }
+            }
+            return modifiers.concat(options.modifiers ?? []);
+        },
         rollOptions: ["all", checkType, stat, "action:shove"],
         extraOptions: ["action:shove"],
         traits: ["attack"],
@@ -22,6 +34,5 @@ export function shove(options: SkillActionOptions) {
             ActionMacroHelpers.note(selector, "PF2E.Actions.Shove", "success"),
             ActionMacroHelpers.note(selector, "PF2E.Actions.Shove", "criticalFailure"),
         ],
-        weaponTrait: "shove",
     });
 }
