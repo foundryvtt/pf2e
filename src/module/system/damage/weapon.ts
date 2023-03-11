d86f187e17917d330938212d9ac4051c181e6bcc
cuyima <vivilotta98@live.de>
2023-03-11 15:03:37 +0100

    corrected non functional deconstruct

--- src/module/system/damage/weapon.ts
+++ src/module/system/damage/weapon.ts
@@ -604,10 +604,9 @@ class WeaponDamagePF2e {
                 base.dieSize = override.override?.dieSize ?? base.dieSize;
                 base.damageType = override.override?.damageType ?? base.damageType;
                 base.diceNumber = override.override?.diceNumber ?? base.diceNumber;
-                damage.dice.forEach(({ slug, damageType }) => {
-                    if (slug.startsWith("deadly-")) {
-                      damageType = override.override?.damageType ?? damageType;
-                    }
+                damage.dice.forEach((die) => {
+                    if (die.slug.startsWith("deadly-"))
+                        die.damageType = override.override?.damageType ?? die.damageType;
                 });
             }
         }
