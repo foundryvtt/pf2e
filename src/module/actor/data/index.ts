372345eca3e112f524d6fda15b4a85f2f0fd1975
Shark that walks like a man <rsimoes@gmail.com>
2023-03-16 07:48:57 -0500

    Add Army actor type

--- src/module/actor/data/index.ts
+++ src/module/actor/data/index.ts
@@ -1,17 +1,17 @@
-import { CharacterSource } from "@actor/character/data.ts";
-import { CreatureType } from "@actor/creature/data.ts";
-import { FamiliarSource } from "@actor/familiar/data.ts";
-import { HazardSource } from "@actor/hazard/data.ts";
-import { LootSource } from "@actor/loot/data.ts";
-import { NPCSource } from "@actor/npc/data.ts";
-import { PartySource } from "@actor/party/data.ts";
-import { VehicleSource } from "@actor/vehicle/data.ts";
-import { StatisticRollParameters } from "@system/statistic/index.ts";
+import { ArmySource } from "@actor/army";
+import { CharacterSource } from "@actor/character/data";
+import { CreatureType } from "@actor/creature/data";
+import { FamiliarSource } from "@actor/familiar/data";
+import { HazardSource } from "@actor/hazard/data";
+import { LootSource } from "@actor/loot/data";
+import { NPCSource } from "@actor/npc/data";
+import { PartySource } from "@actor/party/data";
+import { VehicleSource } from "@actor/vehicle/data";
 
 type CreatureSource = CharacterSource | NPCSource | FamiliarSource;
-type ActorType = CreatureType | "hazard" | "loot" | "party" | "vehicle";
+type ActorType = CreatureType | "army" | "hazard" | "loot" | "party" | "vehicle";
 
-type ActorSourcePF2e = CreatureSource | HazardSource | LootSource | PartySource | VehicleSource;
+type ActorSourcePF2e = CreatureSource | ArmySource | HazardSource | LootSource | PartySource | VehicleSource;
 
 interface RollInitiativeOptionsPF2e extends RollInitiativeOptions, StatisticRollParameters {
     secret?: boolean;
@@ -20,6 +20,7 @@ interface RollInitiativeOptionsPF2e extends RollInitiativeOptions, StatisticRoll
 export {
     ActorSourcePF2e,
     ActorType,
+    ArmySource,
     CharacterSource,
     CreatureSource,
     FamiliarSource,
