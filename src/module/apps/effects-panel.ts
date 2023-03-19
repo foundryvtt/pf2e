322ba11b2837be6922529fda52b0981d179f8c1a
Julie Winchester <julie.winchester@duke.edu>
2023-03-18 21:29:54 -0500

    Display description in effect panel and add send to chat

--- src/module/apps/effects-panel.ts
+++ src/module/apps/effects-panel.ts
@@ -3,6 +3,7 @@ import { AbstractEffectPF2e, EffectPF2e } from "@item";
 import { AfflictionPF2e } from "@item/affliction";
 import { EffectExpiryType } from "@item/effect/data";
 import { TokenDocumentPF2e } from "@scene";
+import { InlineRollLinks } from "@scripts/ui/inline-roll-links";
 import { htmlQuery, htmlQueryAll } from "@util";
 import { FlattenedCondition } from "../system/conditions";
 
@@ -33,7 +34,7 @@ export class EffectsPanel extends Application {
     override async getData(options?: ApplicationOptions): Promise<EffectsPanelData> {
         const { actor } = this;
         if (!actor || !game.user.settings.showEffectPanel) {
-            return { afflictions: [], conditions: [], effects: [], actor: null, user: { isGM: false } };
+            return { afflictions: [], conditions: [], effects: [], descriptions: ({} as EffectsDescriptionData), actor: null, user: { isGM: false } };
         }
 
         const effects =
@@ -63,12 +64,21 @@ export class EffectsPanel extends Application {
 
         const conditions = game.pf2e.ConditionManager.getFlattenedConditions(actor.itemTypes.condition);
 
+        const afflictions = actor.itemTypes.affliction;
+        
+        const descriptions = {
+            afflictions: await this.#getEnrichedDescriptions(afflictions),
+            conditions: await this.#getEnrichedDescriptions(conditions),
+            effects: await this.#getEnrichedDescriptions(effects),
+        }
+
         return {
             ...(await super.getData(options)),
-            actor,
-            effects,
+            afflictions,
             conditions,
-            afflictions: actor.itemTypes.affliction,
+            descriptions,
+            effects,
+            actor,
             user: { isGM: game.user.isGM },
         };
     }
@@ -76,6 +86,9 @@ export class EffectsPanel extends Application {
     override activateListeners($html: JQuery): void {
         super.activateListeners($html);
         const html = $html[0]!;
+
+        // For inline roll links in descriptions
+        InlineRollLinks.listen(html, this.actor || undefined);
 
         for (const effectEl of htmlQueryAll(html, ".effect-item[data-item-id]")) {
             const itemId = effectEl.dataset.itemId;
@@ -110,6 +123,13 @@ export class EffectsPanel extends Application {
                 if (item?.isOfType("condition")) {
                     item.rollRecovery();
                 }
+            });
+
+            // Send effect to chat
+            effectEl.querySelector("[data-action=send-to-chat]")?.addEventListener("click", async () => {
+                const { actor } = this;
+                const effect = actor?.items.get(itemId);
+                if (effect) await effect.toMessage();
             });
 
             // Uses a scale transform to fit the text within the box
@@ -192,11 +212,26 @@ export class EffectsPanel extends Application {
             return game.i18n.format(key, { initiative });
         }
     }
+
+    async #getEnrichedDescriptions(
+        effects: AfflictionPF2e[] | EffectPF2e[] | FlattenedCondition[] 
+    ): Promise<String[]> {
+        return await Promise.all(effects.map(async effect =>
+            await TextEditor.enrichHTML(effect.description, { async: true })
+        ));
+    }
+}
+
+interface EffectsDescriptionData {
+    afflictions: String[];
+    conditions: String[];
+    effects: String[];
 }
 
 interface EffectsPanelData {
     afflictions: AfflictionPF2e[];
     conditions: FlattenedCondition[];
+    descriptions: EffectsDescriptionData;
     effects: EffectPF2e[];
     actor: ActorPF2e | null;
     user: { isGM: boolean };
