df0cbeac0732ba208f2e0c15e2847cb4e7c977aa
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2023-04-10 19:42:45 -0400

    Support adding members to parties (#7354)

--- src/module/actor/party/data.ts
+++ src/module/actor/party/data.ts
@@ -29,7 +29,11 @@ interface PartyDetailsSource {
     level: {
         value: number;
     };
-    members: ActorUUID[];
+    members: MemberData[];
+}
+
+interface MemberData {
+    uuid: ActorUUID | TokenDocumentUUID;
 }
 
 interface PartySystemData extends Omit<PartySystemSource, "attributes">, Omit<ActorSystemData, "traits"> {
@@ -47,4 +51,4 @@ interface PartyAttributes
 
 interface PartyDetails extends PartyDetailsSource, ActorDetails {}
 
-export { PartySource, PartySystemData };
+export { MemberData, PartySource, PartySystemData };
