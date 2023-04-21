eb3859a367ca98d9721f1aa4e3c68b5e9a616205
Simon Ward <simonward@mac.com>
2023-04-21 10:45:10 -0700

    Add "area-effect" to spell RollOptions
    For example, hobgoblin NPCs have a bonus to area effect saving throws when in formation, and the hobgoblin ancestry feat Formation Training provides a similar bonus. The rolloption "area-damage" isn't sufficient for spells like Entangle which don't do damage.

--- src/module/item/spell/document.ts
+++ src/module/item/spell/document.ts
@@ -639,10 +639,15 @@ class SpellPF2e<TParent extends ActorPF2e | null = ActorPF2e | null> extends Ite
             }
         }
 
+        const areaEffect = !!this.system.area?.value;
+        if (areaEffect) {
+            options.add("area-effect");
+        }
+
         if (damageValues.length > 0 && this.system.spellType.value !== "heal") {
             options.add("damaging-effect");
 
-            if (this.system.area?.value) {
+            if (areaEffect) {
                 options.add("area-damage");
             }
         }
