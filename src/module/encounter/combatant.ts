9e3736e9cb32e2e9c5a735e259d88274eaab597b
Richard Simões <rsimoes@gmail.com>
2023-05-03 11:59:54 -0500

    Remove the death overlay if present upon hit points being increased

--- src/module/encounter/combatant.ts
+++ src/module/encounter/combatant.ts
@@ -144,9 +144,11 @@ class CombatantPF2e<
     }
 
     /** Toggle the defeated status of this combatant, applying or removing the overlay icon on its token */
-    async toggleDefeated(): Promise<void> {
-        await this.update({ defeated: !this.defeated });
-        await this.token?.object?.toggleEffect(game.settings.get("pf2e", "deathIcon"), { overlay: true });
+    async toggleDefeated({ to = !this.isDefeated } = {}): Promise<void> {
+        if (to === this.isDefeated) return;
+
+        await this.update({ defeated: to });
+        await this.token?.object?.toggleEffect(game.settings.get("pf2e", "deathIcon"), { active: to, overlay: true });
 
         /** Remove this combatant's token as a target if it died */
         if (this.isDefeated && this.token?.object?.isTargeted) {
