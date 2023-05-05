25818b03af85511c0e628f2e84d8f3fa976b0bea
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2023-05-05 02:34:00 -0400

    Fix autofill options for regeneration deactivation (#7689)

--- src/module/item/sheet/rule-elements/fast-healing-form.ts
+++ src/module/item/sheet/rule-elements/fast-healing-form.ts
@@ -5,10 +5,10 @@ import { RuleElementForm, RuleElementFormSheetData } from "./base.ts";
 class FastHealingForm extends RuleElementForm<FastHealingSource, FastHealingRuleElement> {
     override template = "systems/pf2e/templates/items/rules/fast-healing.hbs";
     override activateListeners(html: HTMLElement): void {
-        // Tagify the selector list
+        // Tagify the selector list. Valid defaults should be the IWR weakness types
         const selectorElement = htmlQuery<HTMLInputElement>(html, ".deactivated-by");
         if (selectorElement) {
-            const whitelist = { ...CONFIG.PF2E.damageCategories, ...CONFIG.PF2E.damageTypes };
+            const whitelist = CONFIG.PF2E.weaknessTypes;
             tagify(selectorElement, { whitelist, enforceWhitelist: false });
         }
     }
