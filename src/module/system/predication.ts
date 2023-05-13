8916a8a05fce5358d3088d0e2e9b7a251b8b4008
Simon Ward <simonward@mac.com>
2023-05-13 00:08:59 -0700

    Prevent partial statements from including NaN in the values

--- src/module/system/predication.ts
+++ src/module/system/predication.ts
@@ -76,14 +76,21 @@ class PredicatePF2e extends Array<PredicateStatement> {
             // E.g., `{ "gt": ["actor:level", 5] }` would match against "actor:level:6" and "actor:level:7"
             const [left, right] = Object.values(statement)[0];
             const domainArray = Array.from(domain);
-            const leftValues =
-                typeof left === "number" || !Number.isNaN(Number(left))
-                    ? [Number(left)]
-                    : domainArray.flatMap((d) => (d.startsWith(left) ? Number(/:(-?\d+)$/.exec(d)?.[1]) : []));
-            const rightValues =
-                typeof right === "number" || !Number.isNaN(Number(right))
-                    ? [Number(right)]
-                    : domainArray.flatMap((d) => (d.startsWith(right) ? Number(/:(-?\d+)$/.exec(d)?.[1]) : []));
+
+            const getValues = (operand: string | number) => {
+                return typeof operand === "number" || !Number.isNaN(Number(operand))
+                    ? [Number(operand)]
+                    : domainArray.flatMap((d) => {
+                          if (d.startsWith(operand)) {
+                              const value = Number(/:(-?\d+)$/.exec(d)?.[1]);
+                              if (!Number.isNaN(value)) return value;
+                          }
+                          return [];
+                      });
+            };
+
+            const leftValues = getValues(left);
+            const rightValues = getValues(right);
 
             switch (operator) {
                 case "gt":
