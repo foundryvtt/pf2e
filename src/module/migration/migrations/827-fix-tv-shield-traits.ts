9d67fc20d7448c4bafd8268a1981319a508cc4b9
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-05-22 22:32:51 -0500

    Add some sanity checks to pair of migrations (#7963)
    Noticing some bad data appearing in user issue reports--probably from modules?

--- src/module/migration/migrations/827-fix-tv-shield-traits.ts
+++ src/module/migration/migrations/827-fix-tv-shield-traits.ts
@@ -1,4 +1,5 @@
 import { ItemSourcePF2e } from "@item/data/index.ts";
+import { isObject } from "@util";
 import { MigrationBase } from "../base.ts";
 
 /** Fix unannotated shield traits added from Lost Omens: Treasure Vault */
@@ -6,6 +7,12 @@ export class Migration827FixTVShieldTraits extends MigrationBase {
     static override version = 0.827;
 
     override async updateItem(source: ItemSourcePF2e): Promise<void> {
+        // Sanity check
+        const traits: unknown = source.system.traits;
+        if (isObject(traits) && "value" in traits && !Array.isArray(traits.value)) {
+            traits.value = [];
+        }
+
         if (source.type !== "armor") return;
 
         switch (source.system.slug) {
