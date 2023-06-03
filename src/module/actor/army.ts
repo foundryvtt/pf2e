247579d4be9095377ad6d089549aeed09e66bbfa
Cora <silvative@gmail.com>
2023-06-03 11:05:37 +0100

    Some of the sidebar fields seem to actually store their values now

--- src/module/actor/army.ts
+++ src/module/actor/army.ts
@@ -1,6 +1,31 @@
-// ************************** 
-//          IMPORTS
-// ************************** 
+/*
+// Array of expected statistics for populating sheet based on level, not finished obviously
+
+function buildStats {
+const level = 2
+const statisticsArrays = {
+    scoutingArray : [0, 7, 8, 9, 11, 12, 14, 15, 16, 18, 19, 21, 22, 23, 25, 26, 28, 29, 30, 32, 33],
+    standardDCArray : [0, 15, 16, 18, 19, 20, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40],
+    acArray : [0, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 33, 34, 36, 37, 39, 40, 42, 43, 45],
+    highsaveArray : [0, 10, 11, 12, 14, 15, 17, 18, 19, 21, 22, 24, 25, 26, 28, 29, 30, 32, 33, 35, 36],
+    lowsaveArray : [0, 4, 5, 6, 8, 9, 11, 12, 13, 15, 16, 18, 19, 20, 22, 23, 25, 26, 27, 29, 30],
+    maxtacticsArray : [0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6],
+    };
+const statistics = {
+    scouting : scoutingArray[level],
+    standardDC : standardDCArray[level],
+    ac : acArray[level],
+    highsave : highsaveArray[level],
+    lowsave : lowsaveArray[level],
+    maxtactics: maxtacticsArray[level],
+    };
+console.log(statistics);
+};
+*/
+
+// ************************** //
+//          IMPORTS           //
+// ************************** //
 
 import {
     ActorSystemData,
@@ -16,14 +41,13 @@ import { NPCStrike } from "@actor/npc";
 import { Rarity } from "@module/data";
 import { ActorAlliance } from "./types";
 
+// ************************** //
+//            TS              //
+// ************************** //
 
-// ************************** 
-//            TS
-// ************************** 
-
-    // ************************** 
-    //           ARMY
-    // ************************** 
+    // ************************** //
+    //           ARMY             //
+    // ************************** //
 
     class ArmyPF2e extends ActorPF2e {
 
@@ -54,7 +78,7 @@ import { ActorAlliance } from "./types";
             maneuver: {
                 value: number;
             };
-        morale: {
+            morale: {
                 value: number;
             };
         };
