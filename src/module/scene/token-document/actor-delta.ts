b03d211f8ffd0e0c63dd29a4e93be749d0fdf8da
Richard Simões <rsimoes@gmail.com>
2023-06-10 17:34:22 -0500

    Prevent `ActorDelta`s from preparing item data shared with linked actors

--- src/module/scene/token-document/actor-delta.ts
+++ src/module/scene/token-document/actor-delta.ts
@@ -3,7 +3,16 @@ import { TokenDocumentPF2e } from "./document.ts";
 class ActorDeltaPF2e<TParent extends TokenDocumentPF2e | null> extends ActorDelta<TParent> {
     override prepareData(): void {
         super.prepareData();
-        if (!game.ready && !this.parent?.isLinked) this.syntheticActor?.reset();
+        if (!game.ready && this.parent && !this.parent.isLinked) {
+            this.syntheticActor?.reset();
+        }
+    }
+
+    /** Prevent data preparation of embedded documents shared with linked actor */
+    override prepareEmbeddedDocuments(): void {
+        if (!this.parent?.isLinked) {
+            super.prepareEmbeddedDocuments();
+        }
     }
 }
 
