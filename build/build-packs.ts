ce33556fe3b427dd80ff8198d353034185eecc6e
In3luki <41452412+In3luki@users.noreply.github.com>
2023-06-13 19:47:47 +0200

    Add JSON pack files to build artifacts

--- build/build-packs.ts
+++ build/build-packs.ts
@@ -3,13 +3,14 @@ import path from "path";
 import url from "url";
 import { CompendiumPack, PackError } from "./lib/compendium-pack.ts";
 
+const asJson = process.argv[2]?.toLowerCase() === "json";
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const packsDataPath = path.resolve(__dirname, "../packs");
 const packDirPaths = fs.readdirSync(packsDataPath).map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
 
 // Loads all packs into memory for the sake of making all document name/id mappings available
 const packs = packDirPaths.map((p) => CompendiumPack.loadJSON(p));
-const documentCounts = await Promise.all(packs.map((p) => p.save()));
+const documentCounts = await Promise.all(packs.map((p) => p.save(asJson)));
 const total = documentCounts.reduce((total, c) => total + c, 0);
 
 if (documentCounts.length > 0) {
