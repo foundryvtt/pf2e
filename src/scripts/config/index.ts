e6028de7c1c37e7b4c04251b03efeaeb77cec843
Cora <silvative@gmail.com>
2023-06-17 03:01:47 +0100

    Rebuild sheet with system data and source matching + deal with fallout

--- src/scripts/config/index.ts
+++ src/scripts/config/index.ts
@@ -1,4 +1,4 @@
-import { CharacterPF2e, FamiliarPF2e, HazardPF2e, LootPF2e, NPCPF2e, PartyPF2e, VehiclePF2e, ArmyPF2e } from "@actor";
+import { ArmyPF2e, CharacterPF2e, FamiliarPF2e, HazardPF2e, LootPF2e, NPCPF2e, PartyPF2e, VehiclePF2e } from "@actor";
 import { SenseAcuity, SenseType } from "@actor/creature/sense.ts";
 import { Alignment } from "@actor/creature/types.ts";
 import { ActorType } from "@actor/data/index.ts";
@@ -78,7 +78,7 @@ const actorTypes: Record<ActorType, string> = {
     npc: "TYPES.Actor.npc",
     party: "TYPES.Actor.party",
     vehicle: "TYPES.Actor.vehicle",
-    army: "ACTOR.TypeArmy",
+    army: "TYPES.Actor.army",
 };
 
 const abilities: Record<AbilityString, string> = {
@@ -1284,7 +1284,6 @@ export const PF2ECONFIG = {
 
     Actor: {
         documentClasses: {
-            army: ArmyPF2e,
             character: CharacterPF2e,
             npc: NPCPF2e,
             hazard: HazardPF2e,
@@ -1292,6 +1291,7 @@ export const PF2ECONFIG = {
             familiar: FamiliarPF2e,
             party: PartyPF2e,
             vehicle: VehiclePF2e,
+            army: ArmyPF2e,
         },
     },
 
