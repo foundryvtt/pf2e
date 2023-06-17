40ab2b8bce5de4c837646b27596443d16710211e
Cora <silvative@gmail.com>
2023-06-17 03:00:56 +0100

    Add a bunch of new actor data for the gear and update fields

--- src/module/actor/army/army.ts
+++ src/module/actor/army/army.ts
@@ -28,165 +28,163 @@ console.log(statistics);
 // ************************** //
 
 import {
-    ActorSystemData,
     ActorSystemSource,
     BaseActorSourcePF2e,
-    GangUpCircumstance,
-    FlatFootableCircumstance
 } from "@actor/data/base.ts";
-import { ActorPF2e } from "@actor/base.ts";
 import { ActorSizePF2e } from "@actor/data/size.ts";
+import { TokenDocumentPF2e } from "@scene/index.ts";
 import { Rarity } from "@module/data.ts";
-import { ActorAlliance } from "../types.ts";
-import { Alignment } from "../creature/index.ts";
+import { ActorAlliance } from "@actor/types.ts";
+import { Alignment } from "@actor/creature/index.ts";
+import { ActorPF2e, HitPointsSummary } from "../base.ts";
+
 
 // ************************** //
 //            TS              //
 // ************************** //
 
-    // ************************** //
-    //           ARMY             //
-    // ************************** //
+class ArmyPF2e<TParent extends TokenDocumentPF2e | null = TokenDocumentPF2e | null> extends ActorPF2e<TParent> {
+}
 
-    class ArmyPF2e extends ActorPF2e {
+interface ArmyPF2e<TParent extends TokenDocumentPF2e | null = TokenDocumentPF2e | null> extends ActorPF2e<TParent> {
+    _source: ArmySource;
+    system: ArmySystemSource;
 
-        get rarity(): Rarity {
-            return this.system.traits.rarity;
+    get hitPoints(): HitPointsSummary;
+}
+
+type ArmySource = BaseActorSourcePF2e<"army", ArmySystemSource>;
+    
+interface ArmySystemSource extends ActorSystemSource {
+
+    attributes: {
+
+        immunities: never;
+        weaknesses: never;
+        resistances: never;
+        perception?: never;
+        initiative?: never;
+        shield?: never;
+        flanking: {
+            canFlank: never;
+            canGangUp: never;
+            flankable: never;
+            flatFootable: never;
+        };
+    
+        hp: {
+            value: number;
+            max: number;
+            temp: number;
+            details: string;
+            negativeHealing: boolean;
         };
 
-        override prepareBaseData(): void {
-            super.prepareBaseData();
-            this.system.traits.value = [];
-            this.system.traits.size = new ActorSizePF2e({ value: "med" });
-        };    
-    }
-
-    interface ArmyPF2e extends ActorPF2e {
-        _source: ArmySource;
-        system: ArmySystemData;
-    }
-
-    type ArmySource = BaseActorSourcePF2e<"army", ArmySystemSource>;
-    interface ArmySystemSource extends ActorSystemSource {
-
-        attributes: {
-
-            immunities?: never;
-            weaknesses?: never;
-            resistances?: never;
-            perception?: never;
-            initiative?: never;
-            shield?: never;
-            flanking: {
-                canFlank: boolean;
-                canGangUp: GangUpCircumstance[];
-                flankable: boolean;
-                flatFootable: FlatFootableCircumstance;
-            };
-        
-            hp: {
-                value: number;
-                max: number;
-                temp: number;
-                details: string;
-            };
-    
-            ac: {
-                value: number;
-                details: string;
-                magic: 0 | 1 | 2 | 3;
-            };
-    
-            scouting: {
-                value: number;
-                details: string;
-                darkvision: boolean;
-            };
-    
-            strikes: {
-                melee: {
-                    bonus: number;
-                    name: string;
-                    magic: 0 | 1 | 2 | 3;
-                };
-                ranged: {
-                    bonus: number;
-                    name: string;
-                    magic: 0 | 1 | 2 | 3;
-                };
-            };    
-        };
-
-        saves: {            
-            maneuver: number;
-            morale: number;
+        ac: {
+            value: number;
             details: string;
         };
 
-        details: {
-            level: { value: number; }
-            alignment: Alignment;
-            description: string;
-            blurb: string;
-            potions : number;
-            recruitmentDC: number;
-            consumption: number;
+        scouting: {
+            value: number;
+            details: string;
+            darkvision: boolean;
         };
 
-        conditions: {
-            visibility : "clear" | "dim" | "dark";
-            range : "engaged" | "near" | "distant";
-            status : "OK" | "defeated" | "destroyed";
-            position : "OK" | "outflanked" | "pinned";
-            difficultterrain : boolean;
-            wind : boolean;
-            concealed : boolean;
-            efficient : boolean;
-            fortified : boolean;
-            lost : boolean;
-            mired : number;
-            shaken : number; //1-4, 4 makes you routed
-            routed : boolean;
-            weary : number;
+    };
+
+    saves: {            
+        maneuver: number;
+        morale: number;
+        details: string;
+    };
+
+    details: {
+        alliance: ActorAlliance;
+        level: { value: number; };
+        alignment: Alignment;
+        description: string;
+        blurb: string;
+        recruitmentDC: number;
+        consumption: number;
+    };
+
+    gear : {
+        potions : {
+            name: "Healing Potions";
+            traits: ["Army", "Consumable", "Healing", "Magical", "Necromancy", "Potion"];
+            description: "An army equipped with healing potions (these rules are the same if you instead supply the army with alchemical healing elixirs) can use a single dose as part of any Maneuver action. When an army uses a dose of healing potions, it regains 1 HP. An army can be outfitted with up to 3 doses of healing potions at a time; unlike ranged Strike shots, healing potion doses do not automatically replenish after a war encounter—new doses must be purchased.";
+            price: 15;
+            unlocked: 0 | 1 | 2 | 3;
         }
-
-        traits: {
-            rarity: Rarity;
-            armytype: "infantry" | "cavalry" | "skirmishers" | "siege";
-            size: ActorSizePF2e;
-            value: never[];
-        };
-
+        armor : {
+            magic: {
+                name: "Magical Armor";
+                traits: ["Abjuration", "Army", "Magical"];
+                description: "Magic armor is magically enchanted to bolster the protection it affords to the soldiers.";
+                rank: "Mundane Armor" | "Magic Armor" | "Greater Magic Armor" | "Major Magic Armor";
+                level: 0 | 5 | 11 | 18;
+                price: 0 | 25 | 50 | 75;
+                bonus: 0 | 1 | 2 | 3;
+            }
+        }
+        melee : {
+            unlocked: boolean;
+            name: string;
+            bonus: number;
+            magic: {
+                name: "Magic Weapons";
+                traits: ["Army", "Evocation", "Magical"];
+                description: "The army's weapons are magic. If the army has melee and ranged weapons, choose which one is made magic when this gear is purchased. You can buy this gear twice—once for melee weapons and once for ranged weapons. If you purchase a more powerful version, it replaces the previous version, and the RP cost of the more powerful version is reduced by the RP cost of the replaced weapons.";
+                rank: "Mundane Weapons" | "Magic Weapons" | "Greater Magic Weapons" | "Major Magic Weapons";
+                level: 0 | 2 | 10 | 16;
+                price: 0 | 20 | 40 | 60;
+                bonus: 0 | 1 | 2 | 3;
+            }
+        }
+        ranged : {
+            unlocked: boolean;
+            name: string;
+            bonus: number;
+            magic: {
+                name: "Magic Weapons";
+                traits: ["Army", "Evocation", "Magical"];
+                description: "The army's weapons are magic. If the army has melee and ranged weapons, choose which one is made magic when this gear is purchased. You can buy this gear twice—once for melee weapons and once for ranged weapons. If you purchase a more powerful version, it replaces the previous version, and the RP cost of the more powerful version is reduced by the RP cost of the replaced weapons.";
+                rank: "Mundane Weapons" | "Magic Weapons" | "Greater Magic Weapons" | "Major Magic Weapons";
+                level: 0 | 2 | 10 | 16;
+                price: 0 | 20 | 40 | 60;
+                bonus: 0 | 1 | 2 | 3;
+            }
+        }
     }
 
-    interface ArmySystemData extends ActorSystemData {
-        attributes: {
-            immunities: never[];
-            weaknesses: never[];
-            resistances: never[];
-            flanking: {
-                /** Whether the actor can flank at all */
-                canFlank: boolean;
-                /** Given the actor can flank, the conditions under which it can do so without an ally opposite the target */
-                canGangUp: GangUpCircumstance[];
-                /** Whether the actor can be flanked at all */
-                flankable: boolean;
-                /** Given the actor is flankable, whether it is flat-footed when flanked */
-                flatFootable: FlatFootableCircumstance;
-            };
-        
-        };
-        details: {
-            alliance: ActorAlliance;
-            level: { value: number };
-            potions : number;
-        };
-        traits: {
-            rarity: Rarity;
-            armytype: "infantry" | "cavalry" | "skirmishers" | "siege";
-            size: ActorSizePF2e;
-            value: never[];
-        };
+    conditions: {
+        visibility : "clear" | "dim" | "dark";
+        range : "engaged" | "near" | "distant";
+        status : "OK" | "defeated" | "destroyed";
+        position : "OK" | "outflanked" | "pinned";
+        difficultterrain : boolean;
+        wind : boolean;
+        concealed : boolean;
+        efficient : boolean;
+        fortified : boolean;
+        lost : boolean;
+        mired : number;
+        shaken : number; //1-4, 4 makes you routed
+        routed : boolean;
+        weary : number;
+    }
+
+    traits: {
+        rarity: Rarity;
+        armytype: "infantry" | "cavalry" | "skirmishers" | "siege";
+        size: ActorSizePF2e;
+        value: never[];
     };
+
+    tokenEffects : [];
+    autoChanges : {};
+
+}
     
-    export {ArmyPF2e, ArmySource };
+export {ArmyPF2e, ArmySource };
