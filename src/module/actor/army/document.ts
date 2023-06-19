cc09d124b272daff2cc1728b940913938aa86f56
Richard Simões <rsimoes@gmail.com>
2023-06-18 23:55:01 -0500

    Slightly reduce insanity of army stuff

--- src/module/actor/army/document.ts
+++ src/module/actor/army/document.ts
@@ -1,28 +1,3 @@
-/*
-// Array of expected statistics for populating sheet based on level, not finished obviously
-
-function buildStats {
-const level = 2
-const statisticsArrays = {
-    scoutingArray : [0, 7, 8, 9, 11, 12, 14, 15, 16, 18, 19, 21, 22, 23, 25, 26, 28, 29, 30, 32, 33],
-    standardDCArray : [0, 15, 16, 18, 19, 20, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40],
-    acArray : [0, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 33, 34, 36, 37, 39, 40, 42, 43, 45],
-    highsaveArray : [0, 10, 11, 12, 14, 15, 17, 18, 19, 21, 22, 24, 25, 26, 28, 29, 30, 32, 33, 35, 36],
-    lowsaveArray : [0, 4, 5, 6, 8, 9, 11, 12, 13, 15, 16, 18, 19, 20, 22, 23, 25, 26, 27, 29, 30],
-    maxtacticsArray : [0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6],
-    };
-const statistics = {
-    scouting : scoutingArray[level],
-    standardDC : standardDCArray[level],
-    ac : acArray[level],
-    highsave : highsaveArray[level],
-    lowsave : lowsaveArray[level],
-    maxtactics: maxtacticsArray[level],
-    };
-console.log(statistics);
-};
-*/
-
 import { ActorSystemSource, BaseActorSourcePF2e } from "@actor/data/base.ts";
 import { ActorSizePF2e } from "@actor/data/size.ts";
 import { TokenDocumentPF2e } from "@scene/index.ts";
@@ -161,7 +136,7 @@ interface ArmySystemSource extends ActorSystemSource {
 
     traits: {
         rarity: Rarity;
-        armytype: "infantry" | "cavalry" | "skirmishers" | "siege";
+        type: "infantry" | "cavalry" | "skirmishers" | "siege";
         size: ActorSizePF2e;
         value: never[];
     };
