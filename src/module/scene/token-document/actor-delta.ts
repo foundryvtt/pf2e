7b9d2c5fdb1d9fcd643fae19151571e51e3b290f
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-06-20 15:34:05 -0500

    Remove 11.301 `ActorDelta` workaround, add new one for 11.302 (#8372)

--- src/module/scene/token-document/actor-delta.ts
+++ src/module/scene/token-document/actor-delta.ts
@@ -3,16 +3,6 @@ import { ItemPF2e } from "@item";
 import { TokenDocumentPF2e } from "./document.ts";
 
 class ActorDeltaPF2e<TParent extends TokenDocumentPF2e | null> extends ActorDelta<TParent> {
-    override _initialize(options?: Record<string, unknown>): void {
-        // Do absolutely nothing if the parent token is linked
-        if (!this.parent?.isLinked) {
-            super._initialize(options);
-            // If it is unlinked, ensure the synthetic actor performs full data reinitialization
-            // https://github.com/foundryvtt/foundryvtt/issues/9465
-            if (game.ready) this.syntheticActor?.reset();
-        }
-    }
-
     // Upstream calls _initialize: reset after to ensure clean data
     override prepareData(): void {
         super.prepareData();
@@ -21,11 +11,9 @@ class ActorDeltaPF2e<TParent extends TokenDocumentPF2e | null> extends ActorDelt
         }
     }
 
-    /** Prevent data preparation of embedded documents shared with linked actor */
+    /** The delta has no business preparing its items */
     override prepareEmbeddedDocuments(): void {
-        if (!this.parent?.isLinked) {
-            super.prepareEmbeddedDocuments();
-        }
+        return;
     }
 
     /** Following synthetic actor  updates, send the `Token` a fake update notification to trigger redraws */
