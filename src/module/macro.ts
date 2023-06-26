d646323274f56910a5880c352fb6c04bc32beafc
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-06-25 21:01:46 -0500

    Remove `Macro` script execution override, add `MacroSchema` type (#8421)
    The method was made #private in V11, so we're locked out.

--- src/module/macro.ts
+++ src/module/macro.ts
@@ -1,30 +1,6 @@
-import { ChatMessagePF2e } from "./chat-message/index.ts";
-
 export class MacroPF2e extends Macro {
     /** Raise permission requirement of world macro visibility to observer */
     override get visible(): boolean {
         return this.permission >= CONST.DOCUMENT_OWNERSHIP_LEVELS.OBSERVER;
-    }
-
-    /** Allow unbound variables to be shadowed in script's evaluation scope */
-    protected override _executeScript({
-        actor,
-        token,
-    }: { actor?: Actor<TokenDocument<Scene | null> | null>; token?: Token | null } = {}): void {
-        // Add variables to the evaluation scope
-        const speaker = ChatMessagePF2e.getSpeaker();
-        const character = game.user.character;
-        actor ??= game.actors.get(speaker.actor ?? "");
-        token ??= canvas.ready ? canvas.tokens.get(speaker.token ?? "") : null;
-
-        // Attempt script execution
-        const AsyncFunction = async function () {}.constructor as { new (...args: string[]): Function };
-        const command = ["{", this.command, "}"].join("\n");
-        const fn = new AsyncFunction("speaker", "actor", "token", "character", command);
-        try {
-            return fn.call(this, speaker, actor, token, character);
-        } catch {
-            ui.notifications.error("There was an error in your macro syntax. See the console (F12) for details");
-        }
     }
 }
