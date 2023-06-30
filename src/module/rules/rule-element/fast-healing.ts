b8258ef74e5cefc41a571b6d22bdd41cbf2bcbdd
In3luki <41452412+In3luki@users.noreply.github.com>
2023-06-30 16:56:37 +0200

    Add schemas to `DexterityModifierCapRuleElement` and `FastHealingRuleElement`

--- src/module/rules/rule-element/fast-healing.ts
+++ src/module/rules/rule-element/fast-healing.ts
@@ -1,49 +1,47 @@
 import { ActorType } from "@actor/data/index.ts";
 import { ChatMessagePF2e } from "@module/chat-message/index.ts";
 import { DamageRoll } from "@system/damage/roll.ts";
-import { localizeList, objectHasKey, tupleHasValue } from "@util";
-import { RuleElementData, RuleElementOptions, RuleElementPF2e, RuleElementSource } from "./index.ts";
+import { localizeList, objectHasKey } from "@util";
+import { RuleElementOptions, RuleElementPF2e, RuleElementSchema, RuleElementSource } from "./index.ts";
+import type { ArrayField, StringField } from "types/foundry/common/data/fields.d.ts";
+import { ResolvableValueField } from "./data.ts";
 
 /**
  * Rule element to implement fast healing and regeneration.
  * Creates a chat card every round of combat.
  * @category RuleElement
  */
-class FastHealingRuleElement extends RuleElementPF2e implements FastHealingData {
+class FastHealingRuleElement extends RuleElementPF2e<FastHealingRuleSchema> {
     static override validActorTypes: ActorType[] = ["character", "npc", "familiar"];
 
-    type: "fast-healing" | "regeneration";
-
-    deactivatedBy: string[];
-
-    #details?: string;
-
-    constructor(data: FastHealingSource, options: RuleElementOptions) {
-        data.type ??= "fast-healing";
-
-        super(data, options);
-
-        this.deactivatedBy =
-            Array.isArray(data.deactivatedBy) && data.deactivatedBy.every((db): db is string => typeof db === "string")
-                ? data.deactivatedBy
-                : [];
-
-        if (tupleHasValue(["fast-healing", "regeneration"] as const, data.type)) {
-            this.type = data.type;
-        } else {
-            this.type = "fast-healing";
-            this.failValidation("FastHealing only supports fast-healing or regeneration types");
-        }
-
-        if (typeof data.details === "string") this.#details = data.details;
+    static override defineSchema(): FastHealingRuleSchema {
+        const { fields } = foundry.data;
+        return {
+            ...super.defineSchema(),
+            value: new ResolvableValueField({ required: true, nullable: false }),
+            type: new fields.StringField({
+                required: false,
+                nullable: false,
+                choices: ["fast-healing", "regeneration"],
+                initial: "fast-healing",
+            }),
+            details: new fields.StringField({
+                required: false,
+                nullable: false,
+                initial: undefined,
+            }),
+            deactivatedBy: new fields.ArrayField(
+                new fields.StringField({ required: true, nullable: false, blank: false })
+            ),
+        };
     }
 
-    get details(): string | null {
-        if (this.#details) {
-            return game.i18n.localize(this.#details);
-        }
+    constructor(data: RuleElementSource, options: RuleElementOptions) {
+        super(data, options);
 
-        if (this.deactivatedBy.length > 0) {
+        if (this.details) {
+            this.details = game.i18n.localize(this.details);
+        } else if (this.deactivatedBy.length > 0) {
             const typesArr = this.deactivatedBy.map((type) =>
                 objectHasKey(CONFIG.PF2E.weaknessTypes, type)
                     ? game.i18n.localize(CONFIG.PF2E.weaknessTypes[type])
@@ -51,25 +49,24 @@ class FastHealingRuleElement extends RuleElementPF2e implements FastHealingData 
             );
 
             const types = localizeList(typesArr);
-            return game.i18n.format("PF2E.Encounter.Broadcast.FastHealing.DeactivatedBy", { types });
+            this.details = game.i18n.format("PF2E.Encounter.Broadcast.FastHealing.DeactivatedBy", { types });
+        } else {
+            this.details = this.getReducedLabel();
         }
-
-        return this.getReducedLabel();
     }
 
     /** Send a message with a "healing" (damage) roll at the start of its turn */
     override async onTurnStart(): Promise<void> {
         if (!this.test()) return;
 
-        const value = this.resolveValue();
-        if (typeof value !== "number") {
-            return this.failValidation("Healing requires a non-zero value field or a formula field");
+        const value = this.resolveValue(this.value);
+        if (typeof value !== "number" && typeof value !== "string") {
+            return this.failValidation("value must be a number or a roll formula");
         }
 
         const roll = (await new DamageRoll(`${value}`).evaluate({ async: true })).toJSON();
         const receivedMessage = game.i18n.localize(`PF2E.Encounter.Broadcast.FastHealing.${this.type}.ReceivedMessage`);
-        const details = this.details;
-        const postFlavor = details ? `<div data-visibility="owner">${details}</div>` : "";
+        const postFlavor = `<div data-visibility="owner">${this.details}</div>`;
         const flavor = `<div>${receivedMessage}</div>${postFlavor}`;
         const rollMode = this.actor.hasPlayerOwner ? "publicroll" : "gmroll";
         const speaker = ChatMessagePF2e.getSpeaker({ actor: this.actor, token: this.token });
@@ -77,16 +74,19 @@ class FastHealingRuleElement extends RuleElementPF2e implements FastHealingData 
     }
 }
 
-interface FastHealingData extends RuleElementData {
-    type: "fast-healing" | "regeneration";
-    details?: string | null;
-    deactivatedBy: string[];
-}
+type FastHealingRuleSchema = RuleElementSchema & {
+    value: ResolvableValueField<true, false, false>;
+    type: StringField<FastHealingType, FastHealingType, false, false, true>;
+    details: StringField<string, string, false, false, false>;
+    deactivatedBy: ArrayField<StringField<string, string, true, false, false>>;
+};
 
-interface FastHealingSource extends RuleElementSource {
-    type?: unknown;
-    details?: unknown;
-    deactivatedBy?: unknown;
-}
+interface FastHealingRuleElement
+    extends RuleElementPF2e<FastHealingRuleSchema>,
+        ModelPropsFromSchema<FastHealingRuleSchema> {}
 
-export { FastHealingData, FastHealingRuleElement, FastHealingSource };
+type FastHealingType = "fast-healing" | "regeneration";
+
+type FastHealingSource = SourceFromSchema<FastHealingRuleSchema>;
+
+export { FastHealingRuleElement, FastHealingSource, FastHealingType };
