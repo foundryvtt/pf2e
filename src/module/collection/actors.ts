d79936bb09c3ca4edfed1d7785ccf3b8e640fe39
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-07-02 20:13:00 -0500

    Update NPM dependencies, remove workaround of V9/V10 core issue (#8530)
    * Update NPM dependencies, remove workaround of V9/V10 core issue
    
    * Remove eslint-plugin-import and eslint-import-resolver-typescript
    
    Chonker of a dependency and isn't really doing much for us at this point

--- src/module/collection/actors.ts
+++ src/module/collection/actors.ts
@@ -1,5 +1,4 @@
 import { ActorPF2e, PartyPF2e } from "@actor";
-import { ActorUpdateContext } from "@actor/base.ts";
 
 export class ActorsPF2e<TActor extends ActorPF2e<null>> extends Actors<TActor> {
     /** The world's active party, if one exists */
@@ -13,33 +12,5 @@ export class ActorsPF2e<TActor extends ActorPF2e<null>> extends Actors<TActor> {
         return super
             ._getVisibleTreeContents()
             .filter((a) => (a.isOfType("creature") && !a.parties.size) || !a.isOfType("party", "creature"));
-    }
-
-    /** Work around a bug as of Foundry V9.242 in which token default settings are ignored for compendium imports */
-    override fromCompendium(actor: TActor | TActor["_source"], options?: FromCompendiumOptions): TActor["_source"] {
-        const defaultToken = deepClone(game.settings.get("core", "defaultToken"));
-        delete defaultToken.disposition;
-
-        if (actor instanceof ActorPF2e) {
-            return super.fromCompendium(actor.clone({ prototypeToken: defaultToken }, { keepId: true }), options);
-        } else {
-            return super.fromCompendium(mergeObject(actor, { token: defaultToken }, { inplace: false }), options);
-        }
-    }
-
-    /** Ditto */
-    override async importFromCompendium(
-        pack: CompendiumCollection<TActor>,
-        actorId: string,
-        updateData?: DocumentUpdateData<TActor>,
-        options?: ActorUpdateContext<null>
-    ): Promise<TActor | null> {
-        const actor = await super.importFromCompendium(pack, actorId, updateData, options);
-        if (!actor) return actor;
-
-        const defaultToken = deepClone(game.settings.get("core", "defaultToken"));
-        delete defaultToken.disposition;
-
-        return actor.clone({ prototypeToken: defaultToken }, { keepId: true });
     }
 }
