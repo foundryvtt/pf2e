0f6926232a7d99df5dfe575849fe11806becac07
Cora <silvative@gmail.com>
2023-07-06 21:15:20 +0100

    begin adding localisation fields to the new file

--- src/module/actor/army/values.ts
+++ src/module/actor/army/values.ts
@@ -9,4 +9,30 @@ const ARMY_STATS = {
     maxTactics : [0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6],
 }
 
-export { ARMY_STATS }
+/*
+const ARMY_GEAR_WEAPONS = {
+    name : game.i18n.localize("PF2E.Warfare.Gear.Weapons.name"),
+    traits : game.i18n.localize("PF2E.Warfare.Gear.Weapons.traits"),
+    description : game.i18n.localize("PF2E.Warfare.Gear.Weapons.description"),
+    level : [0, 2, 10, 16],
+    price : [0, 20, 40, 60],
+}
+
+const ARMY_GEAR_POTIONS = {
+    name : game.i18n.localize("PF2E.Warfare.Gear.Potions.name"),
+    traits : game.i18n.localize("PF2E.Warfare.Gear.Potions.traits"),
+    description : game.i18n.localize("PF2E.Warfare.Gear.Potions.description"),
+    level : [0],
+    price : [15],
+}
+
+const ARMY_GEAR_ARMOR = {
+    name : game.i18n.localize("PF2E.Warfare.Gear.Armor.name"),
+    traits : game.i18n.localize("PF2E.Warfare.Gear.Armor.traits"),
+    description : game.i18n.localize("PF2E.Warfare.Gear.Armor.description"),
+    level : [0, 5, 11, 18],
+    price : [0, 25, 50, 75],
+}
+*/
+
+export { ARMY_STATS, /*ARMY_GEAR_WEAPONS, ARMY_GEAR_POTIONS, ARMY_GEAR_ARMOR*/ }
