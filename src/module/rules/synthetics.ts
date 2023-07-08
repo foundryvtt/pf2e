9b6b000c6e0e0e20f233c4b293e3db3f603cb8eb
Richard Simões <rsimoes@gmail.com>
2023-07-08 12:32:32 -0500

    Restore RE-based token effect icons in `ActorPF2e#temporaryEffects`

--- src/module/rules/synthetics.ts
+++ src/module/rules/synthetics.ts
@@ -3,6 +3,7 @@ import { DexterityModifierCapData } from "@actor/character/types.ts";
 import { LabeledSpeed } from "@actor/creature/data.ts";
 import { CreatureSensePF2e } from "@actor/creature/sense.ts";
 import { DamageDicePF2e, DeferredPromise, DeferredValue, ModifierAdjustment, ModifierPF2e } from "@actor/modifiers.ts";
+import type { TokenEffect } from "@actor/token-effect.ts";
 import { MovementType } from "@actor/types.ts";
 import { MeleePF2e, WeaponPF2e } from "@item";
 import { ActionTrait } from "@item/action/index.ts";
@@ -41,6 +42,7 @@ interface RuleElementSynthetics {
     striking: Record<string, StrikingSynthetic[]>;
     targetMarks: Map<TokenDocumentUUID, string>;
     toggles: RollOptionToggle[];
+    tokenEffectIcons: TokenEffect[];
     tokenOverrides: DeepPartial<Pick<foundry.documents.TokenSource, "light" | "name" | "alpha">> & {
         texture?:
             | { src: VideoFilePath; tint?: HexColorString }
@@ -143,9 +145,9 @@ export {
     CritSpecEffect,
     DamageDiceSynthetics,
     DeferredDamageDice,
+    DeferredEphemeralEffect,
     DeferredModifier,
     DeferredMovementType,
-    DeferredEphemeralEffect,
     MAPSynthetic,
     ModifierAdjustmentSynthetics,
     ModifierSynthetics,
