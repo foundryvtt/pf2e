a98c37cc76318a81f694e48681078b51a34ada66
In3luki <41452412+In3luki@users.noreply.github.com>
2023-07-08 16:39:24 +0200

    Add schemas to `MultipleAttackPenaltyRuleElement`, `RollTwiceRuleElement` and `SenseRuleElement`

--- src/module/rules/rule-element/roll-twice.ts
+++ src/module/rules/rule-element/roll-twice.ts
@@ -1,45 +1,18 @@
 import { RollTwiceSynthetic } from "../synthetics.ts";
-import { RuleElementOptions, RuleElementPF2e } from "./base.ts";
-import { RuleElementSource } from "./index.ts";
+import { RuleElementPF2e } from "./base.ts";
+import { RuleElementSchema } from "./index.ts";
+import type { BooleanField, StringField } from "types/foundry/common/data/fields.d.ts";
 
 /** Roll Twice and keep either the higher or lower result */
-export class RollTwiceRuleElement extends RuleElementPF2e {
-    selector = "";
-
-    keep: "higher" | "lower" = "higher";
-
-    /** If the hosting item is an effect, remove or expire it after a matching roll is made */
-    removeAfterRoll = this.item.isOfType("effect");
-
-    constructor(data: RollTwiceSource, options: RuleElementOptions) {
-        super(data, options);
-
-        if (this.#isValid(data)) {
-            this.selector = this.resolveInjectedProperties(data.selector);
-            this.keep = data.keep;
-
-            const expireEffects = game.settings.get("pf2e", "automation.effectExpiration");
-            const removeExpired = game.settings.get("pf2e", "automation.removeExpiredEffects");
-
-            this.removeAfterRoll =
-                (expireEffects || removeExpired) && this.item.isOfType("effect")
-                    ? Boolean(data.removeAfterRoll ?? true)
-                    : false;
-        }
-    }
-
-    #isValid(data: RollTwiceSource): data is RollTwiceData {
-        if (!(typeof data.selector === "string" && data.selector)) {
-            this.failValidation("A Roll Twice rule element requires selector");
-            return false;
-        }
-
-        if (!(typeof data.keep === "string" && ["higher", "lower"].includes(data.keep))) {
-            this.failValidation('A Roll Twice rule element requires a "keep" property of either "higher" or "lower"');
-            return false;
-        }
-
-        return true;
+class RollTwiceRuleElement extends RuleElementPF2e<RollTwiceRuleSchema> {
+    static override defineSchema(): RollTwiceRuleSchema {
+        const { fields } = foundry.data;
+        return {
+            ...super.defineSchema(),
+            selector: new fields.StringField({ required: true, blank: false }),
+            keep: new fields.StringField({ required: true, choices: ["lower", "higher"] }),
+            removeAfterRoll: new fields.BooleanField({ required: false, initial: undefined }),
+        };
     }
 
     override beforePrepareData(): void {
@@ -57,8 +30,13 @@ export class RollTwiceRuleElement extends RuleElementPF2e {
             return;
         }
 
+        const expireEffects = game.settings.get("pf2e", "automation.effectExpiration");
+        const removeExpired = game.settings.get("pf2e", "automation.removeExpiredEffects");
+        const removeAfterRoll =
+            this.removeAfterRoll ?? ((expireEffects || removeExpired) && this.item.isOfType("effect"));
+
         const rolledTwice = roll?.dice.some((d) => ["kh", "kl"].some((m) => d.modifiers.includes(m))) ?? false;
-        if (!(rolledTwice && this.removeAfterRoll && selectors.includes(this.selector) && this.test(rollOptions))) {
+        if (!(rolledTwice && removeAfterRoll && selectors.includes(this.selector) && this.test(rollOptions))) {
             return;
         }
 
@@ -66,23 +44,23 @@ export class RollTwiceRuleElement extends RuleElementPF2e {
             rule.ignored = true;
         }
 
-        if (game.settings.get("pf2e", "automation.removeExpiredEffects")) {
+        if (removeExpired) {
             await this.item.delete();
-        } else if (game.settings.get("pf2e", "automation.effectExpiration")) {
+        } else if (expireEffects) {
             await this.item.update({ "system.duration.value": -1, "system.expired": true });
         }
     }
 }
 
-interface RollTwiceSource extends RuleElementSource {
-    selector?: unknown;
-    keep?: unknown;
-    removeAfterRoll?: unknown;
-}
+interface RollTwiceRuleElement
+    extends RuleElementPF2e<RollTwiceRuleSchema>,
+        ModelPropsFromSchema<RollTwiceRuleSchema> {}
 
-interface RollTwiceData {
-    key: "RollTwice";
-    selector: string;
-    keep: "higher" | "lower";
-    removeAfterRoll?: boolean;
-}
+type RollTwiceRuleSchema = RuleElementSchema & {
+    selector: StringField<string, string, true, false, false>;
+    keep: StringField<"higher" | "lower", "higher" | "lower", true, false, false>;
+    /** If the hosting item is an effect, remove or expire it after a matching roll is made */
+    removeAfterRoll: BooleanField<boolean, boolean, false, false, false>;
+};
+
+export { RollTwiceRuleElement };
