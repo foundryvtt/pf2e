330f40d072dd3fa1e28877ba55dfc5097a23fa56
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-07-09 23:29:13 -0500

    Handle RE-added traits that may affect actors' modes of being (#8638)
    Also check applicability of added traits

--- src/module/rules/rule-element/actor-traits.ts
+++ src/module/rules/rule-element/actor-traits.ts
@@ -1,7 +1,11 @@
-import { RuleElementOptions, RuleElementPF2e, RuleElementSchema, RuleElementSource } from "./index.ts";
+import { ActorType } from "@actor/data/index.ts";
+import { ErrorPF2e } from "@util";
 import type { ArrayField, StringField } from "types/foundry/common/data/fields.d.ts";
+import { RuleElementOptions, RuleElementPF2e, RuleElementSchema, RuleElementSource } from "./index.ts";
 
 class ActorTraitsRuleElement extends RuleElementPF2e<ActorTraitsRuleSchema> {
+    protected static override validActorTypes: ActorType[] = ["character", "npc", "familiar", "hazard", "vehicle"];
+
     static override defineSchema(): ActorTraitsRuleSchema {
         const { fields } = foundry.data;
         return {
@@ -15,23 +19,54 @@ class ActorTraitsRuleElement extends RuleElementPF2e<ActorTraitsRuleSchema> {
         super({ ...data, priority: 99 }, options);
     }
 
+    get #traitsDictionary(): Record<string, string> {
+        switch (this.actor.type) {
+            case "character":
+            case "familiar":
+            case "npc":
+                return CONFIG.PF2E.creatureTraits;
+            case "hazard":
+                return CONFIG.PF2E.hazardTraits;
+            case "vehicle":
+                return CONFIG.PF2E.vehicleTraits;
+            default:
+                throw ErrorPF2e("unexpected actor type");
+        }
+    }
+
     override beforePrepareData(): void {
         if (!this.test()) return;
 
         if (this.actor.system.traits) {
             const traits: { value: string[] } = this.actor.system.traits;
             const newTraits = this.resolveInjectedProperties(this.add).filter((t) => !traits.value.includes(t));
+            const traitsDictionary = this.#traitsDictionary;
             for (const trait of newTraits) {
+                if (!(trait in traitsDictionary)) {
+                    return this.failValidation(`${trait} is not a recognized trait`);
+                }
                 traits.value.push(trait);
                 this.actor.rollOptions.all[`self:trait:${trait}`] = true;
+                if (["construct", "undead"].includes(trait)) this.#handleModeAffectingTrait();
             }
 
             const toRemoves = this.resolveInjectedProperties(this.remove);
             for (const trait of toRemoves) {
                 traits.value = traits.value.filter((t) => t !== trait);
                 delete this.actor.rollOptions.all[`self:trait:${trait}`];
+                if (["construct", "undead"].includes(trait)) this.#handleModeAffectingTrait();
             }
         }
+    }
+
+    /** Handle the addition or removal of a trait that may affect the actor's mode of being */
+    #handleModeAffectingTrait(): void {
+        const { actor } = this;
+        const { rollOptions } = actor;
+        for (const mode of ["construct", "living", "undead"]) {
+            delete rollOptions.all[`self:mode:${mode}`];
+        }
+        rollOptions.all[`self:mode:${actor.modeOfBeing}`] = true;
     }
 }
 
