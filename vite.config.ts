4a4d92efe83492cdb673da85c8fc9833ef8b44dd
Richard Simões <rsimoes@gmail.com>
2023-07-18 22:02:45 -0500

    Run condition sources through `CompendiumPack` before interpolating

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import type { ConditionSource } from "@item/condition/data.ts";
+import { ConditionSource } from "@item/data/index.ts";
+import { execSync } from "child_process";
 import esbuild from "esbuild";
 import fs from "fs-extra";
 import path from "path";
@@ -8,15 +9,10 @@ import checker from "vite-plugin-checker";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
 import packageJSON from "./package.json" assert { type: "json" };
-import { sluggify } from "./src/util/misc.ts";
 
 const CONDITION_SOURCES = ((): ConditionSource[] => {
-    const filenames = fs.readdirSync("packs/conditions");
-    return filenames.map((filename) => {
-        const source = fs.readJSONSync(path.join("packs", "conditions", filename));
-        source.system.slug = sluggify(source.name);
-        return source;
-    });
+    const output = execSync("npm run build:conditions", { encoding: "utf-8" });
+    return JSON.parse(output.slice(output.indexOf("[")));
 })();
 
 const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
