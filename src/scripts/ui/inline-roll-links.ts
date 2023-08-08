51f96eee5a48cca90c5c868e307cfff4be13425d
dcoffee <40677907+dcoffee@users.noreply.github.com>
2023-08-07 20:31:39 -0400

    Support options: param for inline @Damage and @Check (#9173)

--- src/scripts/ui/inline-roll-links.ts
+++ src/scripts/ui/inline-roll-links.ts
@@ -81,7 +81,9 @@ export const InlineRollLinks = {
         }
 
         for (const link of links.filter((l) => l.dataset.pf2Check && !l.dataset.invalid)) {
-            const { pf2Check, pf2Dc, pf2Traits, pf2Label, pf2Defense, pf2Adjustment, pf2Roller } = link.dataset;
+            const { pf2Check, pf2Dc, pf2Traits, pf2Label, pf2Defense, pf2Adjustment, pf2Roller, pf2RollOptions } =
+                link.dataset;
+
             if (!pf2Check) return;
 
             link.addEventListener("click", async (event) => {
@@ -108,11 +110,10 @@ export const InlineRollLinks = {
 
                 if (actors.length === 0) return;
 
-                const extraRollOptions =
-                    pf2Traits
-                        ?.split(",")
-                        .map((o) => o.trim())
-                        .filter((o) => !!o) ?? [];
+                const extraRollOptions = [
+                    ...(pf2Traits?.split(",").map((o) => o.trim()) ?? []),
+                    ...(pf2RollOptions?.split(",").map((o) => o.trim()) ?? []),
+                ];
                 const eventRollParams = eventToRollParams(event);
 
                 switch (pf2Check) {
