ccbfe77c02af6a9b8898e974e3dfadcf8ac7d1a7
Vesselchuck <70449411+Vesselchuck@users.noreply.github.com>
2023-08-10 12:32:46 +0300

    Add files via upload

---
+++ src/module/system/action-macros/athletics/reposition.ts
@@ -0,0 +1,41 @@
+import { ActorPF2e } from "@actor";
+import { ActionMacroHelpers, SkillActionOptions } from "../index.ts";
+import { WeaponPF2e } from "@item";
+
+export function reposition(options: SkillActionOptions): void {
+    const slug = options?.skill ?? "athletics";
+    const rollOptions = ["action:reposition"];
+    ActionMacroHelpers.simpleRollActionCheck<WeaponPF2e<ActorPF2e>>({
+        actors: options.actors,
+        actionGlyph: options.glyph ?? "A",
+        title: "PF2E.Actions.Reposition.Title",
+        checkContext: (opts) => {
+            // weapon
+            const item = (ActionMacroHelpers.getApplicableEquippedWeapons(opts.actor, "reposition") ?? []).shift();
+
+            // modifiers
+            const modifiers = options.modifiers?.length ? [...options.modifiers] : [];
+            if (item && item.traits.has("reposition")) {
+                const modifier = ActionMacroHelpers.getWeaponPotencyModifier(item, slug);
+                if (modifier) {
+                    modifiers.push(modifier);
+                }
+            }
+
+            return ActionMacroHelpers.defaultCheckContext(opts, { item, modifiers, rollOptions, slug });
+        },
+        traits: ["attack"],
+        event: options.event,
+        callback: options.callback,
+        difficultyClass: options.difficultyClass,
+        difficultyClassStatistic: (target) => target.saves.fortitude,
+        extraNotes: (selector: string) => [
+            ActionMacroHelpers.note(selector, "PF2E.Actions.Reposition", "criticalSuccess"),
+            ActionMacroHelpers.note(selector, "PF2E.Actions.Reposition", "success"),
+            ActionMacroHelpers.note(selector, "PF2E.Actions.Reposition", "criticalFailure"),
+        ],
+    }).catch((error: Error) => {
+        ui.notifications.error(error.message);
+        throw error;
+    });
+}
