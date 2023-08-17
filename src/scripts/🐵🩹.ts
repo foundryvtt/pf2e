9303cd4edd1a5bd11815fc5c48833630c8d09c42
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-08-16 21:49:28 -0500

    Speed up `TextEditor._enrichContentLinks` (#9347)
    Upstream retrieves documents from UUID links sequentially, which has a noticable load time with text containing many
    links: retrieve every linked document at once beforehand with the faster `UUIDUtils.fromUUIDs` system helper so that
    subsequent calls to `fromUuid` finds all documents in caches.

--- src/scripts/🐵🩹.ts
+++ src/scripts/🐵🩹.ts
@@ -2,6 +2,7 @@ import { TextEditorPF2e } from "@system/text-editor.ts";
 
 export function monkeyPatchFoundry(): void {
     TextEditor.enrichHTML = TextEditorPF2e.enrichHTML;
+    TextEditor._enrichContentLinks = TextEditorPF2e._enrichContentLinks;
     TextEditor._createInlineRoll = TextEditorPF2e._createInlineRoll;
     TextEditor._onClickInlineRoll = TextEditorPF2e._onClickInlineRoll;
 }
