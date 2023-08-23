5730cfd43c496edf9cfff2b389db5b6609a9b977
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2023-08-23 11:57:12 -0400

    Enable roll notes for damage-received (#9497)

--- src/module/chat-message/helpers.ts
+++ src/module/chat-message/helpers.ts
@@ -96,6 +96,7 @@ async function applyDamageFromMessage({
         if (!messageRollOptions.some((o) => o.startsWith("target"))) {
             messageRollOptions.push(...token.actor.getSelfRollOptions("target"));
         }
+        const domain = multiplier > 0 ? "damage-received" : "healing-received";
         const ephemeralEffects =
             multiplier > 0
                 ? await extractEphemeralEffects({
@@ -103,7 +104,7 @@ async function applyDamageFromMessage({
                       origin: message.actor,
                       target: token.actor,
                       item: message.item,
-                      domains: multiplier > 0 ? ["damage-received"] : ["healing-received"],
+                      domains: [domain],
                       options: messageRollOptions,
                   })
                 : [];
@@ -117,7 +118,6 @@ async function applyDamageFromMessage({
         // target-specific healing adjustments
         const outcome = message.flags.pf2e.context?.outcome;
         const breakdown: string[] = [];
-        const notes: string[] = [];
         const rolls: Rolled<Roll>[] = [];
         if (typeof damage === "number" && damage < 0) {
             const critical = outcome === "criticalSuccess";
@@ -164,13 +164,16 @@ async function applyDamageFromMessage({
                     .filter((modifier) => modifier.enabled)
                     .map((modifier) => `${modifier.label} ${modifier.modifier < 0 ? "" : "+"}${modifier.modifier}`)
             );
+        }
 
-            const rollNotes = (contextClone.synthetics.rollNotes["healing-received"] ?? [])
+        const hasDamage = typeof damage === "number" ? damage !== 0 : damage.total !== 0;
+        const notes = (() => {
+            if (!hasDamage) return [];
+            return (contextClone.synthetics.rollNotes[domain] ?? [])
                 .filter((note) => !outcome || note.outcome.length === 0 || note.outcome.includes(outcome))
                 .filter((note) => note.predicate.test(applicationRollOptions))
                 .map((note) => note.text);
-            notes.push(...rollNotes);
-        }
+        })();
 
         await contextClone.applyDamage({
             damage,
