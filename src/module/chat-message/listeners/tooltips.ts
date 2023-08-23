ad3b9a27e5b1a96a13aa4c8b796a0312433b327b
Richard Simões <rsimoes@gmail.com>
2023-08-23 00:21:00 -0500

    Kill the jquery in chat tooltips listener

--- src/module/chat-message/listeners/tooltips.ts
+++ src/module/chat-message/listeners/tooltips.ts
@@ -1,20 +1,18 @@
-import { ErrorPF2e } from "@util";
+import { ErrorPF2e, htmlQuery, htmlQueryAll } from "@util";
 
 export const MessageTooltips = {
-    listen: ($html: JQuery): void => {
-        const html = $html[0];
-
+    listen: (html: HTMLElement): void => {
         // Remove entire .target-dc and .dc-result elements if they are empty after user-visibility processing
-        const targetDC = html.querySelector<HTMLElement>(".target-dc");
+        const targetDC = htmlQuery(html, ".target-dc");
         if (targetDC?.innerHTML.trim() === "") targetDC.remove();
-        const dcResult = html.querySelector<HTMLElement>(".dc-result");
+        const dcResult = htmlQuery(html, ".dc-result");
         if (dcResult?.innerHTML.trim() === "") dcResult.remove();
 
         // Check DC adjusted by circumstance bonuses or penalties
         try {
-            const $adjustedDC = $html.find(".adjusted[data-circumstances]");
-            if ($adjustedDC.length === 1) {
-                const circumstances = JSON.parse($adjustedDC.attr("data-circumstances") ?? "");
+            const adjustedDCLabel = htmlQuery(html, ".adjusted[data-circumstances]");
+            if (adjustedDCLabel) {
+                const circumstances = JSON.parse(adjustedDCLabel.dataset.circumstances ?? "");
                 if (!Array.isArray(circumstances)) throw ErrorPF2e("Malformed adjustments array");
 
                 const content = circumstances
@@ -25,33 +23,32 @@ export const MessageTooltips = {
                     .reduce(($concatted, $a) => $concatted.append($a), $("<div>"))
                     .prop("outerHTML");
 
-                $adjustedDC.tooltipster({ content, contentAsHTML: true, theme: "crb-hover" });
+                $(adjustedDCLabel).tooltipster({ content, contentAsHTML: true, theme: "crb-hover" });
             }
         } catch (error) {
             if (error instanceof Error) console.error(error.message);
         }
 
         // Adjusted check degree of success
-        const $degreeOfSuccess = $html.find(".degree-of-success .adjusted");
-        if ($degreeOfSuccess[0]?.dataset.adjustment) {
-            $degreeOfSuccess.tooltipster({
-                content: game.i18n.localize($degreeOfSuccess[0].dataset.adjustment),
+        const dosLabel = htmlQuery(html, ".degree-of-success .adjusted");
+        if (dosLabel?.dataset.adjustment) {
+            $(dosLabel).tooltipster({
+                content: game.i18n.localize(dosLabel.dataset.adjustment),
                 contentAsHTML: true,
                 theme: "crb-hover",
             });
         }
 
         // Trait and material tooltips
-        $html.find(".tag[data-material], .tag[data-slug], .tag[data-trait]").each((_idx, span) => {
-            const $tag = $(span);
-            const description = $tag.attr("data-description");
+        for (const tag of htmlQueryAll(html, ".tag[data-material], .tag[data-slug], .tag[data-trait]")) {
+            const description = tag.dataset.description;
             if (description) {
-                $tag.tooltipster({
+                $(tag).tooltipster({
                     content: game.i18n.localize(description),
                     maxWidth: 400,
                     theme: "crb-hover",
                 });
             }
-        });
+        }
     },
 };
