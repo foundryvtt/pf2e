737f9cf59041642791e689bf6c5884ac459ed1b0
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2023-08-23 00:49:43 -0400

    Filter parties from user configuration (#9488)

--- src/module/user/sheet.ts
+++ src/module/user/sheet.ts
@@ -2,6 +2,12 @@ import { UserPF2e } from "./document.ts";
 
 /** Player-specific settings, stored as flags on each User */
 export class UserConfigPF2e<TUser extends UserPF2e> extends UserConfig<TUser> {
+    override async getData(options: DocumentSheetOptions): Promise<UserConfigData<TUser>> {
+        const data = await super.getData(options);
+        data.actors = data.actors.filter((a) => a.type !== "party");
+        return data;
+    }
+
     override get template(): string {
         return "systems/pf2e/templates/user/sheet.hbs";
     }
