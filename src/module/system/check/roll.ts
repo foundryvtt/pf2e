c00de4a256a231109454af3889dd9a96beeb841f
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-08-24 02:39:54 -0500

    Fill out check data to be usable by damaging actions (#9533)
    This is a step towards being able to use the same roll class for strikes. It adds a dedicate template for `CheckRoll`s
    that includes action, damage, and other information.

--- src/module/system/check/roll.ts
+++ src/module/system/check/roll.ts
@@ -3,25 +3,54 @@ import { ZeroToThree } from "@module/data.ts";
 import { UserPF2e } from "@module/user/index.ts";
 import { DegreeOfSuccessIndex } from "@system/degree-of-success.ts";
 import { RollDataPF2e } from "@system/rolls.ts";
+import { CheckType } from "./types.ts";
 
 class CheckRoll extends Roll {
-    roller: UserPF2e | null;
+    static override CHAT_TEMPLATE = "systems/pf2e/templates/chat/check/roll.hbs";
 
-    isReroll: boolean;
+    get roller(): UserPF2e | null {
+        return game.users.get(this.options.rollerId ?? "") ?? null;
+    }
 
-    isRerollable: boolean;
+    get type(): CheckType {
+        return this.options.type ?? "check";
+    }
 
-    constructor(formula: string, data = {}, options: CheckRollDataPF2e = {}) {
-        super(formula, data, options);
-
-        this.isReroll = options.isReroll ?? false;
-        this.isRerollable =
-            !this.isReroll && !this.dice.some((d) => d.modifiers.includes("kh") || d.modifiers.includes("kl"));
-        this.roller = game.users.get(this.options.rollerId ?? "") ?? null;
+    /** A string of some kind to help system API identify the roll */
+    get identifier(): string | null {
+        return this.options.identifier ?? null;
     }
 
     get degreeOfSuccess(): DegreeOfSuccessIndex | null {
         return this.options.degreeOfSuccess ?? null;
+    }
+
+    get isReroll(): boolean {
+        return this.options.isReroll ?? false;
+    }
+
+    get isRerollable(): boolean {
+        return !this.isReroll && !this.dice.some((d) => d.modifiers.includes("kh") || d.modifiers.includes("kl"));
+    }
+
+    override async render(this: Rolled<CheckRoll>, options: RollRenderOptions = {}): Promise<string> {
+        if (!this._evaluated) await this.evaluate({ async: true });
+        const { isPrivate, flavor, template } = options;
+
+        const chatData: Record<string, unknown> = {
+            formula: isPrivate ? "???" : this._formula,
+            flavor: isPrivate ? null : flavor,
+            user: game.user.id,
+            tooltip: isPrivate ? "" : await this.getTooltip(),
+            total: isPrivate ? "?" : Math.round(this.total * 100) / 100,
+            identifier: this.options.identifier,
+            action: this.options.action,
+            degree: this.degreeOfSuccess,
+            damaging: this.options.damaging,
+            canRollDamage: this.roller === game.user || game.user.isGM,
+        };
+
+        return renderTemplate(template ?? CheckRoll.CHAT_TEMPLATE, chatData);
     }
 }
 
@@ -30,6 +59,9 @@ interface CheckRoll extends Roll {
 }
 
 interface CheckRollDataPF2e extends RollDataPF2e {
+    type?: CheckType;
+    identifier?: Maybe<string>;
+    action?: Maybe<string>;
     isReroll?: boolean;
     degreeOfSuccess?: ZeroToThree;
     strike?: StrikeLookupData;
