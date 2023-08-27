bfefbcc8fb7aa63a7e3e32cf81bd7b4809fc0449
Gunnar Busch <gunnar.busch@googlemail.com>
2023-08-27 20:06:34 +0200

    Add keybind to toggle party sheet

--- src/scripts/register-keybindings.ts
+++ src/scripts/register-keybindings.ts
@@ -1,9 +1,57 @@
+import type { ActorPF2e } from "@actor";
+
 export function registerKeybindings(): void {
     game.keybindings.register("pf2e", "cycle-token-stack", {
         name: "PF2E.Keybinding.CycleTokenStack.Label",
         hint: "PF2E.Keybinding.CycleTokenStack.Hint",
         editable: [{ key: "KeyZ", modifiers: [] }],
         onUp: (): boolean => canvas.tokens.cycleStack(),
+    });
+
+    game.keybindings.register("pf2e", "toggle-party-sheet", {
+        name: "PF2E.Keybinding.TogglePartySheet.Label",
+        hint: "PF2E.Keybinding.TogglePartySheet.Hint",
+        editable: [{ key: "KeyP", modifiers: [] }],
+        onDown: (): boolean | null => {
+            const activePartyId = game.settings.get("pf2e", "activeParty");
+            const party = ((): ActorPF2e | null => {
+                if (game.user.isGM) {
+                    const token =
+                        canvas.ready && canvas.tokens.controlled.length === 1 ? canvas.tokens.controlled[0] : null;
+                    if (token?.actor?.isOfType("party")) {
+                        // Controlled token is a party
+                        return token.actor;
+                    } else {
+                        // Active party
+                        return game.actors.get(activePartyId) ?? null;
+                    }
+                } else if (game.user.character?.isOfType("character")) {
+                    // User has an assigned character with an assigned party
+                    const parties = Array.from(game.user.character.parties);
+                    return parties.find((p) => p.active) ?? parties.at(0) ?? null;
+                } else {
+                    // Active party and has a member the player owns
+                    const activeParty = game.actors.get(activePartyId);
+                    return activeParty?.isOfType("party") && activeParty.members.some((m) => m.isOwner)
+                        ? activeParty
+                        : null;
+                }
+            })();
+            if (!party) return false;
+
+            const { sheet } = party;
+            if (sheet.rendered) {
+                if (sheet._minimized) {
+                    sheet.maximize();
+                } else {
+                    sheet.close();
+                }
+            } else {
+                sheet.render(true);
+            }
+
+            return true;
+        },
     });
 
     // Defer to the Perfect Vision module if enabled
