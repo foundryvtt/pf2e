ddafe59acf927da5bba4503790f88fe897be8fc5
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2023-09-08 01:05:33 -0400

    Update src/module/item/sheet/rule-elements/flat-modifier-form.ts

--- src/module/item/sheet/rule-elements/flat-modifier-form.ts
+++ src/module/item/sheet/rule-elements/flat-modifier-form.ts
@@ -11,7 +11,7 @@ class FlatModifierForm extends RuleElementForm<FlatModifierSource, FlatModifierR
 
     get isDamage(): boolean {
         const selectors = [this.rule.selector ?? []].flat();
-        return selectors.some((s) => s === "damage" || s.endsWith("-damage"));
+        return selectors.some((s) => s === "damage" || String(s).endsWith("-damage"));
     }
 
     override activateListeners(html: HTMLElement): void {
