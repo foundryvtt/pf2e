0a12907936b5ca79a1dae18dd8146337c4c217f1
Richard Simões <rsimoes@gmail.com>
2023-09-09 01:58:42 -0500

    Move `CoinsPF2e` to dedicated file

--- src/module/item/data/helpers.ts
+++ src/module/item/data/helpers.ts
@@ -1,7 +1,7 @@
-import { isObject, setHasElement } from "@util/misc.ts";
-import { ItemSourcePF2e, MagicItemSource, PhysicalItemSource } from "./index.ts";
-import { ItemSystemData } from "./base.ts";
 import { PHYSICAL_ITEM_TYPES } from "@item/physical/values.ts";
+import { isObject, setHasElement } from "@util/misc.ts";
+import { ItemSystemData } from "./base.ts";
+import { ItemSourcePF2e, MagicItemSource, PhysicalItemSource } from "./index.ts";
 
 function isItemSystemData(data: unknown): data is ItemSystemData {
     return (
