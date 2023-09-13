foundryvtt/pf2e

commit d790e236ec0dd4cbb38287fb3f21ec374218dcc9

Fix ancestry feats without an ancestry trait being filtered in the compendium browser feats tab (#10044)

src/module/apps/compendium-browser/tabs/feat.ts
@@ -2,7 +2,7 @@ import { isObject, sluggify } from "@util";
 import { CompendiumBrowser } from "../index.ts";
 import { ContentTabName } from "../data.ts";
 import { CompendiumBrowserTab } from "./base.ts";
-import { CompendiumBrowserIndexData, FeatFilters } from "./data.ts";
+import { CompendiumBrowserIndexData, FeatFilters, MultiselectData } from "./data.ts";
 
 export class CompendiumBrowserFeatTab extends CompendiumBrowserTab {
     tabName: ContentTabName = "feat";
@@ -13,11 +13,15 @@ export class CompendiumBrowserFeatTab extends CompendiumBrowserTab {
     override searchFields = ["name"];
     override storeFields = ["type", "name", "img", "uuid", "level", "category", "skills", "traits", "rarity", "source"];
 
+    #ancestryTraits: string[];
+
     constructor(browser: CompendiumBrowser) {
         super(browser);
 
         // Set the filterData object of this tab
         this.filterData = this.prepareFilterData();
+
+        this.#ancestryTraits = Object.keys(CONFIG.PF2E.ancestryTraits);
     }
 
     protected override async loadData(): Promise<void> {
@@ -131,6 +135,21 @@ export class CompendiumBrowserFeatTab extends CompendiumBrowserTab {
         this.filterData.multiselects.traits.options = this.generateMultiselectOptions({ ...CONFIG.PF2E.featTraits });
 
         console.debug("PF2e System | Compendium Browser | Finished loading feats");
+    }
+
+    protected override filterTraits(
+        traits: string[],
+        selected: MultiselectData["selected"],
+        condition: MultiselectData["conjunction"]
+    ): boolean {
+        // Always return true for ancestry feats that have no ancestry trait
+        if (
+            this.filterData.checkboxes.category.selected.includes("ancestry") &&
+            !this.arrayIncludes(traits, this.#ancestryTraits)
+        ) {
+            return true;
+        }
+        return super.filterTraits(traits, selected, condition);
     }
 
     protected override filterIndexData(entry: CompendiumBrowserIndexData): boolean {

commit d0b580e4f4b487ec021ad7237ac37884592fbaaf

Fix regression in feat tab trait filtering introduced by `#10044`

src/module/apps/compendium-browser/tabs/feat.ts
@@ -142,12 +142,13 @@ export class CompendiumBrowserFeatTab extends CompendiumBrowserTab {
         selected: MultiselectData["selected"],
         condition: MultiselectData["conjunction"]
     ): boolean {
-        // Always return true for ancestry feats that have no ancestry trait
+        // Pre-filter the selected traits if the current ancestry item has no ancestry traits
         if (
             this.filterData.checkboxes.category.selected.includes("ancestry") &&
             !this.arrayIncludes(traits, this.#ancestryTraits)
         ) {
-            return true;
+            const withoutAncestryTraits = selected.filter((t) => !this.#ancestryTraits.includes(t.value));
+            return super.filterTraits(traits, withoutAncestryTraits, condition);
         }
         return super.filterTraits(traits, selected, condition);
     }

src/scripts/config/traits.ts
@@ -53,6 +53,7 @@ const ancestryTraits = {
     oread: "PF2E.TraitOread",
     poppet: "PF2E.TraitPoppet",
     ratfolk: "PF2E.TraitRatfolk",
+    reflection: "PF2E.TraitReflection",
     shisk: "PF2E.TraitShisk",
     shoony: "PF2E.TraitShoony",
     skeleton: "PF2E.TraitSkeleton",