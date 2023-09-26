eee846cf1e78908f5b8ab2f2d738fa634eb4904a
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2023-09-25 21:50:16 -0400

    Migrate roll option domain from damage-roll to damage (#10342)

---
+++ src/module/migration/migrations/867-damage-roll-domain-fix.ts
@@ -0,0 +1,14 @@
+import type { ItemSourcePF2e } from "@item/data/index.ts";
+import { MigrationBase } from "../base.ts";
+
+export class Migration867DamageRollDomainFix extends MigrationBase {
+    static override version = 0.867;
+
+    override async updateItem(source: ItemSourcePF2e): Promise<void> {
+        for (const rule of source.system.rules ?? []) {
+            if ("domain" in rule && rule.domain === "damage-roll") {
+                rule.domain = "damage";
+            }
+        }
+    }
+}
