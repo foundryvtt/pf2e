5ae0f84b78f3294685ce55342e8a03c516bb90ce
Richard Simões <rsimoes@gmail.com>
2023-10-02 01:49:52 -0500

    Add schema definitions for `TileDocument` and `WallDocument`

--- types/foundry/common/documents/setting.d.ts
+++ types/foundry/common/documents/setting.d.ts
@@ -1,4 +1,5 @@
-import type { Document } from "../abstract/module.d.ts";
+import type { Document, DocumentMetadata } from "../abstract/module.d.ts";
+import type * as fields from "../data/fields.d.ts";
 
 /**
  * The Document definition for a Setting.
@@ -8,4 +9,37 @@ import type { Document } from "../abstract/module.d.ts";
  * @param data    Initial data from which to construct the Setting
  * @param context Construction context options
  */
-export default class BaseSetting extends Document<null> {}
+export default class BaseSetting extends Document<null, SettingSchema> {
+    /* -------------------------------------------- */
+    /*  Model Configuration                         */
+    /* -------------------------------------------- */
+
+    static override get metadata(): SettingMetadata;
+
+    static override defineSchema(): SettingSchema;
+}
+
+export default interface BaseSetting extends Document<null, SettingSchema>, ModelPropsFromSchema<SettingSchema> {
+    readonly _source: SettingSource;
+}
+
+interface SettingMetadata extends DocumentMetadata {
+    name: "Setting";
+    collection: "settings";
+    label: "DOCUMENT.Setting";
+    labelPlural: "DOCUMENT.Settings";
+    permissions: {
+        create: "SETTINGS_MODIFY";
+        update: "SETTINGS_MODIFY";
+        delete: "SETTINGS_MODIFY";
+    };
+}
+
+type SettingSchema = {
+    _id: fields.DocumentIdField;
+    key: fields.StringField<string, string, true, false, false>;
+    value: fields.JSONField<unknown, true, true, false>;
+    _stats: fields.DocumentStatsField;
+};
+
+type SettingSource = SourceFromSchema<SettingSchema>;
