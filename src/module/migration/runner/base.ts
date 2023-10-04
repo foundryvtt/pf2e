3056e875c8d9018d7a032c512e7abd99ee566b6c
In3luki <41452412+In3luki@users.noreply.github.com>
2023-10-04 13:44:52 +0200

    Migrate rollActionMacro function paramas to an object

--- src/module/migration/runner/base.ts
+++ src/module/migration/runner/base.ts
@@ -13,7 +13,7 @@ interface CollectionDiff<T extends foundry.documents.ActiveEffectSource | ItemSo
 export class MigrationRunnerBase {
     migrations: MigrationBase[];
 
-    static LATEST_SCHEMA_VERSION = 0.87;
+    static LATEST_SCHEMA_VERSION = 0.871;
 
     static MINIMUM_SAFE_VERSION = 0.618;
 
