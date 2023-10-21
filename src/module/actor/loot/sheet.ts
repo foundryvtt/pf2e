bd63e748521dcc8796552cdbded83f511cab65bc
Richard Simões <rsimoes@gmail.com>
2023-10-21 00:25:46 -0500

    Kill the jquery in loot sheet

--- src/module/actor/loot/sheet.ts
+++ src/module/actor/loot/sheet.ts
@@ -1,11 +1,11 @@
-import { ActorSheetPF2e } from "../sheet/base.ts";
-import { LootPF2e } from "@actor/loot/index.ts";
-import { DistributeCoinsPopup } from "../sheet/popups/distribute-coins-popup.ts";
-import { LootNPCsPopup } from "../sheet/loot/loot-npcs-popup.ts";
-import { DropCanvasItemDataPF2e } from "@module/canvas/drop-canvas-data.ts";
+import type { ActorPF2e, LootPF2e } from "@actor";
 import { ActorSheetDataPF2e } from "@actor/sheet/data-types.ts";
-import { ItemPF2e } from "@item";
-import type { ActorPF2e } from "@actor";
+import type { ItemPF2e } from "@item";
+import { DropCanvasItemDataPF2e } from "@module/canvas/drop-canvas-data.ts";
+import { htmlClosest, htmlQuery } from "@util";
+import { ActorSheetPF2e } from "../sheet/base.ts";
+import { LootNPCsPopup } from "../sheet/loot/loot-npcs-popup.ts";
+import { DistributeCoinsPopup } from "../sheet/popups/distribute-coins-popup.ts";
 
 export class LootSheetPF2e<TActor extends LootPF2e> extends ActorSheetPF2e<TActor> {
     static override get defaultOptions(): ActorSheetOptions {
@@ -45,37 +45,21 @@ export class LootSheetPF2e<TActor extends LootPF2e> extends ActorSheetPF2e<TActo
 
     override activateListeners($html: JQuery): void {
         super.activateListeners($html);
+        if (!this.isEditable) return;
+        const html = $html[0];
 
-        if (this.isEditable) {
-            $html
-                .find(".split-coins")
-                .removeAttr("disabled")
-                .on("click", (event) => this.#distributeCoins(event));
-            $html
-                .find(".loot-npcs")
-                .removeAttr("disabled")
-                .on("click", (event) => this.#lootNPCs(event));
-            $html.find("i.fa-info-circle.help[title]").tooltipster({
-                maxWidth: 275,
-                position: "right",
-                theme: "crb-hover",
-                contentAsHTML: true,
-            });
-        }
-    }
-
-    async #distributeCoins(event: JQuery.ClickEvent): Promise<void> {
-        event.preventDefault();
-        await new DistributeCoinsPopup(this.actor).render(true);
-    }
-
-    async #lootNPCs(event: JQuery.ClickEvent): Promise<void> {
-        event.preventDefault();
-        if (canvas.tokens.controlled.some((token) => token.actor?.id !== this.actor.id)) {
-            await new LootNPCsPopup(this.actor).render(true);
-        } else {
-            ui.notifications.warn("No tokens selected.");
-        }
+        htmlQuery(html, "[data-sidebar-buttons]")?.addEventListener("click", (event) => {
+            const button = htmlClosest(event.target, "button[data-action]");
+            if (button?.dataset.action === "split-coins") {
+                new DistributeCoinsPopup(this.actor).render(true);
+            } else if (button?.dataset.action === "loot-npcs") {
+                if (canvas.tokens.controlled.some((token) => token.actor?.id !== this.actor.id)) {
+                    new LootNPCsPopup(this.actor).render(true);
+                } else {
+                    ui.notifications.warn("No tokens selected.");
+                }
+            }
+        });
     }
 
     protected override async _onDropItem(
