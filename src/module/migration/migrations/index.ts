87bc52343a2d02859b21e1ef6935da84448602f7
Richard Simões <rsimoes@gmail.com>
2023-11-05 01:13:54 -0600

    Migrate "showRollDialogs" user setting

--- src/module/migration/migrations/index.ts
+++ src/module/migration/migrations/index.ts
@@ -277,3 +277,4 @@ export { Migration876FeatLevelTaken } from "./876-feat-level-taken.ts";
 export { Migration877PublicationData } from "./877-publication-data.ts";
 export { Migration878TakeABreather } from "./878-take-a-breather.ts";
 export { Migration879DeviseAStratagemAndFriends } from "./879-devise-a-stratagem-and-friends.ts";
+export { Migration880SplitShowDialogsSettings } from "./880-split-show-dialogs-setting.ts";
