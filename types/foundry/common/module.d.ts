0488bca720c1a3ee1357859fd57c1d0bfb54253b
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-11-05 22:54:11 -0600

    Update type definitions of Foundry's prototype extensions (#11127)

--- types/foundry/common/module.d.ts
+++ types/foundry/common/module.d.ts
@@ -2,10 +2,11 @@ import * as Abstract from "./abstract/module.ts";
 import * as Constants from "./constants.ts";
 import * as Data from "./data/module.ts";
 import * as Documents from "./documents/module.ts";
-import * as Utils from "./utils/module.ts";
 import * as Packages from "./packages/module.ts";
+import * as Utils from "./utils/module.ts";
 
 // global-modifying module
+import "./primitives/module.d.ts";
 import "./types.ts";
 
 declare global {
