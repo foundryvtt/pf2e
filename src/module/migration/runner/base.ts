7548ae9ffb2ada0a905a627f069f3795dfaa513c
Richard Simões <rsimoes@gmail.com>
2023-11-16 16:14:41 -0600

    Remove "hb_" prefix in tags among homebrew settings

--- src/module/migration/runner/base.ts
+++ src/module/migration/runner/base.ts
@@ -13,7 +13,7 @@ interface CollectionDiff<T extends foundry.documents.ActiveEffectSource | ItemSo
 export class MigrationRunnerBase {
     migrations: MigrationBase[];
 
-    static LATEST_SCHEMA_VERSION = 0.892;
+    static LATEST_SCHEMA_VERSION = 0.893;
 
     static MINIMUM_SAFE_VERSION = 0.618;
 
