3b64ced85f5446bd23409cd8123d551167fb629f
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-11-18 06:51:39 -0600

    Fix traits on remaining spell variants (#11550)

--- build/run-migration.ts
+++ build/run-migration.ts
@@ -23,7 +23,7 @@ import { Migration889RemoveFocusMaxIncreases } from "@module/migration/migration
 import { Migration890RMClassItemClassDC } from "@module/migration/migrations/890-rm-class-item-class-dc.ts";
 import { Migration891DruidicToWildsong } from "@module/migration/migrations/891-druidic-to-wildsong.ts";
 import { Migration894NoLayOnHandsVsUndead } from "@module/migration/migrations/894-no-lay-on-hands-vs-undead.ts";
-import { Migration895FixHealHarmTraits } from "@module/migration/migrations/895-fix-heal-harm-traits.ts";
+import { Migration895FixVariantSpellTraits } from "@module/migration/migrations/895-fix-variant-spell-traits.ts";
 // ^^^ don't let your IDE use the index in these imports. you need to specify the full path ^^^
 
 const { window } = new JSDOM();
@@ -46,7 +46,7 @@ const migrations: MigrationBase[] = [
     new Migration890RMClassItemClassDC(),
     new Migration891DruidicToWildsong(),
     new Migration894NoLayOnHandsVsUndead(),
-    new Migration895FixHealHarmTraits(),
+    new Migration895FixVariantSpellTraits(),
 ];
 
 global.deepClone = <T>(original: T): T => {
