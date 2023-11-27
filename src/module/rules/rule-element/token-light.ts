0286dd8aad039783821d5161aca552bd7c7ef504
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-11-26 23:09:27 -0600

    Allow `TokenLightRuleElement#color` to be injectable (#11801)

--- src/module/rules/rule-element/token-light.ts
+++ src/module/rules/rule-element/token-light.ts
@@ -1,7 +1,7 @@
-import { ResolvableValueField, RuleElementSchema, RuleElementSource } from "./data.ts";
-import { RuleElementOptions, RuleElementPF2e } from "./index.ts";
-import type { SchemaField } from "types/foundry/common/data/fields.d.ts";
 import type { LightDataSchema } from "types/foundry/common/data/data.d.ts";
+import type { SchemaField, StringField } from "types/foundry/common/data/fields.d.ts";
+import { ResolvableValueField, RuleElementSchema } from "./data.ts";
+import { RuleElementPF2e } from "./index.ts";
 
 /**
  * Add or change the light emitted by a token
@@ -14,41 +14,45 @@ class TokenLightRuleElement extends RuleElementPF2e<TokenLightRuleSchema> {
             ...super.defineSchema(),
             value: new fields.SchemaField({
                 ...foundry.data.LightData.defineSchema(),
-                dim: new ResolvableValueField({ required: false, nullable: false, initial: undefined }),
                 bright: new ResolvableValueField({ required: false, nullable: false, initial: undefined }),
+                color: new fields.StringField({ required: false, nullable: true, blank: false, initial: null }),
+                dim: new ResolvableValueField({ required: false, nullable: false, initial: undefined }),
             }),
         };
     }
 
-    constructor(data: RuleElementSource, options: RuleElementOptions) {
-        super(data, options);
-        this.validateData();
-    }
-
-    validateData(): void {
+    getLightData(): SourceFromSchema<LightDataSchema> | null {
         const light = this.value;
 
-        for (const key of ["dim", "bright"] as const) {
+        light.color &&= this.resolveInjectedProperties(light.color);
+
+        for (const key of ["bright", "dim"] as const) {
             if (light[key] !== undefined) {
                 const resolvedValue = this.resolveValue(light[key]);
                 if (typeof resolvedValue === "number") {
                     light[key] = resolvedValue;
                 } else {
-                    return this.failValidation(`${key} must resolve to a number`);
+                    this.failValidation(`${key} must resolve to a number`);
+                    return null;
                 }
             }
         }
 
         try {
-            new foundry.data.LightData(light);
+            return new foundry.data.LightData(light).toObject();
         } catch (error) {
             if (error instanceof Error) this.failValidation(error.message);
+            return null;
         }
     }
 
     override afterPrepareData(): void {
         if (!this.test()) return;
-        this.actor.synthetics.tokenOverrides.light = deepClone(this.value as ValidatedProps);
+
+        const data = this.getLightData();
+        if (data) {
+            this.actor.synthetics.tokenOverrides.light = data;
+        }
     }
 }
 
@@ -56,17 +60,12 @@ interface TokenLightRuleElement
     extends RuleElementPF2e<TokenLightRuleSchema>,
         ModelPropsFromSchema<TokenLightRuleSchema> {}
 
-type TokenLightValueSchema = Omit<LightDataSchema, "dim" | "bright"> & {
-    dim: ResolvableValueField<false, false, false>;
+type TokenLightValueSchema = Omit<LightDataSchema, "bright" | "color" | "dim"> & {
     bright: ResolvableValueField<false, false, false>;
+    /** `LightData#color` as an injectable property */
+    color: StringField<string, string, false, true, true>;
+    dim: ResolvableValueField<false, false, false>;
 };
-
-type ValidatedProps = DeepPartial<
-    Omit<ModelPropsFromSchema<TokenLightValueSchema>, "dim" | "bright"> & {
-        dim: number;
-        bright: number;
-    }
->;
 
 type TokenLightRuleSchema = RuleElementSchema & {
     value: SchemaField<TokenLightValueSchema>;
