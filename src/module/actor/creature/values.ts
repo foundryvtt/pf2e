fc766dd69fb5b19abe2da8d24b340fd8bd5b249e
Richard Simões <rsimoes@gmail.com>
2023-12-04 19:25:01 -0600

    Migrate instances of "undercommon" to "sakvroth"

--- src/module/actor/creature/values.ts
+++ src/module/actor/creature/values.ts
@@ -130,7 +130,6 @@ const LANGUAGES = [
     "thalassic",
     "thassilonian",
     "tien",
-    "undercommon",
     "utopian",
     "vanara",
     "varisian",
--- src/module/migration/runner/base.ts
+++ src/module/migration/runner/base.ts
@@ -13,7 +13,7 @@ interface CollectionDiff<T extends foundry.documents.ActiveEffectSource | ItemSo
 export class MigrationRunnerBase {
     migrations: MigrationBase[];
 
-    static LATEST_SCHEMA_VERSION = 0.903;
+    static LATEST_SCHEMA_VERSION = 0.904;
 
     static MINIMUM_SAFE_VERSION = 0.634;
 
