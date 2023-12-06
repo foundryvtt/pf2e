17170565e7c8c88e354d0ec9fa47cb5c1a1b1c3b
Carlos Fernandez <cfern1990@gmail.com>
2023-12-06 03:34:25 -0500

    Start on posting army gear messages to chat

--- src/module/actor/army/values.ts
+++ src/module/actor/army/values.ts
@@ -1,3 +1,7 @@
+import type { ActionTrait } from "@item/ability/types.ts";
+import type { KingmakerTrait } from "@item/campaign-feature/types.ts";
+import * as R from "remeda";
+
 const BASIC_WAR_ACTIONS_FOLDER = "Vqp8b64uH35zkncy";
 
 const ALIGNMENTS = new Set(["LG", "NG", "CG", "LN", "N", "CN", "LE", "NE", "CE"] as const);
@@ -15,51 +19,91 @@ const ARMY_STATS = {
     maxTactics: [0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6],
 };
 
+interface ArmyGearData {
+    img: string;
+    name: string;
+    traits: (KingmakerTrait | ActionTrait)[];
+    description: string;
+    price?: number;
+    level?: number;
+    ranks?: {
+        name: string;
+        description: string;
+        price: number;
+        level: number;
+    }[];
+}
+
+type ArmyGearType = "melee" | "ranged" | "additional-melee" | "additional-ranged" | "potions" | "armor";
+
 // Data for the gear equipment cards
-function fetchArmyGearData(gearType: String): Object {
-    switch (gearType) {
-        case "melee":
-        case "ranged":
-            return {
+function getArmyGearData(): Record<ArmyGearType, ArmyGearData> {
+    return {
+        // Additional melee/ranged weapon
+        ...R.mapToObj(["melee", "ranged"] as const, (type) => [
+            `additional-${type}`,
+            {
                 img:
-                    gearType === "melee"
+                    type === "melee"
                         ? "icons/weapons/axes/axe-battle-black.webp"
                         : "icons/weapons/crossbows/crossbow-simple-brown.webp",
-                name: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Weapons.name"),
-                description: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Weapons.description"),
+                name: game.i18n.format("PF2E.Kingmaker.Army.Gear.AdditionalWeapon.Name", {
+                    type: game.i18n.format(`PF2E.Kingmaker.Army.Strikes.${type}`),
+                }),
+                description: "PF2E.Kingmaker.Army.Gear.AdditionalWeapon.Description",
+                traits: ["army"],
+                level: 1,
+                price: 10,
+            },
+        ]),
+        // Magic melee/ranged weapons
+        ...R.mapToObj(["melee", "ranged"] as const, (type) => [
+            type,
+            {
+                img:
+                    type === "melee"
+                        ? "icons/weapons/axes/axe-battle-black.webp"
+                        : "icons/weapons/crossbows/crossbow-simple-brown.webp",
+                name: game.i18n.format("PF2E.Kingmaker.Army.Gear.MagicWeapons.Name", {
+                    type: game.i18n.format(`PF2E.Kingmaker.Army.Strikes.${type}`),
+                }),
+                description: "PF2E.Kingmaker.Army.Gear.MagicWeapons.Description",
                 traits: ["army", "magical"],
-                level: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Weapons.level"),
                 ranks: [
                     { price: 20, level: 2 },
                     { price: 40, level: 10 },
                     { price: 60, level: 16 },
-                ],
-            };
-        case "potions":
-            return {
-                img: "icons/consumables/potions/bottle-round-corked-orante-red.webp",
-                name: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Potions.name"),
-                description: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Potions.description"),
-                traits: ["army", "consumable", "healing", "magical", "potion"],
-                level: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Potions.level"),
-                price: 15,
-            };
-        case "armor":
-            return {
-                img: "icons/equipment/shield/heater-wooden-brown-axe.webp",
-                name: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Armor.name"),
-                description: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Armor.description"),
-                traits: ["army", "magical"],
-                level: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Armor.level"),
-                ranks: [
-                    { price: 25, level: 5 },
-                    { price: 50, level: 11 },
-                    { price: 75, level: 18 },
-                ],
-            };
-        default:
-            return {};
-    }
+                ].map((d, idx) => ({
+                    ...d,
+                    name: `PF2E.Kingmaker.Army.Gear.MagicWeapons.rank${idx + 1}.Name`,
+                    description: `PF2E.Kingmaker.Army.Gear.MagicWeapons.rank${idx + 1}.Description`,
+                })),
+            },
+        ]),
+        potions: {
+            img: "icons/consumables/potions/bottle-round-corked-orante-red.webp",
+            name: "PF2E.Kingmaker.Army.Gear.Potions.Name",
+            description: "PF2E.Kingmaker.Army.Gear.Potions.Description",
+            traits: ["army", "consumable", "healing", "magical", "potion"],
+            level: 1,
+            price: 15,
+        },
+        armor: {
+            img: "icons/equipment/shield/heater-wooden-brown-axe.webp",
+            name: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Armor.Name"),
+            description: game.i18n.localize("PF2E.Kingmaker.Army.Gear.Armor.Description"),
+            traits: ["army", "magical"],
+            ranks: [
+                { price: 25, level: 5 },
+                { price: 50, level: 11 },
+                { price: 75, level: 18 },
+            ].map((d, idx) => ({
+                ...d,
+                name: `PF2E.Kingmaker.Army.Gear.Armor.rank${idx + 1}.Name`,
+                description: `PF2E.Kingmaker.Army.Gear.Armor.rank${idx + 1}.Description`,
+            })),
+        },
+    };
 }
 
-export { ALIGNMENTS, ARMY_STATS, ARMY_TYPES, BASIC_WAR_ACTIONS_FOLDER, fetchArmyGearData };
+export { ALIGNMENTS, ARMY_STATS, ARMY_TYPES, BASIC_WAR_ACTIONS_FOLDER, getArmyGearData };
