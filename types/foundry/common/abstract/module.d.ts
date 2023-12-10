2937aba4fb7e96422c33e3ea2033c46686c72884
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-12-09 18:07:07 -0600

    Update `EmbeddedCollection` type (#12124)

--- types/foundry/common/abstract/module.d.ts
+++ types/foundry/common/abstract/module.d.ts
@@ -2,5 +2,5 @@ export { default as DatabaseBackend } from "./backend.ts";
 export { default as DataModel } from "./data.ts";
 export type * from "./document.d.ts";
 export { default as Document, DocumentMetadata } from "./document.ts";
-export type { default as EmbeddedCollection } from "./embedded-collection.ts";
+export { default as EmbeddedCollection } from "./embedded-collection.ts";
 export { default as TypeDataModel } from "./type-data.ts";
