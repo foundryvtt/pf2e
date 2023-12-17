dcbfd83160f024ef2c679b58ff73acb7eaeca958
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-12-16 18:07:49 -0600

    Fix definition of unholy IWR (#12315)

--- src/module/actor/data/iwr.ts
+++ src/module/actor/data/iwr.ts
@@ -155,7 +155,7 @@ abstract class IWR<TType extends IWRType> {
             case "unarmed-attacks":
                 return ["item:category:unarmed"];
             case "unholy":
-                return [{ or: ["action:trait:unholy", "item:trait:unholy"] }];
+                return [{ or: ["origin:action:trait:unholy", "item:trait:unholy"] }];
             default: {
                 if (iwrType in CONFIG.PF2E.damageTypes) {
                     return [`damage:type:${iwrType}`];
