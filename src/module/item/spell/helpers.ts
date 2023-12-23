21ec3a2a3cffcf0e3b834f0cb525366d19326e30
Richard Simões <rsimoes@gmail.com>
2023-12-23 16:36:34 -0600

    Expose `duration.sustained` system-data value in spell sheets

--- src/module/item/spell/helpers.ts
+++ src/module/item/spell/helpers.ts
@@ -1,3 +1,4 @@
+import { localizer } from "@util";
 import type { SpellPF2e } from "./index.ts";
 
 function createSpellRankLabel(spell: SpellPF2e, castRank?: number): string {
@@ -22,8 +23,18 @@ async function createDescriptionPrepend(
               .sort((a, b) => a.localeCompare(b, game.i18n.lang))
               .join(", ")
         : null;
+    const durationLabel = ((): string => {
+        const duration = spell.system.duration;
+        const textDuration = duration.value.trim();
+        if (duration.sustained) {
+            const localize = localizer("PF2E.Item.Spell.Sustained");
+            const label = textDuration === "" ? localize("Label") : localize("Duration", { maximum: textDuration });
+            return game.i18n.lang === "de" ? label : label.toLocaleLowerCase(game.i18n.lang);
+        }
+        return textDuration;
+    })();
     const templatePath = "systems/pf2e/templates/items/partials/spell-description-prepend.hbs";
-    const rendered = (await renderTemplate(templatePath, { spell, traditions })).trim();
+    const rendered = (await renderTemplate(templatePath, { duration: durationLabel, spell, traditions })).trim();
 
     return rendered ? `${rendered}\n<hr />` : "";
 }
