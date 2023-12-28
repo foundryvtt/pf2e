3952c253c1a17c98676eaa31c0e3061be1cca5cd
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2023-12-28 12:36:56 -0600

    Make type changes in older migrations for perception refactor (#12559)

--- src/module/migration/migrations/672-remove-npc-base-properties.ts
+++ src/module/migration/migrations/672-remove-npc-base-properties.ts
@@ -7,7 +7,7 @@ import { MigrationBase } from "../base.ts";
 export class Migration672RemoveNPCBaseProperties extends MigrationBase {
     static override version = 0.672;
 
-    private removeBase(property: PropertyWithBase, replace: "value" | "max" = "value"): void {
+    #removeBase(property: PropertyWithBase, replace: "value" | "max" = "value"): void {
         property[replace] = Number(property[replace]) || 0;
         if (typeof property.base === "number") {
             property[replace] = property.base;
@@ -15,23 +15,25 @@ export class Migration672RemoveNPCBaseProperties extends MigrationBase {
         }
     }
 
-    override async updateActor(actorSource: ActorSourcePF2e): Promise<void> {
-        if (actorSource.type !== "npc") return;
+    override async updateActor(source: ActorSourcePF2e): Promise<void> {
+        if (source.type !== "npc") return;
 
-        const { ac, hp, perception } = actorSource.system.attributes;
-        this.removeBase(ac);
-        this.removeBase(hp, "max");
-        this.removeBase(perception);
+        const attributes: OldNPCAttributes = source.system.attributes;
+        const { ac, hp, perception } = attributes;
+        if (ac) this.#removeBase(ac);
+        if (hp) this.#removeBase(hp, "max");
+        if (perception) this.#removeBase(perception);
 
-        const { saves } = actorSource.system;
+        const { saves } = source.system;
         for (const saveType of SAVE_TYPES) {
-            this.removeBase(saves[saveType]);
+            this.#removeBase(saves[saveType]);
         }
     }
 
-    override async updateItem(itemSource: ItemSourcePF2e, actorSource?: ActorSourcePF2e): Promise<void> {
-        if (!(actorSource?.type === "npc" && itemSource.type === "lore")) return;
-        this.removeBase(itemSource.system.mod);
+    override async updateItem(source: ItemSourcePF2e, actorSource?: ActorSourcePF2e): Promise<void> {
+        if (actorSource?.type === "npc" && source.type === "lore") {
+            this.#removeBase(source.system.mod);
+        }
     }
 }
 
@@ -40,4 +42,10 @@ interface PropertyWithBase {
     max?: number;
     base?: number;
     "-=base"?: null;
+}
+
+interface OldNPCAttributes {
+    ac?: PropertyWithBase;
+    hp?: PropertyWithBase;
+    perception?: PropertyWithBase;
 }
