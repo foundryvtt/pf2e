1f0ba8ccd9d57b55baaeafa31f68edb84df3fae3
Nikolaj Andresen <na@deck.dk>
2024-01-01 05:33:46 +0100

    Add grab an edge action object (#12607)

--- src/module/system/action-macros/index.ts
+++ src/module/system/action-macros/index.ts
@@ -57,6 +57,7 @@ import { avertGaze } from "./specialty-basic/avert-gaze.ts";
 import { burrow } from "./specialty-basic/burrow.ts";
 import { dismiss } from "./specialty-basic/dismiss.ts";
 import { fly } from "./specialty-basic/fly.ts";
+import { grabAnEdge } from "./specialty-basic/grab-an-edge.ts";
 import { mount } from "./specialty-basic/mount.ts";
 import { pointOut } from "./specialty-basic/point-out.ts";
 import { sustain } from "./specialty-basic/sustain.ts";
@@ -184,6 +185,7 @@ export const SystemActions: Action[] = [
     fly,
     forceOpen.action,
     gatherInformation.action,
+    grabAnEdge,
     grapple.action,
     hide.action,
     highJump.action,
