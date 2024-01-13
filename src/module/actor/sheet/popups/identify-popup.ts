c9b57c2b9bf1fa3b2e9ec82b0687764fa1646e5b
Nikolaj Andresen <na@deck.dk>
2024-01-13 04:47:24 +0100

    Wire up identify alchemy and identify magic actions for item identification

--- src/module/actor/sheet/popups/identify-popup.ts
+++ src/module/actor/sheet/popups/identify-popup.ts
@@ -48,18 +48,14 @@ export class IdentifyItemPopup extends FormApplication<PhysicalItemPF2e> {
             const item = this.object;
             const identifiedName = item.system.identification.identified.name;
             const dcs: Record<string, number> = this.dcs;
-
-            const actionOption = item.isMagical
-                ? "action:identify-magic"
-                : item.isAlchemical
-                  ? "action:identify-alchemy"
-                  : null;
+            const action = item.isMagical ? "identify-magic" : item.isAlchemical ? "identify-alchemy" : null;
 
             const content = await renderTemplate("systems/pf2e/templates/actors/identify-item-chat-skill-checks.hbs", {
                 identifiedName,
-                rollOptions: R.compact(["concentrate", "exploration", "secret", actionOption]),
+                action,
                 skills: R.omit(dcs, ["dc"]),
                 unidentified: item.system.identification.unidentified,
+                uuid: item.uuid,
             });
 
             await ChatMessagePF2e.create({ user: game.user.id, content });
