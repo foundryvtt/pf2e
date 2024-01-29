4e1815ce0d87ddab17fae411581bdf9e99fead43
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-01-29 04:30:10 -0600

    Move item-trait unions to types.ts files (#13356)

--- src/module/item/melee/document.ts
+++ src/module/item/melee/document.ts
@@ -1,15 +1,16 @@
 import type { ActorPF2e } from "@actor";
 import { SIZE_TO_REACH } from "@actor/creature/values.ts";
-import { ItemPF2e, WeaponPF2e } from "@item";
-import { RangeData } from "@item/types.ts";
-import { BaseWeaponType, WeaponCategory, WeaponGroup } from "@item/weapon/types.ts";
+import { ItemPF2e, type WeaponPF2e } from "@item";
+import type { RangeData } from "@item/types.ts";
+import type { BaseWeaponType, WeaponCategory, WeaponGroup } from "@item/weapon/types.ts";
 import type { ChatMessagePF2e } from "@module/chat-message/document.ts";
 import { simplifyFormula } from "@scripts/dice.ts";
 import { DamageCategorization } from "@system/damage/helpers.ts";
 import { ConvertedNPCDamage, WeaponDamagePF2e } from "@system/damage/weapon.ts";
 import { sluggify, tupleHasValue } from "@util";
 import * as R from "remeda";
-import { MeleeFlags, MeleeSource, MeleeSystemData, NPCAttackTrait } from "./data.ts";
+import type { MeleeFlags, MeleeSource, MeleeSystemData } from "./data.ts";
+import type { NPCAttackTrait } from "./types.ts";
 
 class MeleePF2e<TParent extends ActorPF2e | null = ActorPF2e | null> extends ItemPF2e<TParent> {
     /** Set during data preparation if a linked weapon is found */
