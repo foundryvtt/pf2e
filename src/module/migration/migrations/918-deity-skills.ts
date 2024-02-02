1169adfa784d21d852439deff31543b297b2216d
Andrew Schlachter <DocSchlock@users.noreply.github.com>
2024-02-01 19:53:13 -0500

    change migration

--- src/module/migration/migrations/918-deity-skills.ts
+++ src/module/migration/migrations/918-deity-skills.ts
@@ -1,6 +1,7 @@
 import { ItemSourcePF2e } from "@item/base/data/index.ts";
 import { MigrationBase } from "../base.ts";
 import * as R from "remeda";
+import { SKILL_DICTIONARY } from "@actor/values.ts";
 
 /** Fix skills section of deities */
 export class Migration918DeitySkills extends MigrationBase {
@@ -11,7 +12,7 @@ export class Migration918DeitySkills extends MigrationBase {
         if (source.type !== "deity") return;
 
         source.system.skill = R.compact(
-            typeof source.system.skill === "string" ? [source.system.skill] : source.system.skill ?? [],
+            typeof source.system.skill === "string" ? [SKILL_DICTIONARY[source.system.skill]] : source.system.skill ?? [],
         );
     }
 }
