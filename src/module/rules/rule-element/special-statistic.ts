c6ee3341df0acf72d47d10b0c13812d94544350e
Richard Simões <rsimoes@gmail.com>
2024-02-01 21:14:28 -0600

    Allow setting base modifiers for NPC special statistics

--- src/module/rules/rule-element/special-statistic.ts
+++ src/module/rules/rule-element/special-statistic.ts
@@ -1,4 +1,5 @@
-import type { CharacterPF2e } from "@actor";
+import type { CreaturePF2e } from "@actor";
+import { ModifierPF2e } from "@actor/modifiers.ts";
 import { AttributeString } from "@actor/types.ts";
 import { ATTRIBUTE_ABBREVIATIONS, SAVE_TYPES, SKILL_LONG_FORMS } from "@actor/values.ts";
 import { MagicTradition } from "@item/spell/types.ts";
@@ -7,12 +8,15 @@ import { PredicatePF2e, RawPredicate } from "@system/predication.ts";
 import { PredicateField, SlugField } from "@system/schema-data-fields.ts";
 import { Statistic, StatisticData } from "@system/statistic/index.ts";
 import { setHasElement, tupleHasValue } from "@util";
-import type { SchemaField, StringField } from "types/foundry/common/data/fields.d.ts";
+import * as R from "remeda";
+import type { NumberField, SchemaField, StringField } from "types/foundry/common/data/fields.d.ts";
 import { RuleElementPF2e } from "../index.ts";
 import type { RuleElementSchema } from "./data.ts";
 
 /** Create a special-purpose statistic for use in checks and as a DC */
 class SpecialStatisticRuleElement extends RuleElementPF2e<SpecialStatisticSchema> {
+    static override validActorTypes = ["character", "npc"] satisfies ("character" | "npc")[];
+
     static override defineSchema(): SpecialStatisticSchema {
         const fields = foundry.data.fields;
         return {
@@ -43,6 +47,14 @@ class SpecialStatisticRuleElement extends RuleElementPF2e<SpecialStatisticSchema
                 nullable: true,
                 initial: null,
             }),
+            baseModifier: new fields.SchemaField(
+                {
+                    mod: new fields.NumberField({ required: false, nullable: true, integer: true, initial: null }),
+                    check: new fields.NumberField({ required: false, nullable: true, integer: true, initial: null }),
+                    dc: new fields.NumberField({ required: false, nullable: true, integer: true, initial: null }),
+                },
+                { required: false, nullable: true, initial: null },
+            ),
             itemCasting: new fields.SchemaField(
                 {
                     predicate: new PredicateField({ required: true, nullable: false }),
@@ -53,7 +65,7 @@ class SpecialStatisticRuleElement extends RuleElementPF2e<SpecialStatisticSchema
                         initial: null,
                     }),
                 },
-                { required: false, nullable: false, initial: undefined },
+                { required: false, nullable: true, initial: null },
             ),
         };
     }
@@ -61,30 +73,41 @@ class SpecialStatisticRuleElement extends RuleElementPF2e<SpecialStatisticSchema
     override afterPrepareData(): void {
         if (this.type === "simple") return;
 
+        const actor = this.actor;
         const checkDomains = this.type === "check" ? [`${this.slug}-check`] : [`${this.slug}-attack-roll`];
         const extendedFrom = this.extends
-            ? this.actor.getStatistic(this.extends) ?? this.actor.synthetics.statistics.get(this.extends) ?? null
+            ? actor.getStatistic(this.extends) ?? actor.synthetics.statistics.get(this.extends) ?? null
             : null;
         const label = this.itemCasting ? extendedFrom?.label ?? this.label : this.label;
-        const data: StatisticData = {
+        const checkType = this.type === "check" ? "check" : "attack-roll";
+        const modCheckDC =
+            this.baseModifier && actor.type === "npc"
+                ? R.mapValues(this.baseModifier, (value, key) => {
+                      const [slug, label] = key === "mod" ? ["modifier", "PF2E.ModifierTitle"] : [`base-${key}`, ""];
+                      return value ? [new ModifierPF2e({ slug, label, type: "untyped", modifier: value })] : [];
+                  })
+                : { mod: [], check: [], dc: [] };
+
+        const data: Required<Omit<StatisticData, "filter" | "lore" | "proficient" | "rank" | "rollOptions">> = {
             slug: this.slug,
             label,
             attribute: this.attribute ?? extendedFrom?.attribute ?? null,
             domains: [this.slug],
-            check: { type: this.type === "check" ? "check" : "attack-roll", domains: checkDomains },
-            dc: { domains: [`${this.slug}-dc`] },
+            modifiers: modCheckDC.mod,
+            check: { type: checkType, domains: checkDomains, modifiers: modCheckDC.check },
+            dc: { domains: [`${this.slug}-dc`], modifiers: modCheckDC.dc },
         };
 
-        const statistic = extendedFrom?.extend(data) ?? new Statistic(this.actor, data);
+        const statistic = extendedFrom?.extend(data) ?? new Statistic(actor, data);
         if (statistic) {
-            this.actor.synthetics.statistics.set(this.slug, statistic);
+            actor.synthetics.statistics.set(this.slug, statistic);
             if (this.itemCasting) {
-                this.actor.spellcasting.set(
+                actor.spellcasting.set(
                     this.slug,
                     new ItemSpellcasting({
                         id: this.slug,
                         name: this.label,
-                        actor: this.actor,
+                        actor,
                         statistic,
                         castPredicate: this.itemCasting.predicate,
                         tradition: this.itemCasting.tradition,
@@ -102,20 +125,35 @@ interface SpecialStatisticRuleElement
         Omit<ModelPropsFromSchema<SpecialStatisticSchema>, "label"> {
     slug: string;
 
-    get actor(): CharacterPF2e;
+    get actor(): CreaturePF2e;
 }
 
 type SpecialStatisticSchema = RuleElementSchema & {
     type: StringField<StatisticType, StatisticType, true, false, true>;
+    /** A base statistic from which to extend */
     extends: StringField<string, string, true, true, true>;
+    /** An attribute to associate with the statistic */
     attribute: StringField<AttributeString, AttributeString, false, true, true>;
+    /** A base modifier for use with NPC special statistics: separate check and DC values may also be specified. */
+    baseModifier: SchemaField<
+        {
+            mod: NumberField<number, number, false, true, true>;
+            check: NumberField<number, number, false, true, true>;
+            dc: NumberField<number, number, false, true, true>;
+        },
+        { mod: number | null; check: number | null; dc: number | null },
+        { mod: number | null; check: number | null; dc: number | null },
+        false,
+        true,
+        true
+    >;
     itemCasting: SchemaField<
         ItemCastingSchema,
         { predicate: RawPredicate; tradition: MagicTradition | null },
         { predicate: PredicatePF2e; tradition: MagicTradition | null },
         false,
-        false,
-        false
+        true,
+        true
     >;
 };
 
