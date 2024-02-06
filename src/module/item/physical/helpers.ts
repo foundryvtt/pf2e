608df76b62c46adf0bad1e3bb5fe5553a7b14a07
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-02-06 06:04:01 -0600

    If possible, avoid re-IDing subitems when attaching/detaching (#13579)

--- src/module/item/physical/helpers.ts
+++ src/module/item/physical/helpers.ts
@@ -236,17 +236,15 @@ async function detachSubitem(subitem: PhysicalItemPF2e, skipConfirm: boolean): P
         const deletePromise = subitem.delete();
         const createPromise = (async (): Promise<unknown> => {
             // Find a stack match, cloning the subitem as worn so the search won't fail due to it being equipped
-            const stack = parentItem.actor?.inventory.findStackableItem(
-                subitem.clone({ "system.equipped.carryType": "worn" }),
-            );
+            const stack = subitem.isOfType("consumable")
+                ? parentItem.actor?.inventory.findStackableItem(subitem.clone({ "system.equipped.carryType": "worn" }))
+                : null;
+            const keepId = !!parentItem.actor && !parentItem.actor.items.has(subitem.id);
             return (
                 stack?.update({ "system.quantity": stack.quantity + 1 }) ??
                 Item.implementation.create(
-                    fu.mergeObject(subitem.toObject(), {
-                        _id: null,
-                        "system.containerId": parentItem.system.containerId,
-                    }),
-                    { parent: parentItem.actor },
+                    fu.mergeObject(subitem.toObject(), { "system.containerId": parentItem.system.containerId }),
+                    { parent: parentItem.actor, keepId },
                 )
             );
         })();
