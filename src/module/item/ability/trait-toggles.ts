83013c58a5a439aa74244a94ebfc61dd0d71d506
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-02-13 01:27:05 -0600

    Add per-action toggle for mindshift trait (#13692)

---
+++ src/module/item/ability/trait-toggles.ts
@@ -0,0 +1,79 @@
+import { DamageDicePF2e, type ModifierPF2e } from "@actor/modifiers.ts";
+import type { AbilityItemPF2e, FeatPF2e } from "@item";
+import * as R from "remeda";
+
+/** A helper class to handle toggleable ability traits */
+class AbilityTraitToggles {
+    parent: AbilityItemPF2e | FeatPF2e;
+
+    constructor(item: AbilityItemPF2e | FeatPF2e) {
+        this.parent = item;
+        Object.defineProperty(this, "parent", { enumerable: false });
+    }
+
+    get item(): AbilityItemPF2e | FeatPF2e {
+        return this.parent;
+    }
+
+    get operableTraits(): "mindshift"[] {
+        return this.item.system.traits.value.includes("mindshift") ? ["mindshift"] : [];
+    }
+
+    get mindshift(): { selected: boolean } | null {
+        if (!this.operableTraits.includes("mindshift")) return null;
+
+        const selected = this.item._source.system.traits.toggles?.mindshift?.selected ?? false;
+
+        return { selected };
+    }
+
+    getDamageModifications(): { modifiers: ModifierPF2e[]; dice: DamageDicePF2e[] } {
+        if (!this.mindshift?.selected) {
+            return { modifiers: [], dice: [] };
+        }
+
+        const override = new DamageDicePF2e({
+            slug: "mindshift",
+            selector: `${this.item.id}-damage`,
+            override: { damageType: "mental" },
+        });
+
+        return { modifiers: [], dice: [override] };
+    }
+
+    getSheetData(): TraitToggleViewData[] {
+        return R.compact(
+            (["mindshift"] as const).map((t) => {
+                const data = this[t];
+                return data
+                    ? {
+                          trait: t,
+                          selected: data.selected,
+                          icon: "brain",
+                          classes: "damage color mental",
+                          tooltip: CONFIG.PF2E.actionTraits[t],
+                      }
+                    : null;
+            }),
+        );
+    }
+
+    async update({ trait, selected }: { trait: "mindshift"; selected: boolean }): Promise<boolean> {
+        const current = this[trait]?.selected;
+        if (current === selected || typeof current !== "boolean") {
+            return false;
+        }
+
+        return !!(await this.item.update({ [`system.traits.toggles.${trait}.selected`]: selected }));
+    }
+}
+
+interface TraitToggleViewData {
+    trait: string;
+    selected: boolean;
+    icon: string;
+    classes: string;
+    tooltip: string;
+}
+
+export { AbilityTraitToggles, type TraitToggleViewData };
