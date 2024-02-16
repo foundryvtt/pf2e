6023314c9b0557620c8d905ef3c46a30cd9f852c
Drental <74384067+Drental@users.noreply.github.com>
2024-02-16 10:16:43 +0100

    Add targetOwner option to @Check syntax (#13700)

--- src/scripts/ui/inline-roll-links.ts
+++ src/scripts/ui/inline-roll-links.ts
@@ -100,6 +100,7 @@ export const InlineRollLinks = {
                 pf2Defense,
                 pf2Adjustment,
                 pf2Roller,
+                targetOwner,
                 pf2RollOptions,
                 overrideTraits,
             } = link.dataset;
@@ -182,7 +183,11 @@ export const InlineRollLinks = {
                                 continue;
                             }
 
-                            const targetActor = pf2Defense ? game.user.targets.first()?.actor : null;
+                            const targetActor = pf2Defense
+                                ? targetOwner
+                                    ? parent
+                                    : game.user.targets.first()?.actor
+                                : null;
 
                             const dcValue = (() => {
                                 const adjustment = Number(pf2Adjustment) || 0;
