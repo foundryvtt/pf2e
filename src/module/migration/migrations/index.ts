18ea6ef56be69826c33c01fc2e5589816cd8c6c9
Tikael <tikael@gmail.com>
2024-02-28 00:32:46 -0500

    Migrate swashbuckler finishers to new rule elements

--- src/module/migration/migrations/index.ts
+++ src/module/migration/migrations/index.ts
@@ -287,3 +287,4 @@ export { Migration918DeitySkills } from "./918-deity-skills.ts";
 export { Migration919WeaponToggleStructure } from "./919-trait-toggle-structure.ts";
 export { Migration920SuboptionSelection } from "./920-suboption-selection.ts";
 export { Migration921SpellSlotArrays } from "./921-spell-slot-arrays.ts";
+export { Migration922SwashbucklerFinishers } from "./922-swashbuckler-finisher-suboptions.ts";
