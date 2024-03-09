9293ebe1373fda0219a76900141e4e8ef7d75941
Avagdu Mortesin <55938242+avagdumortesin@users.noreply.github.com>
2024-03-09 12:27:56 -0800

    Remove the legacy Healing version of Touch of Corruption (#14102)

--- build/run-migration.ts
+++ build/run-migration.ts
@@ -27,6 +27,7 @@ import { Migration921SpellSlotArrays } from "@module/migration/migrations/921-sp
 import { Migration922SwashbucklerFinishers } from "@module/migration/migrations/922-swashbuckler-finisher-suboptions.ts";
 import { Migration923KineticistRestructure } from "@module/migration/migrations/923-kineticist-restructure.ts";
 import { Migration924JiuHuanDoa } from "@module/migration/migrations/924-jiu-huan-dao.ts";
+import { Migration925TouchOfCorruption } from "@module/migration/migrations/925-touch-of-corruption.ts";
 // ^^^ don't let your IDE use the index in these imports. you need to specify the full path ^^^
 
 const { window } = new JSDOM();
@@ -50,6 +51,7 @@ const migrations: MigrationBase[] = [
     new Migration922SwashbucklerFinishers(),
     new Migration923KineticistRestructure(),
     new Migration924JiuHuanDoa(),
+    new Migration925TouchOfCorruption(),
 ];
 
 const packsDataPath = path.resolve(process.cwd(), "packs");
