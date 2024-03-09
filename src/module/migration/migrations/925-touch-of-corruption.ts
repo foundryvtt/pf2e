50a014ac290ca8feaea105679107a0026c79fc73
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-03-09 14:20:04 -0600

    Update src/module/migration/migrations/925-touch-of-corruption.ts

--- src/module/migration/migrations/925-touch-of-corruption.ts
+++ src/module/migration/migrations/925-touch-of-corruption.ts
@@ -6,11 +6,9 @@ import { MigrationBase } from "../base.ts";
 export class Migration925TouchOfCorruption extends MigrationBase {
     static override version = 0.925;
 
-    #replaceStrings<TObject extends object>(data: TObject): TObject {
-        return recursiveReplaceString(data, (s) => s.replace("ekGHLJSHGgWMUwkY", "jFmWSIpJGGebim6y"));
-    }
-
     override async updateItem(source: ItemSourcePF2e): Promise<void> {
-        source.system = this.#replaceStrings(source.system);
+        source.system.description = recursiveReplaceString(source.system.description, (s) =>
+            s.replace(/\bekGHLJSHGgWMUwkY\b/g, "jFmWSIpJGGebim6y"),
+        );
     }
 }
