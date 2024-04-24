76c6dacca254a9a372d993595292cdcbf0f57aac
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2024-04-24 07:37:05 -0400

    Restore hearing detection for V12 (#14517)

--- types/foundry/client-esm/canvas/sources/point-darkness-source.d.ts
+++ types/foundry/client-esm/canvas/sources/point-darkness-source.d.ts
@@ -1,5 +1,5 @@
 import type { PointEffectLightSource } from "./point-effect-source-mixes.ts";
 
 export default class PointDarknessSource<
-    TObject extends Token | AmbientLight,
+    TObject extends Token | AmbientLight | null,
 > extends PointEffectLightSource<TObject> {}
