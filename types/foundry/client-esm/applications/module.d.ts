ca591e8f5613e7d15efe78d10dfdbb6ac160cdbe
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2024-04-25 04:17:44 -0400

    Begin creating types of application V2 (#14523)

---
+++ types/foundry/client-esm/applications/module.d.ts
@@ -0,0 +1,13 @@
+import type ApplicationV2 from "./api/application.d.ts";
+
+export * as api from "./api/module.ts";
+export * as sheets from "../../client/foundry/applications/sheets.ts";
+
+export const instances: Map<number, ApplicationV2>;
+
+/**
+ * Parse an HTML string, returning a processed HTMLElement or HTMLCollection.
+ * A single HTMLElement is returned if the provided string contains only a single top-level element.
+ * An HTMLCollection is returned if the provided string contains multiple top-level elements.
+ */
+export function parseHTML(htmlString: string): HTMLCollection | HTMLElement;
