7b2460636d195b13aee4b2fac3ba4d5c7ac922bb
In3luki <41452412+In3luki@users.noreply.github.com>
2024-04-28 05:04:38 +0200

    Convert more select helpers to selectOptions (#14532)

--- src/module/actor/hazard/types.ts
+++ src/module/actor/hazard/types.ts
@@ -6,6 +6,8 @@ import type { AbilityItemPF2e } from "@item";
 
 interface HazardSheetData extends ActorSheetDataPF2e<HazardPF2e> {
     actions: HazardActionSheetData;
+    complexityOptions: { value: string; label: string }[];
+    emitsSoundOptions: { value: string; label: string }[];
     editing: boolean;
     actorTraits: TraitViewData[];
     rarity: Record<string, string>;
