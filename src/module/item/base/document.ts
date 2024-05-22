baae750e692547a052755eb01d06057b95d3f7f7
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2024-05-22 13:32:25 -0400

    Block the existence of item and actor module sub-types (#14795)

--- src/module/item/base/document.ts
+++ src/module/item/base/document.ts
@@ -892,7 +892,10 @@ const ItemProxyPF2e = new Proxy(ItemPF2e, {
             source?.type === "armor" && (source.system?.category as string | undefined) === "shield"
                 ? "shield"
                 : source?.type;
-        const ItemClass: typeof ItemPF2e = CONFIG.PF2E.Item.documentClasses[type] ?? ItemPF2e;
+        const ItemClass: typeof ItemPF2e = CONFIG.PF2E.Item.documentClasses[type];
+        if (!ItemClass) {
+            throw ErrorPF2e(`Item type ${type} does not exist and item module sub-types are not supported`);
+        }
         return new ItemClass(...args);
     },
 });
