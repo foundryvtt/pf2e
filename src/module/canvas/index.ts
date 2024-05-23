a94d1ff0f7c04f488e39655ea91536d819d2dd81
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-05-23 16:40:25 -0500

    Remove cycle-token-stack keybinding and `TokenLayerPF2e` (#14806)

--- src/module/canvas/index.ts
+++ src/module/canvas/index.ts
@@ -3,7 +3,6 @@ import { AmbientLightPF2e } from "./ambient-light.ts";
 import { EffectsCanvasGroupPF2e } from "./group/effects.ts";
 import { LightingLayerPF2e } from "./layer/lighting.ts";
 import { TemplateLayerPF2e } from "./layer/template.ts";
-import { TokenLayerPF2e } from "./layer/token.ts";
 import { MeasuredTemplatePF2e } from "./measured-template.ts";
 import { TokenPF2e } from "./token/object.ts";
 
@@ -22,6 +21,5 @@ export {
     LightingLayerPF2e,
     MeasuredTemplatePF2e,
     TemplateLayerPF2e,
-    TokenLayerPF2e,
     TokenPF2e,
 };
