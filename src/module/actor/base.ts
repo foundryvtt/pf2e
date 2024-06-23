d2c6d058f5b8c29e0dd0afc4ec00c9836dbb71bc
MrVauxs <mrvauxs@gmail.com>
2024-06-23 15:46:40 +0200

    Remove duplicate flag

--- src/module/actor/base.ts
+++ src/module/actor/base.ts
@@ -1282,7 +1282,6 @@ class ActorPF2e<TParent extends TokenDocumentPF2e | null = TokenDocumentPF2e | n
             speaker: ChatMessagePF2e.getSpeaker({ token }),
             flags: {
                 pf2e: {
-                    type: "damage-taken",
                     appliedDamage,
                     context: {
                         type: "damage-taken",
