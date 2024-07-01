b78040299791a8236030f516863f561b1ae2cad5
Richard Simões <rsimoes@gmail.com>
2024-07-01 05:47:53 -0500

    Organize remaining foundry types to mirror foundry paths

---
+++ types/foundry/client/apps/forms/combat-config.d.ts
@@ -0,0 +1,12 @@
+/** The Application responsible for configuring the CombatTracker and its contents. */
+declare class CombatTrackerConfig<TObject extends Combat = Combat> extends FormApplication<TObject> {
+    static override get defaultOptions(): FormApplicationOptions;
+
+    override getData(options?: Partial<FormApplicationOptions>): Promise<FormApplicationData<TObject>>;
+
+    protected override _updateObject(event: Event, formData: {}): Promise<void>;
+
+    override activateListeners(html: JQuery): void;
+
+    protected override _onChangeInput(event: Event): Promise<void>;
+}
--- types/foundry/client/index.d.ts
+++ types/foundry/client/index.d.ts
@@ -1,7 +1,16 @@
 import "gsap";
 import "handlebars";
 import SHOWDOWN from "showdown";
+import "./apps/index.d.ts";
+import "./config.d.ts";
+import "./core/index.d.ts";
+import "./data/index.d.ts";
 import * as Foundry from "./foundry/index.ts";
+import "./game.d.ts";
+import "./keyboard/index.d.ts";
+import "./pixi/index.d.ts";
+import "./roll.d.ts";
+import "./ui/index.d.ts";
 
 declare global {
     namespace globalThis {
