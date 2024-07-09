66b5b840b7892ab4550728bbba706c32c07caa79
Matheus Clemente <matheus.xb@gmail.com>
2024-07-09 01:59:39 -0300

    Update note.ts

--- src/module/canvas/note.ts
+++ src/module/canvas/note.ts
@@ -2,7 +2,6 @@ class NotePF2e extends Note {
     override _drawControlIcon(): ControlIcon {
         const { flags, texture, iconSize } = this.document;
         const systemIcon = this.page?.getControlIcon?.({ size: iconSize, tint: texture.tint });
-        console.log(flags?.pf2e?.code, this.page?.system?.code);
         if (systemIcon && flags?.pf2e?.code) systemIcon.text._text = flags.pf2e.code;
         else return super._drawControlIcon();
         systemIcon.x -= iconSize / 2;
