96ab05e51c86d244a47131e4d73a9e57e99ff0cc
Gunnar Busch <gunnar.busch@googlemail.com>
2024-07-13 20:39:18 +0200

    first draft of dynamic token integration

--- src/scripts/hooks/target-token.ts
+++ src/scripts/hooks/target-token.ts
@@ -3,7 +3,7 @@ import { getSelectedActors } from "@util/token-actor-utils.ts";
 
 export const TargetToken = {
     listen: (): void => {
-        Hooks.on("targetToken", (user, token): void => {
+        Hooks.on("targetToken", (user, token, targeted): void => {
             const tokenDocument = token.document as TokenDocumentPF2e;
             ui.combat.refreshTargetDisplay(tokenDocument);
 
@@ -15,6 +15,16 @@ export const TargetToken = {
                 if (canvas.tokens.highlightObjects && user === game.user) {
                     actorToken?.flankingHighlight.draw();
                 }
+            }
+
+            // Flash the token ring for all users
+            if (targeted) {
+                token.ring?.flashColor(user.color, {
+                    duration: 1000,
+                    easing: (pt: number) => {
+                        return (Math.sin(2 * Math.PI * pt - Math.PI / 2) + 1) / 2;
+                    },
+                });
             }
         });
     },
