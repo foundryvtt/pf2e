28b3d0f77128f7a37817c2360c41d9c917dab298
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-07-29 00:03:03 -0500

    Update src/module/canvas/layer/template.ts

--- src/module/canvas/layer/template.ts
+++ src/module/canvas/layer/template.ts
@@ -41,9 +41,7 @@ export class TemplateLayerPF2e<
         const dimensions = canvas.dimensions;
 
         // Snap the destination to the grid
-        const { x, y } = canvas.grid.getSnappedPoint(destination, {
-            mode: template.snappingMode,
-        });
+        const { x, y } = canvas.grid.getSnappedPoint(destination, { mode: template.snappingMode });
         destination.x = x;
         destination.y = y;
         const ray = new Ray(origin, destination);
