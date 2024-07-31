703276f220d8d4e18b6a5b8b628a23d6d7400960
Nikolaj Andresen <na@deck.dk>
2024-07-31 22:08:25 +0200

    Add access infosphere action implementation

--- src/scripts/hooks/init.ts
+++ src/scripts/hooks/init.ts
@@ -18,7 +18,7 @@ import { SetGamePF2e } from "@scripts/set-game-pf2e.ts";
 import { registerSettings } from "@system/settings/index.ts";
 import { htmlQueryAll } from "@util";
 import * as R from "remeda";
-import { Action } from "@actor/actions/index.ts";
+import { accessInfosphere } from "@system/action-macros/computers/access-infosphere.ts";
 
 export const Init = {
     listen: (): void => {
@@ -170,10 +170,7 @@ export const Init = {
             game.pf2e.StatusEffects.initialize();
 
             if (game.modules.get("starfinder-field-test-for-pf2e")?.active) {
-                const actions: Action[] = [
-                    // SF2E-specific actions
-                ];
-                actions.forEach((action) => game.pf2e.actions.set(action.slug, action));
+                [accessInfosphere].forEach((action) => game.pf2e.actions.set(action.slug, action));
             }
         });
     },
