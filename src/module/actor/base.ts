51d0c996495b2d9a6b9920379322ce7213ab8c62
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-08-04 04:36:24 -0500

    Fix retrieval of origin roll options from aura-transmitted effects (#15775)

--- src/module/actor/base.ts
+++ src/module/actor/base.ts
@@ -492,7 +492,6 @@ class ActorPF2e<TParent extends TokenDocumentPF2e | null = TokenDocumentPF2e | n
                         token: origin.token.uuid,
                         item: null,
                         spellcasting: null,
-                        rollOptions: [],
                     },
                     roll: null,
                 };
