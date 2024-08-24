d7ff7fc0eeb954b05ff25bdadeaae8c7db2f0627
farling42 <github@amusingtime.uk>
2024-08-25 00:11:06 +0100

    more lint

--- src/module/item/campaign-feature/document.ts
+++ src/module/item/campaign-feature/document.ts
@@ -166,7 +166,7 @@ class CampaignFeaturePF2e<TParent extends ActorPF2e | null = ActorPF2e | null> e
     protected override embedHTMLString(_config: DocumentHTMLEmbedConfig, _options: EnrichmentOptions): string {
         const list = this.system.prerequisites?.value?.map((item) => item.value).join(",") ?? "";
         return (
-            (list.length 
+            (list.length
                 ? `<p><strong>${game.i18n.localize("PF2E.FeatPrereqLabel")}</strong> ${list}</p>` +
                   (_config.hr !== false ? "<hr>" : "")
                 : "") + super.embedHTMLString(_config, _options)
--- src/module/item/feat/document.ts
+++ src/module/item/feat/document.ts
@@ -418,7 +418,7 @@ class FeatPF2e<TParent extends ActorPF2e | null = ActorPF2e | null> extends Item
     protected override embedHTMLString(_config: DocumentHTMLEmbedConfig, _options: EnrichmentOptions): string {
         const list = this.system.prerequisites?.value?.map((item) => item.value).join(",") ?? "";
         return (
-            (list.length 
+            (list.length
                 ? `<p><strong>${game.i18n.localize("PF2E.FeatPrereqLabel")}</strong> ${list}</p>` +
                   (_config.hr !== false ? "<hr>" : "")
                 : "") + super.embedHTMLString(_config, _options)
