fc2f7963a9a18ef5728491a3e32d469e331ac970
Richard Simões <rsimoes@gmail.com>
2024-10-03 16:11:10 -0500

    Convert ability system data to `TypeDataModel`

--- src/module/item/ability/sheet.ts
+++ src/module/item/ability/sheet.ts
@@ -2,7 +2,7 @@ import type { AbilityItemPF2e } from "@item/ability/document.ts";
 import { ItemSheetDataPF2e, ItemSheetOptions, ItemSheetPF2e } from "@item/base/sheet/sheet.ts";
 import { ancestryTraits } from "@scripts/config/traits.ts";
 import * as R from "remeda";
-import { SelfEffectReference } from "./data.ts";
+import type { SelfEffectReference } from "./data.ts";
 import { activateActionSheetListeners, createSelfEffectSheetData, handleSelfEffectDrop } from "./helpers.ts";
 
 // Gather traits to restrict to avoid trait selection noise in the selection
