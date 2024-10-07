cbab1ab0da725275a9a84a44e58dc4c1617f3851
Richard Simões <rsimoes@gmail.com>
2024-10-06 19:44:00 -0500

    Preserve `_stats.compendiumSource` in embedded items on build

--- build/lib/compendium-pack.ts
+++ build/lib/compendium-pack.ts
@@ -261,7 +261,8 @@ class CompendiumPack {
             this.#assertSizeValid(docSource);
             docSource.system._migration = { version: MigrationRunnerBase.LATEST_SCHEMA_VERSION, previous: null };
             for (const item of docSource.items) {
-                item._stats = { ...partialStats } as SourceFromSchema<DocumentStatsSchema<ItemUUID>>;
+                const compendiumSource = item._stats?.compendiumSource ?? null;
+                item._stats = { ...partialStats, compendiumSource } as SourceFromSchema<DocumentStatsSchema<ItemUUID>>;
                 item.effects = [];
                 item.system._migration = { version: MigrationRunnerBase.LATEST_SCHEMA_VERSION, previous: null };
                 CompendiumPack.convertUUIDs(item, { to: "ids", map: CompendiumPack.#namesToIds.Item });
