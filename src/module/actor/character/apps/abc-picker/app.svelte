12306581414f657b0bfe915068e003522ba91f19
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2024-11-21 21:10:43 -0600

    Remove need for second confirmation click in ABC picker (#17426)

--- src/module/actor/character/apps/abc-picker/app.svelte
+++ src/module/actor/character/apps/abc-picker/app.svelte
@@ -9,13 +9,6 @@
         items: typePlural,
     });
 
-    let selection: string | null = $state(null);
-    /** Show the confirmation button. */
-    const showConfirmation: MouseEventHandler<HTMLButtonElement> = (event): void => {
-        const row = event.currentTarget.closest("li");
-        selection = selection === row?.dataset.uuid ? null : (row?.dataset.uuid ?? null);
-    };
-
     /** Open an item sheet to show additional details. */
     const viewItemSheet: MouseEventHandler<HTMLButtonElement> = async (event): Promise<void> => {
         const uuid = event.currentTarget.closest("li")?.dataset?.uuid ?? "";
@@ -51,30 +44,29 @@
 
 <ul>
     {#each data.items as item}
-        <li class:selected={selection === item.uuid} data-uuid={item.uuid}>
-            <img src={item.img} loading="lazy" alt="Class icon" />
-            <button type="button" class="flat name-source" onclick={showConfirmation}>
+        <li data-uuid={item.uuid}>
+            <button
+                type="button"
+                class="flat name-source"
+                class:omit-rarity={!item.rarity}
+                data-tooltip="PF2E.Actor.Character.ABCPicker.Tooltip.ViewSheet"
+                onclick={viewItemSheet}
+            >
+                <img src={item.img} loading="lazy" alt="Class icon" />
                 <div class="name">{item.name}</div>
+                {#if item.rarity}
+                    <div class="tags paizo-style">
+                        <span class="tag rarity {item.rarity.slug}">{item.rarity.label}</span>
+                    </div>
+                {/if}
                 <div class="source" class:publication={item.source.publication}>{item.source.name}</div>
             </button>
-            {#if item.rarity}
-                <div class="tags paizo-style">
-                    <span class="tag rarity {item.rarity.slug}">{item.rarity.label}</span>
-                </div>
-            {/if}
-            <div class="buttons">
-                <button
-                    type="button"
-                    class="confirm"
-                    data-tooltip="PF2E.Actor.Character.ABCPicker.Tooltip.ConfirmSelection"
-                    onclick={saveSelection}><i class="fa-solid fa-check"></i></button
-                >
-                <button
-                    type="button"
-                    data-tooltip="PF2E.Actor.Character.ABCPicker.Tooltip.ViewSheet"
-                    onclick={viewItemSheet}><i class="fa-solid fa-info fa-fw"></i></button
-                >
-            </div>
+            <button
+                type="button"
+                class="confirm"
+                data-tooltip="PF2E.Actor.Character.ABCPicker.Tooltip.ConfirmSelection"
+                onclick={saveSelection}><i class="fa-solid fa-check"></i></button
+            >
         </li>
     {/each}
 </ul>
@@ -102,86 +94,88 @@
         padding: var(--space-4) 0;
 
         > li {
-            display: grid;
             align-items: center;
             border-top: 1px solid var(--color-border);
-            column-gap: var(--space-8);
-            grid-template:
-                "icon name-source rarity" auto
-                "icon name-source buttons" auto / 3rem auto 5rem;
+            display: flex;
             margin: 0;
             padding: var(--space-1) var(--space-6) var(--space-4);
 
-            img {
-                border: none;
-                grid-area: icon;
-                height: 3rem;
-                width: 3rem;
-            }
-
             button.name-source {
-                display: flex;
-                flex-flow: column nowrap;
-                grid-area: name-source;
+                column-gap: var(--space-8);
+                display: grid;
+                flex: 1;
                 height: unset;
-                justify-content: center;
+                justify-content: start;
+                grid-template:
+                    "icon name" auto
+                    "icon rarity" auto
+                    "icon source" auto / 3rem auto;
+
+                &.omit-rarity {
+                    grid-template:
+                        "icon name"
+                        "icon source" / 3rem auto;
+
+                    > .name {
+                        align-self: end;
+                    }
+
+                    > .source {
+                        align-self: start;
+                    }
+                }
+
+                img {
+                    border: none;
+                    height: 3rem;
+                    grid-area: icon;
+                    width: 3rem;
+                }
 
                 .name {
+                    grid-area: name;
                     color: var(--color-text-primary);
+                    padding-left: var(--space-1);
+                }
+
+                .tags {
+                    grid-area: rarity;
+                    padding: 0;
                 }
 
                 .source {
                     color: var(--color-form-hint);
                     font-size: var(--font-size-12);
+                    grid-area: source;
+                    padding-left: var(--space-1);
+                    &.no-rarity {
+                        grid-row: span 2;
+                        align-self: start;
+                    }
 
                     &.publication {
                         font-style: italic;
                     }
                 }
-            }
 
-            .buttons {
-                align-items: center;
-                display: flex;
-                flex-flow: row nowrap;
-                gap: var(--space-2);
-                grid-area: buttons;
-                justify-content: end;
-
-                button {
-                    font-size: var(--font-size-12);
-                    height: 1.5rem;
-                    width: 1.5rem;
-
-                    i {
-                        margin-right: 0;
-                    }
-
-                    &.confirm {
-                        opacity: 0;
-                        visibility: hidden;
-
-                        &:not(:hover) {
-                            color: darkgreen;
-                        }
-                    }
+                &:hover .tag.rarity {
+                    text-shadow: 0 0 8px var(--color-shadow-primary);
                 }
             }
 
-            .tags {
-                font-size: var(--font-size-9);
-                grid-area: rarity;
-                justify-content: end;
+            button.confirm {
+                color: darkgreen;
+                font-size: var(--font-size-12);
+                height: 1.5rem;
+                width: 1.5rem;
+                margin-left: auto;
+                visibility: hidden;
+                i {
+                    margin-right: 0;
+                }
             }
 
-            &:hover:not(.selected) button.confirm {
-                opacity: 0.33;
-                pointer-events: none;
-                visibility: visible;
-            }
-
-            &.selected button.confirm {
-                opacity: 1;
+            &:hover button.confirm {
                 visibility: visible;
             }
         }
