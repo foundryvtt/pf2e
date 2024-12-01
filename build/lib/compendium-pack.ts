ad7f9ae98e4b29ac2c4e43e81aad14341e52dddf
Simon Ward <simonward@mac.com>
2024-11-30 20:31:59 -0800

    Use `R.entries()`

--- build/lib/compendium-pack.ts
+++ build/lib/compendium-pack.ts
@@ -8,6 +8,7 @@ import { RuleElementSource } from "@module/rules/index.ts";
 import { isObject, recursiveReplaceString, setHasElement, sluggify, tupleHasValue } from "@util/misc.ts";
 import fs from "fs";
 import path from "path";
+import * as R from "remeda";
 import type { DocumentStatsData, DocumentStatsSchema } from "types/foundry/common/data/fields.d.ts";
 import systemJSON from "../../static/system.json";
 import coreIconsJSON from "../core-icons.json";
@@ -351,8 +352,8 @@ class CompendiumPack {
             }
         } else if (itemIsOfType(source, "deity")) {
             const spells = source.system.spells;
-            for (const key in spells) {
-                spells[key] = CompendiumPack.convertUUID(spells[key], convertOptions);
+            for (const [key, spell] of R.entries(spells)) {
+                spells[key] = CompendiumPack.convertUUID(spell, convertOptions);
             }
         }
 
