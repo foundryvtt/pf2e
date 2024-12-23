6634e5dd52f8fd571f7e04b288a2e1e04014ad1f
LebombJames <sam.marine418@gmail.com>
2024-12-23 04:08:24 +0000

    Don't await document create/delete in effect macros

--- src/scripts/macros/hotbar.ts
+++ src/scripts/macros/hotbar.ts
@@ -189,9 +189,9 @@ if (item?.type === "condition") {
     for (const actor of actors) {
         const existing = actor.itemTypes.effect.find((e) => e.flags.core?.sourceId === ITEM_UUID);
         if (existing) {
-            await existing.delete();
+            existing.delete();
         } else {
-            await actor.createEmbeddedDocuments("Item", [source]);
+            actor.createEmbeddedDocuments("Item", [source]);
         }
     }
 } else {
