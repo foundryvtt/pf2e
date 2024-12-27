ff6a0eae57529c131a48737213082711b2a6bd43
In3luki <41452412+In3luki@users.noreply.github.com>
2024-12-27 13:05:27 +0100

    Tighten logic for compendium browser trait filtering

--- src/module/apps/compendium-browser/tabs/base.svelte.ts
+++ src/module/apps/compendium-browser/tabs/base.svelte.ts
@@ -150,15 +150,13 @@ export abstract class CompendiumBrowserTab {
     ): boolean {
         const selectedTraits = selected.filter((s) => !s.not).map((s) => s.value);
         const notTraits = selected.filter((t) => t.not).map((s) => s.value);
-        if (selectedTraits.length || notTraits.length) {
-            if (notTraits.some((t) => traits.includes(t))) {
-                return false;
-            }
-            const fullfilled =
-                condition === "and"
-                    ? selectedTraits.every((t) => traits.includes(t))
-                    : selectedTraits.some((t) => traits.includes(t));
-            if (!fullfilled) return false;
+        if (notTraits.some((t) => traits.includes(t))) {
+            return false;
+        }
+        if (selectedTraits.length) {
+            return condition === "and"
+                ? selectedTraits.every((t) => traits.includes(t))
+                : selectedTraits.some((t) => traits.includes(t));
         }
         return true;
     }
