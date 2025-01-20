fcdb67f9bd55986de034c704e7211c5f483e04d7
Cerapter <cerap@protonmail.com>
2025-01-20 16:45:53 +0100

    Extend ItemAlteration to be able to change base damage dice number

--- src/module/rules/rule-element/item-alteration/alteration.ts
+++ src/module/rules/rule-element/item-alteration/alteration.ts
@@ -25,6 +25,7 @@ class ItemAlteration extends foundry.abstract.DataModel<RuleElementPF2e, ItemAlt
         "category",
         "check-penalty",
         "damage-dice-faces",
+        "damage-dice-number",
         "damage-type",
         "defense-passive",
         "description",
@@ -191,6 +192,23 @@ class ItemAlteration extends foundry.abstract.DataModel<RuleElementPF2e, ItemAlt
                     }
                     item.system.damage.die = `d${data.alteration.value}`;
                 }
+
+                return;
+            }
+            case "damage-dice-number": {
+                const validator = ITEM_ALTERATION_VALIDATORS[this.property];
+                if (!validator.isValid(data) || !(data.item instanceof ItemPF2e)) {
+                    return;
+                }
+
+                const item = data.item;
+                if (!item.system.damage.dice) return;
+                const newValue = AELikeRuleElement.getNewValue(
+                    this.mode,
+                    item.system.damage.dice,
+                    data.alteration.value,
+                );
+                item.system.damage.dice = newValue;
 
                 return;
             }
