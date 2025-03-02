1de9df2a654ce5de7ab98c21c9109ae98c9ee73f
Richard Simões <rsimoes@gmail.com>
2025-03-01 19:37:44 -0600

    Add support for token ring `effects` field in `TokenImage` RE

--- types/foundry/common/documents/token.d.ts
+++ types/foundry/common/documents/token.d.ts
@@ -139,7 +139,7 @@ type TokenSchema = {
             ring: fields.ColorField;
             background: fields.ColorField;
         }>;
-        effects: fields.NumberField;
+        effects: fields.NumberField<number, number, true, false, true>;
         subject: fields.SchemaField<{
             scale: fields.NumberField;
             texture: fields.FilePathField<ImageFilePath>;
