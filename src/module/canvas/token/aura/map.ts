8f9f27295eae1ae807dc438694820cf516153a52
FolkvangrForgent <25625121+FolkvangrForgent@users.noreply.github.com>
2025-05-11 17:34:32 -0400

    Move aura grid type check to scene getter (#19076)

--- src/module/canvas/token/aura/map.ts
+++ src/module/canvas/token/aura/map.ts
@@ -53,7 +53,8 @@ export class AuraRenderers extends Map<string, AuraRenderer> {
                 : this.token.combatant?.encounter.active);
 
         return (
-            canvas.scene?.grid.type === CONST.GRID_TYPES.SQUARE &&
+            canvas.scene !== null &&
+            canvas.scene.canHaveAuras &&
             // Assume if token vision is disabled then the scene is not intended for play.
             canvas.scene.tokenVision &&
             // The scene must be active, or a GM must be the only user logged in.
