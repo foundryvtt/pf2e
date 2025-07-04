4776353e3d76ffadb654526dbcfe263b99b932fe
Richard Simões <rsimoes@gmail.com>
2025-07-04 06:41:08 -0500

    Exclude token image in OOC messages

--- src/module/chat-message/document.ts
+++ src/module/chat-message/document.ts
@@ -197,7 +197,8 @@ class ChatMessagePF2e extends ChatMessage {
         // For non-image types consider video thumbnails using game.video.createThumbnail() depending on cache performance
         // Tokens with smaller scales (such as for small and tiny actors) are set to scale 1
         const header = html?.querySelector("header.message-header");
-        if (actor && header && this.isContentVisible) {
+        const isOOC = this.style === CONST.CHAT_MESSAGE_STYLES.OOC;
+        if (!isOOC && actor && header && this.isContentVisible) {
             const token = this.token ?? actor.prototypeToken;
 
             const [imageUrl, scale] = (() => {
