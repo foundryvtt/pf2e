1f711f017d99fd989fe70826d13e4a187306ba18
Richard Simões <rsimoes@gmail.com>
2025-09-20 05:22:34 -0500

    Guard against Firefox's partially-implemented `IteratorObject`s when preloading fonts

--- src/scripts/hooks/ready.ts
+++ src/scripts/hooks/ready.ts
@@ -163,7 +163,8 @@ export const Ready = {
             }
 
             // Preload Font Awesome Duotone
-            const faDuotone = document.fonts.values().find((f) => f.family === "Font Awesome 6 Duotone");
+            // Check for presence of `find`: Firefox's iterator objects aren't always iterator objects.
+            const faDuotone = document.fonts.values().find?.((f) => f.family === "Font Awesome 6 Duotone");
             if (faDuotone?.status === "unloaded") faDuotone.load();
 
             // Announce the system is ready in case any module needs access to an application not available until now
