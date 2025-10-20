ee47df877ee2525c159182620c82ec43a69db52f
Carlos Fernandez <cfern1990@gmail.com>
2025-10-20 01:18:36 -0400

    Fix player drag drop to their own items

--- src/module/item/base/sheet/sheet.ts
+++ src/module/item/base/sheet/sheet.ts
@@ -611,6 +611,11 @@ class ItemSheetPF2e<TItem extends ItemPF2e> extends fav1.sheets.ItemSheet<TItem,
         return buttons;
     }
 
+    /** Actor sheets have this upstream, but items do not. Verify if this is a foundry bug */
+    protected override _canDragDrop(): boolean {
+        return this.isEditable;
+    }
+
     protected override async _updateObject(event: Event, formData: Record<string, unknown>): Promise<void> {
         const expanded = fu.expandObject(formData) as DeepPartial<ItemSourcePF2e>;
 
