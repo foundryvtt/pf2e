642af822cd89d2f9953fb0d4f6019295908393b1
Richard Simões <rsimoes@gmail.com>
2025-11-23 12:50:59 -0600

    Preserve non-default scales of iconics' prototype tokens

--- src/module/scene/token-document/sheets/prototype-config.ts
+++ src/module/scene/token-document/sheets/prototype-config.ts
@@ -1,7 +1,18 @@
+import type { ActorPF2e } from "@actor";
 import type { FormDataExtended } from "@client/applications/ux/_module.d.mts";
 import { TokenConfigMixinPF2e } from "./mixin.ts";
 
 class PrototypeTokenConfigPF2e extends TokenConfigMixinPF2e(fa.sheets.PrototypeTokenConfig) {
+    override get linkToActorSize(): boolean {
+        const actor = this.actor as unknown as ActorPF2e;
+        return actor.prototypeToken.flags.pf2e.linkToActorSize;
+    }
+
+    override get autoscale(): boolean {
+        const actor = this.actor as unknown as ActorPF2e;
+        return actor.prototypeToken.flags.pf2e.autoscale;
+    }
+
     protected override _processFormData(
         event: SubmitEvent | null,
         form: HTMLFormElement,
