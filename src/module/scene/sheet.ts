7139fafe4cea5d1a8eb0e396620700013f20acd5
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2026-04-13 17:47:10 -0400

    Update src/module/scene/sheet.ts
    Co-authored-by: Shark that walks like a man <106829671+stwlam@users.noreply.github.com>

--- src/module/scene/sheet.ts
+++ src/module/scene/sheet.ts
@@ -10,7 +10,7 @@ import fields = foundry.data.fields;
 export class SceneConfigPF2e<TDocument extends ScenePF2e> extends fa.sheets.SceneConfig<TDocument> {
     static override DEFAULT_OPTIONS: DeepPartial<fa.api.DocumentSheetConfiguration> = {
         sheetConfig: false,
-        position: { width: 650 },
+        position: { width: 660 },
         actions: {
             openAutomationSettings: SceneConfigPF2e.#onClickOpenAutomationSettings,
             openWorldClockSettings: SceneConfigPF2e.#onClickOpenWorldClockSettings,
