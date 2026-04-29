a00d73cf4519282e756e11d9edcce682ff2ff687
SpartanCPA <steakhouse.newsletters@gmail.com>
2026-04-29 16:49:22 -0400

    Migration courtesy of Drental

--- src/module/migration/migrations/index.ts
+++ src/module/migration/migrations/index.ts
@@ -254,3 +254,4 @@ export { Migration953NotStrikeDamage } from "./953-not-strike-damage.ts";
 export { Migration954ExplicitBleedImmunity } from "./954-explicit-bleed-immunity.ts";
 export { Migration955HazardNullSaves } from "./955-hazard-null-saves.ts";
 export { Migration956DragonChoiceLocalizationKeys } from "./956-dragonchoice-localization-keys.ts";
+export { Migration957ChangeShapeLocalizationKeys } from "./957-changeshape-localization-keys.ts";
