76b1dbd9bb6655b3c9733ecdccfacfe1b6939567
iDantar <130079801+iDantar@users.noreply.github.com>
2026-06-17 22:48:38 +0300

    Update src/module/actor/base.ts
    Co-authored-by: Shark that walks like a man <106829671+stwlam@users.noreply.github.com>

--- src/module/actor/base.ts
+++ src/module/actor/base.ts
@@ -1291,7 +1291,7 @@ class ActorPF2e<TParent extends TokenDocumentPF2e | null = TokenDocumentPF2e | n
             persistentDamage.length ? await this.createEmbeddedDocuments("Item", persistentDamage) : []
         ) as ConditionPF2e<this>[];
 
-        const persistentDamages = game.i18n.getListFormatter({ style: "short", type: "conjunction" }).format(
+        const persistentDamages = game.i18n.getListFormatter({ style: "long", type: "conjunction" }).format(
             result.persistent.map((instance) => {
                 const formula = instance.head.expression;
                 const damageType = _loc(CONFIG.PF2E.damageTypes[instance.type]);
