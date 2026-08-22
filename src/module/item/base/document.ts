ece3f9962ba18ccc3b3719813a543790c44e2d1a
Shark that walks like a man <106829671+stwlam@users.noreply.github.com>
2026-08-22 01:45:48 -0500

    Update src/module/item/base/document.ts
    Co-authored-by: jfn4th <43155846+jfn4th@users.noreply.github.com>

--- src/module/item/base/document.ts
+++ src/module/item/base/document.ts
@@ -224,7 +224,7 @@ class ItemPF2e<TParent extends ActorPF2e | null = ActorPF2e | null> extends Item
             actor: this.actor,
             tokenId: token ? `${token.parent?.id}.${token.id}` : null,
             item: this,
-            showIcon: this.img !== `systems/${SYSTEM_ID}/icons/default-icons/${this.type}.svg`,
+            showIcon: this.img !== ItemPF2e.getDefaultArtwork(this._source).img,
             data: await this.getChatData(undefined, rollOptions),
         };
 
