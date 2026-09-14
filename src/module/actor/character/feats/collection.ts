e932303213798dd574c0a250555af75d5a6bc47c
iDantar <130079801+iDantar@users.noreply.github.com>
2026-09-14 20:28:11 +0300

    Separate code changes

--- src/module/actor/character/feats/collection.ts
+++ src/module/actor/character/feats/collection.ts
@@ -37,10 +37,7 @@ class CharacterFeats<TActor extends CharacterPF2e> extends Collection<string, Fe
             label: "PF2E.Actor.Character.FeatSlot.AncestryHeader",
             supported: ["ancestry"],
             filter: {
-                traits: [
-                    ...(ancestry?.countsAs.flatMap((t) => (t in CONFIG.PF2E.featTraits ? t : [])) ?? []),
-                    "ancestry",
-                ],
+                traits: ancestry?.countsAs.flatMap((t) => (t in CONFIG.PF2E.featTraits ? t : [])) ?? [],
             },
             slots: classFeatSlots?.ancestry ?? [],
         });
