6924e89a0568d4643fb4ef5f15feb25819f28369
iDantar <130079801+iDantar@users.noreply.github.com>
2026-09-16 01:48:46 +0300

    Add universal Ancestry trait to feats (#22666)

--- src/scripts/config/traits.ts
+++ src/scripts/config/traits.ts
@@ -1322,6 +1322,7 @@ const traitDescriptions = {
     amphibious: "PF2E.TraitDescriptionAmphibious",
     anadi: "PF2E.TraitDescriptionAnadi",
     analog: "PF2E.TraitDescriptionAnalog",
+    ancestry: "PF2E.TraitDescriptionAncestry",
     anchoring: "PF2E.TraitDescriptionAnchoring",
     android: "PF2E.TraitDescriptionAndroid",
     angel: "PF2E.TraitDescriptionAngel",
