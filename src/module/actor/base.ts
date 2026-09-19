8c69466dbafa82c17cb9c10491202e946e29b305
Idle <651462+reonZ@users.noreply.github.com>
2026-09-19 13:51:44 +0200

    resolve suggestion

--- src/module/actor/base.ts
+++ src/module/actor/base.ts
@@ -42,6 +42,7 @@ import {
 import type { RuleElementSynthetics } from "@module/rules/index.ts";
 import type { RuleElement } from "@module/rules/rule-element/base.ts";
 import type { ChoiceSetSource } from "@module/rules/rule-element/choice-set/data.ts";
+import { ChoiceSetRuleElement } from "@module/rules/rule-element/choice-set/rule-element.ts";
 import type { RollOptionRuleElement } from "@module/rules/rule-element/roll-option/rule-element.ts";
 import type { UserPF2e } from "@module/user/document.ts";
 import type { ScenePF2e } from "@scene/document.ts";
@@ -499,15 +500,14 @@ class ActorPF2e<TParent extends TokenDocumentPF2e | null = TokenDocumentPF2e | n
                     alteration.applyTo(source);
                 }
 
+                const tempEffect = new ItemProxyPF2e(source, { parent: this });
                 for (const [flag, selection] of Object.entries(data.preselectChoices)) {
-                    if (selection === undefined) continue;
-
-                    const rule = source.system.rules.find((rule): rule is ChoiceSetSource => {
-                        return rule.key === "ChoiceSet" && (rule as ChoiceSetSource).flag === flag;
-                    });
-                    if (rule) {
-                        rule.selection = selection;
-                    }
+                    const rule = tempEffect.rules.find(
+                        (r): r is ChoiceSetRuleElement => r instanceof ChoiceSetRuleElement && r.flag === flag,
+                    );
+                    if (!rule) continue;
+                    const ruleSource = source.system.rules[tempEffect.rules.indexOf(rule)] as ChoiceSetSource;
+                    ruleSource.selection = selection;
                 }
 
                 toCreate.push(source);
